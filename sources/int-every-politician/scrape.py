3dae23c786ff62e4a5ebb89adb10fcab7c97d5ad
Friedrich Lindenberg <friedrich@pudo.org>
2016-06-11 17:18:42 +0200

    Load all content into one directory.

--- sources/int-every-politician/scrape.py
+++ sources/int-every-politician/scrape.py
@@ -1,10 +1,10 @@
+import os
 import sys
 import json
 import urllib
 from urlparse import urljoin
-from tempfile import mkstemp
 import requests
-from unicodecsv import DictReader
+# from unicodecsv import DictReader
 
 from pprint import pprint  # noqa
 
@@ -12,9 +12,11 @@ from pprint import pprint  # noqa
 INDEX = "https://raw.githubusercontent.com/everypolitician/everypolitician-data/master/countries.json"
 
 
-def scrape_files(out_file):
+def scrape_files(out_dir):
     res = requests.get(INDEX)
-    politicians = []
+    with open(os.path.join(out_dir, 'countries.json'), 'wb') as fh:
+        json.dump(res.json(), fh, indent=2)
+
     for country in res.json():
         legs = country.pop('legislatures', [])
         country_data = {'country_' + k: v for k, v in country.items()}
@@ -23,27 +25,29 @@ def scrape_files(out_file):
             leg_data = {'legislature_' + k: v for k, v in legislature.items()}
             leg_data.update(country_data)
             for period in periods:
-                data = {'period_' + k: v for k, v in period.items()}
-                data.update(leg_data)
-                data['source_url'] = urljoin(INDEX, data.get('period_csv'))
-                politicians.extend(scrape_csv(data))
-
-    with open(out_file, 'wb') as fh:
-        json.dump({'politicians': politicians}, fh, indent=2)
+                source_url = urljoin(INDEX, period.get('csv'))
+                scrape_csv(out_dir, source_url)
 
 
-def scrape_csv(data):
-    _, local_file = mkstemp()
-    urllib.urlretrieve(data.get('source_url'), local_file)
-    print 'CSV: %(source_url)s' % data
-    rows = []
-    with open(local_file, 'rn') as fh:
-        for row in DictReader(fh):
-            row.update(data)
-            # row['person_id'] = row.pop('id', None)
-            # pprint(row)
-            rows.append(row)
-    return rows
+def scrape_csv(out_dir, source_url):
+    local_name = source_url.split('/data/')[-1]
+    local_name = os.path.join(out_dir, local_name)
+    try:
+        os.makedirs(os.path.dirname(local_name))
+    except:
+        pass
+    print 'CSV: ', local_name
+    urllib.urlretrieve(source_url, local_name)
+
+    # print 'CSV: %(source_url)s' % data
+    # rows = []
+    # with open(local_file, 'rn') as fh:
+    #     for row in DictReader(fh):
+    #         row.update(data)
+    #         # row['person_id'] = row.pop('id', None)
+    #         # pprint(row)
+    #         rows.append(row)
+    # return rows
 
 if __name__ == '__main__':
     scrape_files(sys.argv[1])
