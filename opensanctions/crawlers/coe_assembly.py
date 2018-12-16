089e160bb0bab89026390a6088e7f60c45a3590f
Friedrich Lindenberg <friedrich@pudo.org>
2018-12-16 10:46:16 +0100

    Convert three additional crawlers

--- opensanctions/crawlers/coe_assembly.py
+++ opensanctions/crawlers/coe_assembly.py
@@ -2,43 +2,46 @@ import string
 from pprint import pprint  # noqa
 from urllib.parse import urljoin
 
-from opensanctions.models import Entity
+from opensanctions.util import EntityEmitter, normalize_country
+
+URL = 'http://www.assembly.coe.int/nw/xml/AssemblyList/MP-Alpha-EN.asp?initial=%s&offset=0'  # noqa
 
 
-def parse_entry(context, entry):
+def parse_entry(emitter, entry):
     link = entry.find('.//a')
-    url_template = context.params.get('url')
-    url = urljoin(url_template, link.get('href'))
+    url = urljoin(URL, link.get('href'))
     _, member_id = url.rsplit('=', 1)
-    entity = Entity.create('coe_assembly', member_id)
-    entity.type = Entity.TYPE_INDIVIDUAL
-    entity.url = url
-    entity.last_name, entity.first_name = link.text.split(', ', 1)
-    entity.function = entry.findtext('.//span[@class="fonction"]')
+    person = emitter.make('Person')
+    person.make_id(member_id)
+    person.add('name', link.text)
+    person.add('sourceUrl', url)
+    last_name, first_name = link.text.split(', ', 1)
+    person.add('lastName', last_name)
+    person.add('firstName', first_name)
+    person.add('position', entry.findtext('.//span[@class="fonction"]'))
     role, country = entry.findall('.//span[@class="infos"]')
-    entity.summary = role.text_content().strip()
-    nationality = entity.create_nationality()
-    nationality.country = country.text_content().strip()
-
-    # pprint(entity.to_dict())
-    context.emit(data=entity.to_dict())
+    person.add('summary', role.text_content().strip())
+    country = normalize_country(country.text_content().strip())
+    person.add('nationality', country)
+    emitter.emit(person)
 
 
 def parse(context, data):
+    emitter = EntityEmitter(context)
+    seen = set()
     for letter in string.ascii_uppercase:
-        seen = set()
-        url_template = context.params.get('url')
-        url = url_template % letter
+        url = URL % letter
         while True:
+            context.log.info("URL: %s", url)
             res = context.http.get(url)
             doc = res.html
             for member in doc.findall('.//ul[@class="member-results"]/li'):
-                parse_entry(context, member)
+                parse_entry(emitter, member)
 
             seen.add(url)
             url = None
             for a in doc.findall('.//div[@id="pagination"]//a'):
-                next_url = urljoin(url_template, a.get('href'))
+                next_url = urljoin(URL, a.get('href'))
                 if next_url not in seen:
                     url = next_url
             if url is None:
--- opensanctions/crawlers/us_bis_denied.py
+++ opensanctions/crawlers/us_bis_denied.py
@@ -1,33 +1,30 @@
+import csv
 from pprint import pprint  # noqa
 
-import csv
-from memorious.helpers import make_id
-
-from opensanctions.models import Entity
+from opensanctions.util import EntityEmitter, normalize_country
 
 
-def parse_row(context, data):
-    row = data.get('row')
-    uid = make_id(row.get('Effective_Date'), row.get('Name'))
-    entity = Entity.create('us-bis-denied', uid)
-    entity.type = Entity.TYPE_ENTITY
-    entity.name = row.get('Name')
-    entity.updated_at = row.get('Effective_Date')
-    entity.program = row.get('FR_Citation')
-    entity.summary = row.get('Action')
-    address = entity.create_address()
-    address.street = row.get('Street_Address')
-    address.postal_code = row.get('Postal_Code')
-    address.region = row.get('State')
-    address.city = row.get('City')
-    address.country = row.get('Country')
+def parse_row(emitter, row):
+    entity = emitter.make('LegalEntity')
+    entity.make_id(row.get('Effective_Date'), row.get('Name'))
+    entity.add('name', row.get('Name'))
+    entity.add('summary', row.get('Action'))
+    entity.add('program', row.get('FR_Citation'))
+    entity.add('country', normalize_country(row.get('Country')))
+    # entity.updated_at = row.get('Effective_Date')
 
-    # pprint(entity.to_dict())
-    context.emit(data=entity.to_dict())
+    address = (row.get('Street_Address'),
+               row.get('Postal_Code'),
+               row.get('City'),
+               row.get('State'))
+    address = ', '.join(address)
+    entity.add('address', address)
+    emitter.emit(entity)
 
 
 def parse(context, data):
-    res = context.http.rehash(data)
-    with open(res.file_path, 'r') as csvfile:
-        for row in csv.DictReader(csvfile, delimiter='\t'):
-            context.emit(data={'row': row})
+    emitter = EntityEmitter(context)
+    with context.http.rehash(data) as res:
+        with open(res.file_path, 'r') as csvfile:
+            for row in csv.DictReader(csvfile, delimiter='\t'):
+                parse_row(emitter, row)
