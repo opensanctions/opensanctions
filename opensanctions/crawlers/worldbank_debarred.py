d728450f324062f60c29233d15b3cad861b24cf8
Friedrich Lindenberg <friedrich@pudo.org>
2020-07-15 15:47:09 +0200

    Cleanup old emitter code

--- opensanctions/crawlers/worldbank_debarred.py
+++ opensanctions/crawlers/worldbank_debarred.py
@@ -5,20 +5,20 @@ from ftmstore.memorious import EntityEmitter
 
 from opensanctions.util import normalize_country
 
-SPLITS = r'(a\.k\.a\.?|aka|f/k/a|also known as|\(formerly |, also d\.b\.a\.|\(currently (d/b/a)?|d/b/a|\(name change from|, as the successor or assign to)'  # noqa
-SOURCE = 'https://www.worldbank.org/en/projects-operations/procurement/debarred-firms'  # noqa
+SPLITS = r"(a\.k\.a\.?|aka|f/k/a|also known as|\(formerly |, also d\.b\.a\.|\(currently (d/b/a)?|d/b/a|\(name change from|, as the successor or assign to)"  # noqa
+SOURCE = "https://www.worldbank.org/en/projects-operations/procurement/debarred-firms"  # noqa
 
 
 def clean_date(date):
     try:
-        dt = datetime.strptime(date, '%d-%b-%Y')
+        dt = datetime.strptime(date, "%d-%b-%Y")
         return dt.date().isoformat()
     except Exception:
         pass
 
 
 def clean_name(text):
-    text = text.replace('M/S', 'MS')
+    text = text.replace("M/S", "MS")
     parts = re.split(SPLITS, text, re.I)
     names = []
     keep = True
@@ -33,52 +33,52 @@ def clean_name(text):
 
     clean_names = []
     for name in names:
-        if '*' in name:
-            name, _ = name.rsplit('*', 1)
+        if "*" in name:
+            name, _ = name.rsplit("*", 1)
         # name = re.sub(r'\* *\d{1,4}$', '', name)
-        name = name.strip(')').strip('(').strip(',')
+        name = name.strip(")").strip("(").strip(",")
         name = name.strip()
         clean_names.append(name)
     return clean_names
 
 
 def fetch(context, data):
-    url = context.params.get('url')
-    apikey = context.params.get('apikey')
-    response = context.http.get(url, headers={'apikey': apikey})
-    for ent in response.json['response']['ZPROCSUPP']:
+    url = context.params.get("url")
+    apikey = context.params.get("apikey")
+    response = context.http.get(url, headers={"apikey": apikey})
+    for ent in response.json["response"]["ZPROCSUPP"]:
         context.emit(data=ent)
 
 
 def parse(context, data):
     emitter = EntityEmitter(context)
-    entity = emitter.make('LegalEntity')
+    entity = emitter.make("LegalEntity")
 
-    name = data.get('SUPP_NAME')
-    ent_id = data.get('SUPP_ID')
-    reason = data.get('DEBAR_REASON')
-    country = data.get('COUNTRY_NAME')
-    city = data.get('SUPP_CITY')
-    address = data.get('SUPP_ADDR')
-    start_date = data.get('DEBAR_FROM_DATE')
-    end_date = data.get('DEBAR_TO_DATE')
+    name = data.get("SUPP_NAME")
+    ent_id = data.get("SUPP_ID")
+    reason = data.get("DEBAR_REASON")
+    country = data.get("COUNTRY_NAME")
+    city = data.get("SUPP_CITY")
+    address = data.get("SUPP_ADDR")
+    start_date = data.get("DEBAR_FROM_DATE")
+    end_date = data.get("DEBAR_TO_DATE")
 
     entity.make_id(name, ent_id, country)
     names = clean_name(name)
-    entity.add('name', names[0])
-    entity.add('address', address)
-    entity.add('address', city)
-    entity.add('country', normalize_country(country))
+    entity.add("name", names[0])
+    entity.add("address", address)
+    entity.add("address", city)
+    entity.add("country", normalize_country(country))
     for name in names[1:]:
-        entity.add('alias', name)
+        entity.add("alias", name)
 
-    sanction = emitter.make('Sanction')
-    sanction.make_id('Sanction', entity.id)
-    sanction.add('authority', 'World Bank Debarrment')
-    sanction.add('program', reason)
-    sanction.add('startDate', clean_date(start_date))
-    sanction.add('endDate', clean_date(end_date))
-    sanction.add('sourceUrl', SOURCE)
+    sanction = emitter.make("Sanction")
+    sanction.make_id("Sanction", entity.id)
+    sanction.add("authority", "World Bank Debarrment")
+    sanction.add("program", reason)
+    sanction.add("startDate", clean_date(start_date))
+    sanction.add("endDate", clean_date(end_date))
+    sanction.add("sourceUrl", SOURCE)
     emitter.emit(entity)
     emitter.emit(sanction)
 
--- opensanctions/util.py
+++ opensanctions/util.py
@@ -1,42 +1,15 @@
 import logging
 import countrynames
 from normality import stringify
-from ftmstore.memorious import get_dataset
-from followthemoney import model
 
 log = logging.getLogger(__name__)
-ORIGIN = 'opensanctions'
-
-
-class EntityEmitter(object):
-
-    def __init__(self, context):
-        self.fragment = 0
-        self.log = context.log
-        self.name = context.crawler.name
-        self.dataset = get_dataset(context)
-        self.bulk = self.dataset.bulk()
-
-    def make(self, schema):
-        entity = model.make_entity(schema, key_prefix=self.name)
-        return entity
-
-    def emit(self, entity, rule='pass'):
-        if entity.id is None:
-            raise RuntimeError("Entity has no ID: %r", entity)
-        fragment = str(self.fragment)
-        self.bulk.put(entity, fragment=fragment, origin=ORIGIN)
-        self.fragment += 1
-
-    def finalize(self):
-        self.bulk.flush()
 
 
 def normalize_country(name):
     return countrynames.to_code(name)
 
 
-def jointext(*parts, sep=' '):
+def jointext(*parts, sep=" "):
     parts = [stringify(p) for p in parts]
     parts = [p for p in parts if p is not None]
     return sep.join(parts)
