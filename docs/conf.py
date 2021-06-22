d64e2595bf8bd070c4f39ced1bf6d83d3ccb37bb
Friedrich Lindenberg <friedrich@pudo.org>
2021-06-22 11:07:15 +0200

    intersphinx followthemoney

--- docs/conf.py
+++ docs/conf.py
@@ -5,9 +5,16 @@ author = "OpenSanctions Team"
 # Do not modify manually
 release = "3.0.0"
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+intersphinx_mapping = {
+    "followthemoney": ("https://followthemoney.readthedocs.io/en/latest/", None),
+}
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
