6d33bb476dfc13c6bec102bd44bed08a8e77e768
Friedrich Lindenberg <friedrich@pudo.org>
2021-08-08 19:06:00 +0200

    Fix up metadata handling

--- opensanctions/helpers/sanctions.py
+++ opensanctions/helpers/sanctions.py
@@ -7,8 +7,6 @@ def make_sanction(entity, key=None):
     sanction.add("entity", entity)
     if dataset.publisher.country != "zz":
         sanction.add("country", dataset.publisher.country)
-    sanction.add("authority", dataset.publisher.authority)
-    if not sanction.has("authority"):
-        sanction.add("authority", dataset.publisher.organization)
+    sanction.add("authority", dataset.publisher.name)
     sanction.add("sourceUrl", dataset.url)
     return sanction
