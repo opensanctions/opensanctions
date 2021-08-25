90e0052bbcbb38abf2284bf05510aaf8ae532fe9
Friedrich Lindenberg <friedrich@pudo.org>
2021-08-25 16:20:10 +0200

    weird scaling thing makes no sense to me

--- site/components/Layout.tsx
+++ site/components/Layout.tsx
@@ -31,7 +31,7 @@ function LayoutBase({ title, description, structured, children }: React.PropsWit
         <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@open_sanctions" />
         <meta name="twitter:creator" content="@pudo" />
