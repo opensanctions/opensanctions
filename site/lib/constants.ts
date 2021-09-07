83767ef15196e599f15f607556c496588e5b7c84
Friedrich Lindenberg <friedrich@pudo.org>
2021-09-07 08:53:08 +0200

    Add links and better descriptions to home page

--- site/lib/constants.ts
+++ site/lib/constants.ts
@@ -4,6 +4,7 @@ export const BASE_URL = "https://opensanctions.org"
 export const INDEX_URL = "https://data.opensanctions.org/datasets/latest/index.json"
 export const LICENSE_URL = "https://creativecommons.org/licenses/by/4.0/"
 export const CLAIM = "The open database of sanctions targets and persons of interest"
+export const SUBCLAIM = "OpenSanctions lets investigators find leads, helps companies manage risk and enables technologists to build data-driven products."
 export const EMAIL = "info@opensanctions.org"
 
 export const SPACER = " · ";
