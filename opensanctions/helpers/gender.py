f189de4f019379d46f51cf2dd69c81779dbf81dc
Friedrich Lindenberg <friedrich@pudo.org>
2021-11-21 20:06:46 +0100

    Scrape two datasets from Russian Rosfinmonitoring, one of which will be excluded from collections

--- opensanctions/helpers/gender.py
+++ opensanctions/helpers/gender.py
@@ -1,10 +1,17 @@
+import structlog
 from typing import List
 from opensanctions.core.lookups import common_lookups
+
+log = structlog.get_logger(__name__)
 
 
 def clean_gender(value: str) -> List[str]:
     """Not clear if this function name is offensive or just weird."""
     lookup = common_lookups().get("gender")
-    if lookup is None:
+    if lookup is None or value is None:
         return [value]
-    return lookup.get_values(value, default=[value])
+    results = lookup.get_values(value, default=[])
+    if not len(results):
+        log.warning("Gender not mapped", gender=value)
+        return [value]
+    return results
