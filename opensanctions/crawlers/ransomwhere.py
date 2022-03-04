58686eca00247a5cb6a94f72f70914e4e4f5e8fa
Friedrich Lindenberg <friedrich@pudo.org>
2022-03-04 15:07:44 +0100

    format numbers, fixes #194.

--- opensanctions/crawlers/ransomwhere.py
+++ opensanctions/crawlers/ransomwhere.py
@@ -5,6 +5,11 @@ from opensanctions.core import Context
 from opensanctions import helpers as h
 
 
+def format_number(value):
+    if value is not None:
+        return "%.2f" % float(value)
+
+
 def crawl(context: Context):
     path = context.fetch_resource("source.json", context.dataset.data.url)
     context.export_resource(path, JSON, title=context.SOURCE_TITLE)
@@ -18,8 +23,8 @@ def crawl(context: Context):
         wallet.add("createdAt", entry.pop("createdAt"))
         wallet.add("modifiedAt", entry.pop("updatedAt"))
         wallet.add("alias", entry.pop("family"))
-        wallet.add("balance", entry.pop("balance"))
-        wallet.add("amountUsd", entry.pop("balanceUSD"))
+        wallet.add("balance", format_number(entry.pop("balance")))
+        wallet.add("amountUsd", format_number(entry.pop("balanceUSD")))
         wallet.add("currency", entry.pop("blockchain"))
         h.audit_data(entry, ignore=["transactions"])
         context.emit(wallet)
