876f5e1bd3320e683446a60712962d27f97a687b
Friedrich Lindenberg <friedrich@pudo.org>
2022-03-14 15:40:48 +0100

    Switch RuPEP to using the online data releases

--- opensanctions/crawlers/ru_rupep.py
+++ opensanctions/crawlers/ru_rupep.py
@@ -1,278 +1,166 @@
-import csv
+import json
 import requests
 from lxml import html
-from typing import Dict
+from typing import Any, Dict, Optional
 from contextlib import closing
 from codecs import iterdecode
 from datapatch.result import Result
 from followthemoney.types import registry
+from pantomime.types import JSON
 
 from opensanctions.core import Context
 from opensanctions import helpers as h
-
-TYPE_OF_OFFICIAL = {
-    "": (None, None),
-    # Національний публічний діяч - National public figure
-    "1": ("Національний публічний діяч", "role.pep"),
-    # Іноземний публічний діяч - Foreign public figure
-    "2": ("Іноземний публічний діяч", "role.pep"),
-    "3": ("Діяч, що виконуює значні функції в міжнародній організації", "gov.igo"),
-    # "Пов'язана особа"
-    "4": (None, "poi"),
-    # "Член сім'ї" - family:
-    "5": (None, "role.rca"),
-}
+from opensanctions.util import multi_split
 
 
-def stream_csv(table_name):
-    url = "https://data.opensanctions.org/contrib/rupep/20220224/%s.csv" % table_name
-    with closing(requests.get(url, stream=True)) as r:
-        lines = iterdecode(r.iter_lines(), "utf-8")
-        reader = csv.DictReader(lines, delimiter=",", quotechar='"')
-        for row in reader:
-            yield row
-
-
-def strip_html(snippet):
-    if snippet is None or not len(snippet.strip()):
+def clean_wdid(wikidata_id: Optional[str]):
+    if wikidata_id is None:
         return None
-    doc = html.fromstring(snippet)
-    return doc.text_content()
+    wikidata_id = wikidata_id.strip().upper()
+    if len(wikidata_id) == 0 or wikidata_id != "NONE":
+        return None
+    return wikidata_id
 
 
-def get_countries(context: Context):
-    countries: Dict[str, str] = {}
-    for row in stream_csv("core_country"):
-        country_id = row.get("id")
-        country_code = registry.country.clean_text(row.get("name_en"))
-        if country_code is None:
-            country_code = registry.country.clean_text(row.get("iso2"))
-        if country_code is None:
-            country_code = registry.country.clean_text(row.get("name_ru"))
-        if row.get("iso2") == "DR":
-            country_code = "dd"
-        if row.get("iso2") == "AB":
-            country_code = "ge-ab"
-        # if country_code is None:
-        #     context.pprint(row)
-        if country_code is not None:
-            countries[country_id] = country_code
-    return countries
-
-
-def person_id(context: Context, id: str):
+def person_id(context: Context, id: str, wikidata_id: Optional[str]):
+    if wikidata_id is not None:
+        return wikidata_id
     return context.make_slug("person", id)
 
 
-def company_id(context: Context, id: str):
-    return context.make_slug("company", id)
+# def company_id(context: Context, id: str):
+#     return context.make_slug("company", id)
 
 
-def crawl_people(context: Context, countries):
-    # for row in stream_csv("core_person2country"):
-    #     entity = context.make("Person")
-    #     entity.id = person_id(context, row.pop("from_person_id"))
-    #     type_ = row.pop("relationship_type")
-    #     type_res = context.lookup("country_links", type_)
-    #     if type_res is None:
-    #         context.log.warning("Unknown country/person relation", type_=type_)
-    #         continue
-    #     if type_res.prop is None:
-    #         continue
-    #     country_code = countries.get(row.pop("to_country_id"))
-    #     entity.add(type_res.prop, country_code)
-    #     context.emit(entity)
-    #     # h.audit_data(row)
-
-    # for row in stream_csv("core_person2person"):
-    #     from_type = row.pop("from_relationship_type")
-    #     to_type = row.pop("to_relationship_type")
-    #     type_ = "%s / %s" % (from_type, to_type)
-    #     type_res = context.lookup("person_links", type_)
-    #     if type_res is None:
-    #         context.log.warning("Unknown person/person relation", type_=type_)
-    #         continue
-    #     from_person_id = person_id(context, row.pop("from_person_id"))
-    #     to_person_id = person_id(context, row.pop("to_person_id"))
-    #     entity = context.make(type_res.schema)
-    #     entity.id = context.make_slug("p2p", row.pop("id"))
-    #     entity.add(type_res.from_prop, from_person_id)
-    #     entity.add(type_res.to_prop, to_person_id)
-    #     entity.add(type_res.desc_prop, type_)
-    #     entity.add("summary", strip_html(row.pop("relationship_details")))
-    #     entity.add("summary", strip_html(row.pop("relationship_details_ru")))
-    #     entity.add("startDate", row.pop("date_established"))
-    #     entity.add("endDate", row.pop("date_finished"))
-    #     context.emit(entity)
-    #     # h.audit_data(row)
-
-    for row in stream_csv("core_person"):
-        entity = context.make("Person")
-        entity.id = person_id(context, row.pop("id"))
-        target = row.pop("is_pep") == "TRUE"
-        if target:
-            entity.add("topics", "role.pep")
-        entity.add("birthDate", row.pop("dob"))
-        entity.add("birthPlace", row.pop("city_of_birth"))
-        entity.add("birthPlace", row.pop("city_of_birth_ru"))
-        entity.add("modifiedAt", row.pop("last_change"))
-        entity.add("innCode", row.pop("inn"))
-
-        h.apply_name(
-            entity,
-            first_name=row.pop("first_name"),
-            patronymic=row.pop("patronymic"),
-            last_name=row.pop("last_name"),
-        )
-        h.apply_name(
-            entity,
-            first_name=row.pop("first_name_en"),
-            patronymic=row.pop("patronymic_en"),
-            last_name=row.pop("last_name_en"),
-        )
-        h.apply_name(
-            entity,
-            first_name=row.pop("first_name_ru"),
-            patronymic=row.pop("patronymic_ru"),
-            last_name=row.pop("last_name_ru"),
-        )
-        (type_text, type_topic) = TYPE_OF_OFFICIAL[row.pop("type_of_official")]
-        entity.add("position", type_text)
-        entity.add("topics", type_topic)
-
-        # TODO: can these be split somehow?
-        aliases = row.pop("names")
-        if aliases is not None:
-            if "\u200b" in aliases:
-                entity.add("alias", aliases.split("\u200b"))
-            else:
-                entity.add("weakAlias", aliases)
-
-        context.emit(entity, target=target)
-        # h.audit_data(row)
+def split_names(names):
+    return multi_split(names, ["\n", ", "])
 
 
-def crawl_companies(context: Context, countries):
-    for row in stream_csv("core_company2country"):
-        entity = context.make("Organization")
-        entity.id = company_id(context, row.pop("from_company_id"))
-        type_ = row.pop("relationship_type")
-        type_res = context.lookup("country_links", type_)
-        if type_res is None:
-            context.log.warning("Unknown country/company relation", type_=type_)
-            continue
-        if type_res.prop is None:
-            continue
-        country_code = countries.get(row.pop("to_country_id"))
-        entity.add(type_res.prop, country_code)
-        context.emit(entity)
-        # h.audit_data(row)
+def parse_date(date):
+    return h.parse_date(date, ["%d.%m.%Y", "%m.%Y", "%Y"])
 
-    for row in stream_csv("core_person2company"):
-        type_ = row.pop("relationship_type_en")
-        type_ = type_ or row.pop("relationship_type")
-        type_res = context.lookup("company_person_links", type_)
-        if type_res is None:
-            # context.log.warning("Unknown person/company relation", type_=type_)
-            type_res = Result(
-                dict(
-                    schema="UnknownLink",
-                    from_prop="subject",
-                    to_prop="object",
-                    desc_prop="role",
-                )
+
+def fetch(context: Context):
+    companies_url = "https://rupep.org/opendata/companies/json"
+    path = context.fetch_resource("companies.json", companies_url)
+    context.export_resource(path, JSON, title=context.SOURCE_TITLE)
+    path = context.fetch_resource("persons.json", context.dataset.data.url)
+    context.export_resource(path, JSON, title=context.SOURCE_TITLE)
+    with open(path, "r") as fh:
+        return json.load(fh)
+
+
+def crawl_person(context: Context, data: Dict[str, Any]):
+    is_pep = data.pop("is_pep", False)
+    entity = context.make("Person", target=is_pep)
+    wikidata_id = clean_wdid(data.pop("wikidata_id", None))
+    entity.id = person_id(context, data.pop("id"), wikidata_id)
+    entity.add("sourceUrl", data.pop("url_en", None))
+    data.pop("url_ru", None)
+    entity.add("modifiedAt", data.pop("last_change", None))
+    entity.add("wikidataId", wikidata_id)
+    entity.add("name", data.pop("full_name_en", None))
+    entity.add("name", data.pop("full_name_ru", None))
+    entity.add("alias", data.pop("inversed_full_name_en", None))
+    entity.add("alias", data.pop("inversed_full_name_ru", None))
+    entity.add("alias", data.pop("also_known_as_en", None))
+    entity.add("alias", data.pop("also_known_as_ru", None))
+    entity.add("alias", split_names(data.pop("names", [])))
+    entity.add("birthDate", parse_date(data.pop("date_of_birth", None)))
+    entity.add("deathDate", parse_date(data.pop("termination_date_human", None)))
+    entity.add("birthPlace", data.pop("city_of_birth_ru", None))
+    entity.add("birthPlace", data.pop("city_of_birth_en", None))
+    entity.add("innCode", data.pop("inn", None))
+    entity.add("firstName", data.pop("first_name_en", None))
+    entity.add("firstName", data.pop("first_name_ru", None))
+    entity.add("fatherName", data.pop("patronymic_en", None))
+    entity.add("fatherName", data.pop("patronymic_ru", None))
+    entity.add("lastName", data.pop("last_name_en", None))
+    entity.add("lastName", data.pop("last_name_ru", None))
+
+    for suffix in ("", "_en", "_ru"):
+        role = data.pop(f"last_job_title{suffix}", None)
+        org = data.pop(f"last_workplace{suffix}", None)
+        entity.add("position", f"{org} ({role})")
+
+    for country_data in data.pop("related_countries", []):
+        rel_type = country_data.pop("relationship_type")
+        country_name = country_data.pop("to_country_en")
+        country_name = country_name or country_data.pop("to_country_ru")
+        # print(country_name)
+        res = context.lookup("country_links", rel_type)
+        if res is None:
+            context.log.warn(
+                "Unknown country link",
+                rel_type=rel_type,
+                entity=entity,
+                country=country_name,
             )
-        from_person_id = person_id(context, row.pop("from_person_id"))
-        to_company_id = company_id(context, row.pop("to_company_id"))
-        entity = context.make(type_res.schema)
-        entity.id = context.make_slug("p2c", row.pop("id"))
-        entity.add(type_res.from_prop, from_person_id)
-        entity.add(type_res.to_prop, to_company_id)
-        entity.add(type_res.desc_prop, type_)
-        entity.add("startDate", row.pop("date_established"))
-        entity.add("endDate", row.pop("date_finished"))
-        context.emit(entity)
-        # h.audit_data(row)
-
-    for row in stream_csv("core_company2company"):
-        type_ = row.pop("relationship_type")
-        type_res = context.lookup("company_links", type_)
-        if type_res is None:
-            context.log.warning("Unknown company/company relation", type_=type_)
-            # type_res = Result(
-            #     dict(
-            #         schema="UnknownLink",
-            #         from_prop="subject",
-            #         to_prop="object",
-            #         desc_prop="role",
-            #     )
-            # )
             continue
-        from_company_id = company_id(context, row.pop("from_company_id"))
-        to_company_id = company_id(context, row.pop("to_company_id"))
-        entity = context.make(type_res.schema)
-        entity.id = context.make_slug("c2c", row.pop("id"))
-        entity.add(type_res.from_prop, from_company_id)
-        entity.add(type_res.to_prop, to_company_id)
-        entity.add(type_res.desc_prop, type_)
-        entity.add("startDate", row.pop("date_established"))
-        entity.add("endDate", row.pop("date_finished"))
-        context.emit(entity)
-        # h.audit_data(row)
+        if res.prop is not None:
+            entity.add(res.prop, country_name)
+        # h.audit_data(country_data)
 
-    for row in stream_csv("core_company"):
-        entity = context.make("Organization")
-        entity.id = company_id(context, row.pop("id"))
-        if row.pop("state_company") == "TRUE":
-            entity.add("topics", "gov.soe")
-        entity.add("name", row.pop("name"))
-        entity.add("name", row.pop("name_en"))
-        entity.add("name", row.pop("name_ru"))
-        entity.add("alias", row.pop("short_name"))
-        entity.add("alias", row.pop("short_name_en"))
-        entity.add("alias", row.pop("short_name_ru"))
-        entity.add("alias", row.pop("also_known_as"))
-        entity.add("website", row.pop("website"))
-        entity.add("incorporationDate", row.pop("founded"))
-        entity.add("dissolutionDate", row.pop("closed_on"))
-        entity.add_cast("Company", "ogrnCode", row.pop("ogrn_code"))
-        entity.add_cast("Company", "registrationNumber", row.pop("edrpou"))
-        entity.add("modifiedAt", row.pop("last_change"))
+    for rel_data in data.pop("related_persons", []):
+        other_pep = rel_data.pop("is_pep", False)
+        other_wdid = clean_wdid(rel_data.pop("person_wikidata_id"))
+        other = context.make("Person", target=other_pep)
+        other.id = person_id(context, rel_data.pop("person_id"), other_wdid)
+        other.add("name", rel_data.pop("person_en", None))
+        other.add("name", rel_data.pop("person_ru", None))
+        other.add("wikidataId", other_wdid)
 
-        zip_code = row.pop("zip_code")
-        addr = h.make_address(
-            context,
-            remarks=row.pop("raw_address"),
-            street=row.pop("street"),
-            street2=row.pop("appt"),
-            city=row.pop("city"),
-            postal_code=zip_code,
-        )
-        h.apply_address(context, entity, addr)
+        rel_type = rel_data.pop("relationship_type_en", None)
+        rel_type_ru = rel_data.pop("relationship_type_ru", None)
+        rel_type = rel_type or rel_type_ru
+        res = context.lookup("person_relations", rel_type)
+        if res is None:
+            context.log.warn(
+                "Unknown person/person relation type",
+                rel_type=rel_type,
+                entity=entity,
+                other=other,
+            )
+            continue
 
-        addr = h.make_address(
-            context,
-            street=row.pop("street_ru"),
-            street2=row.pop("appt_ru"),
-            city=row.pop("city_ru"),
-            postal_code=zip_code,
-        )
-        h.apply_address(context, entity, addr)
+        # print("LINK", (entity.id, other.id))
+        id_a, id_b = sorted((entity.id, other.id))
+        rel = context.make(res.schema)
+        rel.id = context.make_slug(id_a, id_b)
+        rel.add(res.from_prop, id_a)
+        rel.add(res.to_prop, id_b)
+        rel.add(res.desc_prop, rel_type)
+        rel.add("modifiedAt", parse_date(rel_data.pop("date_confirmed")))
+        rel.add("startDate", parse_date(rel_data.pop("date_established")))
+        rel.add("endDate", parse_date(rel_data.pop("date_finished")))
 
-        addr = h.make_address(
-            context,
-            street=row.pop("street_en"),
-            street2=row.pop("appt_en"),
-            city=row.pop("city_en"),
-            postal_code=zip_code,
-        )
-        h.apply_address(context, entity, addr)
+        # h.audit_data(rel_data)
+        context.emit(other, target=other_pep)
+        context.emit(rel)
 
-        context.emit(entity)
-        # h.audit_data(row)
+    data.pop("type_of_official_ru", None)
+    person_type = data.pop("type_of_official_en", None)
+    person_topic = context.lookup_value("person_type", person_type)
+    if person_topic is None:
+        context.log.warn("Unknown type of official", type=person_type)
+    entity.add("topics", person_topic)
+    if is_pep:
+        entity.add("topics", "role.pep")
+    entity.add("status", person_type)
+
+    data.pop("died", None)
+    data.pop("tags", None)
+    data.pop("reason_of_termination_en", None)
+    data.pop("reason_of_termination_ru", None)
+    # TODO: store images
+    data.pop("photo", None)
+    data.pop("related_companies", None)
+    data.pop("declarations", None)
+    # h.audit_data(data)
+    context.emit(entity, target=is_pep)
 
 
 def crawl(context: Context):
-    countries = get_countries(context)
-    crawl_people(context, countries)
-    # crawl_companies(context, countries)
+    persons = fetch(context)
+    for data in persons:
+        crawl_person(context, data)
