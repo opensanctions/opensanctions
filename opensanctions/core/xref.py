6c8f9a5ad91debc60ebdb95880636ff7babaad2d
Friedrich Lindenberg <friedrich@pudo.org>
2022-04-13 12:37:39 +0200

    use nomenklatura xref

--- opensanctions/core/xref.py
+++ opensanctions/core/xref.py
@@ -1,6 +1,5 @@
 import structlog
-from nomenklatura.index import Index
-from nomenklatura.util import is_qid
+from nomenklatura.xref import xref
 from followthemoney.types import registry
 
 from opensanctions.core.dataset import Dataset
@@ -10,36 +9,10 @@ from opensanctions.core.resolver import get_resolver
 log = structlog.get_logger(__name__)
 
 
-def blocking_xref(dataset: Dataset, limit: int = 5000, fuzzy: bool = False):
+def blocking_xref(dataset: Dataset, limit: int = 5000):
     resolver = get_resolver()
     resolver.prune()
-    db = Database(dataset, resolver, cached=True)
+    db = Database(dataset, resolver, cached=False)
     loader = db.view(dataset)
-    index = Index(loader)
-    index.build(fuzzy=fuzzy, adjacent=False)
-    suggested = 0
-    for idx, (pair, score) in enumerate(index.pairs()):
-        if idx % 1000 == 0:
-            log.info("Evaluating pairs: %d (%d candidates)..." % (idx, suggested))
-        left = loader.get_entity(str(pair[0]))
-        right = loader.get_entity(str(pair[1]))
-        if left is None or right is None:
-            continue
-        if left.schema not in right.schema.matchable_schemata:
-            if right.schema not in left.schema.matchable_schemata:
-                continue
-        if not resolver.check_candidate(left.id, right.id):
-            continue
-        if is_qid(left.id) and is_qid(right.id):
-            continue
-        countries = left.get_type_values(registry.country)
-        countries.extend(right.get_type_values(registry.country))
-        # if "ru" in countries or "by" in countries or "ua" in countries:
-        #     score = score * 10
-        if len(left.datasets.intersection(right.datasets)) > 0:
-            score = score * 0.5
-        resolver.suggest(left.id, right.id, score)
-        if suggested > limit:
-            break
-        suggested += 1
+    xref(loader, resolver, limit=limit, scored=True)
     resolver.save()
