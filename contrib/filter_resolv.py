9b294d65165bc631caa34057068c4de989ab7a5d
Friedrich Lindenberg <friedrich@pudo.org>
2022-04-27 15:35:31 +0200

    Australia match remover

--- contrib/filter_resolv.py
+++ contrib/filter_resolv.py
@@ -1,6 +1,19 @@
 import json
+from re import L
 
 from opensanctions import settings
+
+
+def check_raw(ident):
+    prefix = "au-dfat-"
+    if not ident.startswith(prefix):
+        return False
+    suffix = ident[len(prefix) :]
+    try:
+        int(suffix)
+        return True
+    except:
+        return False
 
 
 def filter_resolver():
@@ -9,10 +22,16 @@ def filter_resolver():
         with open(out_file, "w") as outfh:
             while line := infh.readline():
                 data = json.loads(line)
-                ts = data[5]
-                if ts > "2022-04-19":
-                    print(ts)
+                if check_raw(data[0]):
+                    print(data[0])
                     continue
+                if check_raw(data[1]):
+                    print(data[1])
+                    continue
+                # ts = data[5]
+                # if ts > "2022-04-19":
+                #     print(ts)
+                #     continue
                 outfh.write(line)
 
 
