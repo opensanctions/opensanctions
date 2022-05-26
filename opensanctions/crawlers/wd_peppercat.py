1b44e06054d6ca50c397e91ef3f4d5d0217db13c
Friedrich Lindenberg <friedrich@pudo.org>
2022-05-26 18:04:41 +0200

    use wikidata as an enricher

--- opensanctions/crawlers/wd_peppercat.py
+++ opensanctions/crawlers/wd_peppercat.py
@@ -1,36 +1,27 @@
 import csv
+from typing import Optional
 from pantomime.types import CSV
+from nomenklatura.util import is_qid
 
 from opensanctions.core import Context
-from opensanctions.wikidata import get_entity, entity_to_ftm
-
-
-def crawl_qid(context, qid, country):
-    if qid is None:
-        return
-    data = get_entity(context, qid)
-    if data is not None:
-        entity_to_ftm(
-            context,
-            data,
-            position=data.get("position"),
-            topics="role.pep",
-            country=country,
-        )
-        # context.log.info("Target entity", entity=entity)
 
 
 def crawl(context: Context):
     path = context.fetch_resource("source.csv", context.dataset.data.url)
     context.export_resource(path, CSV, title=context.SOURCE_TITLE)
-    context.log.info("Loading wikidata API cache...")
-    context.cache.preload("https://www.wikidata.org/w/api.php%")
     prev_country = None
     with open(path, "r") as fh:
         for row in csv.DictReader(fh):
-            qid = row.get("personID")
             country = row.get("catalog")
             if country != prev_country:
                 context.log.info("Crawl country", country=country)
                 prev_country = country
-            crawl_qid(context, qid, country)
+            entity = context.make("Person")
+            qid: Optional[str] = row.get("personID")
+            if qid is None or not is_qid(qid):
+                continue
+            entity.id = qid
+            entity.add("name", row.get("person"))
+            entity.add("topics", "role.pep")
+            entity.add("country", country)
+            context.emit(entity, target=True)
--- opensanctions/wikidata/__init__.py
+++
@@ -1,4 +0,0 @@
-from opensanctions.wikidata.api import get_entity
-from opensanctions.wikidata.entity import entity_to_ftm
-
-__all__ = ["get_entity", "entity_to_ftm"]
