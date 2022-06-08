3000aa7da4f75d257cdb1abc2819790a4446f5d4
Friedrich Lindenberg <friedrich@pudo.org>
2022-06-08 10:29:52 +0200

    fix up xref-prune

--- opensanctions/cli.py
+++ opensanctions/cli.py
@@ -94,8 +94,8 @@ def xref(dataset, limit):
 def xref_prune(keep=0):
     resolver = get_resolver()
     for edge in list(resolver.edges.values()):
-        if edge.user == AUTO_USER:
-            resolver._remove(edge)
+        if edge.judgement == Judgement.NO_JUDGEMENT:
+            resolver._remove_edge(edge)
     resolver.prune(keep=keep)
     resolver.save()
 
