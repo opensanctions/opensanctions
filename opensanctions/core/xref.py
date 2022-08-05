28121e566156a5633368552ad6fa96dc4e4f0b47
Friedrich Lindenberg <friedrich@pudo.org>
2022-08-05 11:13:53 +0200

    Always resolve statements before running xref

--- opensanctions/core/xref.py
+++ opensanctions/core/xref.py
@@ -3,6 +3,8 @@ from nomenklatura.xref import xref
 
 from opensanctions.core.dataset import Dataset
 from opensanctions.core.loader import Database
+from opensanctions.core.db import engine_tx
+from opensanctions.core.statements import resolve_all_canonical
 from opensanctions.core.resolver import AUTO_USER, get_resolver
 
 log = get_logger(__name__)
@@ -10,6 +12,8 @@ log = get_logger(__name__)
 
 def blocking_xref(dataset: Dataset, limit: int = 5000):
     resolver = get_resolver()
+    with engine_tx() as conn:
+        resolve_all_canonical(conn, resolver)
     resolver.prune()
     db = Database(dataset, resolver, cached=True, external=True)
     loader = db.view(dataset)
