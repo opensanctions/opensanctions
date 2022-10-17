efa55061b59b11405f8b5220e9c1c6b03ba03f73
Friedrich Lindenberg <friedrich@pudo.org>
2022-10-17 15:48:01 +0200

    Filter out some terms from ACF list

--- opensanctions/crawlers/ru_acf_bribetakers.py
+++ opensanctions/crawlers/ru_acf_bribetakers.py
@@ -1,9 +1,15 @@
 import csv
+from functools import cache
 from pantomime.types import CSV
-from normality import collapse_spaces
+from normality import collapse_spaces, normalize
 
 from opensanctions.core import Context
 from opensanctions import helpers as h
+
+
+@cache
+def norm_tag(text):
+    return normalize(text)
 
 
 def parse_date(date):
@@ -14,16 +20,31 @@ def parse_date(date):
 
 def crawl_row(context: Context, row):
     entity = context.make("Person")
-    tag = row.pop("Tag")
+    tags = row.pop("Tag")
     name_en = row.pop("Name eng")
     dob = row.pop("DOB")
-    entity.id = context.make_id(name_en, tag, dob)
+    entity.id = context.make_id(name_en, tags, dob)
     entity.add("name", name_en)
     entity.add("alias", row.get("Name cyrillic"))
     entity.add("birthDate", parse_date(dob))
-    entity.add("notes", collapse_spaces(row.get("Description")))
-    entity.add("position", tag.split("\n"))
     entity.add("gender", row.get("Gender"))
+    description = collapse_spaces(row.get("Description"))
+    result = context.lookup("descriptions", description)
+    if result is not None:
+        description = result.values
+    entity.add("notes", description)
+    for tag in tags.split("\n"):
+        tag = tag.strip()
+        ntag = norm_tag(tag)
+        if ntag is None:
+            continue
+        if "oligarch" in ntag:
+            entity.add("topics", "role.oligarch")
+            continue
+        elif "corrupt" in ntag:
+            continue
+        else:
+            entity.add("position", tag)
 
     context.emit(entity, target=True)
     # context.inspect(row)
