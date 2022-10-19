afee7002bdf91d731d5fb8bd05505869d33b922a
Friedrich Lindenberg <friedrich@pudo.org>
2022-10-19 12:26:23 +0200

    re-work NABC crawler

--- opensanctions/crawlers/ua_nabc_sanctions.py
+++ opensanctions/crawlers/ua_nabc_sanctions.py
@@ -1,9 +1,9 @@
 import json
-from typing import Any, Dict
+from typing import Any, Dict, Union
 from urllib.parse import urljoin
 from pantomime.types import JSON
 
-from opensanctions.core import Context
+from opensanctions.core import Context, Entity
 from opensanctions import helpers as h
 from opensanctions.util import multi_split
 
@@ -17,13 +17,21 @@ COUNTRIES = {
     None: None,
 }
 
+TRACK_COUNTRIES = ["ua", "us", "au", "ca", "ch", "es", "gb", "jp", "nz", "pl"]
 
-def clean_row(row: Dict[str, Any]) -> Dict[str, str]:
+
+def clean_row(row: Dict[str, Any]) -> Dict[str, Union[str, Dict[str, str]]]:
     data = {}
     for k, v in row.items():
         if v is None:
             continue
         if isinstance(v, int):
+            data[k] = v
+            continue
+        if isinstance(v, dict):
+            data[k] = clean_row(v)
+            continue
+        if isinstance(v, list):
             data[k] = v
             continue
         v = v.strip()
@@ -42,12 +50,61 @@ def parse_date(date):
     return dates
 
 
+def url_split(urls):
+    urls = []
+    for url in multi_split(urls, [";", "\n"]):
+        if len(url) > 5:
+            urls.append(url)
+    return urls
+
+
 def json_resource(context: Context, url, name):
     full_url = urljoin(url, name)
     path = context.fetch_resource(f"{name}.json", full_url)
     context.export_resource(path, JSON, title=context.SOURCE_TITLE)
     with open(path, "r") as fh:
         return json.load(fh)
+
+
+def crawl_common(context: Context, entity: Entity, row: Dict[str, Any]):
+    entity.add("topics", "sanction")
+    country = row.pop("country", None)
+    entity.add("country", COUNTRIES.get(country, country))
+    entity.add("keywords", row.pop("category", None))
+    entity.add("keywords", row.pop("subcategory_1", None))
+    entity.add("keywords", row.pop("subcategory_2", None))
+    entity.add("keywords", row.pop("subcategory_3", None))
+
+    entity.add("website", url_split(row.pop("link", "")))
+    entity.add("innCode", row.pop("itn", None))
+    entity.add("address", row.pop("address_ru", None))
+    entity.add("address", row.pop("address_uk", None))
+    entity.add("address", row.pop("address_en", None))
+
+    sanction = h.make_sanction(context, entity)
+    sanction.add("startDate", row.pop("sanctions_ua_date", None))
+    if row.pop("sanctions_ua") == 1:
+        sanction.add("status", "active")
+
+    sanction.add("sourceUrl", url_split(row.pop("url_ua", "")))
+
+    sanction.add("reason", row.pop("reasoning_en", None))
+    sanction.add("reason", row.pop("reasoning_ru", None))
+    sanction.add("reason", row.pop("reasoning_uk", None))
+
+    row.pop("status", None)
+    row.pop("synchron", None)
+    row.pop("top_50", None)
+    row.pop("link_archive", None)
+    row.pop("sort_order", None)
+
+    row.pop("relations_person", None)
+    row.pop("relations_company", None)
+
+    for cc in TRACK_COUNTRIES:
+        row.pop(f"sanctions_{cc}", None)
+        row.pop(f"sanctions_{cc}_date", None)
+        row.pop(f"url_{cc}", None)
 
 
 def crawl_person(context: Context) -> None:
@@ -65,6 +122,7 @@ def crawl_person(context: Context) -> None:
         entity.add("alias", name_ru)
         entity.add("alias", name_uk)
         entity.add("birthDate", parse_date(row.pop("date_bd", None)))
+        entity.add("deathDate", parse_date(row.pop("date_dead", None)))
         url = f"https://sanctions.nazk.gov.ua/sanction-person/{person_id}/"
         entity.add("sourceUrl", url)
         if row.get("city_bd_en") != "N/A":
@@ -74,22 +132,19 @@ def crawl_person(context: Context) -> None:
         entity.add("position", row.pop("position_en", None))
         entity.add("position", row.pop("position_ru", None))
         entity.add("position", row.pop("position_uk", None))
-        entity.add("notes", row.pop("reasoning_en", None))
-        entity.add("notes", row.pop("reasoning_ru", None))
-        entity.add("notes", row.pop("reasoning_uk", None))
 
-        country = row.get("country", None)
-        entity.add("country", COUNTRIES.get(country, country))
-        entity.add("topics", "sanction")
+        # TODO: emit image
+        photo_url = row.pop("photo_name", None)
+
+        crawl_common(context, entity, row)
         context.emit(entity, target=True)
-        # h.audit_data(row)
+        h.audit_data(row)
 
 
 def crawl_company(context: Context) -> None:
     data = json_resource(context, context.dataset.data.url, "company")
     for row in data["data"]:
         row = clean_row(row)
-        # context.inspect(row)
         company_id = row.pop("company_id")
         name_en = row.pop("name_en", None)
         name = row.pop("name", None) or name_en
@@ -111,16 +166,10 @@ def crawl_company(context: Context) -> None:
         entity.add("sourceUrl", url)
         entity.add_cast("Company", "ogrnCode", row.pop("ogrn", None))
 
-        country = row.pop("country", None)
-        entity.add("country", COUNTRIES.get(country, country))
-        entity.add("topics", "sanction")
-        entity.add("notes", row.pop("reasoning_en", None))
-        entity.add("notes", row.pop("reasoning_ru", None))
-        entity.add("notes", row.pop("reasoning_uk", None))
-
+        crawl_common(context, entity, row)
         context.emit(entity, target=True)
         row.pop("logo_en", None)
-        # context.inspect(row)
+        h.audit_data(row)
 
 
 def crawl(context: Context) -> None:
