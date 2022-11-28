ec6872e918163f203d08a96606b6e3fbda2959eb
Friedrich Lindenberg <friedrich@pudo.org>
2022-11-28 21:09:55 +0100

    threads are slower

--- opensanctions/processing.py
+++ opensanctions/processing.py
@@ -4,10 +4,8 @@ from concurrent.futures import Future, wait, ThreadPoolExecutor
 from opensanctions import settings
 from opensanctions.core import Dataset, Context
 from opensanctions.core.resolver import get_resolver
-from opensanctions.core.statements import resolve_all_canonical
 from opensanctions.exporters import export_metadata, export_dataset
 from opensanctions.core.loader import Database
-from opensanctions.core.db import engine_tx
 
 
 def _compute_futures(futures: List[Future]):
@@ -23,20 +21,18 @@ def run_export(
     """Export dump files for all datasets in the given scope."""
     scope = Dataset.require(scope_name)
     resolver = get_resolver()
-    with engine_tx() as conn:
-        resolve_all_canonical(conn, resolver)
     database = Database(scope, resolver, cached=True)
     database.view(scope)
-    # for dataset_ in scope.datasets:
-    #     export_dataset(dataset_, database)
-    # export_metadata()
-    with ThreadPoolExecutor(max_workers=threads) as executor:
-        futures: List[Future] = []
-        futures = []
-        for dataset_ in scope.datasets:
-            futures.append(executor.submit(export_dataset, dataset_, database))
-        futures.append(executor.submit(export_metadata))
-        _compute_futures(futures)
+    for dataset_ in scope.datasets:
+        export_dataset(dataset_, database)
+    export_metadata()
+    # with ThreadPoolExecutor(max_workers=threads) as executor:
+    #     futures: List[Future] = []
+    #     futures = []
+    #     for dataset_ in scope.datasets:
+    #         futures.append(executor.submit(export_dataset, dataset_, database))
+    #     futures.append(executor.submit(export_metadata))
+    #     _compute_futures(futures)
 
 
 def run_enrich(scope_name: str, external_name: str, threshold: float):
