e37ac682ecb93c2b8285e10d90b6ea20c6d8977c
Friedrich Lindenberg <friedrich@pudo.org>
2023-02-05 10:59:13 +0100

    Fix up CIA crawler, but it feels brittle

--- opensanctions/crawlers/us_cia_world_leaders.py
+++ opensanctions/crawlers/us_cia_world_leaders.py
@@ -1,33 +1,24 @@
-from lxml import html
+from normality import slugify
 
-from opensanctions import settings
 from opensanctions.core import Context
 from opensanctions.util import is_empty
 
-UI_URL = "https://www.cia.gov%s"
-DATA_URL = "https://www.cia.gov/page-data%spage-data.json"
+WEB_URL = "https://www.cia.gov/resources/world-leaders/foreign-governments/%s"
+DATA_URL = "https://www.cia.gov/resources/world-leaders/page-data/foreign-governments/%s/page-data.json"
 
 
-def crawl_country(context: Context, params, path, country):
-    source_url = UI_URL % path
+def crawl_country(context: Context, country: str):
     context.log.debug("Crawling country: %s" % country)
-    res = context.fetch_json(DATA_URL % path, params=params)
-    data = res.get("result", {}).get("data", {}).get("page", {})
-    blocks = data.get("acf", {}).get("blocks", [{}])[0]
-    content = blocks.get("free_form_content", []).get("content")
-    doc = html.fromstring(content)
-    function = None
-    for i, el in enumerate(doc.getchildren()):
-        text = el.text_content().strip()
-        if el.tag == "h2":
-            continue
-        if el.tag == "h3":
-            function = text
-            continue
-        if i == 0 and el.tag == "p":
-            # this paragraph at the start is a note, not a person
-            continue
-        name = text.replace("(Acting)", "")
+    country_slug = slugify(country.replace("'", ""), sep="-")
+    data_url = DATA_URL % country_slug
+    source_url = WEB_URL % country_slug
+    res = context.fetch_json(data_url)
+    page_data = res["result"]["data"]["page"]
+    leaders = page_data["leaders"]
+    for leader in leaders:
+        name = leader["name"]
+        name = name.replace("(Acting)", "")
+        function = leader["title"]
         if is_empty(name):
             continue
         context.log.debug(
@@ -40,7 +31,7 @@ def crawl_country(context: Context, params, path, country):
         person = context.make("Person")
         person.id = context.make_slug(country, name, function)
         person.add("name", name)
-        person.add("country", country)
+        person.add("country", page_data["country"])
         person.add("position", function)
         person.add("sourceUrl", source_url)
         person.add("topics", "role.pep")
@@ -48,9 +39,7 @@ def crawl_country(context: Context, params, path, country):
 
 
 def crawl(context: Context):
-    params = {"_": settings.RUN_DATE}
-    res = context.fetch_json(context.source.data.url, params=params)
-    data = res.get("result", {}).get("data", {})
-    for edge in data.get("governments", {}).get("edges", []):
-        node = edge.get("node", {})
-        crawl_country(context, params, node.get("path"), node.get("title"))
+    data = context.fetch_json(context.source.data.url)
+    countries = data["data"]["leaders"]["nodes"]
+    for c in countries:
+        crawl_country(context, c["country"])
