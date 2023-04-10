81d1ea0dc4d94e7de201a64cc90d7346640335ef
Friedrich Lindenberg <friedrich@pudo.org>
2023-04-10 19:06:29 +0200

    cache dataset stats in the DB

--- opensanctions/exporters/metadata.py
+++ opensanctions/exporters/metadata.py
@@ -1,12 +1,13 @@
 from typing import Any, Dict
-from functools import cache
 from urllib.parse import urljoin
 from followthemoney import model
 from zavod.logs import get_logger
 from nomenklatura.matching import explain_matcher
+from nomenklatura.cache import Cache
+from nomenklatura.util import datetime_iso
 
 from opensanctions import settings
-from opensanctions.core.db import engine_read
+from opensanctions.core.db import engine, metadata, engine_tx, engine_read
 from opensanctions.core.dataset import Dataset
 from opensanctions.core.issues import all_issues, agg_issues_by_level
 from opensanctions.core.resources import all_resources
@@ -23,30 +24,42 @@ log = get_logger(__name__)
 THINGS = [s.name for s in model if s.is_a("Thing")]
 
 
-@cache
 def dataset_to_index(dataset: Dataset) -> Dict[str, Any]:
     log.info("Computing metadata stats...", dataset=dataset.name)
-    with engine_read() as conn:
+    cache = Cache(engine, metadata, dataset)
+    with engine_tx() as conn:
+        last_seen = max_last_seen(conn, dataset)
+        stats_key = f"dataset:stats:{datetime_iso(last_seen)}"
+        stats = cache.get_json(stats_key, max_age=3, conn=conn)
+        if stats is None:
+            target_count = count_entities(conn, dataset=dataset, target=True)
+            stats = {
+                "entity_count": count_entities(conn, dataset=dataset),
+                "target_count": target_count,
+                "targets": {
+                    "total": target_count,
+                    "countries": agg_entities_by_country(conn, dataset, target=True),
+                    "schemata": agg_entities_by_schema(conn, dataset, target=True),
+                },
+                "things": {
+                    "total": count_entities(conn, dataset=dataset, schemata=THINGS),
+                    "countries": agg_entities_by_country(
+                        conn, dataset, schemata=THINGS
+                    ),
+                    "schemata": agg_entities_by_schema(conn, dataset, schemata=THINGS),
+                },
+            }
+            cache.set_json(stats_key, stats, conn=conn)
+
         meta = dataset.to_dict()
-        meta["last_change"] = max_last_seen(conn, dataset)
+        meta.update(stats)
+        meta["last_change"] = last_seen
         meta["last_export"] = settings.RUN_TIME
+        meta["entity_count"] = count_entities(conn, dataset=dataset)
         meta["index_url"] = dataset.make_public_url("index.json")
         meta["issues_url"] = dataset.make_public_url("issues.json")
         meta["issue_levels"] = agg_issues_by_level(conn, dataset)
         meta["issue_count"] = sum(meta["issue_levels"].values())
-        meta["entity_count"] = count_entities(conn, dataset=dataset)
-        target_count = count_entities(conn, dataset=dataset, target=True)
-        meta["target_count"] = target_count
-        meta["targets"] = {
-            "total": target_count,
-            "countries": agg_entities_by_country(conn, dataset, target=True),
-            "schemata": agg_entities_by_schema(conn, dataset, target=True),
-        }
-        meta["things"] = {
-            "total": count_entities(conn, dataset=dataset, schemata=THINGS),
-            "countries": agg_entities_by_country(conn, dataset, schemata=THINGS),
-            "schemata": agg_entities_by_schema(conn, dataset, schemata=THINGS),
-        }
         meta["resources"] = list(all_resources(conn, dataset))
         return meta
 
