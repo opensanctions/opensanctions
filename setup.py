1dad2164f58416d61607ee9291d3155a3c744c4c
Friedrich Lindenberg <friedrich@pudo.org>
2023-06-18 09:50:10 -0400

    up nk version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "followthemoney == 3.4.0",
-        "nomenklatura == 2.14.0",
+        "nomenklatura == 2.14.1",
         "orjson == 3.9.0",
         "zavod == 0.6.3",
         "pantomime == 0.6.0",
