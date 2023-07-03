8d5f1e214ffab5a5bb73bc63b22131ccafd9f710
Friedrich Lindenberg <friedrich@pudo.org>
2023-07-03 12:08:47 +0200

    upgrade nk + zavod

--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ setup(
     zip_safe=False,
     install_requires=[
         "followthemoney == 3.4.3",
-        "nomenklatura == 3.0.3",
+        "nomenklatura == 3.1.0",
         "orjson == 3.9.1",
-        "zavod == 0.7.1",
+        "zavod == 0.7.2",
         "pantomime == 0.6.0",
         "plyvel==1.5.0",
         "sqlalchemy[mypy]",
