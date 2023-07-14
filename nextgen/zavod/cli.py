649606a81fbf7e68f9cb194596cea08d4faf8319
Friedrich Lindenberg <friedrich@pudo.org>
2023-07-14 19:37:02 +0200

    begin migrating settings from core

--- nextgen/zavod/cli.py
+++ nextgen/zavod/cli.py
@@ -4,6 +4,9 @@ from pathlib import Path
 from followthemoney.cli.util import InPath
 
 from zavod.logs import configure_logging, get_logger
+from zavod.meta import load_dataset_from_path
+
+# from zavod.context import Context
 
 log = get_logger(__name__)
 
@@ -17,4 +20,6 @@ def cli() -> None:
 @click.argument("path", type=InPath)
 @click.option("-d", "--dry-run", is_flag=True, default=False)
 def run(path: Path, dry_run: bool = False) -> None:
-    pass
+    dataset = load_dataset_from_path(path)
+    # context = Context(dataset)
+    print(dataset)
