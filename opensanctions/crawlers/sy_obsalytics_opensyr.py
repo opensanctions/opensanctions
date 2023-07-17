8bccbd65488833680057edd82f6b2c1930cbdbbe
Friedrich Lindenberg <friedrich@pudo.org>
2023-07-17 17:06:37 +0200

    dumb fix for opensyr

--- opensanctions/crawlers/sy_obsalytics_opensyr.py
+++ opensanctions/crawlers/sy_obsalytics_opensyr.py
@@ -1,14 +1,15 @@
 from pantomime.types import JSON
+from followthemoney.proxy import EntityProxy
 from followthemoney.cli.util import binary_entities
 
-from opensanctions.core import Context, Entity
+from opensanctions.core import Context
 
 
 def crawl(context: Context):
     path = context.fetch_resource("source.json", context.source.data.url)
     context.export_resource(path, JSON, title=context.SOURCE_TITLE)
     with open(path, "rb") as fh:
-        for entity in binary_entities(fh, Entity):
+        for entity in binary_entities(fh, EntityProxy):
             proxy = context.make(entity.schema)
             proxy.id = entity.id
             for prop, value in entity.itervalues():
