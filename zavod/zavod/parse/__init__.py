021794fe6f71d2b2dfc9add5b45165efd4bc934e
Friedrich Lindenberg <friedrich@pudo.org>
2023-07-21 12:25:03 +0200

    improve docstrings

--- zavod/zavod/parse/__init__.py
+++ zavod/zavod/parse/__init__.py
@@ -1,3 +1,12 @@
+"""Data cleaning and entity generation helpers.
+
+This module contains a number of functions that are useful for parsing
+real-world data (like XML, CSV, date formats) and converting it into
+FollowTheMoney entity structures. Factory methods are provided for
+handling common entity patterns as a way to reduce boilerplate code
+and improve consistency across datasets.
+"""
+
 from zavod.parse.addresses import make_address, format_address
 from zavod.parse.xml import remove_namespace
 from zavod.parse.names import make_name, apply_name
