2b7aadb5bb39936969cc5496b68931fd53703545
JD Bothma <jbothma@gmail.com>
2023-07-24 09:05:48 +0200

    Rough entity creation dependent on position age

--- legacy/opensanctions/crawlers/us_congress.py
+++ legacy/opensanctions/crawlers/us_congress.py
@@ -1,24 +1,98 @@
+from functools import cache
+import json
+import os
+from typing import Optional
+from urllib.parse import urlencode, urljoin
+from datetime import timedelta
+
 from opensanctions import helpers as h
 from opensanctions.core import Context
+from zavod.entity import Entity
 from zavod.parse.positions import make_position
-import os
-from urllib.parse import urlencode
-import json
+from zavod import settings
+
 
 API_KEY = os.environ.get("OPENSANCTIONS_US_CONGRESS_KEY")
 LIMIT = 250
+CACHE_DAYS = 5
+AFTER_OFFICE = 5 * 365
 
 
-def make_member(context, member):
-  print(member.get("name", member.get("directOrderName", None)))
-  if terms := member.get("terms"):
-    for term in terms["item"]:
-      print("  ", term["chamber"], term.get("startYear", None), term.get("endYear", None))
+# TODO: Copied from wd_pep, factor out
+@cache
+def to_date(days: int) -> str:
+    dt = settings.RUN_TIME - timedelta(days=days)
+    return dt.isoformat()[:10]
 
-    # make person
-    # make positions
-    # make occupancies for terms that are within the periods we care about
-    # iff there are occupancies, emit all the things
+
+def make_occupancy(
+    context: Context,
+    person,
+    position,
+    start_date,
+    end_date: Optional[str],
+    no_end_implies_current: bool,
+) -> Optional[Entity]:
+    """Helper for making occupancies if they meet our criteria for PEP position occupancy"""
+    # TODO: Change dates to be dates to generalise
+    # TODO: The general version will need more of the logic from wd_peps than just this clause
+    if end_date is None or end_date > to_date(AFTER_OFFICE):
+        occupancy = context.make("Occupancy")
+        parts = [person.id, position.id, start_date, end_date]
+        occupancy.id = context.make_id(*parts)
+        occupancy.add("holder", person)
+        occupancy.add("post", position)
+        occupancy.add("startDate", start_date)
+        occupancy.add("endDate", end_date)
+        if no_end_implies_current and not end_date:
+            status = "Current"
+        else:
+            status = "Ended"
+        occupancy.add("status", status)
+        return occupancy
+
+
+def crawl_positions(context, member, entity):
+    terms: List[dict] = member.pop("terms")
+    entities = []
+    topics = set()
+    for term in terms:
+        res = context.lookup("position", term["chamber"])
+
+        position = make_position(context, res.name, country="us")
+        occupancy = make_occupancy(
+            context,
+            entity,
+            position,
+            str(term.pop("startYear")),
+            str(term.pop("endYear", None)),
+            True,
+        )
+        if occupancy:
+            entities.append(position)
+            entities.append(occupancy)
+            topics.update(res.topics)
+    return entities, topics
+
+
+def crawl_member(context: Context, bioguide_id: str):
+    url = urljoin(context.source.data.url, bioguide_id)
+    headers = {"x-api-key": API_KEY}
+    member = context.fetch_json(url, headers=headers, cache_days=CACHE_DAYS)["member"]
+
+    person = context.make("Person")
+    person.id = context.make_id(bioguide_id)
+    person.add(
+        "name", member.pop("directOrderName")
+    )  # TODO: use name parts from full member endpoint
+    person.add("country", "us")
+
+    entities, topics = crawl_positions(context, member, person)
+    if entities:
+        person.add("topics", topics)
+        context.emit(person, target=True)
+        for entity in entities:
+            context.emit(entity)
 
 
 def fetch(context: Context, offset):
@@ -32,27 +106,17 @@ def fetch(context: Context, offset):
 
 
 def crawl(context: Context):
-  offset = 0
-  while True:
-    members, offset = fetch(context, offset)
-    if not members:
-      break
+    offset = 0
+    while True:
+        members, offset = fetch(context, offset)
+        if not members:
+            break
 
-    for member in members:
-      make_member(context, member)
+        for member in members:
+            if member:  # There's one empty dict
+                crawl_member(context, member["bioguideId"])
 
-    # entity = context.make("Person")
-    # entity.id = "123"
-    # entity.add("name", "Fred Bloggs")
-    # entity.add("topics", "role.pep")
-    # entity.add("country", "us")
-    # context.emit(entity, target=True)
-# 
-    # position = make_position(context, "United states senator", country="us", summary="This is a summary from us_congress", description="This is a description from us_congress")
-    # context.emit(position)
-# 
-    # occupancy = context.make("Occupancy")
-    # occupancy.id = context.make_id("Fred Bloggs", "United statessenator")
-    # occupancy.add("holder", entity)
-    # occupancy.add("post", position)
-    # context.emit(occupancy)
+
+#
+
+#
