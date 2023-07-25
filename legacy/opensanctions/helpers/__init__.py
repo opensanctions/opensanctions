5b3560422b7f26df003b705b23657a775f67f9bc
Friedrich Lindenberg <friedrich@pudo.org>
2023-07-25 15:00:39 +0200

    Move address helpers fully to zavod

--- legacy/opensanctions/helpers/__init__.py
+++ legacy/opensanctions/helpers/__init__.py
@@ -3,8 +3,7 @@ from zavod.parse.names import make_name, apply_name
 from zavod.parse.text import clean_note, is_empty, remove_bracketed
 from zavod.parse.text import multi_split
 from zavod.parse.sanctions import make_sanction
-
-from opensanctions.helpers.addresses import make_address, apply_address
+from zavod.parse.addresses import make_address, apply_address, format_address
 from opensanctions.helpers.identification import make_identification
 from opensanctions.helpers.dates import extract_years, parse_date, check_no_year
 from opensanctions.helpers.dates import parse_formats
@@ -16,6 +15,7 @@ __all__ = [
     "multi_split",
     "remove_bracketed",
     "make_address",
+    "format_address",
     "apply_address",
     "make_sanction",
     "make_identification",
