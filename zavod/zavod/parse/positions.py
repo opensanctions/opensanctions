b20fe7a0eed02963d7a40cdb2a1574febf282549
Friedrich Lindenberg <friedrich@pudo.org>
2023-07-28 15:53:56 +0200

    Docstring and re-work ID generation

--- zavod/zavod/parse/positions.py
+++ zavod/zavod/parse/positions.py
@@ -1,9 +1,8 @@
-from typing import Optional, Iterable
+from banal import ensure_list
+from typing import Optional, Iterable, List
+
 from zavod.context import Context
 from zavod.entity import Entity
-
-from followthemoney.util import join_text, make_entity_id
-from normality import slugify
 
 
 def make_position(
@@ -13,7 +12,7 @@ def make_position(
     description: Optional[str] = None,
     country: Optional[str | Iterable[str]] = None,
     subnational_area: Optional[str] = None,
-    organization: Optional[str] = None,
+    organization: Optional[Entity] = None,
     inception_date: Optional[Iterable[str]] = None,
     dissolution_date: Optional[Iterable[str]] = None,
     number_of_seats: Optional[str] = None,
@@ -21,32 +20,53 @@ def make_position(
     source_url: Optional[str] = None,
     lang: Optional[str] = None,
 ) -> Entity:
-  """Create consistent position entities. Help make sure the same position
-  from different sources will end up with the same id, while different positions
-  don't end up overriding each other."""
+    """Create consistent position entities. Help make sure the same position
+    from different sources will end up with the same id, while different positions
+    don't end up overriding each other.
 
-  position = context.make("Position")
+    Args:
+        context: The context to create the entity in.
+        name: The name of the position.
+        summary: A short summary of the position.
+        description: A longer description of the position.
+        country: The country or countries the position is in.
+        subnational_area: The state or district the position is in.
+        organization: The organization the position is a part of.
+        inception_date: The date the position was created.
+        dissolution_date: The date the position was dissolved.
+        number_of_seats: The number of seats that can hold the position.
+        wiki_id: The Wikidata QID of the position.
+        source_url: The URL of the source the position was found in.
+        lang: The language of the position details.
 
-  position.add("name", name, lang=lang)
-  position.add("summary", summary, lang=lang)
-  position.add("description", description, lang=lang)
-  position.add("country", country, lang=lang)
-  position.add("organization", organization, lang=lang)
-  position.add("subnationalArea", subnational_area, lang=lang)
-  position.add("inceptionDate", inception_date)
-  position.add("dissolutionDate", dissolution_date)
-  position.add("numberOfSeats", number_of_seats)
-  position.add("wikidataId", wikidata_id)
-  position.add("sourceUrl", source_url)
+    Returns:
+        A new entity of type `Position`."""
 
-  parts = [
-    name,
-    country,
-    inception_date,
-    dissolution_date,
-  ]
-  hash_id = make_entity_id(*parts)
-  if hash_id is not None:
-      position.id = f"pos-{hash_id}"
+    position = context.make("Position")
 
-  return position
+    position.add("name", name, lang=lang)
+    position.add("summary", summary, lang=lang)
+    position.add("description", description, lang=lang)
+    position.add("country", country, lang=lang)
+    position.add("organization", organization, lang=lang)
+    position.add("subnationalArea", subnational_area, lang=lang)
+    position.add("inceptionDate", inception_date)
+    position.add("dissolutionDate", dissolution_date)
+    position.add("numberOfSeats", number_of_seats)
+    position.add("wikidataId", wikidata_id)
+    position.add("sourceUrl", source_url)
+
+    parts: List[str] = [name]
+    if country is not None:
+        parts.extend(ensure_list(country))
+    if inception_date is not None:
+        parts.extend(ensure_list(inception_date))
+    if dissolution_date is not None:
+        parts.extend(ensure_list(dissolution_date))
+
+    if wikidata_id is not None:
+        position.id = wikidata_id
+    else:
+        position.id = context.make_id(*parts)
+
+    return position
