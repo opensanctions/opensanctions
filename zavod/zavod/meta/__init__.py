a644738252af4460941f57f4b139b109d4d25644
Friedrich Lindenberg <friedrich@pudo.org>
2023-08-05 00:45:21 +0200

    we're good on this

--- zavod/zavod/meta/__init__.py
+++ zavod/zavod/meta/__init__.py
@@ -17,7 +17,4 @@ def get_catalog() -> ArchiveBackedCatalog:
 
 def load_dataset_from_path(path: Path) -> Optional[Dataset]:
     """Load a dataset from a given path."""
-    if "lv_business" not in str(path):
-        return get_catalog().load_yaml(path)
-    else:
-        return None
+    return get_catalog().load_yaml(path)
