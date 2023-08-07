df39354215c834fb00b85faa209fc3fc8858e102
JD Bothma <jbothma@gmail.com>
2023-08-07 15:37:58 +0200

    Move test to timestampindex

--- zavod/zavod/tests/runtime/test_timestamps.py
+++ zavod/zavod/tests/runtime/test_timestamps.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta
+from shutil import copyfile, rmtree
+from tempfile import mkdtemp
+from pathlib import Path
 
 from zavod import settings
 from zavod.meta import Dataset
@@ -26,3 +29,35 @@ def test_timestamps(vdataset: Dataset):
         assert index.get(stmt.id, default) == prev_time
 
     assert "TimeStampIndex" in repr(index), repr(index)
+
+
+def test_backfill(vdataset: Dataset):   
+    dataset_path = settings.DATA_PATH / "datasets" / vdataset.name
+    rmtree(dataset_path, ignore_errors=True)
+    
+    settings.ARCHIVE_BACKEND = "FilesystemBackend"
+    settings.ARCHIVE_PATH = Path(mkdtemp())
+    prev_time = settings.RUN_TIME_ISO
+    run_dataset(vdataset)
+
+    archive_path = settings.ARCHIVE_PATH / "datasets/latest/testdataset1"
+    archive_path.mkdir(parents=True, exist_ok=True)
+    copyfile(
+        settings.DATA_PATH / "datasets" / vdataset.name / "statements.pack",
+        archive_path / "statements.pack",
+    )
+
+    settings.RUN_TIME = settings.RUN_TIME + timedelta(days=1)
+    settings.RUN_TIME_ISO = settings.RUN_TIME.isoformat(sep="T", timespec="seconds")
+    settings.RUN_DATE = settings.RUN_TIME.date().isoformat()
+    second_time = settings.RUN_TIME_ISO
+    run_dataset(vdataset)
+    
+    # Restore settings
+    settings.ARCHIVE_BACKEND = "CloudStorageBackend"
+
+    stmts = list(iter_dataset_statements(vdataset))
+    index = TimeStampIndex.build(dataset=vdataset)
+    for stmt in stmts:
+        assert index.get(stmt.id, second_time) != ""
+        assert index.get(stmt.id, second_time) == prev_time
--- zavod/zavod/tests/test_store.py
+++ zavod/zavod/tests/test_store.py
@@ -1,7 +1,4 @@
 from datetime import timedelta
-from shutil import copyfile, rmtree
-from tempfile import mkdtemp
-from pathlib import Path
 
 from zavod import settings
 from zavod.meta import Dataset
@@ -33,38 +30,3 @@ def test_store_access(vdataset: Dataset):
     view2.store.close()
     clear_store(vdataset)
     assert not store.path.exists()
-
-
-def test_timestamps(vdataset: Dataset):
-    """Test integration of store and backfilling timestamps"""
-    dataset_path = settings.DATA_PATH / "datasets" / vdataset.name
-    rmtree(dataset_path, ignore_errors=True)
-    
-    settings.ARCHIVE_BACKEND = "FilesystemBackend"
-    settings.ARCHIVE_PATH = Path(mkdtemp())
-    first_time = settings.RUN_TIME_ISO
-    run_dataset(vdataset)
-
-    archive_path = settings.ARCHIVE_PATH / "datasets/latest/testdataset1"
-    archive_path.mkdir(parents=True, exist_ok=True)
-    copyfile(
-        settings.DATA_PATH / "datasets" / vdataset.name / "statements.pack",
-        archive_path / "statements.pack",
-    )
-
-    settings.RUN_TIME = settings.RUN_TIME + timedelta(days=1)
-    settings.RUN_TIME_ISO = settings.RUN_TIME.isoformat(sep="T", timespec="seconds")
-    settings.RUN_DATE = settings.RUN_TIME.date().isoformat()
-    second_time = settings.RUN_TIME_ISO
-    run_dataset(vdataset)
-
-    store = get_store(vdataset, external=True)
-    view = store.default_view(external=True)
-    entity = view.get_entity("osv-john-doe")
-
-    # Restore settings
-    settings.ARCHIVE_BACKEND = "CloudStorageBackend"
-
-    assert entity.last_change == first_time
-    assert entity.first_seen == first_time
-    assert entity.last_seen == second_time
