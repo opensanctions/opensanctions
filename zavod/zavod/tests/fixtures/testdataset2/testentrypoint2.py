83f80ee6d8cfcd8c98fc3335967d2b7e8a077d68
Friedrich Lindenberg <friedrich@pudo.org>
2023-08-09 11:27:09 +0200

    looking good

--- zavod/zavod/tests/fixtures/testdataset2/testentrypoint2.py
+++ zavod/zavod/tests/fixtures/testdataset2/testentrypoint2.py
@@ -5,7 +5,5 @@ def crawl(context: Context):
     entity = context.make("Person")
     entity.id = "freddie"
     entity.add("name", "Freddie Bloggs")
-
     context.emit(entity, target=True)
-
     context.log.warn("Message in a bottle")
