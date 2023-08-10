b44182d693a1b91d34ad3d9bec9200c0fb48319c
JD Bothma <jbothma@gmail.com>
2023-08-10 17:01:54 +0200

    Clean up code

--- zavod/zavod/exporters/peps.py
+++ zavod/zavod/exporters/peps.py
@@ -15,12 +15,44 @@ class PEPSummaryExporter(Exporter):
     def setup(self) -> None:
         super().setup()
 
-        PositionMap = DefaultDict[str, int]
-        Country = Dict[str, PositionMap]
-        CountryMap = DefaultDict[str, Country]
+        CountryCode = str
+        PositionLabel = str
+        PositionId = str
+        PositionCount = Dict[str, str | int]
+        PositionSummary = DefaultDict[PositionId, PositionCount]
+        Country = Dict[str, PositionSummary]
+        CountryMap = DefaultDict[CountryCode, Country]
         self.countries: CountryMap = defaultdict(
-            lambda: {"positions": defaultdict(int)}
+            lambda: {
+                "positions": defaultdict(lambda: {
+                    "position_name": None,
+                    "occupancy_count": 0
+                }),
+                "occupancy_count": 0
+            }
         )
+        CountryCount = Dict[str, str | int]
+        CountrySummary = DefaultDict[CountryCode, PositionCount]
+        Position = Dict[str, CountrySummary]
+        PositionMap = DefaultDict[str, Position]
+        self.positions: PositionMap = defaultdict(
+            lambda: {
+                "countries": defaultdict(lambda: {
+                    "occupancy_count": 0
+                }),
+                "occupancy_count": 0}
+        )
+
+    def observe_position(self, position):
+        country_codes = position.get("country")
+        for code in country_codes:
+            position_name = position.get("name")[0]
+            self.countries[code]["positions"][position.id]["position_name"] = position_name
+            self.countries[code]["positions"][position.id]["occupancy_count"] += 1
+            self.countries[code]["occupancy_count"] += 1
+
+            self.positions[position_name]["countries"][code]["occupancy_count"] += 1
+            self.positions[position_name]["occupancy_count"] += 1
 
     def feed(self, entity: Entity) -> None:
         if entity.schema.name == "Person":
@@ -28,13 +60,13 @@ class PEPSummaryExporter(Exporter):
                 if person_prop.name == "positionOccupancies":
                     for occ_prop, occ_related in self.view.get_adjacent(person_related):
                         if occ_prop.name == "post":
-                            country_codes = occ_related.get("country")
-                            for code in country_codes:
-                                position_name = occ_related.get("name")[0]
-                                self.countries[code]["positions"][position_name] += 1
+                            self.observe_position(occ_related)
 
     def finish(self) -> None:
-        output = {"countries": self.countries}
+        output = {
+            "countries": self.countries,
+            "positions": self.positions,
+        }
         with open(self.path, "wb") as fh:
             write_json(output, fh)
         super().finish()
