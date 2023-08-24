7ce4584d1e31acc149fcdb9703b20efee89eb00f
Edgar Zanella Alvarenga <e@vaz.io>
2023-08-24 12:59:10 +0200

    Use more descriptive name for Position entities and update total numebr of batches in each API request

--- datasets/de/abgeordnetenwatch/crawler.py
+++ datasets/de/abgeordnetenwatch/crawler.py
@@ -9,12 +9,11 @@ from zavod import helpers as h
 def crawl(context: Context):
     # Fetch the source data URL specified in the metadata to a local path:
     api_response = context.fetch_json(context.dataset.data.url)
-    # TODO: Investigate why first query api_response["meta"]["result"]["total"] is 57726
-    # total_results = api_response["meta"]["result"]["total"]
-    total_results = 2733
+    total_results = api_response["meta"]["result"]["total"]
     num_batches = math.ceil(total_results / 100)
 
-    for bi in range(num_batches):
+    bi = 0
+    while bi < num_batches:
         context.log.info(
             "Get Politicians from range {} to {}".format(bi * 100, (bi + 1) * 100)
         )
@@ -22,16 +21,25 @@ def crawl(context: Context):
             api_response = context.fetch_json(
                 context.dataset.data.url, params={"range_start": bi * 100}
             )
+            batch_total_results = api_response["meta"]["result"]["total"]
+            if batch_total_results != total_results:
+                num_batches = math.ceil(batch_total_results / 100)
 
         for candidate in api_response.get("data"):
             parliament_info = candidate["parliament_period"]["label"].split(" ")
+            state = parliament_info[0]
+            inception_date = parliament_info[1]
+            dissolution_date = parliament_info[3]
+
             position = h.make_position(
                 context,
-                name="parliament_period",
+                name="Member of Parliament ({} - {}) - Germany/{}".format(
+                    inception_date, dissolution_date, state
+                ),
                 country="Germany",
-                subnational_area=parliament_info[0],
-                inception_date=parliament_info[1],
-                dissolution_date=parliament_info[3],
+                subnational_area=state,
+                inception_date=inception_date,
+                dissolution_date=dissolution_date,
             )
 
             person = context.make("Person")
@@ -40,3 +48,6 @@ def crawl(context: Context):
             h.make_occupancy(context, person, position, False)
             context.emit(person)
             context.emit(position)
+
+        # Increment batch number
+        bi += 1
