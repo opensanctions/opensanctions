38cea46306451bc2959251639eb3b3427df87f62
Edgar Zanella Alvarenga <e@vaz.io>
2023-09-03 21:30:21 +0200

    Format with black

--- datasets/ky/judicial/crawler.py
+++ datasets/ky/judicial/crawler.py
@@ -12,45 +12,57 @@ def crawl(context: Context):
     request_response = context.fetch_html(context.dataset.data.url, cache_days=30)
 
     # Get current judges
-    current_judges_block = request_response.xpath("//div[contains(@class, 'accordian')]")[0]
-    current_judges = current_judges_block.xpath(".//div[contains(@class, 'fusion-panel')]")
+    current_judges_block = request_response.xpath(
+        "//div[contains(@class, 'accordian')]"
+    )[0]
+    current_judges = current_judges_block.xpath(
+        ".//div[contains(@class, 'fusion-panel')]"
+    )
 
     # First get the list of different types of courts
     courts = []
     for sm in request_response.xpath('.//ul[contains(@class,"sub-menu")]')[:2]:
-        for court in [s.text_content() for s in sm.xpath('./li/a/span')]:
+        for court in [s.text_content() for s in sm.xpath("./li/a/span")]:
             courts.append(court)
-            
+
     def filter_courts(t):
-        '''
+        """
         Giving a text block, filter the list of courts mentioned in the text
         or remove the excluded courts
-        '''
-        if 'all Divisions' in t:
-            if 'except the' not in t:
+        """
+        if "all Divisions" in t:
+            if "except the" not in t:
                 return courts
             else:
-                not_in = t.split('except the', maxsplit=1)[1].strip()
+                not_in = t.split("except the", maxsplit=1)[1].strip()
                 return [c for c in courts if c not in not_in]
-        elif 'to the' in t:
-            division = t.split('to the', maxsplit=1)[1].strip().replace(')','')
+        elif "to the" in t:
+            division = t.split("to the", maxsplit=1)[1].strip().replace(")", "")
             return [division]
-    
+
     judges = []
     for judge in current_judges:
         new_judge = {}
-        new_judge['name'] = judge.xpath('.//span[contains(@class, "fusion-toggle-heading")]')[0].text.replace('Hon. ', '').split(',')[0].replace(' CBE', '')
-        for line in judge.xpath('.//div[contains(@class, "panel-body")]')[0].text_content().splitlines():
-            if 'Assigned' in line:
-                new_judge['positions'] = filter_courts(line)
+        new_judge["name"] = (
+            judge.xpath('.//span[contains(@class, "fusion-toggle-heading")]')[0]
+            .text.replace("Hon. ", "")
+            .split(",")[0]
+            .replace(" CBE", "")
+        )
+        for line in (
+            judge.xpath('.//div[contains(@class, "panel-body")]')[0]
+            .text_content()
+            .splitlines()
+        ):
+            if "Assigned" in line:
+                new_judge["positions"] = filter_courts(line)
                 break
             else:
-                new_judge['positions'] = [] 
+                new_judge["positions"] = []
                 for c in courts:
                     if c in line:
-                        new_judge['positions'] += [c] 
-                        
-    
+                        new_judge["positions"] += [c]
+
         judges += [new_judge]
 
         judge_proxy = context.make("Person")
@@ -81,4 +93,3 @@ def crawl(context: Context):
         for pos in judge_positions:
             context.emit(pos[0])
             context.emit(pos[1])
-
