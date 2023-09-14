59b4f6ce89505d3b484e88f844b256f996c3cf4b
Friedrich Lindenberg <friedrich@pudo.org>
2023-09-14 14:57:06 +0200

    more metadata export tests

--- zavod/zavod/tests/exporters/test_metadata.py
+++ zavod/zavod/tests/exporters/test_metadata.py
@@ -2,10 +2,26 @@ import json
 from zavod import settings
 from zavod.meta import Dataset
 from zavod.store import get_view
+from zavod.crawl import crawl_dataset
 from zavod.exporters import export_dataset
 
 
-def test_metadata_collection_export(collection: Dataset) -> None:
+def test_metadata_collection_export(testdataset1: Dataset, collection: Dataset) -> None:
+    ds_path = settings.DATA_PATH / "datasets" / testdataset1.name
+    crawl_dataset(testdataset1)
+    view = get_view(testdataset1)
+    export_dataset(testdataset1, view)
+    assert ds_path.is_dir()
+    catalog_path = ds_path / "catalog.json"
+    assert not catalog_path.is_file()
+    index_path = ds_path / "index.json"
+    assert index_path.is_file()
+
+    with open(index_path, "r") as fh:
+        index = json.load(fh)
+        assert index["updated_at"] == settings.RUN_TIME_ISO
+        assert len(index["resources"]) > 2
+
     collection_path = settings.DATA_PATH / "datasets" / collection.name
     view = get_view(collection)
     export_dataset(collection, view)
@@ -18,3 +34,6 @@ def test_metadata_collection_export(collection: Dataset) -> None:
 
     assert catalog["updated_at"] == settings.RUN_TIME_ISO
     assert len(catalog["datasets"]) == len(collection.datasets)
+    for ds in catalog["datasets"]:
+        assert ds["updated_at"] == settings.RUN_TIME_ISO
+        assert len(ds["resources"]) > 2
