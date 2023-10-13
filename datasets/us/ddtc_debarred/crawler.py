4e597756418a38297c9a7548e8dca725b206b5b3
JD Bothma <jbothma@gmail.com>
2023-10-13 14:19:53 +0200

    Basic working crawler

--- datasets/us/ddtc_debarred/crawler.py
+++ datasets/us/ddtc_debarred/crawler.py
@@ -1,11 +1,17 @@
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 from normality import slugify
 import openpyxl
 from zavod import Context
 from pantomime.types import XLSX
+from zavod import helpers as h
 
 STATUTORY_XLSX_URL = "https://www.pmddtc.state.gov/sys_attachment.do?sys_id=27c46b251baf29102b6ca932f54bcb20"
 ADMINISTRATIVE_XLSX_URL = "https://www.pmddtc.state.gov/sys_attachment.do?sys_id=9f8bbc2f1b8f29d0c6c3866ae54bcbdb"
+STATUTORY_PROGRAM = "Statutory debarment pursuant to §38(g)(4) of the AECA and §127.7 of the International Traffic in Arms Regulations (ITAR)"
+ADMINISTRATIVE_PROGRAM = "Administrative debarment for violations of the AECA/ITAR, as specified in 22 CFR §127.7(a)"
+
+FORMATS = ["%B, %Y"]
+
 
 
 def sheet_to_dicts(sheet):
@@ -19,8 +25,42 @@ def sheet_to_dicts(sheet):
         yield row
 
 
-def crawl_debarment(context, row):
-    print(row)
+def crawl_debarment(
+    context: Context, row: Dict[str, Any], program: str, name_field: str, notice_date_field: str, birth_date_field=None
+):
+    date_of_birth = row.pop(birth_date_field, None)
+    if date_of_birth:
+        schema = "Person"
+    else:
+        schema = "LegalEntity"
+
+    entity = context.make(schema)
+    name = row.pop(name_field)
+    entity.id = context.make_slug(name, date_of_birth, strict=False)
+    entity.add("name", name)
+    if schema == "Person":
+        entity.add("birthDate", h.parse_date(date_of_birth, FORMATS))
+    entity.add("topics", "debarment")
+
+    sanction = h.make_sanction(context, entity)
+    sanction.add("program", program)
+    sanction.add("listingDate", row.pop(notice_date_field).isoformat()[:10])
+    sanction.add("listingDate", row.pop("corrected-notice-date", None))
+    sanction.add(
+        "description",
+        "Federal register notice: " + row.pop("federal-register-notice"),
+    )
+    corrected_notice_number = row.pop("corrected-notice", None)
+    if corrected_notice_number:
+        sanction.add(
+            "description",
+            "Corrected federal register notice: " + corrected_notice_number,       
+        )
+
+    context.emit(entity, target=True)
+    context.emit(sanction)
+
+    context.audit_data(row)
 
 
 def crawl(context: Context):
@@ -28,10 +68,17 @@ def crawl(context: Context):
     context.export_resource(path, XLSX, title="Statutory Debarments")
     rows = sheet_to_dicts(openpyxl.load_workbook(path, read_only=True).worksheets[0])
     for row in rows:
-        crawl_debarment(context, row)
+        crawl_debarment(
+            context,
+            row,
+            STATUTORY_PROGRAM,
+            "party-name",
+            "notice-date",
+            "date-of-birth",
+        )
 
     path = context.fetch_resource("administrative.xlsx", ADMINISTRATIVE_XLSX_URL)
     context.export_resource(path, XLSX, title="Administrative Debarments")
     rows = sheet_to_dicts(openpyxl.load_workbook(path, read_only=True).worksheets[0])
     for row in rows:
-        crawl_debarment(context, row)
+        crawl_debarment(context, row, ADMINISTRATIVE_PROGRAM, "name", "date")
