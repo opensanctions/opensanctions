0df46a3fe9d7dc049286620fdabc02dbee878fd3
JD Bothma <jbothma@gmail.com>
2023-10-13 14:52:03 +0200

    Split names and aliases

--- datasets/us/ddtc_debarred/crawler.py
+++ datasets/us/ddtc_debarred/crawler.py
@@ -10,8 +10,7 @@ ADMINISTRATIVE_XLSX_URL = "https://www.pmddtc.state.gov/sys_attachment.do?sys_id
 STATUTORY_PROGRAM = "Statutory debarment pursuant to §38(g)(4) of the AECA and §127.7 of the International Traffic in Arms Regulations (ITAR)"
 ADMINISTRATIVE_PROGRAM = "Administrative debarment for violations of the AECA/ITAR, as specified in 22 CFR §127.7(a)"
 
-FORMATS = ["%B, %Y"]
-
+FORMATS = ["%B, %Y", "%B %Y"]
 
 
 def sheet_to_dicts(sheet):
@@ -25,8 +24,24 @@ def sheet_to_dicts(sheet):
         yield row
 
 
+def split_names(name: str) -> (str, List[str]):
+    parts = name.split("(a.k.a. ", 1)
+    main_name = parts[0]
+    alias_list = []
+    if len(parts) > 1:
+        aliases = parts[1]
+        aliases = aliases.replace(")", "")
+        alias_list = aliases.split("; ")
+    return main_name, alias_list
+
+
 def crawl_debarment(
-    context: Context, row: Dict[str, Any], program: str, name_field: str, notice_date_field: str, birth_date_field=None
+    context: Context,
+    row: Dict[str, Any],
+    program: str,
+    name_field: str,
+    notice_date_field: str,
+    birth_date_field=None,
 ):
     date_of_birth = row.pop(birth_date_field, None)
     if date_of_birth:
@@ -35,9 +50,10 @@ def crawl_debarment(
         schema = "LegalEntity"
 
     entity = context.make(schema)
-    name = row.pop(name_field)
+    name, aliases = split_names(row.pop(name_field))
     entity.id = context.make_slug(name, date_of_birth, strict=False)
     entity.add("name", name)
+    entity.add("alias", aliases)
     if schema == "Person":
         entity.add("birthDate", h.parse_date(date_of_birth, FORMATS))
     entity.add("topics", "debarment")
@@ -54,13 +70,13 @@ def crawl_debarment(
     if corrected_notice_number:
         sanction.add(
             "description",
-            "Corrected federal register notice: " + corrected_notice_number,       
+            "Corrected federal register notice: " + corrected_notice_number,
         )
 
     context.emit(entity, target=True)
     context.emit(sanction)
 
-    context.audit_data(row)
+    context.audit_data(row, ["charging-letter", "debarment-order"])
 
 
 def crawl(context: Context):
