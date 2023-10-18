26b1aec9dd4a3e7cae41d0266e6456f956c1f8c3
JD Bothma <jbothma@gmail.com>
2023-10-18 15:07:33 +0200

    Emitting entities and complete metadata

--- datasets/us/dosforaa_7031/crawler.py
+++ datasets/us/dosforaa_7031/crawler.py
@@ -1,38 +1,65 @@
 from typing import Optional
 from normality import collapse_spaces
+from followthemoney.types import registry
+import re
+
 from zavod import Context
 from zavod import helpers as h
 from zavod.util import ElementOrTree
-import re
 
 
 REGEX_ITEM = re.compile("(\d+\. )?(\(U\) )?(?P<name>[\w \.'’“”-]+),? ?\((?P<country>[\w /]+)\) (– )?(?P<reason>.+)$")
 
 
 def crawl_section(context: Context, url: str, section: ElementOrTree):
-    print("\n--------------")
+    listing_titles = section.xpath(".//h3[contains(@class, 'report__section-title')]")
+    if len(listing_titles) == 1:
+        year = re.match("(\d{4})$", listing_titles[0]).group(1)
+    else:
+        year = None
+
     program = section.find(".//h3[@class='report__section-subtitle']").text_content()
     program = program.replace("(Generally Listed in Chronological Order)", "")
     program = collapse_spaces(program.replace("Since Previous Report", ""))
-    print(program)
-    print()
 
     items = section.findall(".//li")
     if len(items) == 0:
         items = section.findall(".//p")
     if len(items) == 0:
         context.log.warning(f"Empty list for program {program}")
+        
     for item in items:
         item_text = collapse_spaces(item.text_content())
         if item_text == "":
             continue
+        if item_text.startswith("* Denotes an action"):
+            continue
+
         match = REGEX_ITEM.match(item_text)
         if match:
             name = collapse_spaces(match.group("name"))
-            country = collapse_spaces(match.group("country"))
+            countries = [collapse_spaces(c) for c in match.group("country").split("/")]
             reason = collapse_spaces(match.group("reason"))
-            
-            print(f"{name} | {country} | {reason}")
+
+            if not all([registry.country.clean(c) for c in countries]):
+                res = context.lookup("unparsed", item_text)
+                name = res.name
+                countries = res.country
+                reason = res.reason
+    
+            entity = context.make("Person")
+            entity.id = context.make_slug(countries, name)
+            entity.add("name", name)
+            entity.add("country", countries)
+
+            sanction = h.make_sanction(context, entity)
+            sanction.add("program", program)
+            sanction.add("reason", reason)
+            sanction.add("sourceUrl", url)
+            sanction.add("listingDate", year)
+
+            context.emit(entity, target=True)
+            context.emit(sanction)
         else:
             context.log.warning(f"Cannot parse item", item=item_text)
 
@@ -41,6 +68,7 @@ def crawl_section(context: Context, url: str, section: ElementOrTree):
 def crawl_report(context: Context, url: str):
     context.log.info(f"Crawling {url}")
     doc = context.fetch_html(url, cache_days=1)
+    doc.find
     for section in doc.findall(".//section[@class='entry-content']"):
         crawl_section(context, url, section)
 
