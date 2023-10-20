77cae273156855d4c1242f9c380bfaf39240d3d0
JD Bothma <jbothma@gmail.com>
2023-10-20 13:30:52 +0200

    Cast table cell to HtmlElement so we're allowed to text_contet()

--- zavod/zavod/helpers/html.py
+++ zavod/zavod/helpers/html.py
@@ -1,13 +1,20 @@
-from typing import Dict, Generator
+from typing import Dict, Generator, cast
 from normality import slugify, collapse_spaces
+from lxml.html import HtmlElement
+
 from zavod.util import ElementOrTree
 
 
-def parse_table(table: ElementOrTree) -> Generator[Dict[str, str], None, None]:
+def parse_table(table: HtmlElement) -> Generator[Dict[str, str], None, None]:
     headers = None
     for row in table.findall(".//tr"):
         if headers is None:
-            headers = [slugify(el.text_content()) for el in row.findall("./th")]
+            headers = []
+            for el in row.findall("./th"):
+                # Workaround because lxml-stubs doesn't yet support HtmlElement
+                # https://github.com/lxml/lxml-stubs/pull/71
+                eltree = cast(HtmlElement, el)
+                headers.append(slugify(eltree.text_content()))
             continue
 
         cells = [collapse_spaces(el.text_content()) for el in row.findall("./td")]
