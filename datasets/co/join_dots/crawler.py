9894f2076e42e0c6013472d915c1c69fee675963
JD Bothma <jbothma@gmail.com>
2023-10-23 19:13:46 +0200

    Add some notes when they're interesting

--- datasets/co/join_dots/crawler.py
+++ datasets/co/join_dots/crawler.py
@@ -34,7 +34,7 @@ GRAPH_URL = "https://peps.directoriolegislativo.org/json/graph.json"
 def crawl_row(context: Context, row: Dict[str, str]):
     entity = context.make("Person")
     id_number = row.pop("ID / CC")
-    entity.id = context.make_slug(id_number)
+    entity.id = context.make_slug("person", id_number)
     h.apply_name(
         entity,
         first_name=row.pop("Primer Nombre"),
@@ -46,7 +46,7 @@ def crawl_row(context: Context, row: Dict[str, str]):
     return slugify(entity.get("name")), entity
 
 
-def parse_node(node: str) -> Dict[str, str]:
+def parse_node(context: Context, node: str) -> Dict[str, str]:
     description = node["Description"]
     if description.strip() == "":
         return None
@@ -57,6 +57,13 @@ def parse_node(node: str) -> Dict[str, str]:
         if row:
             key, value = row.split(":", 1)
             data[slugify(key, "_")] = collapse_spaces(value)
+
+    links = doc.findall(".//a")
+    if len(links) == 1:
+        data["link"] = links[0].get("href")
+    else:
+        context.log.warning("More than one link for node", id=node["ID"])
+
     return data
 
 
@@ -81,22 +88,26 @@ def crawl(context: Context):
     graph = context.fetch_json(GRAPH_URL, cache_days=1)
     for node in graph["nodes"]:
         if node["Type"] in ["PEP", "FLAG"]:
-            data = parse_node(node)
+            data = parse_node(context, node)
             if data:
-                # print(data)
                 name = data["politically_exposed_person"]
                 name_slug = slugify(name)
-                print(name_slug)
                 entity = pep.get(name_slug, None)
                 if entity:
-                    position_slug = slugify(data.get("position"))
+                    position_slug = slugify(data.pop("position"))
                     res = context.lookup("position", position_slug)
                     if res:
-                        print("######### emitting", name)
-                        print(slugify(data.get("position")))
-                        print("red flags:", data.get("red_flags_found"))
-                        print("ownerships:", data.get("corporation_shares"))
-                        print()
+                        notes = ""
+                        red_flags = data.pop("red_flags_found", None)
+                        if red_flags is not None:
+                            notes += f"Potential red flags: {red_flags}."
+                        ownership = data.get("corporation_shares", None)
+                        if ownership and ownership != "null":
+                            notes += f" Corporation shares: {ownership}"
+                        if notes:
+                            entity.add("notes", notes)
+                            entity.add("sourceUrl", data.pop("link"))
+
                         position = h.make_position(
                             context, res.name, country="co", topics=res.topics
                         )
@@ -106,8 +117,5 @@ def crawl(context: Context):
                         context.emit(occupancy)
                         context.emit(position)
                         context.emit(entity, target=True)
-                    else:
-                        print(position_slug)
-                        print()
                 else:
                     context.log.info("PEP not found", name=name)
