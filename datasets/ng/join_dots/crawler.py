9942327449f6ae08e4d998524f37427a7693ebfa
JD Bothma <jbothma@gmail.com>
2023-10-23 11:45:17 +0200

    Add relatives of nigerian PEPs

--- datasets/ng/join_dots/crawler.py
+++ datasets/ng/join_dots/crawler.py
@@ -79,6 +79,80 @@ def parse_position_dates(string: Optional[str]) -> Tuple[Optional[str], Optional
     return start, end
 
 
+def crawl_pep(context: Context, row) -> Tuple[Optional[str], Optional[str]]:
+    name = row.pop("name")
+    birth_date = row.pop("date_of_birth", None)
+    birth_date = birth_date.isoformat()[:10] if birth_date else None
+    district = row.pop("district", None)
+
+    entity = context.make("Person")
+    entity.id = context.make_slug(
+        name, birth_date, "district", district, strict=False
+    )
+
+    entity.add("name", name)
+    entity.add("birthDate", birth_date)
+    entity.add("gender", row.pop("gender", None))
+
+    position_name = clean_position(context, row.pop("position"), district, name)
+    if position_name:
+        topics = position_topics(position_name)
+        subnational_area = district if "gov.state" in topics else None
+        position = h.make_position(
+            context,
+            position_name,
+            country="NG",
+            topics=topics,
+            subnational_area=subnational_area
+        )
+
+        start_date, end_date = parse_position_dates(row.pop("period", None))
+        occupancy = h.make_occupancy(
+            context,
+            entity,
+            position,
+            no_end_implies_current=False,
+            start_date=start_date,
+            end_date=end_date,
+            birth_date=birth_date,
+        )
+
+        if occupancy:
+            context.emit(entity, target=True)
+            context.emit(occupancy)
+            context.emit(position)
+
+            context.audit_data(row, ignore=["age"])
+
+            return name, entity.id
+    return None, None
+
+
+def crawl_relative(context: Context, row, pep_ids):
+    name = row.pop("name")
+    pep_name = row.pop("pep_name")
+    pep_id = pep_ids.get(slugify(pep_name), None)
+    if pep_id is None:
+        # In spot-checked cases, their positions were too old to be relevant
+        context.log.info("PEP not found for relative", pep=pep_name, relative=name)
+        return
+    entity = context.make("Person")
+    entity.id = context.make_slug(name, "of", pep_id)
+    entity.add("name", name)
+    entity.add("topics", "role.rca")
+
+    relationship = row.pop("relationship", None)
+    rel = context.make("Family")
+    rel.id = context.make_slug(name, relationship or "relative", "of", pep_name, strict=False)
+    rel.add("person", pep_id)
+    rel.add("relative", entity)
+    rel.add("relationship", relationship)
+
+    context.emit(entity)
+    context.emit(rel)
+
+
+
 def worksheet_rows(sheet) -> List[Dict[str, Any]]:
     headers: Optional[List[str]] = None
     for row in sheet.rows:
@@ -90,50 +164,31 @@ def worksheet_rows(sheet) -> List[Dict[str, Any]]:
 
 
 def crawl(context: Context):
+    pep_ids = {}
+    dupes = set()
+
     peps_path = context.fetch_resource(
         "peps.xlsx", context.data_url + "datasets/nigeria/PEP_data.xlsx"
     )
     context.export_resource(peps_path, XLSX, title="PEPs source data")
-
     workbook = openpyxl.load_workbook(peps_path, read_only=True)
     for row in worksheet_rows(workbook.worksheets[0]):
-        name = row.pop("name")
-        birth_date = row.pop("date_of_birth", None)
-        birth_date = birth_date.isoformat()[:10] if birth_date else None
-        district = row.pop("district", None)
+        name, id = crawl_pep(context, row)
+        if name is None:
+            continue
+        name_slug = slugify(name)
+        if name_slug in pep_ids:
+            dupes.add(name_slug)
+            pep_ids.pop((name_slug))
+        else:
+            if name_slug not in dupes:
+                pep_ids[name_slug] = id
 
-        entity = context.make("Person")
-        entity.id = context.make_slug(
-            name, birth_date, "district", district, strict=False
-        )
-
-        entity.add("name", name)
-        entity.add("birthDate", birth_date)
-        entity.add("gender", row.pop("gender", None))
-
-        position_name = clean_position(context, row.pop("position"), district, name)
-
-        if position_name:
-            position = h.make_position(
-                context,
-                position_name,
-                country="NG",
-                topics=position_topics(position_name),
-            )
-            start_date, end_date = parse_position_dates(row.pop("period", None))
-            occupancy = h.make_occupancy(
-                context,
-                entity,
-                position,
-                no_end_implies_current=False,
-                start_date=start_date,
-                end_date=end_date,
-                birth_date=birth_date,
-            )
-
-            if occupancy:
-                context.emit(entity, target=True)
-                context.emit(occupancy)
-                context.emit(position)
-
-        context.audit_data(row, ignore=["age"])
+    peps_path = context.fetch_resource(
+        "pep_relatives.xlsx", context.data_url + "datasets/nigeria/PEP_relatives_data.xlsx"
+    )
+    context.export_resource(peps_path, XLSX, title="PEP Relatives source data")
+    workbook = openpyxl.load_workbook(peps_path, read_only=True)
+    for row in worksheet_rows(workbook.worksheets[0]):
+        if row:
+            crawl_relative(context, row, pep_ids)
