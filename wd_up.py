665d04a79f61ef9c4b4ac3b70c4b4f5dde213b60
JD Bothma <jbothma@gmail.com>
2023-10-25 16:24:55 +0200

    Use wikidata API to compare with what wikidata has right now

--- wd_up.py
+++ wd_up.py
@@ -7,6 +7,14 @@ from followthemoney.proxy import EntityProxy
 from zavod.meta import load_dataset_from_path
 from zavod.store import get_view
 from pathlib import Path
+import requests
+from nomenklatura.enrich.wikidata.props import PROPS_DIRECT
+from nomenklatura.enrich.wikidata import WD_API
+from nomenklatura.enrich.wikidata.model import Claim
+
+# quickstatements
+# if we add the same property and value multiple times, each time with a different
+# reference but no other differences, will they converge in one?
 
 
 def get_position_held(view, entity: EntityProxy) -> None:
@@ -73,16 +81,41 @@ def iterate_entities(source_dataset: str):
         if not entity.id.startswith("Q"):
             continue
 
+        qid = entity.id
         print("\n--------------------")
-        print(entity.id, entity.get("name"))
-        
-        for prop in entity.iterprops():
-            print(" ", prop.name)
-            for statement in entity.get_statements(prop):
-                print("   ", statement.value, statement.dataset)
+        print(qid, entity.get("name"))
+
+        params = { "format": "json", "ids": qid, "action": "wbgetentities"}
+        r = requests.get(WD_API, params)
+        r.raise_for_status()
+        item = r.json().get("entities", {}).get(qid)
+        claims = defaultdict(list)
+        for wd_prop, claim_dicts in item.get("claims", {}).items():
+            claims[wd_prop] = [Claim(c, wd_prop) for c in claim_dicts]
+
+        # TODO: check if it has a label, other non-claim properties?
+
+        seen_props = set()
+
+        for wd_prop, prop in PROPS_DIRECT.items():
+            if prop in {"country", "title"}:
+                continue
+            if prop in seen_props:
+                continue
+            vals = entity.get_statements(prop)
+            if not vals:
+                continue
+            wd_vals = claims.get(wd_prop, [])
+            if wd_vals:
+                seen_props.add(prop)
+                continue
+            seen_props.add(prop)
+            print(wd_prop, prop, [(v.value, v.lang, v.dataset) for v in vals], [c._value for c in wd_vals])
 
         
 
 
 if __name__ == "__main__":
+    print(argv[1])
     iterate_entities(argv[1])
+    
