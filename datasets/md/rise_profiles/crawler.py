33e1af5b15dae2af4e2918882042fd736f5b0dec
JD Bothma <jbothma@gmail.com>
2023-11-09 12:14:41 +0200

    Make ID before adding attributes

--- datasets/md/rise_profiles/crawler.py
+++ datasets/md/rise_profiles/crawler.py
@@ -77,45 +77,43 @@ def make_person(
 ):
     person = context.make("Person")
     identification = [COUNTRY, name]
+    birth_date = parse_date(attributes.pop("data-nasterii", None))
+    if birth_date:
+        identification.append(birth_date)
+    person.id = context.make_id(*identification)
+
     person.add("sourceUrl", url)
     person.add("name", name)
     person.add("position", position, lang="ron")
-
-    if "data-nasterii" in attributes:
-        dob = parse_date(attributes.pop("data-nasterii"))
-        identification.append(dob)
-        person.add("birthDate", dob)
-
+    person.add("birthDate", birth_date)
     person.add("birthPlace", attributes.pop("locul-nasterii", None), lang="ron")
     person.add("nationality", attributes.pop("cetatenie", "").split(","))
+    person.add("topics", "poi")
 
     if attributes:
         context.log.info(f"More info to be added to {name}", attributes, url)
-    person.id = context.make_id(*identification)
-    person.add("topics", "poi")
     return person
 
 
 def make_company(context: Context, url: str, name: str, attributes: dict):
     company = context.make("Company")
     identification = [COUNTRY, name]
-    company.add("sourceUrl", url)
-    company.add("name", name)
-    if "data-inregistrarii" in attributes:
-        founded = parse_date(attributes.pop("data-inregistrarii"))
+    founded = parse_date(attributes.pop("data-inregistrarii", None))
+    if founded:
         identification.append(founded)
-        company.add("incorporationDate", founded)
-
-    country = attributes.pop("tara", "").split(",")[0]
-    company.add("mainCountry", country)
-
-    if "numar-de-identificare" in attributes:
-        regno = attributes.pop("numar-de-identificare")
+    regno = attributes.pop("numar-de-identificare", None)
+    if regno:        
         identification.append(regno)
-        company.add("registrationNumber", regno)
+    company.id = context.make_id(*identification)
+
+    company.add("name", name)
+    company.add("registrationNumber", regno)
+    company.add("sourceUrl", url)
+    company.add("mainCountry", attributes.pop("tara", "").split(",")[0])
+    company.add("incorporationDate", founded)
+
     if attributes:
         context.log.info(f"More info to be added to {name}", attributes, url)
-    company.id = context.make_id(*identification)
     return company
 
 
