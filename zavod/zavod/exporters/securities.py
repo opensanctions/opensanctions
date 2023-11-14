1167e20416309dd1bf9143e2f4c7680ef1b948a1
Friedrich Lindenberg <friedrich@pudo.org>
2023-11-14 10:39:30 +0100

    re-work securities exporter

--- zavod/zavod/exporters/securities.py
+++ zavod/zavod/exporters/securities.py
@@ -1,10 +1,11 @@
 import csv
-from typing import Set
+from typing import Set, Iterable
 from normality import collapse_spaces
 from followthemoney.types import registry
 from nomenklatura.util import bool_text
 
 from zavod.entity import Entity
+from zavod.logs import get_logger
 from zavod.exporters.common import Exporter
 
 COLUMNS = [
@@ -22,6 +23,20 @@ COLUMNS = [
 SANCTIONED = "sanction"
 EO_14071 = "ru_nsd_isin"
 
+log = get_logger(__name__)
+
+
+def join_cell(texts: Iterable[str], sep: str = ";") -> str:
+    values: Set[str] = set()
+    for value in texts:
+        if value is None:
+            continue
+        value = value.strip().replace(sep, ",")
+        if len(value) == 0:
+            continue
+        values.add(value)
+    return sep.join(sorted(values))
+
 
 class SecuritiesExporter(Exporter):
     TITLE = "Security-centric tabular format"
@@ -33,23 +48,25 @@ class SecuritiesExporter(Exporter):
         self.fh = open(self.path, "w", encoding="utf-8")
         self.csv = csv.writer(self.fh, dialect=csv.unix_dialect, delimiter=",")
         self.csv.writerow(COLUMNS)
+        self._count_entities = 0
+        self._count_isins = 0
+        self._count_leis = 0
 
-    def _get_isins(self, entity: Entity) -> str:
+    def _get_isins(self, entity: Entity) -> Set[str]:
         # TODO: normalize ISINs
         isins = set(entity.get("isinCode", quiet=True))
         for _, adjacent in self.view.get_adjacent(entity):
             if adjacent.schema.is_a("Security"):
                 isins.update(adjacent.get("isin"))
-        return "; ".join(isins)
+        return isins
 
-    def _get_aliases(self, entity: Entity) -> str:
+    def _get_aliases(self, entity: Entity) -> Set[str]:
         names: Set[str] = set()
         for name in entity.get_type_values(registry.name, matchable=True):
-            name = name.replace(";", ",")
             name_ = collapse_spaces(name)
             if name_ is not None:
                 names.add(name_)
-        return "; ".join(names)
+        return names
 
     def feed(self, entity: Entity) -> None:
         if not entity.schema.is_a("Organization"):
@@ -58,20 +75,31 @@ class SecuritiesExporter(Exporter):
         is_eo_14071 = EO_14071 in entity.datasets
         if not is_sanctioned and not is_eo_14071:
             return
+        self._count_entities += 1
+        leis = entity.get("leiCode", quiet=True)
+        self._count_leis += len(leis)
+        isins = self._get_isins(entity)
+        self._count_isins += len(isins)
         row = [
             entity.caption,
-            "; ".join(entity.get("leiCode", quiet=True)),
-            self._get_isins(entity),
-            "; ".join(entity.get_type_values(registry.country, matchable=True)),
+            join_cell(leis),
+            join_cell(isins),
+            join_cell(entity.get_type_values(registry.country, matchable=True)),
             bool_text(is_sanctioned),
             bool_text(is_eo_14071),
             entity.id,
             f"https://www.opensanctions.org/entities/{entity.id}/",
-            "; ".join(entity.datasets),
-            self._get_aliases(entity),
+            join_cell(entity.datasets),
+            join_cell(self._get_aliases(entity)),
         ]
         self.csv.writerow(row)
 
     def finish(self) -> None:
         self.fh.close()
         super().finish()
+        log.info(
+            "Exported %d entities, %d LEIs and %d ISINs",
+            self._count_entities,
+            self._count_leis,
+            self._count_isins,
+        )
