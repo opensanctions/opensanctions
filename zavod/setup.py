a1c77bd3120dd6cc73933588d8dd1ee3e1cb5541
Friedrich Lindenberg <friedrich@pudo.org>
2023-12-01 18:16:11 +0100

    up nk

--- zavod/setup.py
+++ zavod/setup.py
@@ -22,7 +22,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "followthemoney == 3.5.*",
-        "nomenklatura == 3.8.*",
+        "nomenklatura == 3.9.*",
         "datapatch == 1.1.*",
         "fingerprints == 1.2.*",
         "addressformatting == 1.3.*",
