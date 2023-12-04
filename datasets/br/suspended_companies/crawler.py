eff7a94f76c7b81955dae46da5598d2ea4b978d3
Friedrich Lindenberg <friedrich@pudo.org>
2023-12-04 19:54:08 +0100

    small changes to br_ceis

--- datasets/br/suspended_companies/crawler.py
+++ datasets/br/suspended_companies/crawler.py
@@ -1,14 +1,15 @@
-from zavod import Context
-from zavod import helpers as h
-from zipfile import ZipFile
-import csv
 import io
 import re
+import csv
 from typing import List
+from zipfile import ZipFile
+
+from zavod import Context
+from zavod import helpers as h
 
 HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
 
 
 def get_csv_url(context: Context) -> str:
@@ -26,8 +27,10 @@ def get_csv_url(context: Context) -> str:
 
     # The header is necessary because the website blocks requests without a user agent.
     doc = context.fetch_html(context.data_url, headers=HEADERS)
-    path = '//script'
-    date_pattern = re.compile(r'"ano"\s*:\s*"(\d+)",\s*"mes"\s*:\s*"(\d+)",\s*"dia"\s*:\s*"(\d+)"')
+    path = "//script"
+    date_pattern = re.compile(
+        r'"ano"\s*:\s*"(\d+)",\s*"mes"\s*:\s*"(\d+)",\s*"dia"\s*:\s*"(\d+)"'
+    )
     for script in doc.xpath(path):
         if script.text:
             match = date_pattern.search(script.text)
@@ -53,10 +56,10 @@ def get_data(csv_url: str, context: Context) -> List[dict]:
     zip_file = ZipFile(io.BytesIO(response.content))
     file_name = zip_file.namelist()[0]
 
-    csv_str = zip_file.read(file_name).decode('iso-8859-1')
+    csv_str = zip_file.read(file_name).decode("iso-8859-1")
     lines = csv_str.splitlines()
 
-    reader = csv.DictReader(lines, delimiter=';')
+    reader = csv.DictReader(lines, delimiter=";")
 
     return [row for row in reader]
 
@@ -70,25 +73,26 @@ def create_entities(data: List[dict], context: Context) -> None:
     """
 
     for raw_entity in data:
-        if raw_entity['TIPO DE PESSOA'] == 'F':
-            entity = context.make('Person')
-            entity.id = context.make_id(raw_entity['CPF OU CNPJ DO SANCIONADO'])
-            entity.add('name', raw_entity['NOME DO SANCIONADO'])
-            entity.add('taxNumber', raw_entity['CPF OU CNPJ DO SANCIONADO'])
-
-            context.emit(entity, target=True)
-
-        if raw_entity['TIPO DE PESSOA'] == 'J':
-            entity = context.make('Company')
-            entity.id = context.make_id(raw_entity['CPF OU CNPJ DO SANCIONADO'])
-            entity.add('name', raw_entity['NOME DO SANCIONADO'])
-            entity.add('taxNumber', raw_entity['CPF OU CNPJ DO SANCIONADO'])
-
-            context.emit(entity, target=True)
+        entity = context.make("LegalEntity")
+        entity_type = raw_entity["TIPO DE PESSOA"]
+        entity.id = context.make_id(raw_entity["CPF OU CNPJ DO SANCIONADO"])
+        if entity_type == "F":
+            entity.add_schema("Person")
+        elif entity_type == "J":
+            entity.add_schema("Company")
+        elif entity_type == "":
+            pass
+        else:
+            context.log.error("Unknown entity type", tipo=entity_type)
+            continue
+        entity.add("name", raw_entity["NOME DO SANCIONADO"])
+        entity.add("taxNumber", raw_entity["CPF OU CNPJ DO SANCIONADO"])
+        entity.add("country", "br")
 
         sanction = h.make_sanction(context, entity)
         sanction.add("program", "Brazil disreputed and sanctioned companies")
         sanction.add("reason", raw_entity["FUNDAMENTAÇÃO LEGAL"], lang="por")
+        context.emit(entity, target=True)
         context.emit(sanction)
 
 
