5d69b00ec7eb694c8a418a5e1d1ee0114a9e425a
JD Bothma <jbothma@gmail.com>
2023-12-11 16:00:07 +0200

    WIP

--- datasets/_wikidata/peps/crawler.py
+++ datasets/_wikidata/peps/crawler.py
@@ -1,5 +1,5 @@
 import csv
-from typing import Optional, Any
+from typing import Dict, Optional, Any
 from pantomime.types import CSV
 from nomenklatura.util import is_qid
 
@@ -30,64 +30,111 @@ def date_value(value: Any) -> Optional[str]:
     return text[:10]
 
 
+def crawl_holder(context: Context, holder: Dict[str, str]) -> None:
+    entity = context.make("Person")
+    qid: Optional[str] = holder.get("person_qid")
+    if not is_qid(qid) or qid == "Q1045488":
+        return
+    entity.id = qid
+
+    keyword = DECISIONS.get(holder.get("decision", ""))
+    if keyword is None:
+        return
+
+    position_qid = holder.get("position_qid")
+    position_label = remove_emoji(holder.get("position_label"))
+    if not position_label:
+        position_label = position_qid
+
+    position = h.make_position(
+        context,
+        position_label,
+        country=holder.get("country_code"),
+        topics=TOPICS.get(holder.get("decision", ""), []),
+        wikidata_id=position_qid,
+    )
+    categorisation = categorise(context, position)
+    if not categorisation.is_pep:
+        return
+    occupancy = h.make_occupancy(
+        context,
+        entity,
+        position,
+        False,
+        death_date=date_value(holder.get("person_death")),
+        birth_date=date_value(holder.get("person_birth")),
+        end_date=date_value(holder.get("end_date")),
+        start_date=date_value(holder.get("start_date")),
+        categorisation=categorisation,
+    )
+    if not occupancy:
+        return
+
+    res = context.lookup("role_topics", position_label)
+    if res:
+        position.add("topics", res.topics)
+
+    # TODO: decide all entities with no P39 dates as false?
+    # print(holder.person_qid, death, start_date, end_date)
+
+    if holder.get("person_label") != qid:
+        entity.add("name", holder.get("person_label"))
+    entity.add("keywords", keyword)
+
+    context.emit(position)
+    context.emit(occupancy)
+    context.emit(entity, target=True)
+
+
+def query_position_holders(position: Dict[str, str]) -> None:
+   
+    vars = {"POSITION": position.qid}
+    context.log.info("Crawling position [%s]: %s", position.qid, position.label)
+    response = run_query(conn, "holders/holders", vars, expire=CACHE_MEDIUM)
+    holders: Dict[str, Holder] = {}
+    for binding in response.results:
+        start_date = truncate_date(
+            binding.plain("positionStart")
+            or binding.plain("bodyStart")
+            or binding.plain("bodyInception")
+        )
+        end_date = truncate_date(
+            binding.plain("positionEnd")
+            or binding.plain("bodyEnd")
+            or binding.plain("bodyAbolished")
+        )
+        holder_id = make_id(
+            binding.plain("ps"),
+            binding.plain("person"),
+            position.qid,
+            start_date,
+            end_date,
+        )
+        if holder_id is None:
+            log.error("No holder ID: %r", binding)
+            continue
+
+        yield {
+            "id": holder_id,
+            "person_qid": binding.plain("person"),
+            "person_label": binding.plain("personLabel"),
+            "person_description": binding.plain("personDescription"),
+            "person_birth": truncate_date(binding.plain("birth")),
+            "person_death": truncate_date(binding.plain("death")),
+            "start_date": start_date,
+            "end_date": end_date,
+            "position_qid": position.qid,
+            "country_qid": position.country_qid,
+        }
+
+
 def crawl(context: Context):
     path = context.fetch_resource("source.csv", context.data_url)
     context.export_resource(path, CSV, title=context.SOURCE_TITLE)
-    with open(path, "r") as fh:
-        for row in csv.DictReader(fh):
-            entity = context.make("Person")
-            qid: Optional[str] = row.get("person_qid")
-            if not is_qid(qid) or qid == "Q1045488":
-                continue
-            entity.id = qid
 
-            keyword = DECISIONS.get(row.get("decision", ""))
-            if keyword is None:
-                continue
-
-            position_qid = row.get("position_qid")
-            position_label = remove_emoji(row.get("position_label"))
-            if not position_label:
-                position_label = position_qid
-
-            position = h.make_position(
-                context,
-                position_label,
-                country=row.get("country_code"),
-                topics=TOPICS.get(row.get("decision", ""), []),
-                wikidata_id=position_qid,
-            )
-            categorisation = categorise(context, position)
-            if not categorisation.is_pep:
-                continue
-            occupancy = h.make_occupancy(
-                context,
-                entity,
-                position,
-                False,
-                death_date=date_value(row.get("person_death")),
-                birth_date=date_value(row.get("person_birth")),
-                end_date=date_value(row.get("end_date")),
-                start_date=date_value(row.get("start_date")),
-                categorisation=categorisation,
-            )
-            if not occupancy:
-                continue
-
-            res = context.lookup("role_topics", position_label)
-            if res:
-                position.add("topics", res.topics)
-
-            # TODO: decide all entities with no P39 dates as false?
-            # print(holder.person_qid, death, start_date, end_date)
-
-            if row.get("person_label") != qid:
-                entity.add("name", row.get("person_label"))
-            entity.add("keywords", keyword)
-
-            context.emit(position)
-            context.emit(occupancy)
-            context.emit(entity, target=True)
+    for position in fetch_positions(context):
+        for holder in query_position_holders(context, position):
+            crawl_holder(context, holder)
 
     entity = context.make("Person")
     entity.id = "Q21258544"
@@ -95,3 +142,5 @@ def crawl(context: Context):
     entity.add("topics", "role.pep")
     entity.add("country", "us")
     context.emit(entity, target=True)
+
+
---
+++ datasets/_wikidata/peps/query.py
@@ -0,0 +1,39 @@
+from typing import Dict
+from jinja2 import Template
+from pathlib import Path
+
+from zavod.context import Context
+
+from .struct import SparqlResponse
+
+
+queries_path = Path(__file__).resolve().parent / "queries"
+
+QUERY_URL = "https://query.wikidata.org/sparql"
+HEADERS = {
+    "Accept": "application/sparql-results+json",
+}
+CACHE_SHORT = 1
+CACHE_MEDIUM = CACHE_SHORT * 7
+CACHE_LONG = CACHE_SHORT * 30
+
+
+def make_query(name: str, variables: Dict[str, str]) -> str:
+    query_path = queries_path.joinpath(f"{name}.sparql")
+    with open(query_path, "r") as fh:
+        query_tmpl = fh.read()
+
+    template = Template(query_tmpl)
+    return template.render(**variables)
+
+
+def run_query(
+    context: Context,
+    name: str,
+    variables: Dict[str, str] = {},
+    cache_days: int = CACHE_SHORT,
+) -> SparqlResponse:
+    query_text = make_query(name, variables)
+    params = {"query": query_text}
+    data = context.fetch_json(QUERY_URL, params=params, headers=HEADERS, cache_days=cache_days)
+    return SparqlResponse(query_text, data)
