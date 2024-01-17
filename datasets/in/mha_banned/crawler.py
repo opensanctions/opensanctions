b346206e04ba3bf068935f01978109d22feb8aad
Friedrich Lindenberg <friedrich@pudo.org>
2024-01-17 15:17:26 +0100

    add associations table

--- datasets/in/mha_banned/crawler.py
+++ datasets/in/mha_banned/crawler.py
@@ -1,11 +1,14 @@
-import shutil
 import re
+import csv
 from lxml import html
+from typing import List, Dict
 from normality import collapse_spaces
-from pantomime.types import HTML
+from pantomime.types import HTML, CSV
 
 from zavod import Context
 from zavod import helpers as h
+
+SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTc-EkLWZgLKDPVvcrCoKLp17EEo535uP1EMcLKFl_b6T3z6Tq99BrI3R9GhxKirgRoozND1xQ48O4-/pub?output=csv"
 
 CLEAN = [
     ", all its formations and front organizations.",
@@ -17,7 +20,55 @@ CLEAN = [
 ]
 REGEX_PERSON_PREFIX = re.compile(r"^\d+\.")
 
+def parse_names(field: str) -> List[str]:
+    names: List[str] = []
+    for value in field.split(';'):
+        value = value.strip()
+        if len(value):
+            names.append(value)
+    return names
+
+def crawl_sheet(context: Context):
+    path = context.fetch_resource("accommodations.csv", SHEET_URL)
+    context.export_resource(path, CSV, title=context.SOURCE_TITLE)
+    named_ids: Dict[str, str] = {}
+    with open(path, "r") as fh:
+        for row in csv.DictReader(fh):
+            entity = context.make(row.pop("Type", "LegalEntity"))
+            source_url = row.pop("SourceURL")
+            id_ = row.pop("ID")
+            name = row.pop("Name")
+            if name is None:
+                context.log.warn("No name", row=row)
+                continue
+            entity.id = context.make_id(id_, name, source_url)
+            assert entity.id is not None, row
+            named_ids[name] = entity.id
+            entity.add('name', name)
+            entity.add('notes', row.pop("Notes"))
+            entity.add("topics", "sanction")
+            entity.add("sourceUrl", source_url)
+            entity.add("alias", parse_names(row.pop("Aliases")))
+            entity.add("weakAlias", parse_names(row.pop("Weak")))
+
+            sanction = h.make_sanction(context, entity, id_)
+            sanction.add('program', row.pop("Designation"))
+            sanction.add('authorityId', id_)
+
+            linked = row.pop("Linked", "").strip()
+            if len(linked) and linked in named_ids:
+                rel = context.make("UnknownLink")
+                rel.id = context.make_id(linked, "linked", entity.id)
+                rel.add("subject", named_ids[linked])
+                rel.add("object", entity.id)
+                context.emit(rel)
+            
+            context.emit(entity, target=True)
+            context.emit(sanction)
+
+
 def crawl(context: Context):
+    crawl_sheet(context)
     path = context.fetch_resource(
          "organisations.html",
          context.data_url + "banned-organisations",
