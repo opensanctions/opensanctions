a2ce171cd2c5671b491e6eef83a8213589f3670b
Stephen Atehe <atehestephen@gmail.com>
2024-01-19 11:54:13 +0100

    Process entity values and emit dataset

--- datasets/li/seimas/crawler.py
+++ datasets/li/seimas/crawler.py
@@ -7,82 +7,73 @@ from normality import collapse_spaces
 from datetime import datetime
 
 
-def to_date(date_str):
+def to_date(date_str: str) -> datetime:
     date_format = "%d %B %Y"
     return datetime.strptime(date_str, date_format)
 
 
-def clean_text(text, str_to_remove=[]):
-    for string in str_to_remove:
-        text = text.replace(string, "")
+def get_element_text(doc: ElementTree, xpath_value: str, to_remove=[], position=0):
+    element_tag = doc.xpath(xpath_value)
+    element_text = element_tag[position].text_content() if element_tag else ""
 
-    return text.strip()
+    for string in to_remove:
+        element_text = element_text.replace(string, "")
+
+    return collapse_spaces(element_text.strip())
 
 
-def get_occupany_dates(tenure):
+def get_occupany_dates(tenure: str):
     tenure_year = tenure.split()[-1]
-
     start_year, end_year = tenure_year.split("-")
 
     return start_year, end_year
 
 
-def crawl_member_bio(context, url):
-    print(f"Processing  {url}")
+def crawl_member_bio(context: Context, url: str):
     doc = context.fetch_html(url, cache_days=1)
 
-    person_name = collapse_spaces(
-        doc.xpath('//div[@class="sn_narys_vardas_title"]')[0].text_content()
+    person_name = get_element_text(doc, '//div[@class="sn_narys_vardas_title"]')
+    date_of_birth = get_element_text(
+        doc,
+        '//tr[.//*[contains(.//text(), "Date of birth")]]//td//p|//p[.//*[contains(.//text(), "Date of birth")]]',
+        to_remove=["Date of birth", ","],
+        position=-1,
+    )
+    place_of_birth = get_element_text(
+        doc,
+        '//tr[.//*[contains(.//text(), "Place")]][.//*[contains(.//text(), "birth")]]//td//p|//p[.//*[contains(.//text(), "Place of birth")]]',
+        to_remove=["Place of birth"],
+        position=-1,
     )
 
-    date_of_birth = clean_text(
-        doc.xpath(
-            '//tr[.//*[contains(.//text(), "Date of birth")]]//td//p|//p[.//*[contains(.//text(), "Date of birth")]]'
-        )[-1].text_content(),
-        str_to_remove=["Date of birth", ","],
-    )
-    place_of_birth = clean_text(
-        doc.xpath(
-            '//tr[.//*[contains(.//text(), "Place of birth")]]//td//p|//p[.//*[contains(.//text(), "Place of birth")]]'
-        )[-1].text_content(),
-        str_to_remove=["Place of birth"],
-    )
+    position_name = get_element_text(doc, '//div[@class="sn-nuo-iki"]')
+    party = get_element_text(doc, '//a[contains(@class, "smn-frakcija")]')
 
-    position_name = doc.xpath('//div[@class="sn_narys_vardas_title"]')[0].text_content()
-    tenure = doc.xpath('//div[@class="kadencija"]')[0].text_content()
-    party = (
-        doc.xpath('//a[contains(@class, "smn-frakcija")]')[0].text_content()
-        if doc.xpath('//a[contains(@class, "smn-frakcija")]')
-        else ""
-    )
+    position_name = position_name.split("from")[0].strip()
+    if party:
+        position_name = f"{position_name}, {party}"
 
-    print(
-        {
-            "name": person_name,
-            "date_of_birth": date_of_birth,
-            "place_of_birth": place_of_birth,
-            "position": position_name,
-            "tenure": tenure,
-            "party": party,
-        }
-    )
+    tenure = get_element_text(doc, '//div[@class="kadencija"]')
+    start_year, end_year = get_occupany_dates(tenure)
+
     person = context.make("Person")
     person.id = context.make_slug(person_name)
     person.add("name", person_name)
     person.add("sourceUrl", url)
+
     if date_of_birth:
         person.add("birthDate", to_date(date_of_birth))
     person.add("birthPlace", place_of_birth)
 
-    position_name = position_name.split("from")[0]
-
     position = h.make_position(
         context,
-        f"{position_name}, {party}",
+        position_name,
         country="li",
     )
 
-    start_year, end_year = get_occupany_dates(tenure)
+    categorisation = categorise(context, position)
+    if not categorisation.is_pep:
+        return
 
     occupancy = h.make_occupancy(
         context,
@@ -90,17 +81,16 @@ def crawl_member_bio(context, url):
         position,
         start_date=start_year,
         end_date=end_year,
-        # categorisation=categorisation,
+        categorisation=categorisation,
     )
+
     context.emit(person, target=True)
     context.emit(position)
     context.emit(occupancy)
 
 
-def crawl(context: Context) -> Optional[str]:
-    doc = context.fetch_html(
-        "https://www.lrs.lt/sip/portal.show?p_r=35299&p_k=2", cache_days=1
-    )
+def crawl(context: Context):
+    doc = context.fetch_html(context.data_url, cache_days=1)
 
     for anchor in doc.xpath(
         '//div[contains(@class,"list-member")]//a[contains(@class, "smn-name")]'
