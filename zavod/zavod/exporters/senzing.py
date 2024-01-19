700a375e8fde68e2c3235beeecff19c12b497fda
Friedrich Lindenberg <friedrich@pudo.org>
2024-01-19 15:31:29 +0100

    try to use senzing format more idiomatically

--- zavod/zavod/exporters/senzing.py
+++ zavod/zavod/exporters/senzing.py
@@ -5,36 +5,50 @@
 # As a next step, the matching results could be converted back into a
 # nomenklatura resolver file and then used to generate integrated FtM entities.
 from typing import cast, Dict, Any, Generator, List, Optional, Union
-from typing_extensions import TypedDict
 from followthemoney.types import registry
 from rigour.ids.wikidata import is_qid
 from nomenklatura.store import View
 from nomenklatura.dataset import DS
+from pprint import pprint
 
 from zavod.entity import Entity
 from zavod.exporters.common import Exporter
 from zavod.util import write_json
 
-Feature = Union[str, Dict[str, str]]
+# Feature = Union[str, Dict[str, str]]
+ID_TYPES = {
+
+}
 
 
-class SenzingRecord(TypedDict):
-    DATA_SOURCE: str
-    RECORD_ID: str
-    RECORD_TYPE: Optional[str]
-    FEATURES: List[Feature]
+# def map_feature(entity: Entity, features: List[Feature], prop: str, attr: str) -> None:
+#     for value in entity.get(prop, quiet=True):
+#         features.append({attr: value})
 
 
-def map_feature(entity: Entity, features: List[Feature], prop: str, attr: str) -> None:
+def push(obj: Dict[str, Any], section: str, value: Dict[str, Any]) -> None:
+    if section not in obj:
+        obj[section] = []
+    obj[section].append(value)
+
+
+def map(
+    entity: Entity, prop: str, obj: Dict[str, Any], section: str, attr: str
+) -> None:
     for value in entity.get(prop, quiet=True):
-        features.append({attr: value})
+        push(obj, section, {attr: value})
 
+def clean(obj: Dict[str, Any]) -> Dict[str, Any]:
+    out: Dict[str, Any] = {}
+    for k, v in obj.items():
+        if v is not None:
+            out[k] = v
+    return out
 
 def senzing_adjacent_features(
-    entity: Entity, view: View[DS, Entity]
-) -> Generator[Feature, None, None]:
+    entity: Entity, record: Dict[str, Any], view: View[DS, Entity]
+) -> None:
     for _, adj in view.get_adjacent(entity):
-        adj_data: Optional[Dict[str, Optional[str]]] = None
         if adj.schema.name == "Address":
             adj_data = {
                 "ADDR_FULL": adj.first("full"),
@@ -45,20 +59,21 @@ def senzing_adjacent_features(
                 "ADDR_COUNTRY": adj.first("country"),
                 "ADDR_POSTAL_CODE": adj.first("postalCode"),
             }
+            push(record, "ADDRESSES", clean(adj_data))
         elif adj.schema.name == "Identification":
             adj_data = {
                 "NATIONAL_ID_NUMBER": adj.first("number"),
                 "NATIONAL_ID_COUNTRY": adj.first("country"),
             }
+            push(record, "IDENTIFIERS", clean(adj_data))
         elif adj.schema.name == "Passport":
             adj_data = {
                 "PASSPORT_NUMBER": adj.first("number"),
                 "PASSPORT_COUNTRY": adj.first("country"),
             }
-        if adj_data is not None:
-            values = {k: v for k, v in adj_data.items() if v is not None}
-            if len(values):
-                yield values
+            push(record, "IDENTIFIERS", clean(adj_data))
+
+        # TODO: make graph edges
 
 
 class SenzingExporter(Exporter):
@@ -74,7 +89,7 @@ class SenzingExporter(Exporter):
             self.source_name = "OPENSANCTIONS"
 
     def feed(self, entity: Entity) -> None:
-        if not entity.schema.is_a("LegalEntity"):
+        if not entity.schema.matchable:
             return None
         if entity.id is None:
             return None
@@ -92,59 +107,66 @@ class SenzingExporter(Exporter):
         elif entity.schema.is_a("Vehicle"):
             record_type = "VEHICLE"
 
-        record: SenzingRecord = {
+        record: Dict[str, Any] = {
             "DATA_SOURCE": self.source_name,
             "RECORD_ID": entity.id,
             "RECORD_TYPE": record_type,
-            "FEATURES": [],
+            "LAST_CHANGE": entity.last_change,
         }
 
-        features: List[Feature] = []
         for name in entity.get_type_values(registry.name):
             name_type = "PRIMARY" if name == entity.caption else "ALIAS"
             name_field = "NAME_ORG" if is_org else "NAME_FULL"
-            features.append({"NAME_TYPE": name_type, name_field: name})
+            push(record, "NAMES", {"NAME_TYPE": name_type, name_field: name})
 
-        for gender in entity.get("gender", quiet=True):
-            if gender == "male":
-                features.append({"GENDER": "M"})
-            if gender == "female":
-                features.append({"GENDER": "F"})
+        genders = entity.get("gender", quiet=True)
+        if len(genders) == 1:
+            if genders[0] == "male":
+                record["GENDER"] = "M"
+            if genders[0] == "female":
+                record["GENDER"] = "F"
 
-        map_feature(entity, features, "address", "ADDR_FULL")
-        map_feature(entity, features, "birthDate", "DATE_OF_BIRTH")
-        map_feature(entity, features, "deathDate", "DATE_OF_DEATH")
-        map_feature(entity, features, "birthPlace", "PLACE_OF_BIRTH")
-        map_feature(entity, features, "nationality", "NATIONALITY")
-        map_feature(entity, features, "country", "CITIZENSHIP")
-        map_feature(entity, features, "incorporationDate", "REGISTRATION_DATE")
-        map_feature(entity, features, "jurisdiction", "REGISTRATION_COUNTRY")
-        map_feature(entity, features, "website", "WEBSITE_ADDRESS")
-        map_feature(entity, features, "email", "EMAIL_ADDRESS")
-        map_feature(entity, features, "phone", "PHONE_NUMBER")
-        map_feature(entity, features, "passportNumber", "PASSPORT_NUMBER")
-        map_feature(entity, features, "idNumber", "NATIONAL_ID_NUMBER")
-        map_feature(entity, features, "registrationNumber", "NATIONAL_ID_NUMBER")
-        map_feature(entity, features, "ogrnCode", "NATIONAL_ID_NUMBER")
-        map_feature(entity, features, "taxNumber", "TAX_ID_NUMBER")
-        map_feature(entity, features, "innCode", "TAX_ID_NUMBER")
-        map_feature(entity, features, "vatCode", "TAX_ID_NUMBER")
-        map_feature(entity, features, "leiCode", "LEI_NUMBER")
-        map_feature(entity, features, "dunsCode", "DUNS_NUMBER")
+        map(entity, "address", record, "ADDRESSES", "ADDR_FULL")
+        map(entity, "birthDate", record, "DATES", "DATE_OF_BIRTH")
+        map(entity, "deathDate", record, "DATES", "DATE_OF_DEATH")
+        map(entity, "incorporationDate", record, "DATES", "REGISTRATION_DATE")
+        map(entity, "birthPlace", record, "ADDRESSES", "PLACE_OF_BIRTH")
+        map(entity, "nationality", record, "COUNTRIES", "NATIONALITY")
+        map(entity, "country", record, "COUNTRIES", "CITIZENSHIP")
+        map(entity, "jurisdiction", record, "COUNTRIES", "REGISTRATION_COUNTRY")
+        map(entity, "website", record, "CONTACTS", "WEBSITE_ADDRESS")
+        map(entity, "email", record, "CONTACTS", "EMAIL_ADDRESS")
+        map(entity, "phone", record, "CONTACTS", "PHONE_NUMBER")
+        map(entity, "passportNumber", record, "IDENTIFIERS", "PASSPORT_NUMBER")
+        map(entity, "idNumber", record, "IDENTIFIERS", "NATIONAL_ID_NUMBER")
+        map(entity, "registrationNumber", record, "IDENTIFIERS", "NATIONAL_ID_NUMBER")
+        map(entity, "ogrnCode", record, "IDENTIFIERS", "NATIONAL_ID_NUMBER")
+        map(entity, "taxNumber", record, "IDENTIFIERS", "TAX_ID_NUMBER")
+        map(entity, "innCode", record, "IDENTIFIERS", "TAX_ID_NUMBER")
+        map(entity, "vatCode", record, "IDENTIFIERS", "TAX_ID_NUMBER")
+        map(entity, "leiCode", record, "IDENTIFIERS", "LEI_NUMBER")
+        map(entity, "dunsCode", record, "IDENTIFIERS", "DUNS_NUMBER")
+        map(entity, "sourceUrl", record, "LINKS", "SOURCE_URL")
 
-        for adj_feature in senzing_adjacent_features(entity, self.view):
-            features.append(adj_feature)
+        senzing_adjacent_features(entity, record, self.view)
 
         for wd_id in (entity.id, entity.first("wikidataId")):
             if wd_id is not None and is_qid(wd_id):
-                features.append(
-                    {
-                        "TRUSTED_ID_TYPE": "WIKIDATA",
-                        "TRUSTED_ID_NUMBER": wd_id,
-                    }
-                )
+                wd = {
+                    "TRUSTED_ID_TYPE": "WIKIDATA",
+                    "TRUSTED_ID_NUMBER": wd_id,
+                }
+                push(record, "IDENTIFIERS", wd)
 
-        record["FEATURES"] = features
+        if not is_qid(entity.id):
+            ident = {"OTHER_ID_TYPE": "OS_LIST_ID", "OTHER_ID_NUMBER": entity.id}
+            push(record, "IDENTIFIERS", ident)
+
+        record["URL"] = f"https://www.opensanctions.org/entities/{entity.id}/"
+        if entity.schema.is_a('Organization'):
+            for addr in record.get("ADDRESSES", []):
+                addr["ADDR_TYPE"] = "BUSINESS"
+        pprint(record)
         write_json(cast(Dict[str, Any], record), self.fh)
 
     def finish(self) -> None:
