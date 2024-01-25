364fd413db970840784344acd67b6c368b935383
Stephen Atehe <atehestephen@gmail.com>
2024-01-25 08:45:54 +0100

    Add program and debarment to legal entity, update crawler publisher

--- datasets/si/br/crawler.py
+++ datasets/si/br/crawler.py
@@ -2,25 +2,9 @@ from zavod import Context
 from typing import Any, Dict
 
 
-HEADERS = {"Cookie": "ORIGIN=KPK1&KPK2.KPK1"}
-
-
-def fetch_data(context: Context, offset, limit):
-    page_url = f"https://erar.si/api/omejitve/?&draw=2&columns%5B0%5D%5Bdata%5D=org_naziv&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=omejitev_do&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=subjekt_naziv&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=od&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=do&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=st_transakcij&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=0&order%5B0%5D%5Bdir%5D=asc&start={offset}&length={limit}&search%5Bvalue%5D=&search%5Bregex%5D=false&_=1706090961920"
-
-    response = context.fetch_json(page_url, headers=HEADERS, cache_days=1)
-    return response
-
-
 def create_entities(context: Context, record: Dict[str, Any]):
-    organization = context.make("Organization")
-
     organization_name = record.pop("org_naziv")
     organization_number = record.pop("org_sifrapu")
-
-    organization.id = context.make_id(organization_number or organization_name)
-    organization.add("name", organization_name)
-    organization.add("idNumber", organization_number)
 
     legal_entity = context.make("LegalEntity")
     subject_name = record.pop("subjekt_naziv")
@@ -42,24 +26,21 @@ def create_entities(context: Context, record: Dict[str, Any]):
             f"https://erar.si/transakcija/placnik/{organization_number}/prejemnik/{registration_number}",
         )
 
-    validity_link = context.make("UnknownLink")
-    validity_link.id = context.make_id(legal_entity.id)
+    start_date = record.pop("od")
     end_date = record.pop("do")
+    if end_date.startswith("9999"):
+        end_date = "until cancelation"
+    else:
+        end_date = f"to {end_date}"
 
-    validity_link.add("startDate", record.pop("od"))
-    if not end_date.startswith("9999"):
-        validity_link.add("endDate", end_date)
-
-    validity_link.add(
-        "description",
-        "When the restriction is valid from, and when it ends. No end date implies it is valid until canceled",
+    legal_entity.add(
+        "program",
+        f"""{subject_name} is subject to business restriction, valid from {start_date} and applies {end_date}. 
+         Following the regulations, the entity is prohibited because a public sector, {organization_name}, must avoid awarding contracts 
+         or special rights to entities where an official or their family has significant involvement or ownership exceeding 5% """,
     )
-    validity_link.add("subject", legal_entity)
-    validity_link.add("object", organization)
 
     context.emit(legal_entity, target=True)
-    context.emit(organization)
-    context.emit(validity_link)
     context.audit_data(record, ignore=["omejitev_do", "st_transakcij"])
 
 
@@ -71,18 +52,5 @@ def parse_data(context: Context, response: Dict[str, Any]):
 
 
 def crawl(context: Context):
-    offset = 0
-    limit = 100
-    records_total = limit
-    while True:
-        response = fetch_data(context, offset, limit)
-        records_total = (
-            response.get("recordsTotal") if records_total == limit else records_total
-        )
-        parse_data(context, response)
-
-        if offset + limit >= records_total:
-            break
-
-        offset += limit
-        context.log.info(f"Processed entities from page {offset//limit}")
+    response = context.fetch_json(context.data_url, cache_days=1)
+    parse_data(context, response)
