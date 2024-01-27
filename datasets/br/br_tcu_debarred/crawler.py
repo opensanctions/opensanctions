e234c2a0435ada8b6fcb0f0c98b91e34dc9f8d73
Bruno Görresen Mello <bruno@aetos.tech>
2024-01-27 19:16:14 -0300

    adding TCU debarred crawler

---
+++ datasets/br/br_tcu_debarred/crawler.py
@@ -0,0 +1,84 @@
+from zavod import Context, helpers as h
+
+def crawl_item(input_dict: dict, context: Context):
+    """
+    Creates an entity from the raw data.
+
+    :param input_dict: Data dict extracted from source.
+                       Should have at least following keys:
+                        - cpf_cnpj
+                        - nome
+                        - processo
+                        - deliberacao
+                        - data_transito_julgado
+                        - data_final
+    :param context: The context object.
+    """
+
+    entity = context.make("LegalEntity")
+
+    raw_tax_number = input_dict["cpf_cnpj"]
+
+    # If it's length 18 with punctuation, then it is a CNPJ (XX.XXX.XXX/XXXX-XX)
+    if len(raw_tax_number) == 18:
+        entity.add_schema("Company")
+        tax_number = h.clean_br_cnpj(raw_tax_number)
+        prefix = "br-cnpj"
+
+    # If it's length 14 with punctuation, then it is a CPF (XXX.XXX.XXX-XX)
+    elif len(raw_tax_number) == 14:
+        entity.add_schema("Person")
+        tax_number = h.clean_br_cpf(raw_tax_number)
+        prefix = "br-cnpj"
+
+    # If it's neither, then we just use the raw number
+    else:
+        context.log.warning("Entity type not defined by tax number")
+        tax_number = raw_tax_number
+        prefix = "br-unknown"
+
+    entity.id = context.make_slug(tax_number, prefix=prefix)
+    entity.add("name", input_dict["nome"])
+    entity.add("taxNumber", tax_number)
+    entity.add("country", "br")
+    entity.add("topics", "debarment")
+
+    process_number = input_dict.pop("processo")
+
+    sanction = h.make_sanction(context, entity, key=process_number)
+
+    # The ID of the process
+    sanction.add(
+        "description",
+        "Process Number: {}, acording to the deliberation number: {}".format(
+            process_number, input_dict.pop("deliberacao")
+        ),
+    )
+
+    # The start will be defined as the date of the final appel (transito em julgado)
+    sanction.add("startDate", input_dict.pop("data_transito_julgado"))
+    sanction.add("endDate", input_dict.pop("data_final"))
+
+    sanction.add("program", '''TCU Debarred entities based on Article 46 of Law 8.443/92 that
+                               restricts entities from participating in 
+                               public biddings''')
+
+    context.emit(entity, target=True)
+    context.emit(sanction)
+
+def crawl(context: Context):
+    """
+    Entrypoint to the crawler.
+
+    The crawler works by fetching the data from the URL as a JSON.
+    The data is already in the format of a list of dicts, so we just need to create the entities.
+
+    :param context: The context object.
+    """
+    response = context.fetch_json(context.data_url)
+    if "items" not in response:
+        context.log.error("Items not found in JSON")
+        return
+
+    for item in response["items"]:
+        crawl_item(item, context)
