1ef5cf86ccf612dd856c37d5d1928b544453312c
David Huggins-Daines <dhd@ecolingui.ca>
2024-01-30 14:01:54 -0500

    fix: handle weird HTML and fix logging

--- datasets/ve/asamblea_nacional/crawler.py
+++ datasets/ve/asamblea_nacional/crawler.py
@@ -19,58 +19,75 @@ IDENTITY = re.compile(r"(?:c\.i|cédula de identidad)\s*:\s*(\S+)", re.I)
 WS = re.compile(r"\s+")
 
 
+def extract_marked_content(infobox: HtmlElement) -> str:
+    """Do a slightly better extraction of content than what lxml
+    offers, in the case where it was converted from a PDF in an
+    attempt to preserve layout.
+    """
+    # Add newlines between markedContent spans
+    mcs = infobox.iterfind(".//span[@class]")
+    for el in mcs:
+        classes = el.get("class")
+        if classes is None:
+            continue
+        if "markedContent" not in classes.split():
+            continue
+        el.tail = "\n"
+    return infobox.text_content()
+
+
+DATE_FORMATS = [
+    "%d/%m/%Y",
+    "%d/%m/%y",
+    "%d-%m-%Y",
+    "%d-%m-%y",
+    "%d de %b de %Y",
+    "%d – %m – %y",
+]
+
+
 def crawl_infobox(context: Context, person: Entity, infobox: HtmlElement):
     """Do a best-effort extraction of some facts from the text of an
     infobox with a member's CV."""
-    text = infobox.text_content()
+    text = extract_marked_content(infobox)
     m = BIRTHDATE.search(text)
     if m:
         birthdate = m.group(1)
         birthdate = WS.sub(" ", birthdate)
         birthdate = birthdate.strip(" .;,")
-        context.log.info(f"Birthdate: {birthdate}")
+        context.log.debug(f"Birthdate: {birthdate}")
         person.add(
             "birthDate",
-            h.parse_date(
-                birthdate,
-                (
-                    "%d/%m/%Y",
-                    "%d/%m/%y",
-                    "%d-%m-%Y",
-                    "%d-%m-%y",
-                    "%d de %b de %Y",
-                    "%d – %m – %y",
-                ),
-            ),
+            h.parse_date(birthdate, DATE_FORMATS),
         )
     m = BIRTHPLACE.search(text)
     if m:
         birthplace = m.group(1)
         birthplace = WS.sub(" ", birthplace)
         birthplace = birthplace.strip(" .;,")
-        context.log.info(f"Place of birth: {birthplace}")
+        context.log.debug(f"Place of birth: {birthplace}")
         person.add("birthPlace", birthplace)
     m = IDENTITY.search(text)
     if m:
         identity = m.group(1)
         identity = identity.strip(" .;,")
-        context.log.info(f"ID Number: {identity}")
+        context.log.debug(f"ID Number: {identity}")
         h.make_identification(context, person, identity)
 
 
 def crawl_member_page(context: Context, person: Entity, name: str, href: str):
     """Attempt to extract information from a member's individual
     page."""
-    context.log.info(f"Fetching page for {name} from {href}")
+    context.log.debug(f"Fetching page for {name} from {href}")
     try:
         page = context.fetch_html(href, cache_days=1)
-    except Exception:
-        context.log.error(f"Exception when fetching {href}")
+    except Exception as err:
+        context.log.info(f"Exception when fetching {href}: {err}")
         return
     # Try to find a CV (not always present)
     tabs = page.find(".//ul[@uk-tab]")
     if tabs is None:
-        context.log.info(f"No info tabs found for {name}")
+        context.log.debug(f"No info tabs found for {name}")
         return
     # Find the index of the CV
     for cv_idx, el in enumerate(tabs.iterfind(".//a")):
@@ -78,10 +95,10 @@ def crawl_member_page(context: Context, person: Entity, name: str, href: str):
             context.log.error(f"No text in tab {cv_idx}")
             continue
         if el.text.strip().lower() == "curriculo":
-            context.log.info(f"CV found for {name} in tab {cv_idx}")
+            context.log.debug(f"CV found for {name} in tab {cv_idx}")
             break
     else:
-        context.log.info(f"No CV found for {name}")
+        context.log.debug(f"No CV found for {name}")
         return
     # Now find the list of tabs and get the corresponding one
     for switcher in page.iterfind(".//ul[@class]"):
@@ -91,7 +108,7 @@ def crawl_member_page(context: Context, person: Entity, name: str, href: str):
         if "uk-switcher" in classes.split():
             break
     else:
-        context.log.info("No switcher found for {name}")
+        context.log.debug("No switcher found for {name}")
         return
     # Unfortunately... these are not formatted in any particular way.
     # Do a best-effort extraction of some facts from the plain text
@@ -109,12 +126,12 @@ def crawl_member(context: Context, member_link=ElementOrTree):
     )
     categorisation = categorise(context, position)
     if not categorisation.is_pep:
-        context.log.info(f"Member {member_name} is not PEP")
+        context.log.debug(f"Member {member_name} is not PEP")
         return
 
     person = context.make("Person")
     person.id = context.make_id(member_name)
-    context.log.info(f"Unique ID {person.id}")
+    context.log.debug(f"Unique ID {person.id}")
     h.apply_name(person, full=member_name, lang="esp")
 
     member_href = member_link.get("href")
@@ -122,13 +139,14 @@ def crawl_member(context: Context, member_link=ElementOrTree):
         context.log.error(f"No link found for {member_name}")
     else:
         crawl_member_page(context, person, member_name, member_href)
+    person.add("sourceUrl", member_href)
 
-    context.emit(person, target=True)
-    context.emit(position)
     occupancy = h.make_occupancy(
         context, person, position, True, categorisation=categorisation
     )
     if occupancy is not None:
+        context.emit(person, target=True)
+        context.emit(position)
         context.emit(occupancy)
 
 
@@ -150,16 +168,14 @@ def crawl_members(context: Context, page: ElementOrTree):
 
 def crawl_member_list(context: Context) -> Iterator[ElementOrTree]:
     """Iterate through pages in member list from the website."""
-    assert context.dataset.data is not None
-    assert context.dataset.data.url is not None
-    context.log.info(f"Fetching front page from {context.dataset.data.url}")
+    context.log.debug(f"Fetching front page from {context.data_url}")
     page_number = 1
-    page: HtmlElement = context.fetch_html(context.dataset.data.url, cache_days=1)
+    page: HtmlElement = context.fetch_html(context.data_url, cache_days=1)
     yield page
     while True:
         next_links = page.find_rel_links("next")
         if not next_links:
-            context.log.info('No rel="next" link found, stopping')
+            context.log.debug('No rel="next" link found, stopping')
             break
         # Make sure there's a link to the next page
         for link in next_links:
@@ -177,7 +193,7 @@ def crawl_member_list(context: Context) -> Iterator[ElementOrTree]:
         else:
             context.log.error(f"Link to {page_number + 1} not found")
         page_number = next_page
-        context.log.info(f"Fetching page {page_number} from {href}")
+        context.log.debug(f"Fetching page {page_number} from {href}")
         page = context.fetch_html(href, cache_days=1)
         yield page
 
