4f82dd1c7531b9667b6ab097496fbfe632d86190
Bruno Görresen Mello <bruno@aetos.tech>
2024-02-06 09:20:09 -0300

    Adding clean name and birth place and date

--- datasets/mx/governors/crawler.py
+++ datasets/mx/governors/crawler.py
@@ -3,11 +3,42 @@ import re
 from zavod import Context, helpers as h
 from zavod.logic.pep import categorise
 
-def crawl_item(input_html, context: Context):
+def extract_birth_place_and_date(link_governor_page, context: Context):
+    """
+    The birth place and date can be found in the page of each governor
+    in a ul tag. It is not always in the same position in the list,
+    so we will iterate over all elements and find the one that has
+    the string "Lugar y fecha de nacimiento:". If it is not found,
+    we raise a warning and return None.
+
+    :param link_governor_page: Link to a page of a specific governor
+    :param context: The context object.
+    """
+
+    response = context.fetch_html(link_governor_page)
+
+    path_to_information = '//*[@id="fContenido"]/div/div/div[2]/div/div/div/div[2]/div/div[1]/ul/li'
+
+    for element in response.xpath(path_to_information):
+        text = ' '.join(element.itertext())
+        if 'Lugar y fecha de nacimiento:' in text:
+            pattern = r'Lugar y fecha de nacimiento: ([^.]+\.) (\d{1,2}/\d{1,2}/\d{4})\.'
+            match = re.search(pattern, text)
+            if match:
+                birth_place = match.group(1)
+                birth_date = match.group(2)
+                return birth_place, birth_date
+
+    context.log.warning("Failed to identify birth place and date, link: {}".format(link_governor_page))
+    return None, None
+
+
+def crawl_item(input_html, link_governor_page, context: Context):
     """
     Creates an entity, a position and a occupancy from the raw HTMLElement.
 
     :param input_html: The div representing that governor
+    :param link_governor_page: Link to that governor page
     :param context: The context object.
     """
 
@@ -27,7 +58,24 @@ def crawl_item(input_html, context: Context):
 
     person = context.make("Person")
     person.id = context.make_id(name)
+
+    # The names in the card always have a title as the first word, for example,
+    # Ing. Carlos Lozano de la Torre, where Ing. stands for Engineer
+    # We will add two name propreties, one with the "full" name
+    # and other with the clean name. The clean name
+    # can be retrived from the url to that governors page
     person.add("name", name)
+    clean_name = (link_governor_page
+                  .replace("https://www.conago.org.mx/miembros/detalle/", "")
+                  .replace("-", " ")
+                  .title())
+    person.add("name", clean_name)
+
+    birth_place, birth_date = extract_birth_place_and_date(link_governor_page, context)
+
+    if birth_place is not None:
+        person.add("birthPlace", birth_place)
+        person.add("birthDate", h.parse_date(birth_date, formats=["%d/%m/%Y"])[0])
 
     name_of_position = "Governor of " + state.title()
     position = h.make_position(context, name_of_position, country="mx")
@@ -43,9 +91,14 @@ def crawl_item(input_html, context: Context):
             categorisation=categorisation,
         )
 
-    context.emit(person, target=True)
-    context.emit(position)
-    context.emit(occupancy)
+
+    # If occupancy is None, then it is not a PEP
+    if occupancy is not None:
+        context.emit(person, target=True)
+        context.emit(position)
+        context.emit(occupancy)
+
+
     
 
 def crawl(context: Context):
@@ -66,5 +119,6 @@ def crawl(context: Context):
         return
 
     path_to_cards = '//*[@class="containerMixitup"]/div/div'
-    for item in response.xpath(path_to_cards):
-        crawl_item(item, context)
+    path_to_links = '//*[@class="containerMixitup"]/div/div/div/div/div/div[1]/a'
+    for (item, a_tag) in zip(response.xpath(path_to_cards), response.xpath(path_to_links)):
+        crawl_item(item, a_tag.get('href'), context)
