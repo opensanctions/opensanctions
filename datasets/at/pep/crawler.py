08257581f7b28e755e0cb0285d4792584deb3af5
Bruno Görresen Mello <bruno@aetos.tech>
2024-02-27 18:00:33 +0100

    Using find method and fixing date function

--- datasets/at/pep/crawler.py
+++ datasets/at/pep/crawler.py
@@ -1,18 +1,24 @@
-from dateutil.parser import parse
+from datetime import datetime
 
 from zavod import Context, helpers as h
 from zavod.logic.pep import categorise
 
+import locale
+locale.setlocale(locale.LC_TIME, 'de_DE')
+
 
 def parse_german_date(date_string):
-    return parse(date_string, dayfirst=True, locale='de')
+    try:
+        return datetime.strptime(date_string, "%d. %B %Y")
+    except ValueError:
+        return None
 
 def crawl_item(url_info_page: str, context: Context):
 
     info_page = context.fetch_html(url_info_page)
 
-    first_name = info_page.xpath("//span[@itemprop='http://schema.org/givenName']/text()")[0]
-    last_name = info_page.xpath("//span[@itemprop='http://schema.org/familyName']/text()")[0]
+    first_name = info_page.findtext(".//span[@itemprop='http://schema.org/givenName']")
+    last_name = info_page.findtext(".//span[@itemprop='http://schema.org/familyName']")
 
     person = context.make("Person")
     person.id = context.make_id(first_name, last_name)
@@ -22,19 +28,20 @@ def crawl_item(url_info_page: str, context: Context):
 
     person.add("sourceUrl", url_info_page)
 
-    birth_date_in_german = info_page.find("//span[@itemprop='birthDate']/text()")
-    if birth_date_in_german:
+    birth_date_in_german = info_page.findtext(".//span[@itemprop='birthDate']")
+
+    if birth_date_in_german and parse_german_date(birth_date_in_german):
         person.add("birthDate", parse_german_date(birth_date_in_german))
 
-    try:
-        person.add("email", info_page.xpath("//a[@itemprop='http://schema.org/email']/text()")[0])
-    except:
-        pass
+    email = info_page.findtext(".//a[@itemprop='http://schema.org/email']")
 
-    try:
-        person.add("phone", info_page.xpath("//a[@itemprop='http://schema.org/telephone']/text()")[0]).replace(' ', '')
-    except:
-        pass
+    if email:
+        person.add("email", email)
+
+    phone = info_page.findtext(".//a[@itemprop='http://schema.org/telephone']")
+    
+    if phone:
+        person.add("phone", phone.replace(' ', ''))
 
     # We first find the tag with the name, then go to the div that containts it, and finally find the p tag that is in bold
     position_name = info_page.xpath("//span[@itemprop='http://schema.org/givenName']/../../p[contains(@class, 'bold')]/text()")[0]
