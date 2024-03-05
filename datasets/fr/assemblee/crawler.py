713dc4e403c0923976fb86cd24243ee520157f74
David Huggins-Daines <dhd@ecolingui.ca>
2024-03-05 14:04:45 -0500

    feat: add collaborateurs as Associate

--- datasets/fr/assemblee/crawler.py
+++ datasets/fr/assemblee/crawler.py
@@ -4,14 +4,42 @@ Crawler for members of the French National Assembly.
 
 import json
 import re
-from typing import Dict, Any
+from typing import Any, Dict, Iterator, List
 from zipfile import ZipFile
 
-from zavod import Context
+from zavod import Context, Entity
 from zavod import helpers as h
 from zavod.logic.pep import categorise
 
 REGEX_PATH = re.compile(r"^json/acteur/")
+
+
+def crawl_collabos(
+    context, person: Entity, uid: str, mandat: Dict[str, Any]
+) -> Iterator[Entity]:
+    """Add staff (parliamentry collaborators) as associates."""
+    collabos = mandat.pop("collaborateurs")["collaborateur"]
+    if not isinstance(collabos, list):
+        collabos = [collabos]
+    for i, c in enumerate(collabos):
+        collabo = context.make("Person")
+        collabo.id = context.make_slug(uid, f"collabo{i}")
+        h.apply_name(
+            collabo,
+            prefix=c.pop("qualite"),
+            first_name=c.pop("prenom"),
+            last_name=c.pop("nom"),
+        )
+        collabo.set("topics", "role.rca")
+        context.audit_data(c, ["dateDebut", "dateFin"])
+        yield collabo
+
+        link = context.make("Associate")
+        link.id = context.make_slug(uid, f"associate-collabo{i}")
+        link.set("person", person)
+        link.set("associate", collabo)
+        link.set("relationship", "collaborateur")
+        yield link
 
 
 def crawl_acteur(context, data: Dict[str, Any]):
@@ -22,7 +50,8 @@ def crawl_acteur(context, data: Dict[str, Any]):
     person = context.make("Person")
     # Member UID
     uid = acteur.pop("uid")
-    person.id = context.make_slug(uid.pop("#text"))
+    uid_text = uid.pop("#text")
+    person.id = context.make_slug(uid_text)
     context.audit_data(uid, ["@xmlns:xsi", "@xsi:type"])
 
     # Name and DOB
@@ -65,9 +94,12 @@ def crawl_acteur(context, data: Dict[str, Any]):
     # Occupancy data (there are many mandats but we only care about AN)
     mandats = acteur.pop("mandats")["mandat"]
     start_date = None
+    entities: List[Entity] = []
     for mandat in mandats:
         if mandat.pop("typeOrgane") == "ASSEMBLEE":
             start_date = mandat.pop("dateDebut")
+            # Parliamentary collaborators (i.e. staff)
+            entities.extend(crawl_collabos(context, acteur, uid_text, mandat))
             break
     occupancy = h.make_occupancy(
         context,
@@ -81,6 +113,8 @@ def crawl_acteur(context, data: Dict[str, Any]):
         context.emit(person, target=True)
         context.emit(position)
         context.emit(occupancy)
+        for entity in entities:
+            context.emit(entity)
 
 
 def crawl(context: Context):
