43104851f0ffe40d57724c93749df53cafa23fc9
David Huggins-Daines <dhd@ecolingui.ca>
2024-03-06 09:32:38 -0500

    fix: add past senators and separate occupancies per mandate

--- datasets/fr/senat/crawler.py
+++ datasets/fr/senat/crawler.py
@@ -3,15 +3,14 @@ Crawler for active French senators.
 """
 
 import csv
-from typing import Dict, Tuple, Iterator
+from typing import Dict, Iterator, List, Tuple, Optional
 from urllib.parse import urljoin
 
-from zavod import Context
+from zavod import Context, Entity
 from zavod import helpers as h
 from zavod.logic.pep import categorise
 
 UNUSED_FIELDS = [
-    "Qualité",
     "Groupe politique",
     "Type d'app au grp politique",
     "Commission permanente",
@@ -21,26 +20,54 @@ UNUSED_FIELDS = [
     "Catégorie professionnelle",
     "Description de la profession",
 ]
+UNUSED_MANDATE_FIELDS = [
+    "Qualité",
+    "Nom usuel",
+    "Prénom usuel",
+    "État Sénateur",
+    "Identifiant mandat",
+    "Date d'élection",
+    "Année de début de mandat","Motif début de mandat",
+    "Année de fin de mandat",
+    "Motif fin de mandat",
+    "Commentaire",
+]
 
 
-def crawl_row(context: Context, row: Dict[str, str], start_dates: Dict[str, str]):
+def crawl_row(
+    context: Context,
+    row: Dict[str, str],
+    mandate_dict: Dict[str, List[Tuple[Optional[str], Optional[str]]]],
+):
     """Process one row of the CSV data"""
-    # Skip not currently serving senators
-    status = row.pop("État")
-    if status != "ACTIF":
-        return
     # Unique senator ID (note: *not* a national ID number)
     senid = row.pop("Matricule")
+    status = row.pop("État")
+    prefix = row.pop("Qualité")
     family_name = row.pop("Nom usuel")
     given_name = row.pop("Prénom usuel")
     birth_date = row.pop("Date naissance")
+    death_date = row.pop("Date de décès")
     email = row.pop("Courrier électronique")
-    context.log.debug(f"Active senator {senid}: {given_name} {family_name}")
-    # Hopefully, death date is not relevant for sitting senators
-    _ = row.pop("Date de décès")
+    context.log.debug(
+        f"Senator {senid}: {prefix} {given_name} {family_name} ({status})"
+    )
     # Ignore various other fields
     context.audit_data(row, UNUSED_FIELDS)
     # Make a PEP entity
+    person = context.make("Person")
+    person.id = context.make_slug(senid)
+    context.log.debug(f"Unique ID {person.id}")
+    h.apply_name(
+        person, prefix=prefix, given_name=given_name, last_name=family_name, lang="fra"
+    )
+    if email and email != "Non public":
+        person.add("email", email)
+    if birth_date:
+        # Luckily they are all consistently formatted!
+        person.add("birthDate", birth_date)
+    if death_date:
+        person.add("deathDate", death_date)
     position = h.make_position(
         context,
         name="Senator of the French Fifth Republic",
@@ -50,51 +77,50 @@ def crawl_row(context: Context, row: Dict[str, str], start_dates: Dict[str, str]
     # is_pep=True because we expect all senators to be PEPs
     categorisation = categorise(context, position, is_pep=True)
     if not categorisation.is_pep:
-        # Unlikely that this would ever happen!
         context.log.warning(f"Senator {given_name} {family_name} is not PEP")
         return
-    person = context.make("Person")
-    person.id = context.make_slug(senid)
-    context.log.debug(f"Unique ID {person.id}")
-    h.apply_name(person, given_name=given_name, last_name=family_name, lang="fra")
-    if email and email != "Non public":
-        person.add("email", email)
-    if birth_date:
-        # Luckily they are all consistently formatted!
-        person.add("birthDate", birth_date)
-    # NOTE: Start dates seem to be missing for senators elected in 2023
-    start_date = start_dates.get(senid)
-    if start_date is not None:
-        context.log.debug(f"Start date for {senid}: {start_date}")
-    occupancy = h.make_occupancy(
-        context,
-        person,
-        position,
-        no_end_implies_current=True,
-        start_date=start_date,
-        categorisation=categorisation,
-    )
-    if occupancy is not None:
+    # Start dates seem to be missing for senators elected in 2023, so
+    # we will need to add an occupancy for current senators even if
+    # they don't have any in the mandates
+    mandates = mandate_dict.get(senid, [])
+    if status == "ACTIF":
+        mandates.append((None, None))
+    entities: List[Entity] = []
+    for start_date, end_date in mandates:
+        context.log.debug(f"Mandate for {senid}: {start_date} - {end_date}")
+        occupancy = h.make_occupancy(
+            context,
+            person,
+            position,
+            no_end_implies_current=True,
+            start_date=start_date,
+            end_date=end_date,
+            categorisation=categorisation,
+        )
+        if occupancy is not None:
+            entities.append(occupancy)
+    if entities:
+        context.log.debug(f"Emitting PEP entities for {senid}")
         context.emit(person, target=True)
         context.emit(position)
-        context.emit(occupancy)
+        for entity in entities:
+            context.emit(entity)
 
 
 def crawl_mandates(
     context: Context, reader: Iterator[Dict[str, str]]
-) -> Iterator[Tuple[str, str]]:
-    """Get start dates for senators by ID."""
+) -> Iterator[Tuple[Optional[str], Optional[str]]]:
+    """Get mandates for senators by ID."""
     for row in reader:
-        # Skip former senators
-        if row.pop("État Sénateur") != "ACTIF":
-            continue
-        # Skip reelection events
-        if row.pop("Motif début de mandat") != "Election":
-            continue
         senid = row.pop("Matricule")
-        dt = row.pop("Date de début de mandat")
-        start_date = dt.split(maxsplit=1)[0]
-        yield senid, start_date
+        start_date = row.pop("Date de début de mandat") or None
+        if start_date is not None:
+            start_date = start_date.split(maxsplit=1)[0]
+        end_date = row.pop("Date de fin de mandat") or None
+        if end_date is not None:
+            end_date = end_date.split(maxsplit=1)[0]
+        context.audit_data(row, UNUSED_MANDATE_FIELDS)
+        yield senid, (start_date, end_date)
 
 
 def crawl(context: Context):
@@ -104,14 +130,15 @@ def crawl(context: Context):
     path = context.fetch_resource(
         "mandates.csv", urljoin(context.data_url, "/data/senateurs/ODSEN_ELUSEN.csv")
     )
-    start_dates = {}
+    mandates = {}
     with open(path, "rt", encoding="cp1252") as infh:
         decomment = (spam for spam in infh if spam[0] != "%")
-        start_dates.update(crawl_mandates(context, csv.DictReader(decomment)))
+        for senid, dates in crawl_mandates(context, csv.DictReader(decomment)):
+            mandates.setdefault(senid, []).append(dates)
 
     # Do main CSV
     path = context.fetch_resource("senators.csv", context.data_url)
     with open(path, "rt", encoding="cp1252") as infh:
         decomment = (spam for spam in infh if spam[0] != "%")
         for row in csv.DictReader(decomment):
-            crawl_row(context, row, start_dates)
+            crawl_row(context, row, mandates)
