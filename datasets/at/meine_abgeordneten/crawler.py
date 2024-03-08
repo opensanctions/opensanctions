4a8ffd85cc0d8652965e635c8f12b777fac802d6
JD Bothma <jbothma@gmail.com>
2024-03-08 14:27:51 +0000

    Don't auto-pep positions - we can't clear out party names suffuciently yet

--- datasets/at/meine_abgeordneten/crawler.py
+++ datasets/at/meine_abgeordneten/crawler.py
@@ -127,7 +127,7 @@ def crawl_mandate(context, url, person, el):
         return
 
     position = h.make_position(context, position_name, country="at")
-    categorisation = categorise(context, position, is_pep=True)
+    categorisation = categorise(context, position, is_pep=None)
 
     occupancy = h.make_occupancy(
         context,
