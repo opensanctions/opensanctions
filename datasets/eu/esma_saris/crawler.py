1651396973f0b90dc0c3bcd7150bf3b46b30401f
SimonThordal <simonthordal@gmail.com>
2024-03-08 13:39:54 +0100

    add option for debugging through vscode

--- datasets/eu/esma_saris/crawler.py
+++ datasets/eu/esma_saris/crawler.py
@@ -2,6 +2,7 @@ from zavod import Context
 
 
 def crawl(context: Context) -> None:
+    print("Waiting for debugger attach")
     context.fetch_html(context.dataset.url)
     source_file = context.fetch_resource(
         "source.csv", context.dataset.data.url, cookies={"criteria": "[]"}
