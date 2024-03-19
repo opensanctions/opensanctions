3d25706e17bac7a9209fc032639d49783d9df24b
Bruno Görresen Mello <bruno@aetos.tech>
2024-03-19 10:11:24 -0300

    applying changes from code review

--- datasets/us/finra_enforcement_actions/crawler.py
+++ datasets/us/finra_enforcement_actions/crawler.py
@@ -7,6 +7,7 @@ from zavod import Context, helpers as h
 
 BASE_URL = "https://www.finra.org"
 
+
 def parse_table(
     table: _Element,
 ) -> Generator[Dict[str, Tuple[str, Optional[str]]], None, None]:
@@ -29,20 +30,21 @@ def parse_table(
         assert len(headers) == len(cells)
         yield {hdr: c for hdr, c in zip(headers, cells)}, row
 
-def crawl_item(input_dict: dict, html_element: _Element, context: Context):
 
-    schema = 'LegalEntity'
+def crawl_item(input_dict: dict, html_element: _Element, context: Context):
+    schema = "LegalEntity"
 
     # We try to find if it's a person or company using the icon class
-    if 'user' in html_element.find(".//i").get('class'):
-        schema = 'Person'
-        
-    if 'building' in html_element.find(".//i").get('class'):
-        schema = 'Company'
+    if "user" in html_element.find(".//i").get("class"):
+        schema = "Person"
+
+    if "building" in html_element.find(".//i").get("class"):
+        schema = "Company"
 
     name = input_dict.pop("firms-individuals")[0]
     case_summary = input_dict.pop("case-summary")[0]
-    source_url = BASE_URL + input_dict.pop("case-id")[0]
+    case_id = input_dict.pop("case-id")[0]
+    source_url = BASE_URL + case_id
     date = input_dict.pop("action-date-sort-ascending")[0]
     document_type = input_dict.pop("document-type")[0]
 
@@ -50,13 +52,14 @@ def crawl_item(input_dict: dict, html_element: _Element, context: Context):
     entity.id = context.make_slug(name)
 
     entity.add("name", name)
+    entity.add("topics", "crime.fin")
+    entity.add("country", "us")
 
-    sanction = h.make_sanction(context, entity)
+    sanction = h.make_sanction(context, entity, key=case_id)
 
     sanction.add("description", case_summary)
     sanction.add("date", h.parse_date(date, formats=["%m/%d/%Y"]))
-    sanction.add("publisher", "Document type: "+document_type)
-    sanction.add("publisherUrl", source_url)
+    sanction.add("sourceUrl", source_url)
 
     context.emit(entity, target=True)
     context.emit(sanction)
@@ -65,14 +68,14 @@ def crawl_item(input_dict: dict, html_element: _Element, context: Context):
 
 
 def crawl(context: Context):
-
     # Each page only displays 15 rows at a time, so we need to loop until we find an empty table
 
     base_url = context.data_url
 
-    page_num = 0
+    page_num = 36
 
     while True:
+        context.log.info(page_num)
         url = base_url + "?page=" + str(page_num)
         response = context.fetch_html(url)
         table = response.find(".//table")
@@ -81,7 +84,7 @@ def crawl(context: Context):
             break
 
         for item, html_element in parse_table(table):
+            context.log.info("ok")
             crawl_item(item, html_element, context)
 
         page_num += 1
-
