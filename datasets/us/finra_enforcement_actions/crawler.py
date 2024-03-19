895dfbd8789c284eeb2b149724827ee67423702d
Bruno Görresen Mello <bruno@aetos.tech>
2024-03-19 15:14:21 -0300

    applying changes from code review

--- datasets/us/finra_enforcement_actions/crawler.py
+++ datasets/us/finra_enforcement_actions/crawler.py
@@ -5,8 +5,6 @@ from lxml.etree import _Element
 from normality import slugify, collapse_spaces
 from zavod import Context, helpers as h
 
-BASE_URL = "https://www.finra.org"
-
 
 def parse_table(
     table: _Element,
@@ -35,18 +33,21 @@ def crawl_item(input_dict: dict, html_element: _Element, context: Context):
     schema = "LegalEntity"
 
     # We try to find if it's a person or company using the icon class
-    if "user" in html_element.find(".//i").get("class"):
+    if html_element.find(".//i") is None:
+        # If we don't have the name, we disconsider this line
+        # Otherwise, we continue the code using the LegalEntity schema
+        if input_dict["firms-individuals"][0] == "":
+            return
+    elif "user" in html_element.find(".//i").get("class"):
         schema = "Person"
 
-    if "building" in html_element.find(".//i").get("class"):
+    elif "building" in html_element.find(".//i").get("class"):
         schema = "Company"
 
     name = input_dict.pop("firms-individuals")[0]
     case_summary = input_dict.pop("case-summary")[0]
-    case_id = input_dict.pop("case-id")[0]
-    source_url = BASE_URL + case_id
+    case_id, source_url = input_dict.pop("case-id")
     date = input_dict.pop("action-date-sort-ascending")[0]
-    document_type = input_dict.pop("document-type")[0]
 
     entity = context.make(schema)
     entity.id = context.make_slug(name)
@@ -72,19 +73,18 @@ def crawl(context: Context):
 
     base_url = context.data_url
 
-    page_num = 36
+    page_num = 0
 
     while True:
-        context.log.info(page_num)
         url = base_url + "?page=" + str(page_num)
         response = context.fetch_html(url)
+        response.make_links_absolute(url)
         table = response.find(".//table")
 
         if table is None:
             break
 
         for item, html_element in parse_table(table):
-            context.log.info("ok")
             crawl_item(item, html_element, context)
 
         page_num += 1
