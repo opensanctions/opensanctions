3e06f9d3ab6660d3a3bb5fdfe92a1db180f0dce9
JD Bothma <jbothma@gmail.com>
2024-03-21 13:31:02 +0000

    Check topicless targets tittelatingly using trivia

--- zavod/zavod/tests/test_verify.py
+++ zavod/zavod/tests/test_verify.py
@@ -2,7 +2,7 @@ from structlog.testing import capture_logs
 
 from zavod.context import Context
 from zavod.store import get_store
-from zavod.verify import check_dangling_references, check_self_references
+from zavod.verify import check_dangling_references, check_self_references, check_topicless_target
 from zavod.archive import clear_data_path
 from zavod.crawl import crawl_dataset
 
@@ -24,10 +24,10 @@ def test_dangling_references(testdataset3) -> None:
 
     logs = [f"{entry['log_level']}: {entry['event']}" for entry in cap_logs]
     assert (
-        "error: td3-child-of-nonexistent-co property parent references missing id td3-nonexistent-co."
+        "error: td3-child-of-nonexistent-co property parent references missing id td3-nonexistent-co"
     ) in logs, logs
     assert (
-        "error: td3-asset-of-nonexistent-co-ownership-nonexistent-co property owner references missing id td3-nonexistent-co."
+        "error: td3-asset-of-nonexistent-co-ownership-nonexistent-co property owner references missing id td3-nonexistent-co"
     ) in logs, logs
     assert len(logs) == 2, logs
 
@@ -46,11 +46,28 @@ def test_self_references(testdataset3) -> None:
 
     logs = [f"{entry['log_level']}: {entry['event']}" for entry in cap_logs]
     assert (
-        "error: td3-owner-of-self-co references itself via ownershipOwner"
-        " -> td3-owner-of-self-co-ownership-owner-of-self-co -> asset."
+        "warning: td3-owner-of-self-co references itself via ownershipOwner"
+        " -> td3-owner-of-self-co-ownership-owner-of-self-co -> asset"
     ) in logs, logs
     assert (
-        "error: td3-owner-of-self-co references itself via ownershipAsset"
-        " -> td3-owner-of-self-co-ownership-owner-of-self-co -> owner."
+        "warning: td3-owner-of-self-co references itself via ownershipAsset"
+        " -> td3-owner-of-self-co-ownership-owner-of-self-co -> owner"
     ) in logs, logs
     assert len(logs) == 2, logs
+
+
+def test_topicless_targets(testdataset3) -> None:
+    context = Context(testdataset3)
+    store = get_store(testdataset3)
+    view = store.view(testdataset3)
+
+    with capture_logs() as cap_logs:
+        for entity in view.entities():
+            check_topicless_target(context, view, entity)
+
+    context.close()
+    store.close()
+
+    logs = [f"{entry['log_level']}: {entry['event']}" for entry in cap_logs]
+    assert "warning: td3-target-no-topic-co is a target but has no topics" in logs, logs
+    assert len(logs) == 1, logs
