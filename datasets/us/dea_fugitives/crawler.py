71f777a09270da8dd3e531652c33fbe9badb8996
Bruno Görresen Mello <bruno@aetos.tech>
2024-03-27 08:42:27 -0300

    applying black formatter

--- datasets/us/dea_fugitives/crawler.py
+++ datasets/us/dea_fugitives/crawler.py
@@ -1,5 +1,6 @@
 from zavod import Context, helpers as h
 
+
 def parse_table(table):
     """This function is used to parse the table of Labels and Descriptions
     about the fugitive and return as a dict.
@@ -13,16 +14,15 @@ def parse_table(table):
         label = row.findtext(".//td[1]")
         description = row.findtext(".//td[2]")
         info_dict[label] = description
-    
+
     return info_dict
 
 
 def crawl_item(fugitive_url: str, context: Context):
-
     response = context.fetch_html(fugitive_url)
 
     name = response.findtext('.//h2[@class="fugitive__title"]')
-    info_dict = parse_table(response.find('.//table'))
+    info_dict = parse_table(response.find(".//table"))
 
     entity = context.make("Person")
     entity.id = context.make_id(name)
@@ -36,8 +36,13 @@ def crawl_item(fugitive_url: str, context: Context):
     if "Year of Birth" in info_dict:
         entity.add("birthDate", info_dict["Year of Birth"])
 
-
-    sanction_description = ''.join([d.text_content() for d in response.findall('.//div[@class="meta"]') if "Wanted for the following" in d.text_content()])
+    sanction_description = "".join(
+        [
+            d.text_content()
+            for d in response.findall('.//div[@class="meta"]')
+            if "Wanted for the following" in d.text_content()
+        ]
+    )
 
     if "NCIC #" in info_dict:
         sanction = h.make_sanction(context, entity, key=info_dict["NCIC #"])
@@ -64,6 +69,6 @@ def crawl(context: Context):
         response.make_links_absolute(url)
 
         for item in response.findall('.//div[@class="teaser "]/div/h3/a'):
-            crawl_item(item.get('href'), context)
+            crawl_item(item.get("href"), context)
 
         page_num += 1
