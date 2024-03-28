45a55311b65e7dbab044ba9044743a9d0b3146e6
JD Bothma <jbothma@gmail.com>
2024-03-28 09:21:03 +0000

    Tidy

--- datasets/lt/fiu_freezes/crawler.py
+++ datasets/lt/fiu_freezes/crawler.py
@@ -29,9 +29,11 @@ def crawl(context: Context):
             continue
         data = dict(zip(headers, cells))
         nr = data.pop("nr")
-        company_name = data.pop(
-            "fizinio_ar_juridinio_asmens_kurio_turtas_isaldytas_pavadinimas"
-        ).split("\n")[0].strip()
+        company_name = (
+            data.pop("fizinio_ar_juridinio_asmens_kurio_turtas_isaldytas_pavadinimas")
+            .split("\n")[0]
+            .strip()
+        )
         reg_nr = data.pop("imones_kodas")
         measures = data.pop("isaldyto_turto_rusis").split("\n")
         legal_grounds = data.pop("reglamentas_kurio_pagrindu_taikomas_turto_isaldymas")
