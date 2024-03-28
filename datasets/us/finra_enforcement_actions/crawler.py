a93e133efb856a3380f7967e58facfbf8ef09b26
JD Bothma <jbothma@gmail.com>
2024-03-28 10:08:46 +0000

    WIP

--- datasets/us/finra_enforcement_actions/crawler.py
+++ datasets/us/finra_enforcement_actions/crawler.py
@@ -1,10 +1,17 @@
-from typing import Generator, Dict, Tuple, Optional
+from typing import Generator, Dict, List, Tuple, Optional
 from lxml.etree import _Element
 from normality import slugify, collapse_spaces
 from zavod import Context, helpers as h
+import re
+
+REGEX_AND = re.compile(r"(\band\b|&)", re.I)
+REGEX_JUST_A_NAME = re.compile(r"^[a-z]+, [a-z]+$", re.I)
 
 
-def clean_name(name: str):
+def clean_names(name: str) -> List[str]:
+    if not name:
+        return []
+    
     matches = {
         ", LLC": " LLC",
         ", L.L.C": " L.L.C",
@@ -16,6 +23,12 @@ def clean_name(name: str):
         ", LP": " LP",
         ", Sr.": " Sr.",
         ", SR.": " SR.",
+        ", II": " II",
+        ", III": " III",
+        ", IV": " IV",
+        ", S.A.": " S.A.",
+        ", LTD": " LTD",
+        ", USA INC": " USA INC",
     }
 
     # We first remove the comma using a pattern match
@@ -24,9 +37,15 @@ def clean_name(name: str):
 
     # If the string ends in a comma, the last comma is unnecessary (e.g. Goldman Sachs & Co. LLC,)
     if name.endswith(","):
-        return name[:-1]
+        name = name[:-1]
+    
+    if not REGEX_AND.search(name) and not REGEX_JUST_A_NAME.match(name):
+        parts = [n.strip() for n in name.split(",")]
+        if len(parts) > 1:
+            print("      ".join(parts))
+        return parts
 
-    return name
+    return [name]
 
 
 def parse_table(
@@ -42,6 +61,10 @@ def parse_table(
 
         cells = []
         for el in row.findall("./td"):
+            for span in el.findall(".//span"):
+                # add newline to split spans later if we want
+                span.tail = "\n" + span.tail if span.tail else "\n"
+
             a = el.find(".//a")
             if a is None:
                 cells.append((collapse_spaces(el.text_content()), None))
@@ -54,30 +77,27 @@ def parse_table(
 
 def crawl_item(input_dict: dict, context: Context):
     schema = "LegalEntity"
-
-    name = clean_name(input_dict.pop("firms-individuals")[0])
+    names = []
+    for dirty_name in input_dict.pop("firms-individuals")[0].split("\n"):
+        names.extend(clean_names(dirty_name))
     case_summary = input_dict.pop("case-summary")[0]
     case_id, source_url = input_dict.pop("case-id")
     date = input_dict.pop("action-date-sort-ascending")[0]
 
-    if not name:
-        return
+    for name in names:
+        print("name: %r" % name)
+        entity = context.make(schema)
+        entity.id = context.make_slug(name)
+        entity.add("name", name)
+        entity.add("topics", "crime.fin")
+        entity.add("country", "us")
+        context.emit(entity, target=True)
 
-    entity = context.make(schema)
-    entity.id = context.make_slug(name)
-
-    entity.add("name", name)
-    entity.add("topics", "crime.fin")
-    entity.add("country", "us")
-
-    sanction = h.make_sanction(context, entity, key=case_id)
-
-    sanction.add("description", case_summary)
-    sanction.add("date", h.parse_date(date, formats=["%m/%d/%Y"]))
-    sanction.add("sourceUrl", source_url)
-
-    context.emit(entity, target=True)
-    context.emit(sanction)
+        sanction = h.make_sanction(context, entity, key=case_id)
+        sanction.add("description", case_summary)
+        sanction.add("date", h.parse_date(date, formats=["%m/%d/%Y"]))
+        sanction.add("sourceUrl", source_url)
+        context.emit(sanction)
 
     context.audit_data(input_dict, ignore=["document-type"])
 
