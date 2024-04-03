dad72e207b85947becdff29af4ff57343a6d0ea3
Bruno Görresen Mello <bruno@aetos.tech>
2024-04-03 10:28:46 -0300

    Using new function to split comma names from zavod helpers

--- datasets/us/sec_harmed_investors/crawler.py
+++ datasets/us/sec_harmed_investors/crawler.py
@@ -1,51 +1,14 @@
-import re
-from typing import List
 from lxml.etree import _Element
 
 from zavod import Context, helpers as h
-from normality import collapse_spaces
-
-REGEX_AND = re.compile(r"(\band\b|&|\+)", re.I)
-REGEX_JUST_A_NAME = re.compile(r"^[a-z]+, [a-z]+$", re.I)
-REGEX_CLEAN_SUFFIX = re.compile(
-    r", \b(LLC|L\.L\.C|Inc|Jr|INC|L\.P|LP|Sr|III|II|IV|S\.A|LTD|USA INC|\(?A/K/A|\(?N\.K\.A|\(?N/K/A|\(?F\.K\.A|formerly known as|INCORPORATED)\b",
-    re.I,
-)
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15",
 }
 
 
-def clean_names(context: Context, text: str) -> List[str]:
-    text = collapse_spaces(text)
-    if not text:
-        return []
-
-    text = REGEX_CLEAN_SUFFIX.sub(r" \1", text)
-    # If the string ends in a comma, the last comma is unnecessary (e.g. Goldman Sachs & Co. LLC,)
-    if text.endswith(","):
-        text = text[:-1]
-
-    if not REGEX_AND.search(text) and not REGEX_JUST_A_NAME.match(text):
-        names = [n.strip() for n in text.split(",")]
-        return names
-    else:
-        if ("," in text or " and " in text):
-            res = context.lookup("comma_names", text)
-            if res:
-                return res.names
-            else:
-                context.log.warning(
-                    "Not sure how to split on comma.", text=text.lower()
-                )
-                return [text]
-        else:
-            return [text]
-
-
 def crawl_item(item: _Element, context: Context):
-    names = clean_names(context, item.text_content())
+    names = h.split_comma_names(context, item.text_content())
 
     source_url = item.get("href")
 
