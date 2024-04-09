c0842e4225d24db6320e8447e66e2a7d007eea8d
Bruno Görresen Mello <bruno@aetos.tech>
2024-04-09 16:25:29 -0300

    applying suggestion from code review

--- datasets/th/designated_person/crawler.py
+++ datasets/th/designated_person/crawler.py
@@ -1,5 +1,4 @@
-import re
-from typing import Generator, Dict, Tuple, Optional
+from typing import Dict
 from lxml.etree import _Element
 
 from zavod import Context, helpers as h
@@ -20,50 +19,56 @@ def crawl_item(url: str, context: Context):
 
     en_name = info_dict.pop("Individual/Entity Name (English)").strip()
     th_name = info_dict.pop("Individual/Entity Name (Thailand)").strip()
+    birth_date = info_dict.pop("Date of Birth", None) or None
 
     entity = context.make("Person")
-    entity.id = context.make_id(en_name)
+    entity.id = context.make_slug(en_name, birth_date)
 
     entity.add("name", en_name, lang="en")
     entity.add("name", th_name, lang="th")
     entity.add("topics", "sanction")
 
-    if "Date of Birth" in info_dict:
-        entity.add(
-            "birthDate",
-            h.parse_date(info_dict.pop("Date of Birth"), formats=["%d-%m-%Y"]),
-        )
+    if birth_date:
+        entity.add("birthDate", h.parse_date(birth_date, formats=["%d-%m-%Y"]))
 
-        entity.add("nationality", info_dict.pop("Nationality", None) or None)
+    entity.add("nationality", info_dict.pop("Nationality", None) or None)
 
-    if "Phone Number" in info_dict and info_dict["Phone Number"] != "":
-        entity.add("phone", info_dict.pop("Phone Number"))
+    entity.add("phone", info_dict.pop("Phone Number", None) or None)
 
-    if "E-mail" in info_dict and info_dict["E-mail"] != "":
-        entity.add("email", info_dict.pop("E-mail"))
+    entity.add("email", info_dict.pop("E-mail", None) or None)
 
-    if "National Identification Number" in info_dict:
-        entity.add("idNumber", info_dict.pop("National Identification Number"))
-
-    if "Address No.1" in info_dict:
-        entity.add("address", info_dict.pop("Address No.1"), lang="th")
-
-    if "Address No.2" in info_dict:
-        entity.add("address", info_dict.pop("Address No.2"), lang="th")
-
-    if "Passport Number" in info_dict and info_dict["Passport Number"] != "":
-        entity.add("passportNumber", info_dict.pop("Passport Number"))
-
-    sanction = h.make_sanction(
-        context, entity, key=info_dict.pop("Notification Number")
+    entity.add(
+        "idNumber", info_dict.pop("National Identification Number", None) or None
     )
 
-    sanction.add("sourceUrl", url)
+    entity.add("address", info_dict.pop("Address No.1", None) or None, lang="th")
 
-    context.audit_data(info_dict, ignore=["Status"])
+    entity.add("address", info_dict.pop("Address No.2", None) or None, lang="th")
+
+    entity.add(
+        "program",
+        "Thailand Counter-Terrorism and Proliferation Financing Act B.E. 2559 section 7, Notificaton Number: {}".format(
+            info_dict.pop("Notification Number")
+        ),
+    )
+
+    entity.add("sourceUrl", url)
 
     context.emit(entity, target=True)
-    context.emit(sanction)
+
+    if "Passport Number" in info_dict and info_dict["Passport Number"] != "":
+        passport = h.make_identification(
+            context,
+            entity,
+            info_dict.pop("Passport Number"),
+            doc_type="passport",
+            country="th",
+            passport=True,
+        )
+
+        context.emit(passport)
+
+    context.audit_data(info_dict, ignore=["Status"])
 
 
 def crawl(context: Context):
