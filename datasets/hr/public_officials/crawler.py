3ad32bc2997c649548ad5af5dd34ec1aa2d14a26
Klil E <keden@protonmail.com>
2024-04-10 23:27:17 +0300

    [feat] add occupancy

--- datasets/hr/public_officials/crawler.py
+++ datasets/hr/public_officials/crawler.py
@@ -5,7 +5,7 @@ from typing import Any, Optional, Mapping, Sequence
 from pantomime.types import CSV
 from zavod import Context, Entity
 from zavod import helpers as h
-#from zavod.logic.pep import categorise
+from zavod.logic.pep import categorise
 
 FIELD_NAMES = [
                 'Ime', 
@@ -21,41 +21,57 @@ FIELD_NAMES = [
 ]
 
 def convert_date(date:Optional[str]) -> Optional[str]:
-        return datetime.strptime(date, '%d/%m/%Y') if date else None
+    return datetime.strptime(date, '%d/%m/%Y').strftime('%Y-%m-%d') if date else None
 
-def make_office(context:Context, data:dict) -> Entity:
-    """_summary_
-
-    Args:
-        context (Context) zavod crawler context
-        data (dict) with keys `dužnost`, `Pravna osoba u kojoj obnaša dužnost`,
-        `Datum početka obnašanja dužnosti` and `Datum kraja obnašanja dužnosti'`
-
-    Returns:
-        Entity: position entity
-    """
+def make_legal_entity(context:Context, legal_entity_name:str) -> Optional[Entity]:
     legal_entity:Optional[Entity] = None
-    if legal_entity_name := data.pop("Pravna osoba u kojoj obnaša dužnost"):
+    if legal_entity_name:
         legal_entity = context.make("LegalEntity")
         legal_entity.id = context.make_slug(legal_entity_name)
         legal_entity.add('name', legal_entity_name)
         context.emit(legal_entity)
+    return legal_entity
 
-    position:Optional[Entity] = None
-    position_name = data.pop('dužnost') or 'Unspecified position'
-    position_start = convert_date(data.pop("Datum početka obnašanja dužnosti"))
-    position_end = convert_date(data.pop("Datum kraja obnašanja dužnosti"))
-    position = h.make_position(context,
-                            position_name, 
-                            topics=None,
-                            organization=legal_entity,
-                            country='HR',
-                            inception_date=position_start,
-                            dissolution_date=position_end
-                            )
-    context.emit(position)
+def handle_affiliation_data(context:Context, person:Entity, data:dict) -> None:
+    """Creates, emits Position, Occupancy, and optionally a LegalEntity.
+    Note that the provided data is incomplete and the modeling reflects this:  
+      * A position name with no legal entity name results in a position with no Organization reference
+      * A legal entity name with no position name results in an position with organizational reference
+         named "Unspecified position"
+      * A start and/or end date with no position name or legal entity name results in an unspecified position
+         with no organizational reference. This is done for fidelity to the source. 
+    """
+    legal_entity_name = data.pop("Pravna osoba u kojoj obnaša dužnost")
+    position_name = data.pop('dužnost')
+    start_date = data.pop("Datum početka obnašanja dužnosti")
+    end_date = data.pop("Datum kraja obnašanja dužnosti")
+    if any([legal_entity_name, position_name, start_date, end_date]):
+        legal_entity = make_legal_entity(context, legal_entity_name)
+        position_name = position_name or 'Unspecified position'
+        position = h.make_position(context,
+                                position_name, 
+                                topics=None,
+                                organization=legal_entity,
+                                country='HR',
+                                )
+
+        if position:
+            context.emit(position)
+
+        categorisation = categorise(context, position, is_pep=True)
+        occupancy = h.make_occupancy(
+            context,
+            person,
+            position,
+            no_end_implies_current=True,
+            categorisation=categorisation,
+            propagate_country=True,
+            start_date=convert_date(start_date),
+            end_date=convert_date(end_date),
+        )
+        if occupancy:
+            context.emit(occupancy)
     context.audit_data(data)
-    return position
 
 def make_person(context:Context, first_name:str, last_name:str) -> Entity:
     person = context.make("Person")
@@ -75,6 +91,11 @@ def validate_headers(context:Context, headers:list) -> None:
     if len(headers) > len(FIELD_NAMES):
         context.log.warn(f"Unexpected column headers: {headers[:len(FIELD_NAMES):]}")
 
+def extract_dict_keys_by_prefix(data:dict, key_names:list[str], prefix:str) -> dict[str:Any]:
+    """Pops dict keys in `key_names` if the key starts with `prefix`.
+    Returns a new dict and removes keys from old dict"""
+    return {k.removeprefix(prefix):data.pop(k) for k in key_names if k.startswith(prefix)}
+
 def crawl(context: Context):
     """Fetches the current CSV file and crawls each row, making persons, legal entities, and positions"""
     file_path = context.fetch_resource("daily_csv_release", context.data_url)
@@ -85,14 +106,11 @@ def crawl(context: Context):
         for row in reader:
             person = make_person(context, row.pop('Ime'), row.pop('Prezime'))
 
-            affiliation_1 = {k.removeprefix('Primarna '):row.pop(k) for k in FIELD_NAMES if k.startswith('Primarna')}
-            if position_1 := make_office(context, affiliation_1):
-                person.add('position', position_1)
+            affiliation_1 = extract_dict_keys_by_prefix(row, FIELD_NAMES, 'Primarna ')
+            handle_affiliation_data(context, person, affiliation_1)
 
-            affiliation_2 = {k.removeprefix('Sekundarna '):row.pop(k) for k in FIELD_NAMES if k.startswith('Sekundarna')}
-            if position_2 := make_office(context, affiliation_2):
-                person.add('position', position_2)
+            affiliation_2 = extract_dict_keys_by_prefix(row, FIELD_NAMES, 'Sekundarna ')
+            handle_affiliation_data(context, person, affiliation_2)
 
-            # TODO add occupancies
             context.audit_data(row)
             context.emit(person, target=True)
