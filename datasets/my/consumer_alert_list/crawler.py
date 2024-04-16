454015249fdfc585741a17b00649b33c5c0fdde5
Bruno Görresen Mello <bruno@aetos.tech>
2024-04-16 14:26:18 -0300

    using local HTML and setting warning when data is too old

--- datasets/my/consumer_alert_list/crawler.py
+++ datasets/my/consumer_alert_list/crawler.py
@@ -1,28 +1,10 @@
 from typing import Generator, Dict
+import datetime
 from lxml.etree import _Element
 from lxml import html
-import requests
 import re
 from zavod import Context, helpers as h
 from normality import collapse_spaces
-
-HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15",
-}
-
-COOKIES = {
-    "AWSALBAPP-0": "AAAAAAAAAAARErbtm8bjoqFdM0rg+RA7PxUsebtFiIsR+AZH6gNtNEMMnefAlWYpVhWN+3ADuEn5bG4e7Lgz0QlLwuXXLqVJQrnPOlX1wSCzZDE+ZG/YvuRKjg807MnUW9BrvYIH6v0gUw==",
-    "AWSALBAPP-1": "_remove_",
-    "AWSALBAPP-2": "_remove_",
-    "AWSALBAPP-3": "_remove_",
-    "AWSALBTG": "EXH0gauKd/AG8ICFqKlhWbLgoT6EBi0JPWjHDErAGlCogeZgX/OUDZtQONJQAGYwGMnJ44fyIiHR0u8bXNS4dYMDl0cza/GzwCxzn6VkO9bbl3f0lg2qFkt9+APsFDcLUgmp28Z7xGTJtG7OdtNvOUyVl7esufJy9iZ2slTJRuon3m2gKV4=",
-    "AWSALBTGCORS": "EXH0gauKd/AG8ICFqKlhWbLgoT6EBi0JPWjHDErAGlCogeZgX/OUDZtQONJQAGYwGMnJ44fyIiHR0u8bXNS4dYMDl0cza/GzwCxzn6VkO9bbl3f0lg2qFkt9+APsFDcLUgmp28Z7xGTJtG7OdtNvOUyVl7esufJy9iZ2slTJRuon3m2gKV4=",
-    "LFR_SESSION_STATE_20106": "1712753949135",
-    "GUEST_LANGUAGE_ID": "en_US",
-    "JSESSIONID": "H-cppef7YvTEyA0ul0f1r9yYan_sJSW2WtrKcXa4.liferay1",
-    "aws-waf-token": "2a32b599-9466-4869-94ac-24d8e41d3959:EQoAizBaZNQPAAAA:ZQI0Kf/4O/qihcseS7JYurUhIKBoyEek/fKFdUfe31/FSEcAqRsjzsoXItgcasvngDSGx19DhXRLB5pJ/vvTqu+CnCDz0Vx08/r747qJg8HbFUq8lYKqMjtDC/Kb8qgOO+qPjUFk21jv6S4oonutsJ7A+OmPHBT5dfe76ipra8/48ByMFhwiST6MSzHNCLT6lfdYdo663vqnBRtrQMKJGYoCktGsMDcWRN2CcPcCejp8ENF7",
-    "COOKIE_SUPPORT": "true",
-}
 
 REGEX_URLS = r"(https?://[^\s]+)"
 
@@ -81,9 +63,11 @@ def crawl_item(input_dict: dict, context: Context):
 
 
 def crawl(context: Context):
-    response = requests.get(context.data_url, headers=HEADERS, cookies=COOKIES)
+    if datetime.datetime.now() > datetime.datetime(2024, 9, 16):
+        context.log.warn("Check if there's an update of the data behind the bot check.")
 
-    doc = html.fromstring(response.text)
+    data_path = context.dataset.base_path / "data.html"
+    doc = html.fromstring(data_path.read_text())
 
     for item in parse_table(doc.find(".//table")):
         crawl_item(item, context)
