f313360e4b7e00a00402e02df4f86f5775de73b1
Bruno Görresen Mello <bruno@aetos.tech>
2024-04-23 18:38:52 -0300

    implementing custom name parser

--- datasets/us/frb_enforcement-actions/crawler.py
+++ datasets/us/frb_enforcement-actions/crawler.py
@@ -2,8 +2,136 @@ from io import StringIO
 import csv
 from datetime import datetime
 from urllib.parse import urljoin
+import re
 
 from zavod import Context, helpers as h
+
+US_STATES_NAMES_AND_ACRONYMS = [
+    "alabama",
+    "alaska",
+    "arizona",
+    "arkansas",
+    "california",
+    "colorado",
+    "connecticut",
+    "delaware",
+    "florida",
+    "georgia",
+    "hawaii",
+    "idaho",
+    "illinois",
+    "indiana",
+    "iowa",
+    "kansas",
+    "kentucky",
+    "louisiana",
+    "maine",
+    "maryland",
+    "massachusetts",
+    "michigan",
+    "minnesota",
+    "mississippi",
+    "missouri",
+    "montana",
+    "nebraska",
+    "nevada",
+    "new hampshire",
+    "new jersey",
+    "new mexico",
+    "new york",
+    "north carolina",
+    "north dakota",
+    "ohio",
+    "oklahoma",
+    "oregon",
+    "pennsylvania",
+    "rhode island",
+    "south carolina",
+    "south dakota",
+    "tennessee",
+    "texas",
+    "utah",
+    "vermont",
+    "virginia",
+    "washington",
+    "west virginia",
+    "wisconsin",
+    "wyoming",
+    "al",
+    "ak",
+    "az",
+    "ar",
+    "ca",
+    "co",
+    "ct",
+    "dc",
+    "de",
+    "fl",
+    "ga",
+    "hi",
+    "id",
+    "il",
+    "in",
+    "ia",
+    "ks",
+    "ky",
+    "la",
+    "me",
+    "md",
+    "ma",
+    "mi",
+    "mn",
+    "ms",
+    "mo",
+    "mt",
+    "ne",
+    "nv",
+    "nh",
+    "nj",
+    "nm",
+    "ny",
+    "nc",
+    "nd",
+    "oh",
+    "ok",
+    "or",
+    "pa",
+    "ri",
+    "sc",
+    "sd",
+    "tn",
+    "tx",
+    "ut",
+    "vt",
+    "va",
+    "wa",
+    "wv",
+    "wi",
+    "wy",
+]
+
+ONE_NAME_PATTERN = r"^[^,]+, [^,]+, ([^,]+)$"
+
+
+def is_only_one_name(txt: str) -> bool:
+    """Function to test if a given test is only the name of one bank. The names of the banks
+    are structured as "[name of bank], [city], [state]". We are going to verify if the text matches
+    this pattern and if the final part is indeed a US state, if it is, we can safely assume the text
+    is the name of only one bank.
+
+    Args:
+        txt (str): The text to be tested
+
+    Returns:
+        bool: If the txt is the name of only one bank.
+    """
+
+    matches = re.match(ONE_NAME_PATTERN, txt)
+
+    if matches is not None and matches.group(1).lower() in US_STATES_NAMES_AND_ACRONYMS:
+        return True
+
+    return False
 
 
 def crawl_item(input_dict: dict, context: Context):
@@ -12,7 +140,18 @@ def crawl_item(input_dict: dict, context: Context):
         names = [input_dict.pop("Individual")]
     else:
         schema = "Company"
-        names = h.split_comma_names(context, input_dict.pop("Banking Organization"))
+        banking_organization = input_dict.pop("Banking Organization")
+
+        if is_only_one_name(banking_organization):
+            names = [banking_organization]
+        elif " and " in banking_organization and all([is_only_one_name(name) for name in banking_organization.split(" and ")]):
+            names = banking_organization.split(" and ")
+        else:
+            names = [
+                name
+                for possible_name in banking_organization.split(";")
+                for name in h.split_comma_names(context, possible_name)
+            ]
 
     effective_date = input_dict.pop("Effective Date")
     termination_date = input_dict.pop("Termination Date")
@@ -47,16 +186,13 @@ def crawl_item(input_dict: dict, context: Context):
         # if it doesn't have a termination date, we assume the entity is still in the crime.fin topic
         else:
             entity.add("topics", "crime.fin")
-            
 
         context.emit(entity, target=True)
         context.emit(sanction)
 
     # Individual Affiliation = The bank of the individual
     # Name = the string that appears in the url column
-    context.audit_data(
-        input_dict, ignore=["Individual Affiliation", "Name"]
-    )
+    context.audit_data(input_dict, ignore=["Individual Affiliation", "Name"])
 
 
 def crawl(context: Context):
