789f2df3775eb59773098a302284bce5ab028515
Friedrich Lindenberg <friedrich@pudo.org>
2024-05-20 14:07:20 +0200

    trigger a build

--- zavod/setup.py
+++ zavod/setup.py
@@ -24,6 +24,8 @@ setup(
         "followthemoney == 3.6.*",
         "nomenklatura[leveldb] == 3.11.*",
         "plyvel == 1.5.1",
+        "redis == 5.0.1",
+        "fakeredis == 2.23.2",
         "rigour == 0.5.2",
         "datapatch >= 1.1,< 1.3",
         "fingerprints == 1.2.*",
