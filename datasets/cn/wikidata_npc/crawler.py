18bad0f9f2ca68b8ac442083864f119989cf738b
Bruno Görresen Mello <bruno@aetos.tech>
2024-05-24 17:32:11 -0300

    Improving ID to prevent duplicates

--- datasets/cn/wikidata_npc/crawler.py
+++ datasets/cn/wikidata_npc/crawler.py
@@ -4,20 +4,20 @@ from pantomime.types import CSV
 from zavod import Context, helpers as h
 from zavod.logic.pep import categorise
 
-
 def crawl_item(input_dict: dict, context: Context):
     name = input_dict.pop("姓名")
     ethnicity = input_dict.pop("民族")
     gender = input_dict.pop("性别")
+    birth_date = input_dict.pop("出生日期")
 
     entity = context.make("Person")
-    entity.id = context.make_slug(name, ethnicity, gender)
+    entity.id = context.make_id(name, ethnicity, gender, birth_date)
 
     entity.add("name", name, lang="chi")
     entity.add("gender", gender)
     entity.add("ethnicity", ethnicity, lang="chi")
     entity.add(
-        "birthDate", h.parse_date(input_dict.pop("出生日期"), formats=["%Y年%m月"])
+        "birthDate", h.parse_date(birth_date, formats=["%Y年%m月"])
     )
     entity.add("political", input_dict.pop("政党"))
 
@@ -50,5 +50,6 @@ def crawl(context: Context):
     path = context.fetch_resource("source.csv", context.data_url)
     context.export_resource(path, CSV, title=context.SOURCE_TITLE)
     with open(path, "r") as fh:
-        for item in csv.DictReader(fh):
+        for i, item in enumerate(csv.DictReader(fh)):
+            context.log.info(i)
             crawl_item(item, context)
