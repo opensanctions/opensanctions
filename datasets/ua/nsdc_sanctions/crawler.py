840f0085e4a81b94a2499379ad190086682f3589
Dmytro Chaplynskyi <chaplinsky.dmitry@gmail.com>
2024-05-29 16:22:17 +0300

    Adding better mapping for the attributes of NSDC, mapping issuers, switching to the new endpoints

--- datasets/ua/nsdc_sanctions/crawler.py
+++ datasets/ua/nsdc_sanctions/crawler.py
@@ -1,5 +1,4 @@
 import os
-from itertools import count
 from urllib.parse import urljoin
 from typing import Dict, Any, List, Optional
 
@@ -10,98 +9,6 @@ PASSWORD = os.environ.get("OPENSANCTIONS_NSDC_PASSWORD")
 API_KEY = os.environ.get("OPENSANCTIONS_NSDC_API_KEY")
 CODES = {"DN": "UA-DPR", "LN": "UA-LPR"}
 CACHE_LONG = 7
-
-
-def crawl_item(context: Context, item: Dict[str, Any]) -> None:
-    subject_type = item.pop("subjectType")
-    schema = context.lookup_value("subject_types", subject_type)
-    if schema is None:
-        context.log.warn("Unknown subject type", subject_type=subject_type)
-        return
-
-    entity = context.make(schema)
-    subject_id = item.pop("subjectId")
-    subject_name = item.pop("subjectName")
-    entity.id = context.make_slug(subject_id, subject_name)
-    entity.add("topics", "sanction")
-    entity.add("name", subject_name)
-    entity.add("alias", item.pop("subjectAliases"))
-
-    dob = item.pop("subjectBirthDate", None)
-    if entity.schema.is_a("Person"):
-        entity.add("birthDate", dob)
-    else:
-        entity.add("incorporationDate", dob)
-
-    for citz in item.pop("subjectCitizenships", None) or []:
-        code = citz.pop("code", None)
-        if code is None:
-            context.log.info("Invalid citizenship", data=citz)
-            continue
-
-        code = CODES.get(code, code)
-        if entity.schema.is_a("Person"):
-            entity.add("nationality", code)
-        else:
-            entity.add("jurisdiction", code)
-
-    for ident in item.pop("subjectIdentifiers", None) or []:
-        ident_type = ident.pop("type", None)
-        ident_code = ident.pop("code", None)
-        result = context.lookup("ident_types", ident_type)
-        if result is None:
-            context.log.warning(
-                "Unknown identifier type",
-                type=ident_type,
-                code=ident_code,
-                country=ident.get("country"),
-            )
-            continue
-        if result.schema:
-            entity.add_schema(result.schema)
-        if result.value is not None:
-            entity.add(result.value, ident_code)
-
-    sanction = h.make_sanction(context, entity)
-    sanction.add("listingDate", item.pop("decreeDate"))
-    sanction.add("startDate", item.pop("decreeActDate"))
-    sanction.add("endDate", item.pop("actionEndDate"))
-    sanction.add("sourceUrl", item.pop("decreeActLink"))
-    sanction.add("sourceUrl", item.pop("decreeLink"))
-    sanction.add("authorityId", item.pop("actionId"))
-    sanction.add("program", item.pop("decreeNumber"))
-    item.pop("actionType")
-    item.pop("actionTypeName")
-    sanction.add("status", item.pop("actionStatus"))
-    sanction.add("duration", item.pop("actionTerm"))
-
-    decree = item.pop("targetDecree", None) or {}
-    sanction.add("sourceUrl", decree.pop("link", None))
-    sanction.add("program", decree.pop("number", None))
-
-    item.pop("internationalSanctions", None)
-    context.audit_data(item)
-
-    context.emit(entity, target=True)
-    context.emit(sanction)
-
-
-def crawl_old_system(context: Context) -> None:
-    if PASSWORD is None:
-        context.log.warn("Missing $OPENSANCTIONS_NSDC_PASSWORD!")
-        return
-    for page in count(1):
-        params = {"perPage": 100, "page": page}
-        response = context.fetch_json(
-            "https://sanctions.nsdc.gov.ua/api/sanctions/",
-            auth=("rnbo_user", PASSWORD),
-            params=params,
-            # cache_days=1,
-        )
-        for item in response["data"]:
-            crawl_item(context, item)
-        if page == response["pages"]:
-            break
 
 
 def fetch_data(
@@ -156,6 +63,7 @@ def crawl_common(
         result = context.lookup("attributes", key)
         value = attr.pop("value")
         value = value.replace("(росія)", "")
+
         if result is not None and result.prop is not None:
             entity.add(result.prop, value, lang="ukr")
         elif key in ("КПП",):
@@ -164,13 +72,13 @@ def crawl_common(
             else:
                 entity.add("registrationNumber", value)
         else:
-            # context.log.warn("Unknown attribute", key=key, value=value)
+            context.log.warn("Unknown attribute", key=key, value=value)
             entity.add("notes", f"{key}: {value}", lang="ukr")
 
     for action in fetch_data(
         context, f"/subjects/{subject_id}/actions", cache_days=CACHE_LONG
     ):
-        sanction = h.make_sanction(context, entity, key=action["aid"])
+        sanction = h.make_sanction(context, entity, key=action.pop("aid"))
         sanction.add("status", action.pop("status"))
         decree = action.pop("decree", None) or {}
         sanction.add("sourceUrl", decree.pop("link", None))
@@ -181,7 +89,11 @@ def crawl_common(
         restrictions = action.pop("restrictions", None) or []
         for restr in restrictions:
             sanction.add("provisions", restr["restriction"])
+
+        # Neither reason nor reasons has value nowadays, keeping it just in case
+        sanction.add("reason", action.pop("reasons", None), lang="ukr")
         sanction.add("reason", action.pop("reason", None), lang="ukr")
+        sanction.add("authority", action.pop("issuers", None), lang="ukr")
         context.emit(sanction)
 
     entity.add("topics", "sanction")
@@ -191,6 +103,7 @@ def crawl_common(
 
 def crawl_indiviudal(context: Context, item: Dict[str, Any]) -> None:
     subject_id = item.pop("sid")
+
     name = item.pop("name")
     entity = context.make("Person")
     entity.id = context.make_slug(subject_id, name)
@@ -218,9 +131,8 @@ def crawl_legal(context: Context, item: Dict[str, Any]) -> None:
 
 
 def crawl(context: Context) -> None:
-    # crawl_old_system(context)
-    for item in fetch_data(context, "/subjects/individual"):
+    for item in fetch_data(context, "/v2/subjects?subjectType=individual"):
         crawl_indiviudal(context, item)
 
-    for item in fetch_data(context, "/subjects/legal"):
+    for item in fetch_data(context, "/v2/subjects?subjectType=legal"):
         crawl_legal(context, item)
