1b7ec87364a05bfe96674e5129f017969669b0a1
Bruno Görresen Mello <bruno@aetos.tech>
2024-06-07 10:42:07 -0300

    using fetch_html

--- datasets/us/dos_terrorist_exclusion/crawler.py
+++ datasets/us/dos_terrorist_exclusion/crawler.py
@@ -1,7 +1,4 @@
 from zavod import Context, helpers as h
-import requests
-from typing import cast
-from lxml import html
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15",
@@ -12,10 +9,12 @@ def crawl_item(raw_name: str, context: Context):
 
     entity = context.make("LegalEntity")
 
-    names = h.multi_split(raw_name, ["; a.k.a.", "(a.k.a", "(f.k.a.", "; f.k.a", ", a.k.a"])
+    names = h.multi_split(
+        raw_name, ["; a.k.a.", "(a.k.a", "(f.k.a.", "; f.k.a", ", a.k.a"]
+    )
 
     entity.id = context.make_id(names)
-    
+
     for name in names:
         entity.add("name", name)
 
@@ -29,9 +28,7 @@ def crawl_item(raw_name: str, context: Context):
 
 def crawl(context: Context):
 
-    response = requests.get(context.data_url, headers=HEADERS)
-
-    doc = html.fromstring(response.text)
+    doc = context.fetch_html(context.data_url, headers=HEADERS)
 
     # Find the title of the list by the text, then find the next sibling (which is the list), then get all the list items texts
     xpath = ".//*[contains(text(), 'Terrorist Exclusion List Designees (alphabetical listing)')]/../following-sibling::*[1]/li/text()"
