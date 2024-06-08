192e5252cc3e1c28116f5306f399aee65a9f5ee6
Friedrich Lindenberg <friedrich@pudo.org>
2024-06-08 10:52:09 +0200

    Use GPT-4o to extract data from UN GA PDF

---
+++ datasets/_global/un_ga_protocol/crawler.py
@@ -0,0 +1,68 @@
+from rigour.mime.types import PDF
+from urllib.parse import urljoin
+
+from zavod import Context
+from zavod import helpers as h
+from zavod.shed.gpt import run_image_prompt
+
+prompt = """
+Extract structured data from the following page of a PDF document. Return 
+a JSON list (`holders`) in which each object represents an office-holder
+(e.g. head of government, head of state, minister of foreign affairs).
+Each object should have the following fields: `country`, `full_title`,
+`honorary_prefix`, `person_name`, `date_of_appointment`.
+Return an empty string for unset fields.
+"""
+
+
+def crawl_pdf_url(context: Context) -> str:
+    html = context.fetch_html(context.data_url)
+    for a in html.findall('.//div[@class="content"]//a'):
+        if "list.pdf" in a.get("href", ""):
+            return urljoin(context.data_url, a.get("href"))
+    raise ValueError("No PDF found")
+
+
+def crawl(context: Context):
+    pdf_url = crawl_pdf_url(context)
+    path = context.fetch_resource("source.pdf", pdf_url)
+    context.export_resource(path, PDF, title=context.SOURCE_TITLE)
+    for page_path in h.make_pdf_page_images(path):
+        data = run_image_prompt(context, prompt, page_path)
+        assert "holders" in data, data
+        for holder in data.get("holders", []):
+            person_name = holder.get("person_name")
+            full_title = holder.get("full_title")
+            country = holder.get("country")
+            norm_name = context.lookup_value("names", person_name, person_name)
+            if norm_name is None or len(norm_name.strip()) == 0:
+                if full_title is not None and len(full_title.strip()):
+                    context.log.info(
+                        "No person name found",
+                        title=full_title,
+                        country=country,
+                    )
+                continue
+            country = holder.get("country")
+            entity = context.make("Person")
+            entity.id = context.make_id(country, person_name)
+            entity.add("topics", "role.pep")
+            entity.add("name", holder.get("person_name"))
+            entity.add("title", holder.get("honorary_prefix"))
+            entity.add("country", country)
+
+            position = h.make_position(
+                context,
+                name=full_title,
+                country=country,
+                topics=["gov.national"],
+            )
+            dates = h.parse_date(holder.get("date_of_appointment"), ["%d-%b-%y"])
+            date = dates[0] if dates else None
+            occupancy = h.make_occupancy(context, entity, position, start_date=date)
+
+            # entity.add("date_of_appointment", )
+            context.emit(entity, target=True)
+            context.emit(position)
+            if occupancy is not None:
+                context.emit(occupancy)
---
+++ zavod/zavod/shed/gpt.py
@@ -0,0 +1,73 @@
+import json
+import base64
+import logging
+import mimetypes
+from hashlib import sha1
+from pathlib import Path
+from openai import OpenAI
+from typing import Optional, Any
+from functools import lru_cache
+
+from zavod import settings
+from zavod.logs import get_logger
+from zavod.context import Context
+
+log = get_logger(__name__)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+
+@lru_cache(maxsize=1)
+def get_client() -> OpenAI:
+    """Get the OpenAI client."""
+    if settings.OPENAI_API_KEY is None:
+        raise ValueError("No $OPENSANCTIONS_OPENAI_API_KEY key provided.")
+    return OpenAI(api_key=settings.OPENAI_API_KEY)
+
+
+def encode_file(file: Path, mime_type: Optional[str] = None) -> str:
+    """Encode a file as a base64 data URL."""
+    if mime_type is None:
+        mime_type, _ = mimetypes.guess_type(file.name)
+    with open(file, "rb") as f:
+        data = f.read()
+        encoded = base64.b64encode(data).decode("utf-8")
+        return f"data:{mime_type};base64,{encoded}"
+
+
+def run_image_prompt(
+    context: Context,
+    prompt: str,
+    image_path: Path,
+    max_tokens: int = 3000,
+    cache_days: int = 60,
+) -> Any:
+    """Run an image prompt."""
+    client = get_client()
+    image_url = encode_file(image_path)
+    cache_hash = sha1(image_url.encode("utf-8"))
+    cache_hash.update(prompt.encode("utf-8"))
+    cache_key = cache_hash.hexdigest()
+    cached_data = context.cache.get_json(cache_key, max_age=cache_days)
+    if cached_data is not None:
+        log.info("GPT cache hit: %s" % image_path.name)
+        return cached_data
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {"type": "image_url", "image_url": {"url": image_url}},
+                ],
+            }
+        ],
+        response_format={"type": "json_object"},
+        max_tokens=max_tokens,
+    )
+    assert len(response.choices) > 0
+    assert response.choices[0].message is not None
+    assert response.choices[0].message.content is not None
+    data = json.loads(response.choices[0].message.content)
+    context.cache.set_json(cache_key, data)
+    return data
