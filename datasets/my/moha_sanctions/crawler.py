d6cf98287a6a77ed10c99faf4ae7caf6bc6682c6
Friedrich Lindenberg <friedrich@pudo.org>
2024-06-10 15:32:21 +0200

    make the moha crawler pass

--- datasets/my/moha_sanctions/crawler.py
+++ datasets/my/moha_sanctions/crawler.py
@@ -1,93 +1,99 @@
-import csv
-import string
-from urllib.parse import urljoin
 from normality import slugify
-from pantomime.types import CSV
+from banal import ensure_list, first
+from urllib.parse import urljoin
+from rigour.mime.types import PDF
+from typing import Dict, Union, List, Optional
 
 from zavod import Context
 from zavod import helpers as h
+from zavod.shed.zyte_api import fetch_html
+from zavod.shed.gpt import run_image_prompt
 
-# PERSONS_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT8r6lvQerO1tMNTUFaECYax-rU3Wra53U7T_yyRAZHMbGqvx6y93W8LO8OMatg08EQupZmAe7Wm-Wx/pub?gid=780934597&single=true&output=csv"
-# GROUPS_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vT8r6lvQerO1tMNTUFaECYax-rU3Wra53U7T_yyRAZHMbGqvx6y93W8LO8OMatg08EQupZmAe7Wm-Wx/pub?gid=676382776&single=true&output=csv"
 
-SPLITS = [f"{la})" for la in string.ascii_letters]
 FORMATS = ["%d %B %Y", "%d.%m.%Y"]
 MONTHS = {"Mei": "May", "Februari": "February", "Ogos": "August"}
 
+PROMPT = """
+Extract structured data from the following page of a PDF document. Return 
+a JSON list (`rows`) in which each object represents a row in the table.
+Each object should have one field for each column in the table, named using 
+the exact text in the column header but leaving out the column numbers
+in brackets. If multiple values exist in a cell, provide an array with each
+of the values in the cell separated as strings.
+"""
 
-def parse_date(date: str):
+
+def parse_date(date: Optional[str]) -> List[str]:
+    if date is None:
+        return []
     for malay, eng in MONTHS.items():
         date = date.replace(malay, eng)
     return h.parse_date(date, FORMATS)
 
 
-# def crawl_persons(context: Context):
-#     path = context.fetch_resource("persons.csv", PERSONS_CSV)
-#     context.export_resource(path, CSV, title=context.SOURCE_TITLE)
-#     with open(path, "r") as fh:
-#         for row in csv.DictReader(fh):
-#             data = {slugify(k, sep="_"): v for k, v in row.items()}
-#             entity = context.make("Person")
-#             code = data.pop("2_rujukan")
-#             entity.id = context.make_slug("person", code)
-#             entity.add("name", data.pop("3_nama"))
-#             entity.add("topics", "sanction")
-#             entity.add("birthDate", parse_date(data.pop("6_tarikh_lahir")))
-#             entity.add("birthPlace", data.pop("7_tempat_lahir"))
-
-#             aliases = h.multi_split(data.pop("8_nama_lain"), SPLITS)
-#             entity.add("alias", aliases)
-#             countries = h.multi_split(data.pop("9_warganegara"), SPLITS)
-#             entity.add("nationality", countries)
-
-#             entity.add("passportNumber", data.pop("10_nombor_pasport"))
-#             entity.add("idNumber", data.pop("11_nombor_kad_pengenalan"))
-#             entity.add("address", data.pop("12_alamat"))
-
-#             # short_alias = str(data.pop("5_nama_lain"))
-#             # if len(short_alias.strip()) > 1:
-#             #     entity.add("weakAlias", short_alias)
-
-#             sanction = h.make_sanction(context, entity)
-#             sanction.add("listingDate", parse_date(data.pop("13_tarikh_disenaraikan")))
-#             sanction.add("authorityId", code)
-
-#             context.emit(entity, target=True)
-#             context.emit(sanction)
-#             context.audit_data(data, ignore=["1_no"])
+def unblock_validator(el) -> bool:
+    return "SANCTION LIST MADE BY THE MINISTRY OF HOME AFFAIRS" in el.text_content()
 
 
-# def crawl_groups(context: Context):
-#     path = context.fetch_resource("groups.csv", GROUPS_CSV)
-#     context.export_resource(path, CSV, title=context.SOURCE_TITLE)
-#     with open(path, "r") as fh:
-#         for row in csv.DictReader(fh):
-#             data = {slugify(k, sep="_"): v for k, v in row.items()}
-#             entity = context.make("Organization")
-#             code = data.pop("2_no_ruj")
-#             entity.id = context.make_slug("group", code)
-#             entity.add("name", data.pop("3_nama"))
-#             entity.add("topics", "sanction")
-#             aliases = h.multi_split(data.pop("4_alias"), SPLITS)
-#             entity.add("alias", aliases)
-#             countries = h.multi_split(data.pop("6_alamat"), SPLITS)
-#             entity.add("country", countries)
+def crawl_person(context: Context, data: Dict[str, Union[str, List[str]]]):
+    entity = context.make("Person")
+    code = first(data.pop("rujukan", []))
+    code = code or first(data.pop("no_rujukan", []))
+    if code is None:
+        context.log.warn("No code for person", data=data)
+        return
+    entity.id = context.make_slug("person", code)
+    for name in ensure_list(data.pop("nama")):
+        entity.add("name", name.split("@"))
+    entity.add("topics", "sanction")
+    for fld in ["tarikh_lahir", "tarikhlahir"]:
+        for dob in ensure_list(data.pop(fld, [])):
+            entity.add("birthDate", parse_date(dob))
+    entity.add("birthPlace", data.pop("tempat_lahir", []))
 
-#             short_alias = str(data.pop("5_nama_lain"))
-#             if len(short_alias.strip()) > 1:
-#                 entity.add("weakAlias", short_alias)
+    entity.add("alias", data.pop("nama_lain", []))
+    entity.add("nationality", data.pop("warganegara", []))
 
-#             sanction = h.make_sanction(context, entity)
-#             sanction.add("listingDate", parse_date(data.pop("7_tarikh_disenaraikan")))
-#             sanction.add("authorityId", code)
+    entity.add("passportNumber", data.pop("nombor_pasport", []))
+    entity.add("idNumber", data.pop("nombor_kad_pengenalan", []))
+    entity.add("address", data.pop("alamat", []))
 
-#             context.emit(entity, target=True)
-#             context.emit(sanction)
-#             context.audit_data(data, ignore=["1_no"])
+    sanction = h.make_sanction(context, entity)
+    for fld in ["tarikh_disenaraikan", "tarikh_disenaraiaken", "tarikh_disenarai"]:
+        for date in ensure_list(data.pop(fld, [])):
+            sanction.add("listingDate", parse_date(date))
+    sanction.add("authorityId", code)
+
+    context.emit(entity, target=True)
+    context.emit(sanction)
+    context.audit_data(data, ignore=["no"])
+
+
+def crawl_group(context: Context, data: Dict[str, Union[str, List[str]]]):
+    entity = context.make("Organization")
+    code = first(data.pop("no_ruj"))
+    if code is None:
+        context.log.warn("No code for group", data=data)
+        return
+    entity.id = context.make_slug("group", code)
+    entity.add("name", data.pop("nama"))
+    entity.add("topics", "sanction")
+    entity.add("alias", data.pop("alias", []))
+    entity.add("country", data.pop("alamat", []))
+    entity.add("weakAlias", data.pop("nama_lain", []))
+
+    sanction = h.make_sanction(context, entity)
+    for date in ensure_list(data.pop("tarikh_disenaraikan", [])):
+        sanction.add("listingDate", parse_date(date))
+    sanction.add("authorityId", code)
+
+    context.emit(entity, target=True)
+    context.emit(sanction)
+    context.audit_data(data, ignore=["no"])
 
 
 def crawl_pdf_url(context: Context) -> str:
-    html = context.fetch_html(context.data_url)
+    html = fetch_html(context, context.data_url, unblock_validator, cache_days=5)
     for a in html.findall('.//div[@class="uk-container"]//a'):
         if ".pdf" in a.get("href", ""):
             return urljoin(context.data_url, a.get("href"))
@@ -96,4 +102,25 @@ def crawl_pdf_url(context: Context) -> str:
 
 def crawl(context: Context):
     pdf_url = crawl_pdf_url(context)
-    print(pdf_url)
+    path = context.fetch_resource("source.pdf", pdf_url)
+    context.export_resource(path, PDF, title=context.SOURCE_TITLE)
+
+    for page_path in h.make_pdf_page_images(path):
+        data = run_image_prompt(context, PROMPT, page_path)
+        for row in data["rows"]:
+            norm_ = {
+                slugify(k, sep="_"): v
+                for k, v in row.items()
+                if v is not None and str(v).strip() != "-" and v != ["-"]
+            }
+            norm = {k: v for k, v in norm_.items() if k is not None}
+            if "rujukan" in norm and "warganegara" in norm:
+                crawl_person(context, norm)
+            elif "no_rujukan" in norm and "warganegara" in norm:
+                crawl_person(context, norm)
+            elif "no_ruj" in norm:
+                crawl_group(context, norm)
+            else:
+                context.log.warn(
+                    "Cannot identify row format", row=row, page=page_path.name
+                )
