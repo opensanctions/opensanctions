66a9c81e24339f94447f2a46049e1d307ea37021
Bruno Görresen Mello <bruno@aetos.tech>
2024-06-25 12:59:43 -0300

    applying black formatter

--- datasets/ee/international_sanctions/crawler.py
+++ datasets/ee/international_sanctions/crawler.py
@@ -5,7 +5,9 @@ from zavod import Context, helpers as h
 from zavod.logic.pep import categorise
 
 
-BELARUS_URL = "https://www.vm.ee/en/sanctions-government-republic-view-situation-belarus"
+BELARUS_URL = (
+    "https://www.vm.ee/en/sanctions-government-republic-view-situation-belarus"
+)
 HUMAN_RIGHTS_URL = "https://www.vm.ee/subjektide-nimekiri-vabariigi-valitsuse-sanktsioon-inimoiguste-jargimise-tagamiseks"
 
 
@@ -23,15 +25,16 @@ def crawl_item_belarus(raw_name: str, context: Context):
     entity.id = context.make_id(name)
     entity.add("name", name)
     entity.add("topics", "sanction")
-    
+
     for alias in aliases:
         entity.add("alias", alias)
 
     context.emit(entity, target=True)
 
+
 def crawl_item_human_rights(raw_name: str, context: Context):
 
-    match = re.search(r'\d+\.\s*([^(\n]+)(?:\s*\(also\s*([^)]+)\))?', raw_name)
+    match = re.search(r"\d+\.\s*([^(\n]+)(?:\s*\(also\s*([^)]+)\))?", raw_name)
     if match:
         name = match.group(1).strip()
         aliases = match.group(2).split("; ") if match.group(2) else []
@@ -42,15 +45,15 @@ def crawl_item_human_rights(raw_name: str, context: Context):
     entity.id = context.make_id(name)
     h.apply_name(entity, first_name=first_name, last_name=last_name)
     entity.add("topics", "sanction")
-    
+
     for alias in aliases:
         entity.add("alias", alias)
 
     context.emit(entity, target=True)
 
+
 def crawl(context: Context):
-    
-    
+
     response = context.fetch_html(BELARUS_URL)
     xpath = ".//*[@class='col-lg-6 col-xl-6 mb-2 mb-xl-0']/div/div/ol/li"
 
