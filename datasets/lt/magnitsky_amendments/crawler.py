3099782fe792a93815462a2d44b097a9b58e6621
nvmbrasserie <ivan@opensanctions.org>
2024-07-09 15:02:10 +0200

    final draft

--- datasets/lt/magnitsky_amendments/crawler.py
+++ datasets/lt/magnitsky_amendments/crawler.py
@@ -21,10 +21,6 @@ def check_data_limits(data):
         raise ValueError(
             f"Error: numFound ({num_found}) is greater than the pre-set limit of 400."
         )
-    if page_size > 400:
-        raise ValueError(
-            f"Error: pageSize ({page_size}) is greater than the pre-set limit of 400."
-        )
 
 
 def crawl(context: Context):
@@ -46,7 +42,7 @@ def crawl(context: Context):
         person = context.make("Person")
         person_id = context.make_id(entry.get("vardas"), entry.get("pavarde"))
         person.id = person_id
-        person.add("name", f"{entry.get('vardas')} {entry.get('pavarde')}")
+        person.add("name", f"{entry.get('vardas')} {entry.get('pavarde')}", lang="lit")
         person.add("birthDate", h.parse_date(entry.get("gimimoData"), DATE_FORMATS))
         gender = map_gender(entry.get("lytis"))
         if gender:
@@ -59,11 +55,11 @@ def crawl(context: Context):
                 nationality.get("valstybe", {}).get("pilietybeClaEntry", {}).get("key")
             )
             if code:
-                person.add("nationality", code)
+                person.add("nationality", code, lang="lit")
 
         # Create a Sanction entity
         sanction = h.make_sanction(context, person)
-        sanction.add("reason", entry.get("priezastis"))
+        sanction.add("reason", entry.get("priezastis"), lang="lit")
         # sanction.add("startDate", h.parse_date("2018-01-01", DATE_FORMATS))
         sanction.add("endDate", h.parse_date(entry.get("uzdraustaIki"), DATE_FORMATS))
 
