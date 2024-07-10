20067eea5ef85d2834cb4e1de98c35efcef64236
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-07-10 18:13:22 +0200

    parse the narcs a bit better

--- datasets/us/dea_fugitives/crawler.py
+++ datasets/us/dea_fugitives/crawler.py
@@ -1,5 +1,6 @@
-from zavod import Context
 from time import sleep
+
+from zavod import Context
 
 # 1s delay seems to be enough to avoid getting blocked, while it takes a long
 # time to get unblocked after about 10 requests.
@@ -30,11 +31,13 @@ def crawl_item(fugitive_url: str, context: Context):
     info_dict = parse_table(response.find(".//table"))
 
     entity = context.make("Person")
-    entity.id = context.make_id(name)
-    entity.add("gender", info_dict.pop("Sex", None))
-
-    entity.add("birthDate", info_dict.pop("Year of Birth", None))
+    entity.id = context.make_id(fugitive_url)
     entity.add("name", name)
+    entity.add("sourceUrl", fugitive_url)
+    entity.add("topics", "crime")
+    entity.add("gender", info_dict.pop("Sex", None))
+    entity.add("birthDate", info_dict.pop("Year of Birth", None))
+    entity.add("address", info_dict.pop("Last Known Address", None))
 
     description = "".join(
         [
@@ -43,24 +46,18 @@ def crawl_item(fugitive_url: str, context: Context):
             if "Wanted for the following" in d.text_content()
         ]
     )
-
-    entity.add("description", description)
-    entity.add("sourceUrl", fugitive_url)
-
-    entity.add("topics", "crime")
-
+    entity.add("notes", description)
     context.emit(entity, target=True)
 
 
 def crawl(context: Context):
     # Each page only displays 10 fugitives at a time, so we need to loop until we don't find any more fugitives
-
     base_url = context.data_url
-
     page_num = 0
 
     while True:
         url = base_url + "?page=" + str(page_num)
+        context.log.info("Fetching page: %s" % page_num, url=url)
         response = context.fetch_html(url, cache_days=1)
         response.make_links_absolute(url)
 
