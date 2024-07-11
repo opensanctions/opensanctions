d4a59bb648f29efe3d73f97d429c0a3e8f4b3d38
Bruno Görresen Mello <bruno@aetos.tech>
2024-07-11 11:26:33 -0300

    applying black formatter

--- datasets/im/disqualified_directors/crawler.py
+++ datasets/im/disqualified_directors/crawler.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from zavod import Context, helpers as h
 
+
 def extract_data(ele):
     data_dict = {}
     current_key = None
@@ -10,17 +11,20 @@ def extract_data(ele):
     # Iterate through each p element
     for p in ele:
 
-        strong_tag = p.find('strong')
-        
+        strong_tag = p.find("strong")
+
         # If we find a strong tag, then it's a new data entry
         if strong_tag is not None:
-            current_key = strong_tag.text_content().strip().strip(':')
-            data_dict[current_key] = p.xpath('string()').replace(strong_tag.text_content(), '').strip()
+            current_key = strong_tag.text_content().strip().strip(":")
+            data_dict[current_key] = (
+                p.xpath("string()").replace(strong_tag.text_content(), "").strip()
+            )
         # Otherwise, it's just a continuation of the previous one
         else:
             if current_key:
-                data_dict[current_key] += ' ' + p.text_content().strip()
+                data_dict[current_key] += " " + p.text_content().strip()
     return data_dict
+
 
 def parse_period(period: str) -> Tuple[str, str]:
     """Given a string in the format "From DD MMMMM YYYY to DD MMMMM YYYY"
@@ -33,16 +37,20 @@ def parse_period(period: str) -> Tuple[str, str]:
         start and end dates.
     """
 
-    period = period.replace("\xa0", " ").replace("From ", "").replace(" to ", " ").replace(" To ", " ")
-    
-    start_str, end_str = period.split(' ', 3)[0:3], period.split(' ', 3)[3:6]
-    
-    # Join the parts back to form date strings
-    start_date_str = ' '.join(start_str)
-    end_date_str = ' '.join(end_str)
-    
-    return start_date_str, end_date_str
+    period = (
+        period.replace("\xa0", " ")
+        .replace("From ", "")
+        .replace(" to ", " ")
+        .replace(" To ", " ")
+    )
 
+    start_str, end_str = period.split(" ", 3)[0:3], period.split(" ", 3)[3:6]
+
+    # Join the parts back to form date strings
+    start_date_str = " ".join(start_str)
+    end_date_str = " ".join(end_str)
+
+    return start_date_str, end_date_str
 
 
 def crawl_item(item: Dict[str, str], context: Context):
@@ -54,7 +62,9 @@ def crawl_item(item: Dict[str, str], context: Context):
     person.add("name", name)
     person.add("address", item.pop("Address (at date of disqualification)"))
     if item["Date of Birth"] != "Not known":
-        person.add("birthDate", h.parse_date(item.pop("Date of Birth"), formats=["%d %B %Y"]))
+        person.add(
+            "birthDate", h.parse_date(item.pop("Date of Birth"), formats=["%d %B %Y"])
+        )
     else:
         item.pop("Date of Birth")
     person.add("topics", "corp.disqual")
@@ -64,8 +74,12 @@ def crawl_item(item: Dict[str, str], context: Context):
 
     if "Dates of Disqualification" in item:
         start_date, end_date = parse_period(item.pop("Dates of Disqualification"))
-        sanction.add("startDate", h.parse_date(start_date, formats=["%d %B %Y", "%d %b %Y"]))
-        sanction.add("endDate", h.parse_date(end_date, formats=["%d %B %Y", "%d %b %Y"]))
+        sanction.add(
+            "startDate", h.parse_date(start_date, formats=["%d %B %Y", "%d %b %Y"])
+        )
+        sanction.add(
+            "endDate", h.parse_date(end_date, formats=["%d %B %Y", "%d %b %Y"])
+        )
 
     if "Notes (if any)" in item:
         sanction.add("summary", item.pop("Notes (if any)"))
@@ -73,12 +87,21 @@ def crawl_item(item: Dict[str, str], context: Context):
         sanction.add("summary", item.pop("Notes"))
 
     if "Particulars of Disqualification Order or Undertaking" in item:
-        sanction.add("reason", item.pop("Particulars of Disqualification Order or Undertaking"))
+        sanction.add(
+            "reason", item.pop("Particulars of Disqualification Order or Undertaking")
+        )
 
     context.emit(person, target=True)
     context.emit(sanction)
 
-    context.audit_data(item, ignore=["Roles in respect of the Disqualification Order or Undertaking", "Means of Disqualification"])
+    context.audit_data(
+        item,
+        ignore=[
+            "Roles in respect of the Disqualification Order or Undertaking",
+            "Means of Disqualification",
+        ],
+    )
+
 
 def crawl(context: Context) -> None:
 
