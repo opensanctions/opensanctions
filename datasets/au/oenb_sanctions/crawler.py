b47a336bd456a432891aca1579d0817ef340c886
nvmbrasserie <ivan@opensanctions.org>
2024-07-17 18:10:21 +0200

    updated csv file draft

--- datasets/au/oenb_sanctions/crawler.py
+++ datasets/au/oenb_sanctions/crawler.py
@@ -8,38 +8,45 @@ DATE_FORMATS = ["%d. %m. %Y"]
 
 
 def crawl_row(context: Context, row: Dict[str, str]):
-    """Process one row of the CSV data"""
-    full_name = h.make_name(row.get("name"))
-    if full_name is not None and "," in full_name:
-        last_name, first_name = map(str.strip, full_name.split(",", 1))
-        h.make_name(first_name=first_name, last_name=last_name)
-    #  name_in_brackets = row.get("alias")
+    full_name = row.get("name")
+    alias = row.get("alias")
     birth_date = h.parse_date(row.get("date of birth"), DATE_FORMATS)
     birth_place = row.get("place of birth")
     passport_number = row.get("ID card no.")
     reason = row.get("reason")
 
     # Create a Person entity
-    entity = context.make("Person")
-    h.apply_name(entity, first_name=first_name, last_name=last_name)
-    entity.id = context.make_id(first_name, birth_date)
+    if row.get("type") == "Person":
+        # if full_name and "," in full_name:
+        #     last_name, first_name = map(str.strip, full_name.split(",", 1))
+        # else:
+        #     first_name, last_name = None, full_name  # Adjust based on contextual needs
 
-    # Add names
-    # entity.add("name", original_name, lang="spa")
-    # entity.add("alias", name_in_brackets, lang="eng")
+        # entity = context.make("Person")
+        # if first_name and last_name:
+        #     h.apply_name(entity, first_name=first_name, last_name=last_name)
+        #     entity.id = context.make_id(first_name, last_name)
+        entity = context.make("Person")
+        entity.id = context.make_id(full_name)
     entity.add("birthDate", birth_date)
     entity.add("birthPlace", birth_place, lang="eng")
     entity.add("passportNumber", passport_number)
-    entity.add("topics", "sanction")
-    # entity.add()
 
-    # Create a Sanction entity
+    # Create an Organization entity if it's an organization
+    if row.get("type") == "Organization":
+        entity = context.make("Organization")
+        entity.id = context.make_id(full_name)
+        entity.add("name", full_name)
+        entity.add("otherNames", row.get("other name"))
+        entity.add("alias", alias, lang="eng")
+        entity.add("birthPlace", birth_place, lang="eng")
+        entity.add("passportNumber", passport_number)
+
+    if entity:
+        entity.add("topics", "sanction")
+
     sanction = h.make_sanction(context, entity)
     sanction.add("reason", reason, lang="deu")
-
-    # Add source URL
-    #   entity.add("sourceUrl", source_url.strip())
-    #  sanction.add("sourceUrl", source_url.strip())
 
     # Emit entities
     context.emit(entity, target=True)
