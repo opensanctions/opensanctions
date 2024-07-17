a7a28c088d1ef025e450c195a019118b505c042a
JD Bothma <jbothma@gmail.com>
2024-07-17 15:20:38 +0100

    small refactor

--- datasets/us/fincen_special_measures/crawler.py
+++ datasets/us/fincen_special_measures/crawler.py
@@ -2,10 +2,8 @@ from lxml import html
 import re
 from zavod import Context, helpers as h
 from normality import slugify
-from typing import Dict, Generator
+from typing import Dict, Generator, cast
 from typing import Optional
-
-BASE_URL = "https://www.fincen.gov"
 
 
 def convert_date(date_str: str) -> Optional[str]:
@@ -24,7 +22,7 @@ def convert_date(date_str: str) -> Optional[str]:
 
 def crawl_item(context: Context, row: Dict[str, str]):
     # Create the entity based on the schema
-    name = row.pop("company")
+    name = row.pop("name").text_content()
     schema = context.lookup_value("target_type", name)
     if schema is None:
         schema = "Company"
@@ -38,49 +36,43 @@ def crawl_item(context: Context, row: Dict[str, str]):
     else:
         entity.add("topics", "reg.warn")
 
-    # Extract PDF links
-    pdf_link_finding = row.get("pdf_link_finding", None)
-    pdf_link_nprm = row.get("pdf_link_nprm", None)
-    pdf_link_final_rule = row.get("pdf_link_final_rule", None)
-    pdf_link_rescinded = row.get("pdf_link_rescinded", None)
-
-    # Convert relative URLs to full URLs
-    if pdf_link_finding and pdf_link_finding.startswith("/"):
-        pdf_link_finding = BASE_URL + pdf_link_finding
-    if pdf_link_nprm and pdf_link_nprm.startswith("/"):
-        pdf_link_nprm = BASE_URL + pdf_link_nprm
-    if pdf_link_final_rule and pdf_link_final_rule.startswith("/"):
-        pdf_link_final_rule = BASE_URL + pdf_link_final_rule
-    if pdf_link_rescinded and pdf_link_rescinded.startswith("/"):
-        pdf_link_rescinded = BASE_URL + pdf_link_rescinded
-
     # Create and add details to the sanction
     sanction = h.make_sanction(context, entity)
-    finding_date = row.get("finding", "")
-    nprm_date = row.get("notice_of_proposed_rulemaking", "")
+
+    # Extract PDF links
+    sanction.add(
+        "sourceUrl", [e.get("href") for e in row.get("finding").findall(".//a")]
+    )
+    sanction.add(
+        "sourceUrl",
+        [
+            e.get("href")
+            for e in row.get("notice-of-proposed-rulemaking").findall(".//a")
+        ],
+    )
+    sanction.add(
+        "sourceUrl", [e.get("href") for e in row.get("rescinded").findall(".//a")]
+    )
+    sanction.add(
+        "sourceUrl", [e.get("href") for e in row.get("final-rule").findall(".//a")]
+    )
+
+    finding_date = row.get("finding").text_content()
+    nprm_date = row.get("notice-of-proposed-rulemaking").text_content()
     listing_date = finding_date if finding_date else nprm_date
     listing_date = convert_date(listing_date)
     sanction.add("listingDate", listing_date)
-    final_rule_date = row.get("final_rule", "")
+
+    final_rule_date = row.get("final-rule").text_content()
     if final_rule_date != "---":
         sanction.add("startDate", convert_date(final_rule_date))
 
-    rescinded_date = row.get("rescinded", "")
+    rescinded_date = row.get("rescinded").text_content()
     if rescinded_date != "---":
         sanction.add("endDate", convert_date(rescinded_date))
         context.emit(entity, target=True)
     else:
         context.emit(entity, target=False)
-
-    # Add description with the PDF link
-    if pdf_link_finding:
-        sanction.add("sourceUrl", pdf_link_finding)  # Finding PDF link
-    if pdf_link_nprm:
-        sanction.add("sourceUrl", pdf_link_nprm)  # NPRM PDF link
-    if pdf_link_final_rule:
-        sanction.add("sourceUrl", pdf_link_final_rule)  # Final Rule PDF link
-    if pdf_link_rescinded:
-        sanction.add("sourceUrl", pdf_link_rescinded)  # Rescinded PDF link
 
     # Emit the entity and the sanction
     # context.emit(entity)
@@ -92,59 +84,28 @@ def parse_table(table: html.HtmlElement) -> Generator[Dict[str, str], None, None
     headers = None
     for row in table.findall(".//tr"):
         if headers is None:
-            headers = [
-                (
-                    slugify(el.text_content(), sep="_")
-                    if el.text_content().strip()
-                    else "company"
-                )
-                for el in row.findall("./th")
-            ]
-            headers.extend(
-                [
-                    "pdf_link_finding",
-                    "pdf_link_nprm",
-                    "pdf_link_final_rule",
-                    "pdf_link_rescinded",
-                ]
-            )
+            headers = []
+            for el in row.findall("./th"):
+                # Workaround because lxml-stubs doesn't yet support HtmlElement
+                # https://github.com/lxml/lxml-stubs/pull/71
+                eltree = cast(html.HtmlElement, el)
+                headers.append(slugify(eltree.text_content()))
+            assert headers[0] == None, headers
+            assert len(set(headers)) == len(headers), headers
+            headers[0] = "name"
             continue
-        cells = [el.text_content().strip() for el in row.findall("./td")]
 
-        # Extract PDF links from the relevant columns
-        pdf_link_finding = None
-        pdf_link_nprm = None
-        pdf_link_final_rule = None
-        pdf_link_rescinded = None
-
-        pdf_links = row.findall(".//a[@href]")
-
-        # Assuming the order of links in the row corresponds to the columns: Finding, NPRM, and Final Rule.
-        for i, pdf_link_element in enumerate(pdf_links):
-            pdf_link = pdf_link_element.get("href")
-            if pdf_link.endswith(".pdf"):
-                if i == 0:
-                    pdf_link_finding = pdf_link
-                elif i == 1:
-                    pdf_link_nprm = pdf_link
-                elif i == 2:
-                    pdf_link_final_rule = pdf_link
-                elif i == 3:
-                    pdf_link_rescinded = pdf_link
-
-        if (
-            len(cells) != len(headers) - 4
-        ):  # Minus four because we added the 'pdf_link_*' headers
+        cells = row.findall("./td")  # <--- edited here
+        if len(cells) == 1:
             continue
-        cells.extend(
-            [pdf_link_finding, pdf_link_nprm, pdf_link_final_rule, pdf_link_rescinded]
-        )
-        yield {header: cell for header, cell in zip(headers, cells)}
+        assert len(headers) == len(cells), (headers, cells)
+        yield {hdr: c for hdr, c in zip(headers, cells)}
 
 
 # Main crawl function to fetch and process data.
 def crawl(context: Context):
     doc = context.fetch_html(context.data_url)
+    doc.make_links_absolute(context.data_url)
     table = doc.get_element_by_id("special-measures-table")
     if table is not None:
         for row in parse_table(table):
