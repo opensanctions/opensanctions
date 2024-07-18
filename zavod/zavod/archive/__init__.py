048753ce4394ab27e455df4e7d7dca7f10b2de90
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-07-18 17:15:02 +0200

    Turns out: variables are variable

--- zavod/zavod/archive/__init__.py
+++ zavod/zavod/archive/__init__.py
@@ -44,7 +44,7 @@ ARTIFACT_FILES = [
     HASH_FILE,
 ]
 # Set a shorter cache TTL for index/meta files:
-SHORT_LIVED = (INDEX_FILE, ISSUES_FILE, CATALOG_FILE)
+SHORT_LIVED = (INDEX_FILE, CATALOG_FILE)
 TTL_SHORT = 5 * 60
 TTL_LONG = 3 * 24 * 60 * 60
 
@@ -191,12 +191,12 @@ def publish_resource(
 ) -> None:
     """Resources are files published to the main publication directory of the dataset."""
     backend = get_archive_backend()
+    ttl = TTL_SHORT if resource in SHORT_LIVED else None
     if dataset_name is not None:
         assert path.relative_to(dataset_data_path(dataset_name))
         resource = f"{dataset_name}/{resource}"
     release_name = f"{DATASETS}/{settings.RELEASE}/{resource}"
     release_object = backend.get_object(release_name)
-    ttl = TTL_SHORT if resource in SHORT_LIVED else None
     release_object.publish(path, mime_type=mime_type, ttl=ttl)
 
     if latest and settings.RELEASE != "latest":
