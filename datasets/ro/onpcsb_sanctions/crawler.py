904e8d4d8cc7ac95b83dc54dd3b87528781a38d4
nvmbrasserie <ivan@opensanctions.org>
2024-07-24 17:54:32 +0200

    final updates

--- datasets/ro/onpcsb_sanctions/crawler.py
+++ datasets/ro/onpcsb_sanctions/crawler.py
@@ -5,30 +5,13 @@ import re
 from typing import List
 
 
-DATE_FORMATS = ["%d.%m.%Y", "%d. %m %Y"]
-MONTHS_EN = {
-    "January": 1,
-    "February": 2,
-    "March": 3,
-    "April": 4,
-    "May": 5,
-    "June": 6,
-    "July": 7,
-    "August": 8,
-    "September": 9,
-    "October": 10,
-    "November": 11,
-    "December": 12,
-}
+DATE_FORMATS = ["%m/%d/%Y", "%Y"]
 
 
-def parse_date_time(text: str) -> List[str]:
+def parse_date(text: str) -> List[str]:
+    # add a function to handle multiple entires
     if not text:
-        return None
-    for de, number in MONTHS_EN.items():
-        # Replace German month names with numbers
-        text = re.sub(rf"\b{de}\b", str(number) + ".", text)
-    text = text.replace(" ", "")
+        return []
     date = h.parse_date(text, DATE_FORMATS)
     return date
 
@@ -36,52 +19,49 @@ def parse_date_time(text: str) -> List[str]:
 def crawl_row(context: Context, row: Dict[str, str]):
     full_name = row.pop("name")
     other_name = row.pop("other name")
-    birth_date = parse_date_time(row.pop("date of birth"))  # still to adjust
+    birth_date_1 = parse_date(row.pop("date of birth"))
+    birth_date_2 = parse_date(row.pop("date of birth 2"))
     birth_place = row.pop("place of birth")
     nationality = row.pop("nationality")
     passport_number = row.pop("passport no.")
     position = row.pop("position")
-    residence = row.pop("residence")
-    domicile = row.pop("domicile")
+    postal_code = row.pop("postal code")
     fiscal_code = row.pop("fiscal code")
     phone_number = row.pop("phone number")
-    address = row.pop("address")
-    address_1 = row.pop("address line 1")
-    address_2 = row.pop("address line 2")
-    address_3 = row.pop("address line 3")
+    address_1 = row.pop("address_1")
+    address_2 = row.pop("address_2")
     city = row.pop("city")
-    region = row.pop("region (province)")
     country = row.pop("country")
     entity_type = row.pop("type")
+    raw_data = row.pop("parsed data")
 
     entity = None
     if entity_type == "Person":
         entity = context.make("Person")
-        entity.id = context.make_id(full_name, other_name, passport_number)
+        entity.id = context.make_id(raw_data)
         entity.add("name", full_name)
-        entity.add("alias", other_name)
-        entity.add("birthDate", birth_date)
+        entity.add("alias", other_name)  # separated by comma
+        entity.add("birthDate", birth_date_1)
+        if birth_date_2:
+            entity.add("birthDate", birth_date_2)
         entity.add("birthPlace", birth_place)
         entity.add("nationality", nationality)
         entity.add("passportNumber", passport_number)
-        entity.add("address", residence)
-        entity.add("address", domicile)
+        entity.add("address", address_1)
+        entity.add("address", address_2)
+        entity.add("address", postal_code)
         entity.add("taxNumber", fiscal_code)
         entity.add("phone", phone_number)
         entity.add("topics", "sanction")
-        entity.add("country", "ro")
         entity.add("position", position)
     elif entity_type == "Organization":
         entity = context.make("Organization")
-        entity.id = context.make_id(full_name, address)
+        entity.id = context.make_id(raw_data)
         entity.add("name", full_name)
-        entity.add("address", address)
+        entity.add("alias", other_name)  # separated by semicolon
         entity.add("address", address_1)
         entity.add("address", address_2)
-        entity.add("address", address_3)
         entity.add("address", city)
-        entity.add("jurisdiction", country)
-        entity.add("address", region)
         entity.add("country", country)
     else:
         context.log.warning("Unhandled entity type", type=entity_type)
