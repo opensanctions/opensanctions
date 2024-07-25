1f62e82def19e8e01637f832e97ad1fc9639fc6c
nvmbrasserie <ivan@opensanctions.org>
2024-07-25 11:22:51 +0200

    working version

--- datasets/us/fbi_lazarus_crypto/crawler.py
+++ datasets/us/fbi_lazarus_crypto/crawler.py
@@ -8,15 +8,25 @@ def crawl_row(context: Context, row: Dict[str, str]):
     network = row.pop("Network")
     linked_to = row.pop("Linked to")
 
-    entity = None
-    entity = context.make("CryptoWallet")
-    entity.id = context.make_id(publicKey, network)
-    entity.add("publicKey", publicKey)
-    entity.add("notes", network)
-    entity.add("holder", linked_to)
-    entity.add("topics", "crime.fin")
+    # Create and emit the Organization entity if linked_to is provided
+    if linked_to:
+        organization = context.make("Organization")
+        organization.id = context.make_slug(linked_to)
+        organization.add("name", linked_to)
+        organization.add("topics", "crime.fin")
+        organization.add("topics", "crime.cyber")
+        context.emit(organization)
 
-    context.emit(entity)
+    # Create and emit the CryptoWallet entity
+    wallet = context.make("CryptoWallet")
+    wallet.id = context.make_id(publicKey, network)
+    wallet.add("publicKey", publicKey)
+    wallet.add("notes", network)
+    if linked_to:
+        wallet.add("holder", organization.id)  # Link the wallet to the organization
+    wallet.add("topics", "crime.fin")
+
+    context.emit(wallet)
 
 
 def crawl(context: Context):
