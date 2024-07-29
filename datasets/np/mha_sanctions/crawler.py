32c83afffa1c358f4ed185dd5d76a8f2bfc94b43
nvmbrasserie <ivan@opensanctions.org>
2024-07-29 11:42:54 +0200

    updated one

--- datasets/np/mha_sanctions/crawler.py
+++ datasets/np/mha_sanctions/crawler.py
@@ -7,47 +7,66 @@ DATE_FORMATS = ["%m/%d/%Y"]
 
 
 def crawl_row(context: Context, row: Dict[str, str]):
-    full_name = row.pop("name")
+    internal_id = row.pop("code")
+    entity_type = row.pop("type")
+    name = row.pop("name")
     # Split `alias` on `;` and trim any extra whitespace
     alias = row.pop("alias").split(";")
     birth_date = h.parse_date(row.pop("date of birth"), DATE_FORMATS)
+    birth_place = row.pop("place of birth")  # .split(";")
+    nationality = row.pop("nationality").split(";")
+    pass_no = row.pop("passport no").split(";")
+    national_id = row.pop("national id")
+    address = row.pop("address").split(";")
     listing_date = h.parse_date(row.pop("listed on"), DATE_FORMATS)
-    internal_id = row.pop("code")
-    address = row.pop("address")
-    entity_type = row.pop("type")
-    status = row.pop("SANCTION STATUS")
+    sanction_status = row.pop("sanction status")
+    remarks = row.pop("remarks")
 
     entity = None
     if entity_type == "INDIVIDUALS":
         entity = context.make("Person")
-        entity.id = context.make_id(full_name, birth_date, internal_id)
-        entity.add("name", full_name)
+        entity.id = context.make_id(name, birth_date, internal_id)
+        entity.add("name", name)
         for a in alias:  # Add aliases
             entity.add("alias", a.strip())
-        entity.add("birthDate", birth_date)
-        entity.add("address", address)
-
-    elif entity_type == "GROUP":
-        entity = context.make("Organization")
-        entity.id = context.make_id(full_name, address)
-        entity.add("name", full_name)
-        for a in alias:  # Add aliases
-            entity.add("alias", a.strip())
-    else:
-        context.log.warning("Unhandled entity type", type=entity_type)
-
-    # Proceed only if the entity was created
-    if entity:
-        if status == "ACTIVE":
+        for d in birth_date:
+            entity.add("birthDate", d.strip())
+        for p in birth_place:
+            entity.add("birthPlace", p.strip())
+        for n in nationality:
+            entity.add("nationality", n.strip())
+        for p in pass_no:
+            entity.add("passportNumber", p.strip())
+        entity.add("idNumber", national_id)
+        for a in address:
+            entity.add("address", address)
+        entity.add("notes", remarks)
+        if sanction_status == "ACTIVE":
             entity.add("topics", "sanction")
             sanction = h.make_sanction(context, entity)
             sanction.add("listingDate", listing_date)
             context.emit(entity, target=True)
             context.emit(sanction)
-        elif status == "REMOVED":
+        elif sanction_status == "REMOVED":
             context.emit(entity, target=False)
-        else:
+
+    elif entity_type == "GROUP":
+        entity = context.make("Organization")
+        entity.id = context.make_id(name, address)
+        entity.add("name", name)
+        entity.add("notes", remarks)
+        for a in alias:  # Add aliases
+            entity.add("alias", a.strip())
+        if sanction_status == "ACTIVE":
+            entity.add("topics", "sanction")
+            sanction = h.make_sanction(context, entity)
+            sanction.add("listingDate", listing_date)
+            context.emit(entity, target=True)
+            context.emit(sanction)
+        elif sanction_status == "REMOVED":
             context.emit(entity, target=False)
+    else:
+        context.log.warning("Unhandled entity type", type=entity_type)
 
 
 def crawl(context: Context):
