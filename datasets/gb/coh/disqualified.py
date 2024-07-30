20ef2d981f5a102a005020487aa1d1aeec7c0a7e
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-07-30 16:57:11 +0200

    retry after sleep

--- datasets/gb/coh/disqualified.py
+++ datasets/gb/coh/disqualified.py
@@ -1,7 +1,8 @@
 import os
 import time
 import string
-from typing import Dict, Any
+from itertools import count
+from typing import Dict, Any, Optional
 from urllib.parse import urljoin
 from requests.exceptions import HTTPError
 
@@ -21,20 +22,38 @@ SEARCH_URL = (
 # SEARCH_URL = "https://api-sandbox.company-information.service.gov.uk/search/disqualified-officers"
 API_URL = "https://api.company-information.service.gov.uk/"
 WEB_URL = "https://find-and-update.company-information.service.gov.uk/register-of-disqualifications/A"
+SLEEP = 315
+
+
+def http_get(
+    context: Context,
+    url,
+    params: Optional[Dict[str, Any]] = None,
+    cache_days: Optional[int] = None,
+) -> Optional[Dict[str, Any]]:
+    for attempt in count(1):
+        try:
+            return context.fetch_json(
+                url,
+                params=params,
+                auth=AUTH,
+                cache_days=cache_days,
+            )
+        except HTTPError as err:
+            if err.response.status_code in (429, 416):
+                if attempt > 3:
+                    raise AbortCrawl()
+                context.log.warn(f"Rate limit exceeded, sleeping {SLEEP}s...")
+                time.sleep(SLEEP)
+            context.log.exception("Failed to fetch data: %s" % url)
+            return None
 
 
 def crawl_item(context: Context, listing: Dict[str, Any]) -> None:
     links = listing.get("links", {})
     url = urljoin(API_URL, links.get("self"))
-    try:
-        data = context.fetch_json(url, auth=AUTH, cache_days=45)
-    except HTTPError as err:
-        if err.response.status_code in (429, 416):
-            raise AbortCrawl()
-        if err.response.status_code == 404:
-            context.log.info("Entity removed: %s" % url)
-            return
-        context.log.exception("HTTP error: %s" % url)
+    data = http_get(context, url, cache_days=45)
+    if data is None:
         return
     person = context.make("Person")
     _, officer_id = url.rsplit("/", 1)
@@ -130,22 +149,12 @@ def crawl(context: Context) -> None:
                     "start_index": str(start_index),
                     "items_per_page": "100",
                 }
-                try:
-                    data = context.fetch_json(
-                        SEARCH_URL,
-                        params=params,
-                        auth=AUTH,
-                        cache_days=10,
-                    )
-                except HTTPError as err:
-                    if err.response.status_code in (429, 416):
-                        raise AbortCrawl()
-                    context.log.exception("HTTP error: %s" % SEARCH_URL)
-                    break
+                data = http_get(context, SEARCH_URL, params=params, cache_days=5)
+                if data is None:
+                    return
                 context.log.info("Search: %s" % letter, start_index=start_index)
                 for item in data.pop("items", []):
                     crawl_item(context, item)
-                    time.sleep(3)
                 start_index = data["start_index"] + data["items_per_page"]
                 if data["total_results"] < start_index:
                     break
