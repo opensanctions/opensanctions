548af763bc63a6090c1b1197f6ca58724e71356a
JD Bothma <jbothma@gmail.com>
2024-07-30 12:49:20 +0100

    Only declarable declarant positions

--- datasets/lt/pep_declarations/crawler.py
+++ datasets/lt/pep_declarations/crawler.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import random
 from time import sleep
 from typing import Optional
@@ -6,11 +7,12 @@ from requests import HTTPError
 
 from zavod import Context, Entity
 from zavod import helpers as h
-from zavod.logic.pep import categorise
+from zavod.logic.pep import OccupancyStatus, categorise
 
 DEKLARACIJA_ID_RANGE = range(301_730, 637_217)
 # sample 50 for dev purposes
-DEKLARACIJA_ID_RANGE = random.sample(DEKLARACIJA_ID_RANGE, 50)
+#DEKLARACIJA_ID_RANGE = random.sample(DEKLARACIJA_ID_RANGE, 5000)
+GUEST_ID = 9179496
 
 
 class PinregSession:
@@ -18,8 +20,7 @@ class PinregSession:
 
     def __init__(self, context: Context):
         self.context = context
-        guest_id = random.randint(0, 9_999_999)  # random guest token
-        self._guest_token = f"c{guest_id:07d}"
+        self._guest_token = f"c{GUEST_ID}"
 
     def get_deklaracija_by_id(self, id: int) -> Optional[dict[any]]:
         id_str = f"{id:06d}"
@@ -36,28 +37,27 @@ class PinregSession:
         except HTTPError as ex:
             response = ex.response.json()
             if status_code := response.pop("status") == 404:
-                self.context.log.info(f"deklaracija {id_str} does not exist")
+                self.context.log.debug(f"deklaracija {id_str} does not exist")
             else:
-                self.context.log.error(
-                    f"deklaracija {id_str} skipped due to {status_code} error."
-                )
+                raise
 
 
-def make_person(context: Context, data: dict) -> Entity:
+def make_person(context: Context, declaration_id: int, data: dict) -> Entity:
     person = context.make("Person")
     first_name = data.pop("vardas")
     last_name = data.pop("pavarde")
-    person_id: Optional[str] = data.pop(
-        "asmensKodas", None
-    )  # this identifier is often missing
-    birth_date: Optional[str] = data.pop("gimimoData", None)
-    person.id = context.make_id(person_id, first_name, last_name)
-    person.add("firstName", first_name)
+    person_id = data.pop("asmensKodas", None)  # this identifier is often missing
+    birth_date = data.pop("gimimoData", None)  # often missing
+    person.id = context.make_id(
+        person_id, first_name, last_name, birth_date or declaration_id
+    )
     person.add("registrationNumber", person_id)
-    person.add("lastName", last_name)
+    h.apply_name(person, first_name=first_name, last_name=last_name)
     person.add("birthDate", birth_date)
     person.add("legalForm", data.pop("asmensTipas", None))
-    context.audit_data(data)
+    # context.audit_data(data, ignore=[
+    #    "yraJuridinisAsmuo",  # is legal person
+    # ])
     return person
 
 
@@ -73,7 +73,7 @@ def make_spouse(context: Context, person: Entity, spouse: Entity) -> Entity:
 
 
 def parse_affiliations(
-    context: Context, person: Entity, affiliations: list[dict], is_pep: bool = True
+    context: Context, person: Entity, affiliations: list[dict]
 ) -> list[tuple[Entity]]:
     """
     Args:
@@ -88,49 +88,63 @@ def parse_affiliations(
         are skipped.
     """
     entities = []
-    for data in affiliations:
-        entity_is_legal_entity: bool = data.pop("yraJuridinisAsmuo")
-        entity_is_lithuanian: bool = data.pop("registruotaLietuvoje")
-        legal_entity_code: Optional[str] = data.pop("jaKodas")
-        entity_name = data.pop("pavadinimas")
-        affiliation_start_date: str = data.pop("rysioPradzia")
-        affiliation_type = data.pop("darbovietesTipas")
-        data_sources = data.pop("duomenuSaltiniai")
-        filed_automatically: bool = data.pop("uzpildytaAutomatiskai")
-        entity_must_be_declared: bool = data.pop("privaluDeklaruoti")
-        entity_legal_form: str = data.pop("jaTeisinesFormosPavadinimas")
+    for affiliation in affiliations:
+        entity_is_lithuanian: bool = affiliation.pop("registruotaLietuvoje")
+        entity_name = affiliation.pop("pavadinimas")
+        affiliation_start_date: str = affiliation.pop("rysioPradzia")
 
-        for role in data.pop("pareigos"):
-            position_name: str = role.pop("pareigos") or "Unknown position"
-            legal_code: Optional[str] = role.pop("teisejoKodas")
-            role_must_be_declared: Optional[bool] = role.pop("privaluDeklaruoti")
-            nature_of_duties: str = role.pop("pareiguTipasPavadinimas")
+        for role in affiliation.pop("pareigos"):
+            if not role.pop("privaluDeklaruoti"):  # role must be declared
+                continue
+
+            position_name: Optional[str] = role.pop("pareigos")
+            assert position_name, (person, position_name)
+            if not entity_is_lithuanian:
+                context.log.warning(
+                    "Foreign declared role", name=person.get("name"), entity=entity_name
+                )
+                continue
             position = h.make_position(
                 context,
                 name=", ".join([position_name, entity_name]),
                 topics=None,
-                country="LT" if entity_is_lithuanian else None,
+                country="LT",
             )
-
             categorisation = categorise(
                 context,
                 position,
-                is_pep=is_pep,
+                is_pep=True,
             )
-
+            if affiliation_start_date > str(datetime.now().year):
+                status = OccupancyStatus.CURRENT
+            else:
+                status = OccupancyStatus.UNKNOWN
             occupancy = h.make_occupancy(
                 context,
                 person,
                 position,
                 no_end_implies_current=False,
-                start_date=affiliation_start_date,
                 categorisation=categorisation,
-                propagate_country=True,
+                status=status,
             )
-            context.audit_data(role)
+            context.audit_data(
+                role,
+                ignore=[
+                    "teisejoKodas",  # code
+                    "pareiguTipasPavadinimas",  # nature of duties
+                ],
+            )
             if occupancy:
                 entities.append((position, occupancy))
-        context.audit_data(data)
+        context.audit_data(affiliation, [
+            "privaluDeklaruoti",  # must declare
+            "yraJuridinisAsmuo",  # is legal person
+            "jaKodas",  # code
+            "darbovietesTipas",  # workplace type
+            "duomenuSaltiniai",  # data sources
+            "uzpildytaAutomatiskai",  # filled automatically
+            "jaTeisinesFormosPavadinimas",  # legal form
+        ])
     return entities
 
 
@@ -145,16 +159,8 @@ def crawl(context: Context) -> None:
 
         assert record.pop("id") == deklaracija_id
 
-        # metadata
-        submission_date = record.pop("pateikimoData")
-        modified_at = record.pop("viesumoNutraukimoData")
-        reasons_for_submission = record.pop("teikimoPriezastysPavadinimai")
-        submission_status = record.pop("viesumoStatusas")
-        nondisclosure_reason = record.pop("neviesinimoPriezastis")
-        declaration_type = record.pop("deklaracijosTipas")
-
         # declarant data
-        declarant = make_person(context, record.pop("teikejas"))
+        declarant = make_person(context, deklaracija_id, record.pop("teikejas"))
         declarant_affiliations: list[dict] = record.pop("darbovietes")
         declarant_offices = parse_affiliations(
             context, declarant, declarant_affiliations
@@ -163,42 +169,44 @@ def crawl(context: Context) -> None:
             context.emit(position, target=False)
             context.emit(occupancy, target=False)
         if not declarant_offices:
-            context.log.info(
+            context.log.debug(
                 f"deklaracija {deklaracija_id} has no relevant occupancies."
             )
             continue
-        context.log.info(f"deklaracija {deklaracija_id} processing.")
+        context.log.debug(f"deklaracija {deklaracija_id} processing.")
 
-        """If the spouse is included in the declaration, and the spouse's occupancies
-        meet OpenSanctions criteria, emit the spouse, the marriage, and the occupancies/positions."""
-        spouse_affiliations: list[dict] = record.pop("sutuoktinioDarbovietes", [])
         if spouse_data := record.pop("sutuoktinis", None):
-            spouse = make_person(context, spouse_data) if spouse_data else None
+            spouse = (
+                make_person(context, deklaracija_id, spouse_data)
+                if spouse_data
+                else None
+            )
+            spouse.add("topics", "role.rca")
             marriage = make_spouse(context, declarant, spouse)
-            spouse_offices = parse_affiliations(context, spouse, spouse_affiliations)
-            if spouse_offices:
-                for position, occupancy in spouse_offices:
-                    context.emit(position, target=False)
-                    context.emit(occupancy, target=False)
-            context.emit(spouse, target=False)
+            context.emit(spouse)
             context.emit(marriage)
 
-        related_legal_entities: Optional[list[dict]] = record.pop("rysiaiSuJa", None)
-
-        # ignored values
-        for ignored_key in [
-            "rysiaiDelSandoriu",
-            "kitiDuomenys",
-            "kitiDuomenysFa",
-            "teikejasYraIstaigosDarbuotojas",
-            "deklaracijosAtsiradesGalimasRysys",
-            "senosPidId",
-            "teikimoPriezastys",
-            "deklaracijosFaRysiai",
-            "yraNaujausiaVersija",
-            "deklaracijosIndividualiosVeiklos",
-        ]:
-            record.pop(ignored_key)
-
         context.emit(declarant, target=True)
-        context.audit_data(record)
+        context.audit_data(
+            record,
+            ignore=[
+                "rysiaiSuJa",  # relationships with declarant
+                "sutuoktinioDarbovietes",  # spouse's affiliations
+                "pateikimoData",  # date of submission
+                "viesumoNutraukimoData",  # date of last modification
+                "teikimoPriezastysPavadinimai",  # reasons for submission
+                "viesumoStatusas",  # status of submission
+                "neviesinimoPriezastis",  # reason for non-disclosure
+                "deklaracijosTipas",  # type of declaration
+                "rysiaiDelSandoriu",  # relationships due to transactions
+                "kitiDuomenys",  # other data
+                "kitiDuomenysFa",  # other data
+                "teikejasYraIstaigosDarbuotojas",  # declarant is an employee of the institution
+                #"deklaracijosAtsiradesGalimasRysys",  # possible relationship
+                "senosPidId",  # old PID ID
+                "teikimoPriezastys",  # reasons for submission
+                #"deklaracijosFaRysiai",  # relationships
+                "yraNaujausiaVersija",  # newest version
+                "deklaracijosIndividualiosVeiklos",  # individual activities
+            ],
+        )
