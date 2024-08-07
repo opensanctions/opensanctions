c85aa222406681811b4877fd725b6c0f8582938e
nvmbrasserie <ivan@opensanctions.org>
2024-08-07 11:02:27 +0200

    working version without pandas

--- datasets/us/bis_oac/crawler.py
+++ datasets/us/bis_oac/crawler.py
@@ -3,16 +3,17 @@ from typing import Dict
 from zavod import Context
 from zavod import helpers as h
 from normality import collapse_spaces, stringify
-from rigour.mime.types import XLS
-import pandas as pd
+from rigour.mime.types import XLSX
+from openpyxl import load_workbook
+
 
 DATE_FORMAT = ["%d-%b"]  # 'DD-MMM' format
 
 
 def crawl_row(context: Context, row: Dict[str, str]):
-    requester = row.pop("REQUESTER")
-    requesting_country = row.pop("REQUESTING COUNTRY")
-    date_listed = h.parse_date(row.pop("DATE LISTED"), DATE_FORMAT)
+    requester = row.pop("REQUESTER", "").strip()
+    requesting_country = row.pop("REQUESTING COUNTRY", "").strip()
+    date_listed = h.parse_date(row.pop("DATE LISTED", ""), DATE_FORMAT)
 
     if not requester or not requesting_country or not date_listed:
         context.log.warning(
@@ -25,26 +26,23 @@ def crawl_row(context: Context, row: Dict[str, str]):
     entity.add("name", requester)
     entity.add("country", requesting_country)
     entity.add("topics", "export.risk")
-
     sanction = h.make_sanction(context, entity)
     sanction.add("listingDate", date_listed)
+
     context.emit(entity, target=True)
     context.emit(sanction)
 
 
-def crawl_xls(context: Context, url: str):
-    path = context.fetch_resource("source.xls", url)
-    context.export_resource(path, XLS, title=context.SOURCE_TITLE)
-    try:
-        xls = pd.read_excel(path, sheet_name=None)  # Load all sheets into a dictionary
-    except Exception as e:
-        context.log.error(f"Failed to open Excel file: {e}")
-        return
+def crawl_xlsx(context: Context, path: str):
+    wb = load_workbook(path, read_only=True)
+    for sheet in wb.worksheets:
+        headers = [
+            collapse_spaces(str(c.value))
+            for c in list(sheet.iter_rows(min_row=1, max_row=1))[0]
+        ]
 
-    for sheet_name, sheet in xls.items():
-        headers = [collapse_spaces(h) for h in sheet.columns]
-        for _, row in sheet.iterrows():
-            data = {headers[i]: stringify(row.iloc[i]) for i in range(len(headers))}
+        for row in sheet.iter_rows(min_row=2, values_only=True):
+            data = {headers[i]: stringify(row[i]) for i in range(len(headers))}
             crawl_row(context, data)
 
 
@@ -53,7 +51,7 @@ def fetch_excel_url(context: Context) -> str:
     doc = context.fetch_html(context.data_url, params=params)
     for link in doc.findall(".//a"):
         href = urljoin(context.data_url, link.get("href"))
-        if href.endswith(".xls"):
+        if href.endswith(".xls") or href.endswith(".xlsx"):
             return href
     raise ValueError("Could not find XLS file on the website")
 
@@ -61,7 +59,9 @@ def fetch_excel_url(context: Context) -> str:
 def crawl(context: Context):
     context.log.info("Fetching data from the source")
     url = fetch_excel_url(context)
-    if url.endswith(".xls"):
-        crawl_xls(context, url)
-    else:
-        raise ValueError("Unknown file type: %s" % url)
+    path = context.fetch_resource(
+        "source.xls", url
+    )  # Even though its initial name is source.xls, actual file is in xlsx format
+    actual_path = path.with_suffix(".xlsx")
+    context.export_resource(path, XLSX, title=context.SOURCE_TITLE)
+    crawl_xlsx(context, actual_path)
