e0cdc27cfffdee5f168503b4b84645c1c848c505
nvmbrasserie <ivan@opensanctions.org>
2024-08-12 13:33:21 +0200

    redundant code removed

--- datasets/fr/amf_regulatory_sanctions/crawler.py
+++ datasets/fr/amf_regulatory_sanctions/crawler.py
@@ -1,15 +1,11 @@
-from typing import Any, Generator, List, Optional, Tuple, Union
+from typing import Any, Generator, Optional, Tuple, Union
 
 from normality import collapse_spaces
 from zavod import Context
-from datetime import datetime
 import re
 from html import unescape
-from lxml import html
 from urllib.parse import urljoin
-from pprint import pprint
 
-from zavod.helpers.text import multi_split
 
 CLEAN_ENTITY = re.compile(r"(<br />\r\n| et |;)", re.IGNORECASE)
 BASE_URL = "https://www.amf-france.org"
@@ -161,124 +157,3 @@ def crawl(context: Context) -> None:
         for rel in entities_res.relationships:
             # make relations here
             print("   ", rel)
-
-        # continue  # just skip this stuff that should probably happen in the loop for now
-        # link = item.get("link", {}).get("url", "")
-        # date_epoch = item.get("date")
-        # if date_epoch:
-        #     listing_date = datetime.utcfromtimestamp(int(date_epoch)).strftime(
-        #         "%Y-%m-%d"
-        #     )
-        # else:
-        #     listing_date = ""
-
-        # for name in entity_names:
-        #     name = name.strip()  # Strip leading/trailing spaces
-        #     if not name or not is_valid_name(name):
-        #         continue
-
-        #     # Handling specific Société cases
-        #     if any(keyword in name for keyword in OMIT_KEYWORDS):
-        #         process_entity(context, title, listing_date, name, theme, link, item)
-        #     elif any(prefix in name for prefix in female_prefixes):
-        #         process_person(context, title, listing_date, name, theme, link, item)
-        #     elif any(prefix in name for prefix in male_prefixes):
-        #         process_person(context, title, listing_date, name, theme, link, item)
-        #     else:
-        #         process_entity(context, title, listing_date, name, theme, link, item)
-    # print(SPLIT_NAMES_REGEX.pattern)
-
-
-# def process_person(
-#    context: Context,
-#    title: str,
-#    listing_date: str,
-#    name: str,
-#    theme: str,
-#    link: str,
-#    item: dict,
-# ):
-#    """Process a person entry."""
-#    person = context.make("Person")
-#    person.id = context.make_id(title, listing_date, name)
-#
-#    gender = determine_gender(name)
-#    person.add("gender", gender)
-#
-#    # Clean the name after determining the gender
-#    cleaned_name = clean_name(name, CLEAN_PERSON_REGEX)
-#
-#    # Catch things that still don't look right
-#    if not roughly_valid_name(cleaned_name):
-#        name_fix_res = context.lookup("roughly_invalid_names", cleaned_name)
-#        if name_fix_res is None:
-#            context.log.warning("...")
-#            return
-#        cleaned_name = name_fix_res.value
-#    if cleaned_name is None:
-#        return
-#    person.add("name", cleaned_name)
-#
-#    # Set other fields for person
-#    person.add("notes", theme)
-#    person.add("notes", title)
-#    if link:
-#        full_link = urljoin(BASE_URL, link)
-#        person.add("sourceUrl", full_link)
-#    if "download" in item:
-#        download_links = item["download"].get("sanction", {}).get("links", {})
-#        if download_links:
-#            download_url = download_links.get("url")
-#            if download_url:
-#                full_download_url = urljoin(BASE_URL, download_url)
-#                person.add("sourceUrl", full_download_url)
-#
-#    context.emit(person, target=True)
-
-
-# def process_entity(
-#    context: Context,
-#    title: str,
-#    listing_date: str,
-#    name: str,
-#    theme: str,
-#    link: str,
-#    item: dict,
-# ):
-#    """Process a legal entity entry."""
-#    entity = context.make("LegalEntity")
-#    entity.id = context.make_id(title, listing_date, name)
-#    # Clean and add name
-#
-#    # Not warning if this one isn't defined, just an overall check
-#    precursor_res = context.lookup("name_fixes", name)
-#    if precursor_res:
-#        name = precursor_res.value
-#
-#    cleaned_name = clean_name(name, CLEAN_ENTITY_REGEX)
-#
-#    # Catch things that still don't look right
-#    if not roughly_valid_name(cleaned_name):
-#        name_fix_res = context.lookup("roughly_invalid_names", cleaned_name)
-#        if name_fix_res is None:
-#            context.log.warning("...")
-#            return
-#        cleaned_name = name_fix_res.value
-#    if cleaned_name is None:
-#        return
-#
-#    print(cleaned_name)
-#    entity.add("name", cleaned_name)
-#    entity.add("notes", theme)
-#    entity.add("notes", title)
-#    if link:
-#        full_link = urljoin(BASE_URL, link)
-#        entity.add("sourceUrl", full_link)
-#    if "download" in item:
-#        download_links = item["download"].get("sanction", {}).get("links", {})
-#        if download_links:
-#            download_url = download_links.get("url")
-#            if download_url:
-#                full_download_url = urljoin(BASE_URL, download_url)
-#                entity.add("sourceUrl", full_download_url)
-#    context.emit(entity, target=True)
