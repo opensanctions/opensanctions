7707896ac4b9e7912c1d4de1d80707b64e15a94a
nvmbrasserie <ivan@opensanctions.org>
2024-08-12 13:30:01 +0200

    updated lookups

--- datasets/fr/amf_regulatory_sanctions/crawler.py
+++ datasets/fr/amf_regulatory_sanctions/crawler.py
@@ -28,7 +28,6 @@ def parse_json(context: Context) -> Generator[dict, None, None]:
         yield item
 
 
-
 def get_value(
     data: dict, keys: Tuple[str, ...], default: Optional[Any] = None
 ) -> Union[Optional[Any], Any]:
@@ -38,6 +37,156 @@ def get_value(
             return val
     return default
 
+
+PREFIXES = [
+    "Sociétés",
+    "Société",
+    "société",
+    "Banque",
+    "la société",
+    "Caisse",
+    "cabinet",
+    "La sociétés",
+    "les CABINETS",
+    "les sociétés",
+    "LA",
+    "de gestion",
+    "cabinets",
+    "SCS",  # Société en commandite simple
+    r"M\.?",
+    "Mme",
+    r"MM\.?",
+    "Madame",
+    "Monsieur",
+    "Monsieur",
+    "Melle",
+]
+
+
+comma_prefix_anon = "(,|et) +(" + "|".join(PREFIXES) + r")[\. \b]+[A-Z]\b"
+print("comma_prefix_anon", comma_prefix_anon)
+prefix_anon = "(" + "|".join(PREFIXES) + r")[\. \b]+[A-Z]\b"
+print("prefix_anon", prefix_anon)
+comma_anon = r"(,|et) +[A-Z]\b"
+print("comma_anon", comma_anon)
+
+
+def clean_names_str(names_str: str) -> Optional[str]:
+    names_str = re.sub(comma_prefix_anon, "", names_str)
+    names_str = re.sub(prefix_anon, "", names_str)
+    names_str = re.sub(comma_anon, "", names_str)
+    names_str = collapse_spaces(names_str)
+    return names_str
+
+
+def process_entity(
+    context: Context,
+    title: str,
+    listing_date: str,
+    name: str,
+    theme: str,
+    link: Optional[str],
+    item: dict,
+) -> None:
+    """Process a legal entity entry."""
+    entity = context.make("LegalEntity")
+    entity.id = context.make_id(title, listing_date, name)
+
+    # Add name directly without cleaning
+    entity.add("name", name)
+
+    # Add additional information
+    entity.add("notes", theme)
+    entity.add("notes", title)
+
+    if link and isinstance(link, str):
+        full_link = urljoin(BASE_URL, link)
+        entity.add("sourceUrl", full_link)
+
+    # Check and add download URL if available
+    if "download" in item:
+        download_links = item["download"].get("sanction", {}).get("links", {})
+        if download_links:
+            download_url = download_links.get("url")
+            if download_url and isinstance(download_url, str):
+                full_download_url = urljoin(BASE_URL, download_url)
+                entity.add("sourceUrl", full_download_url)
+
+    context.emit(entity, target=True)
+
+
+def crawl(context: Context) -> None:
+    # General data
+    for data in parse_json(context):
+        # print("data", data)
+
+        # Extract relevant fields from each item
+        theme = unescape(str(data.get("theme")))
+        item = data.get("infos", {})
+        title = item.get("title")
+        names_str = unescape(item.get("text_egard", ""))
+        # print("original", names_str)
+        names_str = clean_names_str(names_str)
+        # print("cleaned", names_str)
+        if not names_str:
+            continue
+
+        entities_res = context.lookup("entities", names_str)
+        # No mapping yet
+        if entities_res is None:
+            context.log.warning("No entity mapping for %r" % names_str)
+            continue
+
+        # No entities for remaining names str
+        if not entities_res.entities:
+            continue
+
+        penalty = item.pop("text")
+        # print("penalty", penalty)
+        # html.fromstring(penalty)
+        # Todo: figure out if we can assume cases like all/none exonorated and what topic we should give.
+        # Perhaps we should just give all reg.action?
+
+        listing_date = item.get("date", "")
+        link = item.get("link", "")
+
+        for entity in entities_res.entities:
+            # make entities here
+            process_entity(context, title, listing_date, entity, theme, link, item)
+            # print("   ", entity)
+
+        # Make it so you don't need a blank relations field in each entry
+        if not entities_res.relationships:
+            continue
+        for rel in entities_res.relationships:
+            # make relations here
+            print("   ", rel)
+
+        # continue  # just skip this stuff that should probably happen in the loop for now
+        # link = item.get("link", {}).get("url", "")
+        # date_epoch = item.get("date")
+        # if date_epoch:
+        #     listing_date = datetime.utcfromtimestamp(int(date_epoch)).strftime(
+        #         "%Y-%m-%d"
+        #     )
+        # else:
+        #     listing_date = ""
+
+        # for name in entity_names:
+        #     name = name.strip()  # Strip leading/trailing spaces
+        #     if not name or not is_valid_name(name):
+        #         continue
+
+        #     # Handling specific Société cases
+        #     if any(keyword in name for keyword in OMIT_KEYWORDS):
+        #         process_entity(context, title, listing_date, name, theme, link, item)
+        #     elif any(prefix in name for prefix in female_prefixes):
+        #         process_person(context, title, listing_date, name, theme, link, item)
+        #     elif any(prefix in name for prefix in male_prefixes):
+        #         process_person(context, title, listing_date, name, theme, link, item)
+        #     else:
+        #         process_entity(context, title, listing_date, name, theme, link, item)
+    # print(SPLIT_NAMES_REGEX.pattern)
 
 
 # def process_person(
@@ -133,114 +282,3 @@ def get_value(
 #                full_download_url = urljoin(BASE_URL, download_url)
 #                entity.add("sourceUrl", full_download_url)
 #    context.emit(entity, target=True)
-
-PREFIXES = [
-    "Sociétés",
-    "Société",
-    "société",
-    "Banque",
-    "la société",
-    "Caisse",
-    "cabinet",
-    "La sociétés",
-    "les CABINETS",
-    "les sociétés",
-    "LA",
-    "de gestion",
-    "cabinets",
-    "SCS",  # Société en commandite simple
-    r"M\.?",
-    "Mme",
-    r"MM\.?",
-    "Madame",
-    "Monsieur",
-    "Monsieur",
-    "Melle",
-]
-
-
-comma_prefix_anon = "(,|et) +(" + "|".join(PREFIXES) + r")[\. \b]+[A-Z]\b"
-print("comma_prefix_anon", comma_prefix_anon)
-prefix_anon = "(" + "|".join(PREFIXES) + r")[\. \b]+[A-Z]\b"
-print("prefix_anon", prefix_anon)
-comma_anon =r"(,|et) +[A-Z]\b"
-print("comma_anon", comma_anon)
-
-
-def clean_names_str(names_str: str) -> str:
-    names_str = re.sub(comma_prefix_anon, "", names_str)
-    names_str = re.sub(prefix_anon, "", names_str)
-    names_str = re.sub(comma_anon, "", names_str)
-    names_str = collapse_spaces(names_str)
-    return names_str
-
-
-def crawl(context: Context) -> None:
-    # General data
-    for data in parse_json(context):
-        #print("data", data)
-
-        # Extract relevant fields from each item
-        theme = unescape(str(data.get("theme")))
-        item = data.get("infos", {})
-        title = item.get("title")
-        names_str = unescape(item.get("text_egard", ""))
-        print("original", names_str)
-        names_str = clean_names_str(names_str)
-        print("cleaned", names_str)
-        if not names_str:
-            continue
-
-        entities_res = context.lookup("entities", names_str)
-        # No mapping yet
-        if entities_res is None:
-            context.log.warning("No entity mapping for %r" % names_str)
-            continue
-
-        # No entities for remaining names str
-        if not entities_res.entities:
-            continue
-
-        penalty = item.pop("text")
-        print("penalty", penalty)
-        # html.fromstring(penalty)
-        # Todo: figure out if we can assume cases like all/none exonorated and what topic we should give.
-        # Perhaps we should just give all reg.action?
-
-        for entity in entities_res.entities:
-            # make entities here
-            print("   ", entity)
-
-        # Make it so you don't need a blank relations field in each entry
-        if not entities_res.relationships:
-            continue
-        for rel in entities_res.relationships:
-            # make relations here
-            print("   ", rel)
-
-
-        continue # just skip this stuff that should probably happen in the loop for now
-        link = item.get("link", {}).get("url", "")
-        date_epoch = item.get("date")
-        if date_epoch:
-            listing_date = datetime.utcfromtimestamp(int(date_epoch)).strftime(
-                "%Y-%m-%d"
-            )
-        else:
-            listing_date = ""
-
-        for name in entity_names:
-            name = name.strip()  # Strip leading/trailing spaces
-            if not name or not is_valid_name(name):
-                continue
-
-            # Handling specific Société cases
-            if any(keyword in name for keyword in OMIT_KEYWORDS):
-                process_entity(context, title, listing_date, name, theme, link, item)
-            elif any(prefix in name for prefix in female_prefixes):
-                process_person(context, title, listing_date, name, theme, link, item)
-            elif any(prefix in name for prefix in male_prefixes):
-                process_person(context, title, listing_date, name, theme, link, item)
-            else:
-                process_entity(context, title, listing_date, name, theme, link, item)
-    #print(SPLIT_NAMES_REGEX.pattern)
