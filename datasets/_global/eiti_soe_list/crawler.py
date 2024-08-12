b06a2a0c10985a0f550a105b95e8c59e967e262f
nvmbrasserie <ivan@opensanctions.org>
2024-08-12 17:12:47 +0200

    post-review updates

--- datasets/_global/eiti_soe_list/crawler.py
+++ datasets/_global/eiti_soe_list/crawler.py
@@ -6,13 +6,12 @@ def crawl_entity(context: Context, item: Dict[str, Any]) -> None:
     entity = context.make("LegalEntity")
 
     soe = item.pop("SOE")
-    year = item.pop("Year")
     country = item.pop("Country")
     fin_statement = item.pop("Audited Financial Statement or Equivalent")
     website = item.pop("Public Listing or Website")
 
     # Making a unique ID for the entity
-    entity.id = context.make_id(soe, year, country)
+    entity.id = context.make_id(soe, country)
 
     # Adding fields to the entity
     entity.add("name", soe)
@@ -30,10 +29,10 @@ def crawl_entity(context: Context, item: Dict[str, Any]) -> None:
 
 def crawl(context: Context):
     data = context.fetch_json(context.data_url)
-    if not data or not data.get("rows"):
-        context.log.error("No data found or 'rows' key is missing.")
+    if not data:
+        context.log.error("No data found")
         return
 
     # Processing each item in the 'rows' list
-    for item in data["rows"]:
+    for item in data.pop("rows"):
         crawl_entity(context, item)
