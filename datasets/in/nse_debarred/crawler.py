2dc6557203e66d6b3d82a1e4032bb167df36b9c9
JD Bothma <jbothma@gmail.com>
2024-08-16 15:03:06 +0100

    Release a few

--- datasets/in/nse_debarred/crawler.py
+++ datasets/in/nse_debarred/crawler.py
@@ -103,7 +103,8 @@ def crawl_item(input_dict: dict, context: Context):
     topics = "reg.warn" if is_revoked else "debarment"
 
     entity.add("name", name)
-    entity.add("taxNumber", pan)
+    if "not provided" not in pan.lower():
+        entity.add("taxNumber", pan)
     entity.add("topics", topics)
     din_cin: str = input_dict.pop("din_cin_of_entities_debarred", None)
     if din_cin and din_cin.lower != "not available":
