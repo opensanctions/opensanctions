72081dd6e63170fc0d67b776a8e96ebf8677abf8
JD Bothma <jbothma@gmail.com>
2024-08-22 11:32:48 +0100

    Small fixes

--- datasets/za/mm_executive_mayors/crawler.py
+++ datasets/za/mm_executive_mayors/crawler.py
@@ -5,12 +5,6 @@ from zavod import Context
 from zavod import helpers as h
 from zavod.logic.pep import categorise
 
-ROLES_OF_INTEREST = {
-    "Mayor/Executive Mayor",
-    "Municipal Manager",
-    "Chief Financial Officer",
-}
-
 DEMARCATION_CHANGES_URL = (
     "https://municipaldata.treasury.gov.za/api/cubes/demarcation_changes/facts"
 )
@@ -40,63 +34,69 @@ def clean_phones(phones):
 
 def crawl(context: Context):
     # Fetch the data from the provided URL
-    executive_mayors = context.fetch_json(context.data_url)
+    officials = context.fetch_json(context.data_url).get("data")
 
     # Fetch the reestablished municipalities data from the specific URL
-    disestablished_munis = context.fetch_json(DEMARCATION_CHANGES_URL)
+    demarcation_changes = context.fetch_json(DEMARCATION_CHANGES_URL).get("data")
 
     # Initialize a set to store disestablished municipality codes
     disestablished_municipalities = set()
 
     # Process the disestablished municipalities
-    for fact in disestablished_munis.get("data", []):
+    for fact in demarcation_changes:
         if fact.get("old_code_transition.code") == "disestablished":
             # Add the old demarcation code to the set
             disestablished_municipalities.add(fact.get("old_demarcation.code"))
 
     # Process the relevant persons
-    for person_data in executive_mayors.get("data", []):
-        if person_data.get("role.role") in ROLES_OF_INTEREST:
-            municipality_code = person_data.get("municipality.demarcation_code")
+    for person_data in officials:
+        municipality_code = person_data.get("municipality.demarcation_code")
 
-            # Skip the municipality code if it was disestablished
-            if municipality_code in disestablished_municipalities:
-                continue
+        # Skip the municipality code if it was disestablished
+        if municipality_code in disestablished_municipalities:
+            continue
 
-            name = person_data.get("contact_details.name")
+        name = person_data.get("contact_details.name")
 
-            entity = context.make("Person")
-            entity.id = context.make_id(name, municipality_code)
-            entity.add("name", name)
-            entity.add("title", person_data.get("contact_details.title"))
-            entity.add(
-                "email", clean_emails(person_data.get("contact_details.email_address"))
-            )
-            entity.add(
-                "phone", clean_phones(person_data.get("contact_details.phone_number"))
-            )
+        entity = context.make("Person")
+        entity.id = context.make_id(name, municipality_code)
+        entity.add("name", name)
+        entity.add("title", person_data.get("contact_details.title"))
+        entity.add(
+            "email", clean_emails(person_data.get("contact_details.email_address"))
+        )
+        entity.add(
+            "phone", clean_phones(person_data.get("contact_details.phone_number"))
+        )
 
-            role = person_data.get("role.role")
+        role = person_data.get("role.role")
 
-            position = h.make_position(
-                context,
-                role,
-                country="za",
-                topics=["gov.muni"],
-                subnational_area=municipality_code,
-            )
+        is_pep = None
+        role_res = context.lookup("roles", role)
+        if role_res:
+            is_pep = role_res.is_pep
+        else:
+            context.log.warning("Uncategorised role", role=role)
 
-            categorisation = categorise(context, position, True)
+        position = h.make_position(
+            context,
+            role,
+            country="za",
+            topics=["gov.muni"],
+            subnational_area=municipality_code,
+        )
 
-            if categorisation.is_pep:
-                occupancy = h.make_occupancy(
-                    context,
-                    entity,
-                    position,
-                    # no_end_implies_current=False,
-                )
+        categorisation = categorise(context, position, is_pep)
 
-            if occupancy:
-                context.emit(entity, target=True)
-                context.emit(position)
-                context.emit(occupancy)
+        if not categorisation.is_pep:
+            continue
+        occupancy = h.make_occupancy(
+            context,
+            entity,
+            position,
+        )
+
+        if occupancy:
+            context.emit(entity, target=True)
+            context.emit(position)
+            context.emit(occupancy)
