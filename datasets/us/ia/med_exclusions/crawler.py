cffa8f1d33833069f9416bd8639283a245e70405
Bruno Görresen Mello <bruno@aetos.tech>
2024-09-01 17:22:23 -0300

    applying same changes as US Kansas med exclusions crawler

--- datasets/us/ia/med_exclusions/crawler.py
+++ datasets/us/ia/med_exclusions/crawler.py
@@ -1,94 +1,61 @@
-from typing import Dict, Generator
+from typing import Dict
 from rigour.mime.types import XLSX
 from openpyxl import load_workbook
-from normality import slugify, stringify
-from datetime import datetime
-import openpyxl
 
 from zavod import Context, helpers as h
 
 
-def parse_sheet(
-    context: Context,
-    sheet: openpyxl.worksheet.worksheet.Worksheet,
-    skiprows: int = 0,
-) -> Generator[dict, None, None]:
-    headers = None
-
-    row_counter = 0
-
-    for row in sheet.iter_rows():
-        # Increment row counter
-        row_counter += 1
-
-        # Skip the desired number of rows
-        if row_counter <= skiprows:
-            continue
-        cells = [c.value for c in row]
-        if headers is None:
-            headers = []
-            for idx, cell in enumerate(cells):
-                if cell is None:
-                    cell = f"column_{idx}"
-                headers.append(slugify(cell))
-            continue
-
-        record = {}
-        for header, value in zip(headers, cells):
-            if isinstance(value, datetime):
-                value = value.date()
-            record[header] = stringify(value)
-        if len(record) == 0:
-            continue
-        yield record
-
-
 def crawl_item(row: Dict[str, str], context: Context):
 
-    enrollment_type = row.pop("enrollment-type")
+    enrollment_type = row.pop("enrollment_type")
 
     if enrollment_type is None:
         return
 
     if enrollment_type == "Individual":
-        first_name = row.pop("first-name")
-        last_name = row.pop("last-name")
+        first_name = row.pop("first_name")
+        last_name = row.pop("last_name")
         entity = context.make("Person")
-        entity.id = context.make_id(first_name, last_name)
+        entity.id = context.make_id(first_name, last_name, row.get("npi"))
         h.apply_name(entity, first_name=first_name, last_name=last_name)
     elif enrollment_type == "Organization":
-        business_name = row.pop("legal-business-name")
+        business_name = row.pop("legal_business_name")
         entity = context.make("Organization")
-        entity.id = context.make_id(business_name)
+        entity.id = context.make_id(business_name, row.get("npi"))
         entity.add("name", business_name)
     else:
         context.log.warning("Enrollment type not recognized: " + enrollment_type)
         return
 
     if row.get("npi"):
-        entity.add("notes", "NPI: " + row.pop("npi"))
+        entity.add("npiCode", row.pop("npi"))
 
-    entity.add("topics", "sanction")
-    entity.add("description", "State license type/number: {}/{}".format(row.pop("state-license-type"), row.pop("state-license-number")))
+    entity.add("topics", "debarment")
+    entity.add(
+        "description",
+        "State license type/number: {}/{}".format(
+            row.pop("state_license_type"), row.pop("state_license_number")
+        ),
+    )
     entity.add("sector", row.pop("specialty"))
 
     sanction = h.make_sanction(context, entity)
     sanction.add(
         "startDate",
-        h.parse_date(row.pop("effective-date"), formats=["%Y-%m-%d", "%m/%d/%Y"]),
+        h.parse_date(row.pop("effective_date"), formats=["%Y-%m-%d", "%m/%d/%Y"]),
     )
     sanction.add("reason", row.pop("authority"))
-    sanction.add("description", row.pop("type-of-sanction"))
+    sanction.add("description", row.pop("type_of_sanction"))
 
-    if row.get("sanction-end-date") not in ["Indefinite", "Federal Authority"]:
+    if row.get("sanction_end_date") not in ["Indefinite", "Federal Authority"]:
         sanction.add(
             "endDate",
             h.parse_date(
-                row.pop("sanction-end-date"), formats=["%Y-%m-%d", "%m/%d/%Y"]
+                row.pop("sanction_end_date"), formats=["%Y-%m-%d", "%m/%d/%Y"]
             ),
         )
     else:
-        row.pop("sanction-end-date")
+        row.pop("sanction_end_date")
 
     context.emit(entity, target=True)
     context.emit(sanction)
@@ -96,7 +63,7 @@ def crawl_item(row: Dict[str, str], context: Context):
     context.audit_data(
         row,
         ignore=[
-            "eligible-to-reapply-date",
+            "eligible_to_reapply_date",
         ],
     )
 
@@ -115,5 +82,5 @@ def crawl(context: Context) -> None:
 
     wb = load_workbook(path, read_only=True)
 
-    for item in parse_sheet(context, wb.active, skiprows=1):
+    for item in h.parse_xlsx_sheet(context, wb.active, skiprows=1):
         crawl_item(item, context)
