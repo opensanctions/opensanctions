49d777424ccc8694d70bb8dea3832244858315ad
JD Bothma <jbothma@gmail.com>
2024-09-05 09:54:59 +0100

    Nip, cut

--- datasets/tr/fcib/crawler.py
+++ datasets/tr/fcib/crawler.py
@@ -1,8 +1,7 @@
-from datetime import datetime, date
 from lxml import etree
-from normality import collapse_spaces, stringify, slugify
+from normality import collapse_spaces
 from openpyxl import load_workbook
-from typing import Dict, Iterable
+from typing import Dict
 import re
 from pantomime.types import XLSX
 
@@ -18,17 +17,20 @@ DOCX_LINK = "https://ms.hmb.gov.tr/uploads/sites/2/2024/08/A-BIRLESMIS-MILLETLER
 XLSX_LINKS = [
     (
         "https://ms.hmb.gov.tr/uploads/sites/2/2024/05/B-YABANCI-ULKE-TALEPLERINE-ISTINADEN-MALVARLIKLARI-DONDURULANLAR-6415-SAYILI-KANUN-6.-MADDE.xlsx",
-        "Asset freezes pursuant to Article 6 of Law No. 6415, targeting individuals and entities based on requests made by foreign governments.",
+        "Asset freezes pursuant to Article 6 of Law No. 6415, targeting individuals"
+        " and entities based on requests made by foreign governments.",
         "B - Foreign government requests",
     ),
     (
         "https://ms.hmb.gov.tr/uploads/sites/2/2024/09/C-IC-DONDURMA-KARARI-ILE-MALVARLIKLARI-DONDURULANLAR-6415-SAYILI-KANUN-7.-MADDE-2.xlsx",
-        "Asset freezes pursuant to Article 7 of Law No. 6415, targeting individuals and entities through domestic legal actions and decisions.",
+        "Asset freezes pursuant to Article 7 of Law No. 6415, targeting individuals"
+        " and entities through domestic legal actions and decisions.",
         "C - Domestic legal actions",
     ),
     (
         "https://ms.hmb.gov.tr/uploads/sites/2/2024/05/D-7262-SAYILI-KANUN-3.A-VE-3.B-MADDELERI.xlsx",
-        "Asset freezes within the scope of Articles 3.A and 3.B of Law No. 7262, aimed at preventing the financing of proliferation of weapons of mass destruction.",
+        "Asset freezes within the scope of Articles 3.A and 3.B of Law No. 7262,"
+        " aimed at preventing the financing of proliferation of weapons of mass destruction.",
         "D - Prevention of proliferation of weapons of mass destruction",
     ),
 ]
@@ -50,49 +52,7 @@ SPLITS = [
     "n)",
 ]
 
-NEW_LINE_SPLIT = ["\n"] + SPLITS
-
-ADDRESS_SPLITS = (
-    [
-        "Şube 1:",
-        "Şube 2:",
-        "Şube 3:",
-        "Şube 4:",
-        "Şube 5:",
-        "Şube 6:",
-        "Şube 7:",
-        "Şube 8:",
-        "Şube 9:",
-        "Şube 10:",
-        "Şube 11:",
-    ]
-    + SPLITS
-    + NEW_LINE_SPLIT
-)
-
-REGEX_ID_NUMBER = re.compile(r"([A-Z]{0,2}[0-9/-]{6,20})")
 REGEX_GAZZETE_DATE = re.compile(r"(\d{2}\.\d{2}\.\d{4})")
-
-
-def clean_id_numbers(passport_numbers):
-    return REGEX_ID_NUMBER.findall(passport_numbers)
-
-
-def parse_birth_date(birth_date: str) -> str:
-    """Parses the birth date and returns only the year if the date is January 1st."""
-    # Check if the birth date ends with '01-01' indicating January 1st
-    if birth_date.endswith("01-01"):
-        # Return only the year part
-        return birth_date[:4]
-    return birth_date
-
-
-def clean_row(row: Dict[str, str]) -> Dict[str, str]:
-    """Clean non-standard spaces from row keys and values."""
-    return {
-        k.replace("\xa0", " ").strip(): v.replace("\xa0", " ").strip()
-        for k, v in row.items()
-    }
 
 
 def normalize_header(header: str) -> str:
@@ -102,15 +62,6 @@ def normalize_header(header: str) -> str:
     # Replace newlines and tabs with spaces, then collapse multiple spaces into one
     header = header.replace("\n", " ").replace("\r", " ").replace("\t", " ")
     return collapse_spaces(header).strip()
-
-
-def str_cell(cell: object) -> str:
-    """Convert a cell value to string, handling dates."""
-    if cell is None:
-        return ""
-    if isinstance(cell, (datetime, date)):
-        return cell.isoformat()
-    return str(cell)
 
 
 def crawl_row(context: Context, row: Dict[str, str], program: str):
@@ -191,23 +142,6 @@ def crawl_row(context: Context, row: Dict[str, str], program: str):
         context.emit(sanction)
 
 
-def parse_sheet(context: Context, sheet) -> Iterable[Dict[str, str]]:
-    first_row = list(sheet.iter_rows(min_row=1, max_row=1))[0]
-    headers = []
-    for cell in first_row:
-        normalized_header = normalize_header(str(cell.value))
-        header = context.lookup_value("columns", normalized_header)
-        if header is None:
-            context.log.warning(
-                "Unknown column title", column=normalized_header, sheet=sheet.title
-            )
-            header = slugify(str(cell.value))  # Use slugified header if no match found
-        headers.append(header)
-
-    for cells in sheet.iter_rows(min_row=2, values_only=True):
-        yield {headers[i]: stringify(cells[i]) for i in range(len(headers))}
-
-
 def crawl_xlsx(context: Context, url: str, program: str, short_name: str):
     path = context.fetch_resource(f"{short_name}.xlsx", url)
     context.export_resource(
@@ -218,7 +152,7 @@ def crawl_xlsx(context: Context, url: str, program: str, short_name: str):
     wb = load_workbook(path, read_only=True)
     for sheet in wb.worksheets:
         context.log.info(f"Processing sheet {sheet.title} with program {program}")
-        for row in parse_sheet(context, sheet):
+        for row in h.parse_xlsx_sheet(context, sheet, header_lookup="columns"):
             crawl_row(context, row, program)
 
 
@@ -227,41 +161,31 @@ def unblock_validator(doc: etree._Element) -> bool:
 
 
 def crawl(context: Context):
-    # Fetch the main page HTML
+    # Use browser to render javascript-based frontend
     doc = fetch_html(context, context.data_url, unblock_validator, cache_days=3)
     doc.make_links_absolute(context.data_url)
-
-    # Find the table with the relevant links
     table = doc.find('.//table[@class="table table-bordered"]')
-
-    # Find all the <a> tags within the table
-    links = table.findall(".//a")
-
-    # Construct the full URLs
-    full_urls = [link.get("href") for link in links]
+    category_urls = [link.get("href") for link in table.findall(".//a")]
 
     found_links = set()
-    # Print all the parsed links
-    for url in full_urls:
+
+    for url in category_urls:
         section_doc = fetch_html(context, url, unblock_validator, cache_days=3)
 
         # Determine whether to search for .docx or .xlsx based on URL
         if "5madde_ing" in url:
-            # Look for .docx link
             doc_links = section_doc.xpath('//a[contains(@href, ".docx")]')
         else:
-            # Look for .xlsx link
             doc_links = section_doc.xpath('//a[contains(@href, ".xlsx")]')
 
-            # Extract and construct full document links
         for doc_link in doc_links:
             found_links.add(doc_link.get("href"))
 
-    # Check if all expected links are found
     expected_links = set([DOCX_LINK, *(link for link, _, _ in XLSX_LINKS)])
+    # Check if new links have appeared
     new_links = found_links - expected_links
     assert not new_links, f"Unexpected links found: {new_links}"
-
+    # Check if all expected links are found
     missing_links = expected_links - found_links
     assert not missing_links, f"Expected links not found: {missing_links}"
 
