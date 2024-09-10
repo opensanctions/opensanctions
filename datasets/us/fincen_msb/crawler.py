55398a40422099648128e6929377d3cdc345286d
JD Bothma <jbothma@gmail.com>
2024-09-10 08:01:31 +0100

    One man's TSV is another man's XLS

--- datasets/us/fincen_msb/crawler.py
+++ datasets/us/fincen_msb/crawler.py
@@ -1,7 +1,8 @@
+from csv import DictReader
 import xlrd
 from datetime import datetime
 from typing import Dict, List
-from rigour.mime.types import XLS
+from rigour.mime.types import CSV
 from normality import collapse_spaces, stringify
 from zavod import Context
 from zavod import helpers as h
@@ -42,20 +43,9 @@ def crawl_xls(context: Context, path: str):
 
 def crawl(context: Context):
     # Perform the POST request with headers
-    response = context.http.post(context.data_url, data={"site": "AA"}, stream=True)
-
-    # Debug: Print the Content-Type to ensure we're getting the correct file
-    content_type = response.headers.get("Content-Type")
-    print(f"Content-Type: {content_type}")
-
-    # Debug: Print the first few characters of the HTML content for troubleshooting
-    content_preview = response.raw.read(100)
-    print(f"Received content preview: {content_preview.decode(errors='ignore')}")
-
-    # Save the Excel file and process it
-    path = context.get_resource_path("source.xls")
-    with open(path, "wb") as file:
-        file.write(response.content)
-
-    context.export_resource(path, XLS, title=context.SOURCE_TITLE)
-    crawl_xls(context, path)
+    path = context.fetch_resource("source.tsv", "https://msb.fincen.gov/retrieve.msb.list.php", data={"site": "AA"}, method="POST")
+    context.export_resource(path, CSV, title=context.SOURCE_TITLE)
+    with open(path) as fh:
+        reader = DictReader(fh, delimiter="\t")
+        for row in reader:
+            print(row)
