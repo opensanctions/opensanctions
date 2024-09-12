256bd87f956b51ef387769af523b425c0b0f4e1e
Bruno Görresen Mello <bruno@aetos.tech>
2024-09-12 15:15:00 -0300

    fixing for case when there is only a person

--- datasets/us/mi/med_exclusions/crawler.py
+++ datasets/us/mi/med_exclusions/crawler.py
@@ -7,60 +7,71 @@ from zavod import Context, helpers as h
 
 def crawl_item(row: Dict[str, str], context: Context):
 
-    if not row.get("entity_name"):
-        return
+    sanction_date = row.pop("sanction_date1")
+    reason = row.pop("reason")
+    license = row.pop("license")
+    npi = row.pop("npi")
+    provider_category = row.pop("provider_category")
+    source = row.pop("sanction_source")
 
-    entity_name = row.pop("entity_name")
-    entity = context.make("Company")
-    entity.id = context.make_id(entity_name, row.get("npi"))
+    if entity_name := row.pop("entity_name"):
+        entity = context.make("Company")
+        entity.id = context.make_id(entity_name, row.get("npi"))
+        entity.add("name", entity_name)
 
-    if row.get("first_name"):
+        entity.add("npiCode", npi)
+
+        entity.add("topics", "debarment")
+        entity.add("sector", provider_category)
+
+        if license:
+            entity.add("description", "License number: " + license)
+
+        sanction = h.make_sanction(context, entity)
+        h.apply_date(sanction, "startDate", sanction_date)
+        sanction.add("publisher", source)
+        sanction.add("reason", reason)
+
+        context.emit(entity, target=True)
+        context.emit(sanction)
+
+    if last_name := row.pop("last_name"):
         person = context.make("Person")
         person.id = context.make_id(
-            row.get("first_name"), row.get("middle_name"), row.get("last_name"), row.get("npi"), row.pop("city")
+            row.get("first_name"),
+            row.get("middle_name"),
+            last_name,
+            npi,
+            row.pop("city"),
+        )
+
+        h.apply_name(
+            person,
+            first_name=row.pop("first_name"),
+            middle_name=row.pop("middle_name"),
+            last_name=last_name,
         )
 
         person.add("topics", "debarment")
+        person.add("npiCode", npi)
+        person.add("sector", provider_category)
+        person_sanction = h.make_sanction(context, person)
+        h.apply_date(person_sanction, "startDate", sanction_date)
+        person_sanction.add("publisher", source)
+        person_sanction.add("reason", reason)
+        context.emit(person, target=True)
+        context.emit(person_sanction)
+
+    if last_name and entity_name:
 
         link = context.make("UnknownLink")
         link.id = context.make_id(person.id, entity.id)
         link.add("object", entity)
         link.add("subject", person)
 
-        person_sanction = h.make_sanction(context, person)
-        person_sanction.add(
-            "startDate",
-            h.parse_date(row.get("sanction_date1"), formats=["%Y-%m-%d", "%m/%d/%Y"]),
-        )
-        person_sanction.add("publisher", row.get("sanction_source"))
-        person_sanction.add("reason", row.get("reason"))
-
-        context.emit(person, target=True)
-        context.emit(person_sanction)
         context.emit(link)
 
-    entity.add("name", entity_name)
-
-    entity.add("npiCode", row.pop("npi"))
-
-    entity.add("topics", "debarment")
-    entity.add("sector", row.pop("provider_category"))
-
-    if row.get("license"):
-        entity.add("description", "License number: " + row.pop("license"))
-
-    sanction = h.make_sanction(context, entity)
-    sanction.add(
-        "startDate",
-        h.parse_date(row.pop("sanction_date1"), formats=["%Y-%m-%d", "%m/%d/%Y"]),
-    )
-    sanction.add("publisher", row.pop("sanction_source"))
-    sanction.add("reason", row.pop("reason"))
-
-    context.emit(entity, target=True)
-    context.emit(sanction)
-
-    context.audit_data(row, ignore=["sanction_date2"])
+    context.audit_data(row, ignore=["city", "sanction_date2"])
 
 
 def crawl_excel_url(context: Context):
