e5863664bb52cd6becb2a024d7a727370639ae1b
nvmbrasserie <ivan@opensanctions.org>
2024-09-23 18:20:28 +0200

    working version

--- datasets/ir/business_registry/crawler.py
+++ datasets/ir/business_registry/crawler.py
@@ -1,7 +1,8 @@
-from lxml import html
-from rigour.mime.types import HTML
 from urllib.parse import urljoin
+
+
 from zavod import Context, helpers as h
+
 
 URL = "https://www.unitedagainstnucleariran.com/iran-business-registry?page={}"
 
@@ -9,21 +10,18 @@ URL = "https://www.unitedagainstnucleariran.com/iran-business-registry?page={}"
 def crawl(context: Context):
     page_number = 0
     pages_processed = 0
-    max_pages = 10
-    found_data = False
 
     while True:
         # Construct the URL for the current page
         url = URL.format(page_number)
-        print(f"Fetching URL: {url}")  # Debug line to show progress
+        context.log.info(f"Fetching URL: {url}")
+
         doc = context.fetch_html(url)
         table = doc.find(".//div[@class='view-content']//table")
 
         if table is None:
             print("No more tables found.")
             break
-
-        found_data = True
 
         # Iterate over rows in the table
         rows = table.findall(".//tr")
@@ -56,16 +54,10 @@ def crawl(context: Context):
                 if stock_symbol_elem is not None
                 else None
             )
-
-            # Extract the withdrawn status
-            withdrawn_status_elem = row.find(
-                ".//td[@headers='view-status-of-business-table-column']"
+            withdrawn_status_elem = row.xpath(
+                ".//td[@headers='view-status-of-business-table-column']//div[@class='featured']"
             )
-            withdrawn_status = (
-                withdrawn_status_elem.text_content().strip()
-                if withdrawn_status_elem is not None
-                else None
-            )
+            is_withdrawn = bool(withdrawn_status_elem)
 
             # Only create an entity if we have a company name and nationality
             if company_name and nationality:
@@ -73,10 +65,10 @@ def crawl(context: Context):
                 entity.id = context.make_id(company_name, nationality)
                 entity.add("name", company_name)
                 entity.add("country", nationality)
-                entity.add("website", company_link)
+                entity.add("sourceUrl", company_link)
                 entity.add("description", stock_symbol)
-                if withdrawn_status is None:
-                    entity.add("topics", "trade.risk")
+                if is_withdrawn is False:
+                    entity.add("topics", "export.risk")
 
                 context.emit(entity, target=True)
                 context.audit_data(row)
@@ -84,10 +76,4 @@ def crawl(context: Context):
         page_number += 1
         pages_processed += 1
 
-        # Check if we should stop crawling
-        if pages_processed >= max_pages:
-            print("Reached the maximum number of pages to process.")
-            break
-
-    if not found_data:
-        context.log.error("Could not find any tables with company data!")
+        assert pages_processed <= 4, "More pages than expected."
