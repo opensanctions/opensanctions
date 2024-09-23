310b94dc15932584692267d2b3f96e91bb73d099
JD Bothma <jbothma@gmail.com>
2024-09-23 13:59:47 +0100

    Fix dates, add address, country

--- datasets/us/mi/med_exclusions/crawler.py
+++ datasets/us/mi/med_exclusions/crawler.py
@@ -7,28 +7,38 @@ from zavod import Context, helpers as h
 
 def crawl_item(row: Dict[str, str], context: Context):
 
-    sanction_date = row.pop("sanction_date1")
+    sanction_date_1 = row.pop("sanction_date1")
+    sanction_date_1 = sanction_date_1.split() if sanction_date_1 else None
+    sanction_date_2 = row.pop("sanction_date2")
+    sanction_date_2 = sanction_date_2.split() if sanction_date_2 else None
     reason = row.pop("reason")
     license = row.pop("license")
     npi = row.pop("npi")
     provider_category = row.pop("provider_category")
     source = row.pop("sanction_source")
+    city = row.pop("city")
+    address = h.make_address(context, city=city, state="MI", country_code="US")
 
+    entity = None
+    person = None
     if entity_name := row.pop("entity_name"):
         entity = context.make("Company")
         entity.id = context.make_id(entity_name, row.get("npi"))
         entity.add("name", entity_name)
-
         entity.add("npiCode", npi)
-
         entity.add("topics", "debarment")
         entity.add("sector", provider_category)
-
+        h.copy_address(entity, address)
+        h.apply_address(context, entity, address)
+        entity.add("country", "US")
         if license:
             entity.add("description", "License number: " + license)
 
         sanction = h.make_sanction(context, entity)
-        h.apply_date(sanction, "startDate", sanction_date)
+        if sanction_date_1:
+            h.apply_dates(sanction, "startDate", sanction_date_1)
+        if sanction_date_2:
+            h.apply_dates(sanction, "startDate", sanction_date_2)
         sanction.add("publisher", source)
         sanction.add("reason", reason)
 
@@ -42,23 +52,29 @@ def crawl_item(row: Dict[str, str], context: Context):
             row.get("middle_name"),
             last_name,
             npi,
-            row.pop("city"),
+            city,
         )
-
         h.apply_name(
             person,
             first_name=row.pop("first_name"),
             middle_name=row.pop("middle_name"),
             last_name=last_name,
         )
-
         person.add("topics", "debarment")
         person.add("npiCode", npi)
         person.add("sector", provider_category)
+        h.copy_address(person, address)
+        h.apply_address(context, person, address)
+        person.add("country", "US")
+
         person_sanction = h.make_sanction(context, person)
-        h.apply_date(person_sanction, "startDate", sanction_date)
+        if sanction_date_1:
+            h.apply_dates(person_sanction, "startDate", sanction_date_1)
+        if sanction_date_2:
+            h.apply_dates(person_sanction, "startDate", sanction_date_2)
         person_sanction.add("publisher", source)
         person_sanction.add("reason", reason)
+
         context.emit(person, target=True)
         context.emit(person_sanction)
 
@@ -71,7 +87,7 @@ def crawl_item(row: Dict[str, str], context: Context):
 
         context.emit(link)
 
-    context.audit_data(row, ignore=["city", "sanction_date2"])
+    context.audit_data(row)
 
 
 def crawl_excel_url(context: Context):
