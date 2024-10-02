6117eb88f033aa9f39c95f383d0ec61673aabd26
JD Bothma <jbothma@gmail.com>
2024-10-02 13:26:14 +0100

    Address country, copy, small refactor

--- datasets/us/wv/med_exclusions/crawler.py
+++ datasets/us/wv/med_exclusions/crawler.py
@@ -18,7 +18,6 @@ prompt = """
 
 
 def crawl_item(row: Dict[str, str], context: Context):
-
     address = h.make_address(
         context,
         street=row.pop("address_1"),
@@ -26,27 +25,26 @@ def crawl_item(row: Dict[str, str], context: Context):
         postal_code=row.pop("zip"),
         city=row.pop("city"),
         state=row.pop("state"),
+        country_code="us",
     )
+    npi = row.pop("npi")
+    first_name = row.pop("first_name")
+    last_name = row.pop("last_name")
 
-    if not row.get("first_name"):
-        entity = context.make("Company")
-        entity.id = context.make_id(row.get("last_name"), row.get("npi"))
-        entity.add("name", row.pop("last_name"))
-
-    else:
+    if first_name:
         entity = context.make("Person")
-        entity.id = context.make_id(
-            row.get("last_name"), row.get("first_name"), row.get("npi")
-        )
-        h.apply_name(
-            entity, first_name=row.pop("first_name"), last_name=row.pop("last_name")
-        )
+        entity.id = context.make_id(first_name, first_name, npi)
+        h.apply_name(entity, first_name=first_name, last_name=last_name)
+    else:
+        entity = context.make("Company")
+        entity.id = context.make_id(last_name, npi)
+        entity.add("name", last_name)
 
-    if npi := row.pop("npi"):
-        entity.add("npiCode", npi)
+    entity.add("npiCode", h.multi_split(npi, ","))
     entity.add("topics", "debarment")
     entity.add("country", "us")
     h.apply_address(context, entity, address)
+    h.copy_address(entity, address)
 
     sanction = h.make_sanction(context, entity)
     h.apply_date(sanction, "startDate", row.pop("action_date"))
@@ -73,7 +71,7 @@ def crawl(context: Context) -> None:
     path = context.fetch_resource("source.pdf", crawl_pdf_url(context))
     context.export_resource(path, PDF, title=context.SOURCE_TITLE)
 
-    for page_path in h.make_pdf_page_images(path)[1:]:
+    for page_path in h.make_pdf_page_images(path):
         data = run_image_prompt(context, prompt, page_path)
         assert "providers" in data, data
         for item in data.get("providers", []):
