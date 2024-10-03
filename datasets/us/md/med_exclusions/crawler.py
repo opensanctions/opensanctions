1c1d7f517aa36184d3d505cc41f0b2c968c65f2b
Bruno Görresen Mello <bruno@aetos.tech>
2024-10-03 10:49:03 -0300

    using excel instead of pdf

--- datasets/us/md/med_exclusions/crawler.py
+++ datasets/us/md/med_exclusions/crawler.py
@@ -1,19 +1,8 @@
 from typing import Dict
-from rigour.mime.types import PDF
+from rigour.mime.types import XLSX
+from openpyxl import load_workbook
 
 from zavod import Context, helpers as h
-
-from zavod.shed.gpt import run_image_prompt
-
-prompt = """
-Extract structured data from the following page of a PDF document. Return 
-a JSON list (`providers`) in which each object represents an medical provider.
-Distinct record rows alternate between grey and white backgrounds. The name
-of a provider might be split onto the next row within a record.
-Each object should have the following fields: `last_name`, `first_name`,
-`specialty`, `sanction_type`, `termination_date`, `address`, `city_state_zip`.
-Return an empty string for unset fields.
-"""
 
 SANCTION_TYPES = {
     "MCFU-A": "Medicaid Fraud Control Unit Adult Abuse",
@@ -26,28 +15,38 @@ SANCTION_TYPES = {
 
 def crawl_item(row: Dict[str, str], context: Context):
 
-    address = h.make_address(
-        context, row.pop("address") + " " + row.pop("city_state_zip")
-    )
-
     if not row.get("first_name"):
         entity = context.make("Company")
-        entity.id = context.make_id(row.get("last_name"))
-        entity.add("name", row.pop("last_name"))
+        entity.id = context.make_id(row.get("last_name_organization"))
+        entity.add("name", row.pop("last_name_organization"))
     else:
         entity = context.make("Person")
-        entity.id = context.make_id(row.get("last_name"), row.get("first_name"))
+        entity.id = context.make_id(
+            row.get("last_name_organization"), row.get("first_name")
+        )
         h.apply_name(
-            entity, first_name=row.pop("first_name"), last_name=row.pop("last_name")
+            entity,
+            first_name=row.pop("first_name"),
+            last_name=row.pop("last_name_organization"),
         )
 
-    entity.add("sector", row.pop("specialty"))
+    entity.add("npiCode", row.pop("npi"))
+    entity.add("sector", row.pop("type_of_entity_profession"))
     entity.add("topics", "debarment")
     entity.add("country", "us")
-    entity.add("address", address)
+
+    if row.get("license_no"):
+        entity.add("description", "License No: " + row.pop("license_no"))
+
+    if row.get("address") or row.get("city_state_zip"):
+        address = h.make_address(
+            context,
+            (row.pop("address") or "") + " " + (row.pop("city_state_zip") or ""),
+        )
+        entity.add("address", address)
 
     sanction = h.make_sanction(context, entity)
-    h.apply_date(sanction, "startDate", row.pop("termination_date"))
+    h.apply_date(sanction, "startDate", row.pop("termination_sanction_date"))
     sanction.add("description", "Sanction Type: " + row.pop("sanction_type"))
 
     context.emit(entity, target=True)
@@ -56,11 +55,22 @@ def crawl_item(row: Dict[str, str], context: Context):
     context.audit_data(row)
 
 
+def crawl_excel_url(context: Context):
+    doc = context.fetch_html(context.data_url)
+    doc.make_links_absolute(context.data_url)
+    return doc.xpath(
+        ".//a[contains(text(), 'Maryland Medicaid Sanctioned Provider List')]"
+    )[0].get("href")
+
+
 def crawl(context: Context) -> None:
-    path = context.fetch_resource("source.pdf", context.data_url)
-    context.export_resource(path, PDF, title=context.SOURCE_TITLE)
-    for page_path in h.make_pdf_page_images(path):
-        data = run_image_prompt(context, prompt, page_path)
-        assert "providers" in data, data
-        for item in data.get("providers", []):
-            crawl_item(item, context)
+    path = context.fetch_resource("list.xlsx", crawl_excel_url(context))
+    context.export_resource(path, XLSX, title=context.SOURCE_TITLE)
+
+    wb = load_workbook(path, read_only=True)
+
+    for item in h.parse_xlsx_sheet(context, wb.active):
+        # it means the table has ended and the rest is just the sanction types
+        if item.get("last_name_organization") == "Sanction Type":
+            return
+        crawl_item(item, context)
