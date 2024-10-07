42677f1cc77c2c660bc78af95af5c11e01ac0f19
nvmbrasserie <ivan@opensanctions.org>
2024-10-07 19:05:34 +0200

    loop for all

--- datasets/th/cabinet/crawler.py
+++ datasets/th/cabinet/crawler.py
@@ -29,24 +29,30 @@ def crawl(context: Context):
     )
     main_container = doc.find(".//div[@class='wonderplugintabs-panel-inner']")
 
-    for field in main_container.findall(".//div[@class='col-md-12 col-sm-12']"):
-        # Extract the text from the first <h4>, falling back to <h5> and <h6> if necessary
-        name_field = field.find(".//h4[1]")
+    prime_minister = main_container.find(
+        ".//div[@style='min-height: 400; max-height: 600;']"  # to be updated
+    )
+    persons = main_container.findall(
+        ".//div[@style='min-height: 360; max-height: 500;']"
+    )
+    persons.append(prime_minister)
+    for person in persons:
+        name_field = person.find(".//h4[1]")
         if name_field is None or not name_field.text_content().strip():
-            name_field = field.find(".//h5[1]")
+            name_field = person.find(".//h5[1]")
             if name_field is None:
-                name_field = field.find(".//h6[1]")
+                name_field = person.find(".//h6[1]")
 
         name = name_field.text_content().strip() if name_field is not None else None
         if not name:
-            context.log.warning(f"Name missing for {field}")
+            context.log.warning(f"Name missing for {person}")
 
         # Extract the text from the second <h4>, falling back to <h5> and <h6> if necessary
-        role_field = field.find(".//h4[2]")
+        role_field = person.find(".//h4[2]")
         if role_field is None or not role_field.text_content().strip():
-            role_field = field.find(".//h5[2]")
+            role_field = person.find(".//h5[2]")
             if role_field is None:
-                role_field = field.find(".//h6[2]")
+                role_field = person.find(".//h6[2]")
 
         role = role_field.text_content().strip() if role_field is not None else None
         if not role:
@@ -58,27 +64,26 @@ def crawl(context: Context):
         person = context.make("Person")
         person.id = context.make_id(name, role)
         person.add("name", name, lang="tha")
-        # apply_translit_full_name(context, person, "kat", name, TRANSLIT_OUTPUT)
-        person.add("position", role)
+        # apply_translit_full_name(context, person, "tha", name, TRANSLIT_OUTPUT)
         person.add("topics", "role.pep")
 
         position = h.make_position(
             context,
-            name="Member of the Cabinet",
+            name=role,
             summary=position_summary,
             country="th",
             lang="tha",
             topics=["gov.executive", "gov.national"],
         )
         # apply_translit_full_name(
-        #     context, position, "kat", role, TRANSLIT_OUTPUT, POSITION_PROMPT
+        #     context, position, "tha", role, TRANSLIT_OUTPUT, POSITION_PROMPT
         # )
 
         categorisation = categorise(context, position, is_pep=True)
         if categorisation.is_pep:
             occupancy = h.make_occupancy(context, person, position)
             if occupancy:
-                context.emit(person, target=True)
+                context.emit(person)
                 context.emit(position)
                 context.emit(occupancy)
 
