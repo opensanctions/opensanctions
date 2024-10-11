e4002cfbe59bb87c4ddd68e46ca8c516c300af3e
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-10-11 18:52:49 +0200

    work on indonesia - crawler is still a mess

--- datasets/id/dttot/crawler.py
+++ datasets/id/dttot/crawler.py
@@ -1,4 +1,5 @@
 import re
+from typing import Any, Dict
 import xlrd  # type: ignore
 from lxml import etree
 from datetime import datetime
@@ -48,18 +49,19 @@ def find_last_link(context: Context, html: etree._Element) -> str:
     return last_href
 
 
-def value_or_none(row, headers, key):
-    try:
-        return row[headers[key]].value
-    except Exception:
-        return None
+def row_to_dict(row, headers: Dict[str, str]) -> dict:
+    out: Dict[str, Any] = {}
+    for norm, col in headers.items():
+        value = row[col].value
+        if isinstance(value, str):
+            value = value.strip()
+        if value is None or value == "-" or value == "":
+            continue
+        out[norm] = value
+    return out
 
 
-def row_to_dict(row, headers: dict) -> dict:
-    return {k: row[headers[k]].value for k in headers if value_or_none(row, headers, k)}
-
-
-def apply_dob(entity: Entity, value: str) -> None:
+def apply_date(entity: Entity, prop: str, value: str) -> None:
     """Dates come in arbitrary formats, but the most common ones seem to be
     01/04/1983 and 01 apr 1983. Sometimes there are multiple dates in the same cell.
     """
@@ -67,12 +69,12 @@ def apply_dob(entity: Entity, value: str) -> None:
         return None
     if isinstance(value, list):
         for v in value:
-            apply_dob(entity, v)
+            apply_date(entity, prop, v)
     elif isinstance(value, (float, int)):
-        entity.add("birthDate", h.convert_excel_date(value), original_value=str(value))
+        entity.add(prop, h.convert_excel_date(value), original_value=str(value))
     elif isinstance(value, str):
         for value in h.multi_split(value, ["atau"]):
-            h.apply_date(entity, "birthDate", value)
+            h.apply_date(entity, prop, value)
     else:
         raise ValueError(f"Unexpected value type {type(value)}")
 
@@ -90,31 +92,30 @@ def crawl(context: Context):
             val = in_header[col_idx].value.strip().lower()
         except Exception:
             continue
-        headers[context.lookup_value("headers", val)] = col_idx
+        headers[context.lookup_value("headers", val, val)] = col_idx
     for rx in range(1, sh.nrows):
         drow = row_to_dict(sh.row(rx), headers)
         item_id = drow.pop("id")
-        schema = context.lookup_value("type", drow["type"], "LegalEntity")
+        schema = context.lookup_value("type", drow.pop("type"), "LegalEntity")
         entity = context.make(schema)
         names = h.multi_split(drow.pop("name"), ["alias", "ALIAS"])
         entity.id = context.make_id(item_id, *names)
-        sanction = h.make_sanction(context, entity)
-        sanction.add("authorityId", item_id)
         entity.add("topics", "sanction")
         entity.add("name", names[0])
         entity.add("alias", names[1:])
         if addr := drow.pop("address", None):
+            addr = str(addr).strip("-").strip()
             for addr in addr_delim.split(addr):
-                h.apply_address(
-                    context, entity, h.make_address(context, addr, lang="ind")
-                )
+                entity.add("address", addr, lang="ind")
         entity.add("country", drow.pop("country", None))
         entity.add("notes", drow.pop("description", None), lang="ind")
-        print("xxxxx", entity.schema, entity.caption)
-        entity.add_cast("Person", "birthPlace", drow.pop("birth_place", None))
-        dob_raw = drow.pop("birth_date", [])
-        if dob_raw and dob_raw != "00/00/0000":
-            entity.add_schema("Person")
-            apply_dob(entity, dob_raw)
+        if not entity.schema.is_a("Organization"):
+            entity.add_cast("Person", "birthPlace", drow.pop("birth_place", None))
+            dob_raw = drow.pop("birth_date", [])
+            if dob_raw and dob_raw != "00/00/0000":
+                entity.add_schema("Person")
+                apply_date(entity, "birthDate", dob_raw)
+        sanction = h.make_sanction(context, entity)
+        sanction.add("authorityId", item_id)
         context.emit(entity, target=True)
         context.emit(sanction)
