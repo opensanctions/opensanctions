ef1018a7295b9bb4b6e4d530b5405a4f46c93b78
nvmbrasserie <ivan@opensanctions.org>
2024-10-14 15:40:22 +0200

    gb_hmt_sanctions

--- datasets/gb/hmt/crawler.py
+++ datasets/gb/hmt/crawler.py
@@ -8,7 +8,6 @@ import re
 from zavod import Context
 from zavod import helpers as h
 
-FORMATS = ["%d/%m/%Y", "00/%m/%Y", "%m/%Y", "00/00/%Y", "%Y"]
 COUNTRY_SPLIT = ["(1)", "(2)", "(3)", "(4)", "(5)", "(6)", "(7)", ". "]
 REGEX_POSTCODE = re.compile(r"\d+")
 
@@ -98,14 +97,12 @@ def parse_row(context: Context, row: Dict[str, Any]):
     sanction = h.make_sanction(context, entity)
     sanction.add("program", row.pop("RegimeName"))
     sanction.add("authority", row.pop("ListingType", None))
-    listed_date = h.parse_date(row.pop("DateListed"), FORMATS)
-    sanction.add("listingDate", listed_date)
-    designated_date = h.parse_date(row.pop("DateDesignated", None), FORMATS)
-    sanction.add("startDate", designated_date)
+    h.apply_date(sanction, "listingDate", row.get("DateListed"))
 
-    entity.add("createdAt", listed_date)
+    h.apply_date(entity, "startDate", row.get("DateDesignated", None))
+    h.apply_date(entity, "createdAt", row.get("DateListed"))
     if not entity.has("createdAt"):
-        entity.add("createdAt", designated_date)
+        h.apply_date(entity, "createdAt", row.get("DateDesignated", None))
 
     sanction.add("authorityId", row.pop("UKSanctionsListRef", None))
     sanction.add("unscId", row.pop("UNRef", None))
@@ -114,9 +111,8 @@ def parse_row(context: Context, row: Dict[str, Any]):
     sanction.add("summary", row.pop("GroupSanctions", None))
     sanction.add("modifiedAt", row.pop("DateAdditionalSanctions", None))
 
-    last_updated = h.parse_date(row.pop("LastUpdated"), FORMATS)
-    sanction.add("modifiedAt", last_updated)
-    entity.add("modifiedAt", last_updated)
+    h.apply_date(sanction, "modifiedAt", row.get("LastUpdated"))
+    h.apply_date(entity, "modifiedAt", row.get("LastUpdated"))
 
     # TODO: derive topics and schema from this??
     entity_type = row.pop("Entity_Type", None)
@@ -156,7 +152,7 @@ def parse_row(context: Context, row: Dict[str, Any]):
     pobs = split_items(row.pop("Individual_TownOfBirth", None))
     entity.add_cast("Person", "birthPlace", pobs)
 
-    dob = h.parse_date(row.pop("Individual_DateOfBirth", None), FORMATS)
+    dob = h.extract_date(context, row.pop("Individual_DateOfBirth", None))
     entity.add_cast("Person", "birthDate", dob)
 
     cob = parse_countries(row.pop("Individual_CountryOfBirth", None))
