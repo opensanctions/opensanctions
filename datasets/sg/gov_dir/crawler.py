6ad335f9238f42be315e46d71e228f2c742868d1
nvmbrasserie <ivan@opensanctions.org>
2024-10-21 15:38:59 +0200

    iteration over both links

--- datasets/sg/gov_dir/crawler.py
+++ datasets/sg/gov_dir/crawler.py
@@ -6,8 +6,12 @@ from zavod.shed.zyte_api import fetch_html
 
 
 TITLE_REGEX = re.compile(
-    r"^(Mr|Ms|Miss|Mrs|Prof|Dr|Professor|Er. Dr.|Dr.|A/Prof|Clinical A/Prof|Adj A/Prof|Assoc Prof \(Ms\)|Er. Prof.| Er Prof)\.? (?P<name>.+)$"
+    r"^(Mr|Ms|Miss|Mrs|Prof|Dr|Professor Sir|Professor|Er. Dr.|Dr.|A/Prof|Clinical A/Prof|Adj A/Prof|Assoc Prof \(Ms\)|Er. Prof.| Er Prof)\.? (?P<name>.+)$"
 )
+DATA_URLS = [
+    "https://www.sgdi.gov.sg/statutory-boards",
+    "https://www.sgdi.gov.sg/ministries",
+]
 
 
 def unblock_validator(doc) -> bool:
@@ -19,121 +23,130 @@ def board_unblock_validator(doc) -> bool:
 
 
 def crawl(context: Context):
-    doc = fetch_html(
-        context,
-        context.data_url,
-        unblock_validator,
-        html_source="httpResponseBody",
-        cache_days=3,
-    )
-    statutory_boards = doc.findall(
-        ".//div[@class='directory-list']//ul[@class='ministries']//li//a"
-    )
-    boards_dict = {}
-    for board in statutory_boards:
-        link = board.get("href")
-        org_name = board.text_content().strip()
-        if link is None or org_name == "":
-            context.log.error(f"No link or name found for {org_name}")
-        boards_dict[org_name] = link
+    for url in DATA_URLS:
+        data_url = url
+        doc = fetch_html(
+            context,
+            data_url,
+            unblock_validator,
+            html_source="httpResponseBody",
+            cache_days=3,
+        )
+        statutory_boards = doc.findall(
+            ".//div[@class='directory-list']//ul[@class='ministries']//li//a"
+        )
+        boards_dict = {}
+        for board in statutory_boards:
+            link = board.get("href")
+            org_name = board.text_content().strip()
+            if link is None or org_name == "":
+                context.log.error(f"No link or name found for {org_name}")
+            boards_dict[org_name] = link
 
-    for org_name, link in boards_dict.items():
-        if link is not None:
-            board_doc = fetch_html(
-                context,
-                link,
-                board_unblock_validator,
-                html_source="httpResponseBody",
-                cache_days=3,
-            )
+        for org_name, link in boards_dict.items():
+            if link is not None:
+                board_doc = fetch_html(
+                    context,
+                    link,
+                    board_unblock_validator,
+                    html_source="httpResponseBody",
+                    cache_days=3,
+                )
 
-            org_name_elem = board_doc.find(".//div[@id='agencyName']/h1")
-            for br in org_name_elem.xpath(".//br"):
-                if br.tail is None:
-                    br.tail = "\n"
-                else:
-                    br.tail = "\n" + br.tail
-            # Get the text content and split it by newline
-            org_name = org_name_elem.text_content()
-            org_parts = org_name.split("\n")
-            # ministry = org_parts[0].strip() if len(org_parts) > 0 else ""
-            agency = org_parts[1].strip() if len(org_parts) > 1 else ""
+                org_name_elem = board_doc.find(".//div[@id='agencyName']/h1")
+                for br in org_name_elem.xpath(".//br"):
+                    if br.tail is None:
+                        br.tail = "\n"
+                    else:
+                        br.tail = "\n" + br.tail
+                # Get the text content and split it by newline
+                org_name = org_name_elem.text_content()
+                org_parts = org_name.split("\n")
+                ministry = org_parts[0].strip() if len(org_parts) > 0 else ""
+                agency = org_parts[1].strip() if len(org_parts) > 1 else ""
 
-            section_headers = board_doc.findall(".//div[@class='section-header']")
+                section_headers = board_doc.findall(".//div[@class='section-header']")
 
-            # Iterate through sections and their officials
-            for section in section_headers:
-                section_name = section.text_content().strip()
-                # Identify positions related to the current section
-                section_body = section.getnext()
-                if section_body is not None:
-                    officials = section_body.findall(".//li[@id]")
-                    for official in officials:
-                        position = (
-                            official.find(".//div[@class='rank']")
-                            .text_content()
-                            .strip()
-                        )
-                        if any(
-                            keyword in position.lower()
-                            for keyword in ["pa to", "assistant"]
-                        ):
-                            continue
-                        full_name = (
-                            official.find(".//div[@class='name']")
-                            .text_content()
-                            .strip()
-                        )
-                        email = (
-                            official.find(".//div[@class='email info-contact']")
-                            .text_content()
-                            .strip()
-                        )
-                        # phone numbers are also available
-                        if (
-                            section_name.lower() == "board members"
-                            or section_name.lower() == "council members"
-                        ):
-                            position = f"{position} of the Board of {agency}"
-                        else:
-                            position = f"{position} of {agency}"
-                        position = re.sub(
-                            "Board Member of the Board of",
-                            "Member of the Board of",
-                            position,
-                            re.I,
-                        )
-                        # print(f"Formatted Position: {position}")
-                        match = TITLE_REGEX.match(full_name)
-                        if match:
-                            full_name = match.group("name")
-                            title = match.group(1)
-
-                        person = context.make("Person")
-                        person.id = context.make_id(full_name, position)
-                        person.add("name", full_name)
-                        person.add("sourceUrl", link)
-                        person.add("topics", "role.pep")
-                        person.add("title", title)
-                        if email is not None:
-                            if email.startswith("https://"):
-                                person.add("website", email)
+                # Iterate through sections and their officials
+                for section in section_headers:
+                    section_name = section.text_content().strip()
+                    # Identify positions related to the current section
+                    section_body = section.getnext()
+                    if section_body is not None:
+                        officials = section_body.findall(".//li[@id]")
+                        for official in officials:
+                            position = (
+                                official.find(".//div[@class='rank']")
+                                .text_content()
+                                .strip()
+                            )
+                            if any(
+                                keyword in position.lower()
+                                for keyword in ["pa to", "assistant", "secretary to", "please contact"]
+                            ):
+                                continue
+                            full_name = (
+                                official.find(".//div[@class='name']")
+                                .text_content()
+                                .strip()
+                            )
+                            email = (
+                                official.find(".//div[@class='email info-contact']")
+                                .text_content()
+                                .strip()
+                            )
+                            # phone numbers are also available
+                            is_ministry = data_url == "https://www.sgdi.gov.sg/ministries"
+                            is_board_member = (
+                                data_url == "https://www.sgdi.gov.sg/statutory-boards"
+                                and section_name.lower() in {"board members", "council members"}
+                            )
+                            if is_ministry:
+                                position = f"{position} of the {ministry}"
+                            elif is_board_member:
+                                position = f"{position} of the Board of the {agency}"
                             else:
-                                person.add("email", email)
+                                position = f"{position} of the {agency}"
+                            
+                            position = re.sub(
+                                "Board Member of the Board of",
+                                "Member of the Board of",
+                                position,
+                                re.I,
+                            )
+                            print(f"Formatted Position: {position}")
+                            match = TITLE_REGEX.match(full_name)
+                            title = None
+                            if match:
+                                full_name = match.group("name")
+                                title = match.group(1)
 
-                        position = h.make_position(
-                            context,
-                            name=position,
-                            country="sg",
-                            # topics=["gov.executive", "gov.national"],
-                        )
+                            person = context.make("Person")
+                            person.id = context.make_id(full_name, position)
+                            person.add("name", full_name)
+                            person.add("sourceUrl", link)
+                            person.add("topics", "role.pep")
+                            if title is not None:
+                                person.add("title", title)
+                            if email is not None:
+                                if email.startswith("https://"):
+                                    person.add("website", email)
+                                else:
+                                    person.add("email", email)
 
-                        categorisation = categorise(
-                            context, position, is_pep=True
-                        )  # adjust
-                        if categorisation.is_pep:
-                            occupancy = h.make_occupancy(context, person, position)
-                            if occupancy:
-                                context.emit(person, target=True)
-                                context.emit(position)
-                                context.emit(occupancy)
+                            position = h.make_position(
+                                context,
+                                name=position,
+                                country="sg",
+                                # topics=["gov.executive", "gov.national"],
+                            )
+
+                            categorisation = categorise(
+                                context, position, is_pep=True
+                            )
+                            if categorisation.is_pep:
+                                occupancy = h.make_occupancy(context, person, position)
+                                if occupancy:
+                                    context.emit(person, target=True)
+                                    context.emit(position)
+                                    context.emit(occupancy)
