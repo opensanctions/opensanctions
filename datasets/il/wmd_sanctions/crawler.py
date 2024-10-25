ae58fdd6860683ea8238065b279d28f1f6b85615
JD Bothma <jbothma@gmail.com>
2024-10-25 13:07:08 +0100

    Simplify name, id cleaning, consistent style with other crawlers

--- datasets/il/wmd_sanctions/crawler.py
+++ datasets/il/wmd_sanctions/crawler.py
@@ -19,19 +19,30 @@ SKIP_ROWS = {
     'הכרזות – צפון קוריאה יחידים – סה"כ 80 גורמים מוכרזים:',
     'הכרזות – צפון קוריאה ארגונים/קבוצות – סה"כ 75 גורמים מוכרזים:',
 }
+SPLITS = ["a)", "b)", "c)", "d)", "e)"]
+CHOPSKA = [
+    ("מספר זיהוי:", "idNumber"),
+    ("מספר דרכון:", "passportNumber"),
+    ("Passport no:", "passportNumber"),
+]
 
 
-def extract_passport_no(text: str):
+def apply_identifiers(entity: Entity, text: str):
     """
-    Extract passport numbers from a given text.
+    Split and add passport and ID numbers but don't try and parse fully.
     """
     if not text:
         return None
-    text = str(text)
-    pattern = r"\b[A-Z0-9]{5,}\b"
-    matches = re.findall(pattern, text)
-
-    return matches
+    if "n/a" in text.lower():
+        return None
+    items = h.multi_split(text, SPLITS)
+    for item in items:
+        for chop, prop in CHOPSKA:
+            parts = item.rsplit(chop, 1)
+            item = parts[0]
+            if len(parts) > 1:
+                entity.add(prop, parts[1].strip())
+        entity.add("passportNumber", item.strip())
 
 
 def extract_n_pop_address(text: str):
@@ -73,22 +84,10 @@ def clean_date(date_str: str):
 def apply_names(context: Context, entity: Entity, names_string: str) -> None:
     name_split = re.split(REGEX_NAME_SPLIT, names_string, flags=re.IGNORECASE)
     name = name_split[0]
-    aliases = name_split[1:]
-    parts = name.split("2:")
-    if entity.schema.is_a("Person") and len(parts) > 1:
-        last_name = parts[0].replace("1:", "").strip()
-        forenames_str = re.sub(r"\d:\s*na", "", parts[1])
-        forenames = re.split(r"\d:", forenames_str)
-        first_name = forenames.pop(0).strip()
-        second_name = forenames.pop(0).strip() if forenames else None
-        if forenames:
-            context.log.warn("More names found", names=forenames)
-        h.apply_name(
-            entity, first_name=first_name, second_name=second_name, last_name=last_name
-        )
-    else:
-        h.apply_name(entity, full=name)
-    for alias in aliases:
+    name = re.sub(r"\d:\s*(na)?", "", name)
+    h.apply_name(entity, full=name)
+
+    for alias in name_split[1:]:
         h.apply_name(entity, full=alias, alias=True)
 
 
@@ -102,69 +101,47 @@ def crawl_row(context: Context, row: Dict):
             )
         return
 
-    other_info = row.pop("other_info")
-    address_nationality = row.pop("address_nationality")
-
-    # Extract address if it exists from either the info or nationality attibrutes
-    info_address, notes = extract_n_pop_address(other_info)
-    nat_address, nationality = extract_n_pop_address(address_nationality)
-
-    address = info_address or nat_address
-    address = h.multi_split(
-        address,
-        [
-            "a)",
-            "b)",
-            "c)",
-            "d)",
-            "e)",
-        ],
-    )
-
-    dob = row.pop("dob")
-    passport = row.pop("passport")
     names_string = row.pop("name")
-
-    isreal_adoption_date = row.pop("isreal_adoption_date")
     serial_no = row.pop("serial_no")
-    originally_declared_by = row.pop("originally_declared_by")
-    declaration_date = row.pop("declaration_date")
 
-    if "iri" in serial_no.lower() or "pi" in serial_no.lower():
-        entity = context.make("Person")
-        entity.id = context.make_id("Person", serial_no, names_string)
-        entity.add("passportNumber", extract_passport_no(passport))
-        entity.add("nationality", nationality)
+    schema = context.lookup_value("schema", serial_no)
+    if schema is None:
+        context.log.warning(
+            "Entity type not recognized from serial number", serial_no=serial_no
+        )
+        schema = "LegalEntity"
+
+    entity = context.make(schema)
+    entity.id = context.make_id(serial_no, names_string)
+    apply_names(context, entity, names_string)
+    if entity.schema.is_a("Person"):
+        h.apply_dates(entity, "birthDate", clean_date(row.pop("birth_incorp_date")))
+        apply_identifiers(entity, row.pop("passports_ids"))
+    elif entity.schema.is_a("Organization"):
         h.apply_dates(
-            entity,
-            "birthDate",
-            clean_date(dob),
+            entity, "incorporationDate", clean_date(row.pop("birth_incorp_date"))
         )
 
-    elif "ire" in serial_no.lower() or "pe" in serial_no.lower():
-        entity = context.make("Organization")
-        entity.id = context.make_id("Company", f"{record_id}-{serial_no}")
+    # Extract address if it exists from either the info or nationality attibrutes
+    info_address, notes = extract_n_pop_address(row.pop("other_info"))
+    nat_address, country = extract_n_pop_address(row.pop("address_nationality"))
 
-    else:
-        context.log.warn(f"Entity not recognized from serial number: {serial_no}")
-
-    apply_names(context, entity, names_string)
+    entity.add("country", country)
     entity.add("notes", notes)
-    entity.add("address", address)
+    entity.add("address", h.multi_split(info_address, SPLITS))
+    entity.add("address", h.multi_split(nat_address, SPLITS))
     entity.add("topics", "sanction")
 
     sanction = h.make_sanction(context, entity)
-    sanction.add("authority", originally_declared_by, lang="he")
-    h.apply_dates(sanction, "listingDate", clean_date(declaration_date))
+    sanction.add("authority", row.pop("originally_declared_by"), lang="he")
     h.apply_dates(
-        sanction,
-        "startDate",
-        clean_date(isreal_adoption_date),
+        sanction, "startDate", clean_date(row.pop("isreal_temp_adoption_date"))
     )
+    h.apply_dates(sanction, "startDate", clean_date(row.pop("isreal_adoption_date")))
 
     context.emit(entity, target=True)
     context.emit(sanction)
-    context.audit_data(row, ignore=["isreal_temp_adoption_date"])
+    context.audit_data(row, ignore=["declaration_date"])
 
 
 def unblock_validator(doc):
@@ -191,18 +168,18 @@ def crawl(context: Context):
 
     wb = openpyxl.load_workbook(source_path, read_only=True)
     for row_num, row in enumerate(
-        h.parse_xlsx_sheet(
-            context,
-            wb.active,
-            header_lookup="columns",
-            skiprows=2,
-        )
+        h.parse_xlsx_sheet(context, wb.active, header_lookup="columns", skiprows=2)
     ):
         # Check that additional header rows are consistent with first row
         if row["record_id"] == 'מס"ד':
             for key, value in row.items():
                 translated_header = context.lookup_value("columns", value)
-                assert translated_header == key, (row_num, key, translated_header, value)
+                assert translated_header == key, (
+                    row_num,
+                    key,
+                    translated_header,
+                    value,
+                )
             continue
 
         crawl_row(context, row)
