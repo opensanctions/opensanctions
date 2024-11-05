6edc2f8f8b27ade2f31e71a09e3cb704a48a1ab7
JD Bothma <jbothma@gmail.com>
2024-11-05 13:07:08 +0000

    Simplify name splitting, handle more sources

--- datasets/us/la/med_exclusions/crawler.py
+++ datasets/us/la/med_exclusions/crawler.py
@@ -1,3 +1,4 @@
+from itertools import product
 from typing import Dict
 import re
 from rigour.mime.types import CSV
@@ -6,80 +7,51 @@ import csv
 from zavod import Context, helpers as h
 from zavod.shed.zyte_api import fetch_html, fetch_resource
 
-DBA_PATTERN = r"(.+)\s+dba\s+(.+)"
-AKA_PATTERN = r"(.+?)\s*(?:\(\s*aka\s*\s*\w+\s*\)|aka|AKA|,\s*AKA)\s*(.+)"
+REGEX_DBA = re.compile(r"\bdba\b", re.IGNORECASE)
+REGEX_AKA = re.compile(r"\(?a\.?k\.?a\b\.?|\)", re.IGNORECASE)
 
 
 def crawl_item(row: Dict[str, str], context: Context):
-
-    if row.pop(" Type of Exclusion") != "State":
+    if row.pop(" Type of Exclusion") == "OIG":
         return
 
     if raw_first_name := row.pop("First Name"):
         raw_last_name = row.pop(" Last Name or Entity Name")
 
-        aka_match_first = re.search(AKA_PATTERN, raw_first_name, re.IGNORECASE)
-        aliases = []
-        if aka_match_first:
-            first_name, alias = (
-                aka_match_first.group(1).strip(),
-                aka_match_first.group(2).strip(),
-            )
-            aliases.append(alias)
-        else:
-            first_name = raw_first_name
-
-        aka_match_last = re.search(AKA_PATTERN, raw_last_name, re.IGNORECASE)
-        if aka_match_last:
-            last_name, alias = (
-                aka_match_last.group(1).strip(),
-                aka_match_last.group(2).strip(),
-            )
-            aliases.append(alias)
-        else:
-            last_name = raw_last_name
         entity = context.make("Person")
         entity.id = context.make_id(raw_first_name, raw_last_name)
-        h.apply_name(
-            entity,
-            first_name=first_name,
-            last_name=last_name,
-        )
+
+        first_names = REGEX_AKA.split(raw_first_name)
+        last_names = REGEX_AKA.split(raw_last_name)
+
+        for first_name, last_name in product(first_names, last_names):
+            first_name = first_name.strip()
+            last_name = last_name.strip()
+            if not first_name or not last_name:
+                continue
+            h.apply_name(
+                entity,
+                first_name=first_name,
+                last_name=last_name,
+            )
+        if not entity.has("name"):
+            h.apply_name(entity, first_name=raw_first_name, last_name=raw_last_name)
         h.apply_date(entity, "birthDate", row.pop(" Birthdate"))
-        entity.add("alias", aliases)
     else:
         raw_name = row.pop(" Last Name or Entity Name")
-        dba_match = re.search(DBA_PATTERN, raw_name, re.IGNORECASE)
-
-        if dba_match:
-            name, dba_company_name = (
-                dba_match.group(1).strip(),
-                dba_match.group(2).strip(),
-            )
-        else:
-            name = raw_name.strip()
-            dba_company_name = None
 
         entity = context.make("Company")
         entity.id = context.make_id(raw_name)
-        entity.add("name", name)
 
-        if dba_company_name:
-            dba_company = context.make("Company")
-            dba_company.id = context.make_id(dba_company_name)
-            dba_company.add("name", dba_company_name)
-            link = context.make("UnknownLink")
-            link.id = context.make_id(entity.id, dba_company.id)
-            link.add("object", dba_company)
-            link.add("subject", entity)
-            link.add("role", "d/b/a")
-            context.emit(dba_company)
-            context.emit(link)
+        names = REGEX_DBA.split(raw_name)
+        entity.add("name", names[0])
+        entity.add("alias", names[1:])
+        row.pop(" Birthdate")
 
-    if row.get(" Affiliated Entity"):
+    if affiliate := row.pop(" Affiliated Entity").strip():
         affiliated = context.make("LegalEntity")
-        affiliated.id = context.make_id(row.get(" Affiliated Entity"))
-        affiliated.add("name", row.pop(" Affiliated Entity"))
+        affiliated.id = context.make_id(affiliate)
+        affiliated.add("name", affiliate)
         link = context.make("UnknownLink")
         link.id = context.make_id(entity.id, affiliated.id)
         link.add("object", entity)
@@ -99,14 +71,15 @@ def crawl_item(row: Dict[str, str], context: Context):
         row.pop(" NPI#")
     sanction = h.make_sanction(context, entity)
     sanction.add("reason", row.pop(" Reason for Exclusion"))
+    sanction.add("reason", row.pop(" Reason for Termination"))
     sanction.add("duration", row.pop(" Period of Exclusion"))
+    sanction.add("duration", row.pop(" Period of Enrollment Prohibition"))
     h.apply_date(sanction, "startDate", row.pop(" Effective Date"))
 
-    if row.get(" Reinstate"):
-        h.apply_date(sanction, "endDate", row.pop(" Reinstate"))
+    if (reinstate := row.pop(" Reinstate")) and ("9999" not in reinstate):
+        h.apply_date(sanction, "endDate", reinstate)
         is_debarred = False
     else:
-        row.pop(" Reinstate")
         is_debarred = True
 
     if is_debarred:
@@ -131,17 +104,18 @@ def crawl_csv_url(context: Context):
 
 
 def crawl(context: Context) -> None:
-
     csv_url = crawl_csv_url(context)
-
-    cached, path, mediatype, _charset = fetch_resource(
-        context, "source.csv", csv_url, geolocation="US"
+    _, _, _, path = fetch_resource(
+        context,
+        "source.csv",
+        csv_url,
+        expected_media_type=CSV,
+        geolocation="US",
     )
-    if not cached:
-        assert mediatype == CSV
     context.export_resource(path, CSV, title=context.SOURCE_TITLE)
 
     with open(path) as f:
-        next(f)
+        f.readline()  # Skip the date row
+
         for item in csv.DictReader(f):
             crawl_item(item, context)
