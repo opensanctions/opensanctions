3c87cc388a53e612f09c5027d052b56304a84738
JD Bothma <jbothma@gmail.com>
2024-11-11 11:25:05 +0000

    Assert sheets, handle owners

--- datasets/us/oh/med_exclusions/crawler.py
+++ datasets/us/oh/med_exclusions/crawler.py
@@ -1,3 +1,4 @@
+from itertools import product
 from typing import Dict
 from rigour.mime.types import XLSX
 from openpyxl import load_workbook
@@ -8,6 +9,7 @@ from zavod.shed.zyte_api import fetch_html, fetch_resource
 
 REGEX_DBA = re.compile(r"\bdba\b", re.IGNORECASE)
 REGEX_AKA = re.compile(r"\(?a\.?k\.?a\b\.?|\)", re.IGNORECASE)
+LIST_DOWNLOAD_XPATH = "//a[contains(text(), 'Medicaid') and contains(text(), 'Exclusion') and contains(text(), 'Suspension') and contains(@href, 'xlsx')]"
 
 
 def crawl_individual(row: Dict[str, str], context: Context):
@@ -16,15 +18,11 @@ def crawl_individual(row: Dict[str, str], context: Context):
     entity.id = context.make_id(
         row.get("last_name"), row.get("first_name"), row.get("npi")
     )
-    names = REGEX_AKA.split(row.pop("last_name"))
-    last_name = names[0]
-    alias = names[1:]
+    last_names = REGEX_AKA.split(row.pop("last_name"))
+    first_names = REGEX_AKA.split(row.pop("first_name"))
+    for first_name, last_name in product(first_names, last_names):
+        h.apply_name(entity, first_name=first_name, last_name=last_name)
 
-    names = REGEX_AKA.split(row.pop("first_name"))
-    first_name = names[0]
-    alias.extend(names[1:])
-
-    h.apply_name(entity, first_name=first_name, last_name=last_name, alias=alias)
     entity.add("country", "us")
     entity.add("sector", row.pop("provider_type"))
     entity.add("topics", "debarment")
@@ -54,13 +52,29 @@ def crawl_organization(row: Dict[str, str], context: Context):
     entity = context.make("Company")
     entity.id = context.make_id(row.get("organization_name"))
     names = REGEX_DBA.split(row.pop("organization_name"))
+    aliases = names[1:]
+    names = names[0].split("Owner:")
     entity.add("name", names[0])
-    entity.add("alias", names[1:])
+    entity.add("alias", aliases)
     entity.add("country", "us")
     entity.add("sector", row.pop("provider_type"))
     entity.add("topics", "debarment")
     if row.get("provider_id"):
         entity.add("description", "Provider ID: " + row.pop("provider_id"))
+
+    for owner_name in names[1:]:
+        owner = context.make("LegalEntity")
+        owner.id = context.make_id(row.get("organization_name"), owner_name)
+        owner.add("name", owner_name.strip())
+        owner.add("country", "us")
+
+        rel = context.make("Ownership")
+        rel.id = context.make_id(entity.id, owner.id)
+        rel.add("owner", owner)
+        rel.add("asset", entity)
+
+        context.emit(owner)
+        context.emit(rel)
 
     address = h.make_address(
         context,
@@ -92,23 +106,14 @@ def crawl_organization(row: Dict[str, str], context: Context):
 
 
 def unblock_validator(doc) -> bool:
-    return (
-        len(
-            doc.xpath(
-                "//a[contains(text(), 'Ohio') and contains(text(), 'Medicaid') and contains(text(), 'Provider') and contains(text(), 'Exclusion') and contains(text(), 'Suspension') and contains(text(), 'List')]"
-            )
-        )
-        > 0
-    )
+    return len(doc.xpath(LIST_DOWNLOAD_XPATH)) > 0
 
 
 def crawl_excel_url(context: Context):
     doc = fetch_html(
         context, context.data_url, unblock_validator=unblock_validator, geolocation="US"
     )
-    return doc.xpath(
-        "//a[contains(text(), 'Ohio') and contains(text(), 'Medicaid') and contains(text(), 'Provider') and contains(text(), 'Exclusion') and contains(text(), 'Suspension') and contains(text(), 'List')]"
-    )[0].get("href")
+    return doc.xpath(LIST_DOWNLOAD_XPATH)[0].get("href")
 
 
 def crawl(context: Context) -> None:
@@ -120,9 +125,16 @@ def crawl(context: Context) -> None:
     context.export_resource(path, XLSX, title=context.SOURCE_TITLE)
 
     wb = load_workbook(path, read_only=True)
+    sheet_names = wb.sheetnames
+    sheet_names.remove("Instructions")
 
     for item in h.parse_xlsx_sheet(context, wb["Individuals"]):
         crawl_individual(item, context)
+    sheet_names.remove("Individuals")
 
     for item in h.parse_xlsx_sheet(context, wb["Organizations"]):
         crawl_organization(item, context)
+    sheet_names.remove("Organizations")
+
+    # Detect if new sheets show up.
+    assert sheet_names == [], sheet_names
