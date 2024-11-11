e7e6858471f956b4fb69b9f5f2e881d79a3b286e
nvmbrasserie <ivan@opensanctions.org>
2024-11-11 15:02:27 +0100

    interim updates

--- datasets/us/mn/med_exclusions/crawler.py
+++ datasets/us/mn/med_exclusions/crawler.py
@@ -13,35 +13,39 @@ def crawl_item(row: Dict[str, str], context: Context):
         city=row.pop("city"),
         # In one file it is called and in the other one they use st
         state=row.pop("state", row.pop("st", None)),
-        postal_code=row.pop("zipcode", row.pop("zip", None)),
+        postal_code=row.pop("zip_code", row.pop("zip", None)),
         country_code="US",
-        street=row.pop("addressline1"),
-        street2=row.pop("addressline2"),
+        street=row.pop("address_line1"),
+        street2=row.pop("address_line2"),
     )
-
-    if "sortname" in row:
+    practice_name = row.pop("practice_name", None)
+    if "practice_name" in row:
         entity = context.make("Company")
         entity.id = context.make_id(
-            row.get("sortname"), row.get("providertypedescription")
+            row.get("sortname"), row.get("provider_type_description")
         )
-        entity.add("name", row.pop("sortname"))
+        entity.add("name", practice_name)
     else:
         entity = context.make("Person")
+        # first_name = row.pop("first_name")
+        # last_name = row.pop("last_name")
+        # middle_name = row.pop("middle_name")
+        # print(first_name, last_name, middle_name)
         entity.id = context.make_id(
-            row.get("firstname"), row.get("middlename"), row.get("lastname")
+            row.get("first_name"), row.get("middle_name"), row.get("last_name")
         )
         h.apply_name(
             entity,
-            first_name=row.pop("firstname"),
-            last_name=row.pop("lastname"),
-            middle_name=row.pop("middlename"),
+            first_name=row.pop("first_name"),
+            last_name=row.pop("last_name"),
+            middle_name=row.pop("middle_name"),
         )
     entity.add("topics", "debarment")
-    entity.add("sector", row.pop("providertypedescription"))
+    entity.add("sector", row.pop("provider_type_description"))
     entity.add("country", "us")
     h.apply_address(context, entity, address)
 
-    start_date = row.pop("effectivedateofexclusion")
+    start_date = row.pop("effective_date_of_exclusion")
     sanction = h.make_sanction(context, entity, key=start_date)
     h.apply_date(sanction, "startDate", start_date)
 
@@ -61,7 +65,11 @@ def unblock_validator(doc) -> bool:
 
 def crawl_excel_urls(context: Context):
     doc = fetch_html(
-        context, context.data_url, geolocation="US", unblock_validator=unblock_validator
+        context,
+        context.data_url,
+        geolocation="US",
+        unblock_validator=unblock_validator,
+        cache_days=2,
     )
     return doc.xpath(".//span[text()='MHCP Excluded Group Providers']/..")[0].get(
         "href"
@@ -71,30 +79,22 @@ def crawl_excel_urls(context: Context):
 
 
 def crawl(context: Context) -> None:
-
     group_url, individuals_url = crawl_excel_urls(context)
-    _, _, _, group_path = fetch_resource(
-        context,
-        "group.xlsx",
-        group_url,
-        geolocation="US",
-        expected_media_type=XLSX,
-    )
-    context.export_resource(group_path, XLSX, title=context.SOURCE_TITLE)
+    urls = [(group_url, "group"), (individuals_url, "individuals")]
 
-    wb = load_workbook(group_path, read_only=True)
-    for item in h.parse_xlsx_sheet(context, wb.active):
-        crawl_item(item, context)
+    for url, title_suffix in urls:
+        resource_name = f"{title_suffix}.xlsx"
+        _, _, _, file_path = fetch_resource(
+            context,
+            resource_name,
+            url,
+            geolocation="US",
+            expected_media_type=XLSX,
+        )
 
-    _, _, _, individuals_path = fetch_resource(
-        context,
-        "individuals.xlsx",
-        individuals_url,
-        geolocation="US",
-        expected_media_type=XLSX,
-    )
-    context.export_resource(individuals_path, XLSX, title=context.SOURCE_TITLE)
+        context.export_resource(file_path, XLSX, title=context.SOURCE_TITLE)
+        wb = load_workbook(file_path, read_only=True)
 
-    wb = load_workbook(individuals_path, read_only=True)
-    for item in h.parse_xlsx_sheet(context, wb.active):
-        crawl_item(item, context)
+        for item in h.parse_xlsx_sheet(context, wb.active):
+            print(item)
+            crawl_item(item, context)
