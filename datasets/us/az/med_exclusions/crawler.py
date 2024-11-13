dc3e1d4ecee7a2b4d8ac0cb1d1524e16459d29f0
JD Bothma <jbothma@gmail.com>
2024-11-13 08:48:37 +0000

    Fix concatenation, remove duplication

--- datasets/us/az/med_exclusions/crawler.py
+++ datasets/us/az/med_exclusions/crawler.py
@@ -45,34 +45,45 @@ def crawl(context: Context) -> None:
     items = list(h.parse_pdf_table(context, path, skiprows=1))
     manual_extraction = [False for _ in items]
 
+    # Mark rows that look like they might need manual extraction,
+    # as well as one row before and after.
     for i, item in enumerate(items):
-
         if (
             (not item.get("name_provider"))
-            or (len(item.get("name_provider").split("\n")) > 1)
-            or (len(item.get("action_type_suspend_terminate").split("\n")) > 1)
+            or "\n" in item.get("name_provider")
+            or "\n" in item.get("action_type_suspend_terminate")
         ):
-
             manual_extraction[i] = True
             if i != 0:
                 manual_extraction[i - 1] = True
+            if i != len(items) - 1:
+                manual_extraction[i + 1] = True
 
-    for i, item in enumerate(items):
-        if not manual_extraction[i]:
-            crawl_item(item, context)
-        else:
-            if i != 0:
-                joined_string = slugify(stringify(items[i - 1 : i + 1]))
+    rows = []
+    manual_strings = []
+    last_manual = manual_extraction[0]
+    if manual_extraction[0]:
+        manual_strings.append(slugify(stringify(items[0])))
+
+    # concatenate contiguous manual-extraction rows
+    for i, item in enumerate(items[1:], 1):
+        if manual_extraction[i]:
+            string = slugify(stringify(item))
+            if last_manual:
+                manual_strings[-1] += "\n" + string
             else:
-                joined_string = slugify(stringify(items[i : i + 1]))
+                manual_strings.append(string)
+        else:
+            rows.append(item)
+        last_manual = manual_extraction[i]
 
-            res = context.lookup("manual_extraction", joined_string)
+    # Look up manual extraction row values
+    for string in manual_strings:
+        res = context.lookup("manual_extraction", string)
+        if not res:
+            context.log.warning("Unable to parse: " + string)
+            continue
+        rows.extend(res.rows)
 
-            if not res:
-                context.log.warning("Unable to parse: " + joined_string)
-                continue
-
-            corrected_items = res.rows
-
-            for corrected_item in corrected_items:
-                crawl_item(corrected_item, context)
+    for row in rows:
+        crawl_item(row, context)
