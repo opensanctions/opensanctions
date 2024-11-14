70ac0a858ada06a67e5504dc5e55211c829da311
JD Bothma <jbothma@gmail.com>
2024-11-14 11:01:31 +0000

    Update excel link finding

--- datasets/us/mo/med_exclusions/crawler.py
+++ datasets/us/mo/med_exclusions/crawler.py
@@ -4,23 +4,18 @@ from openpyxl import load_workbook
 from zavod import Context
 from zavod.shed.zyte_api import fetch_html, fetch_resource
 
+EXCEL_XPATH = "//a[starts-with(@href, 'https://mmac.mo.gov/')][contains(@href, 'Sanction')][contains(@href, 'xlsx')]"
+
 
 def unblock_validator(doc) -> bool:
-    return (
-        len(
-            doc.xpath(
-                "//a[starts-with(@href, 'https://mmac.mo.gov/assets/sites') and .//b[contains(text(), 'Excel Spreadsheet')]]"
-            )
-        )
-        > 0
-    )
+    return len(doc.xpath(EXCEL_XPATH)) > 0
 
 
 def crawl_excel_url(context: Context):
     doc = fetch_html(context, context.data_url, unblock_validator=unblock_validator)
-    return doc.xpath(
-        "//a[starts-with(@href, 'https://mmac.mo.gov/assets/sites') and .//b[contains(text(), 'Excel Spreadsheet')]]"
-    )[0].get("href")
+    links = doc.xpath(EXCEL_XPATH)[0].get("href")
+    assert len(links) == 1, links
+    return links[0]
 
 
 def crawl(context: Context) -> None:
@@ -30,5 +25,5 @@ def crawl(context: Context) -> None:
         context, "source.xlsx", excel_url, expected_media_type=XLSX, geolocation="US"
     )
     context.export_resource(path, XLSX, title=context.SOURCE_TITLE)
-
-    # wb = load_workbook(path, read_only=True)
+    wb = load_workbook(path, read_only=True)
+    print(wb.sheetnames)
