6f6a376c9c7260ab0b363843824b6039784f3245
nvmbrasserie <ivan@opensanctions.org>
2024-11-18 13:57:56 +0100

    crawl_company

--- datasets/ua/war_sanctions/crawler.py
+++ datasets/ua/war_sanctions/crawler.py
@@ -1,24 +1,26 @@
 from zavod import Context, helpers as h
 
 
-# LINKS_COMPANIES = [
-#     f"https://war-sanctions.gur.gov.ua/en/kidnappers/companies?page={page}&per-page=12s",  # child kidnappers
-# ]
-
-
-LINKS_PERSONS = [
+LINKS = [
     {  # child kidnappers
         "url": "https://war-sanctions.gur.gov.ua/en/kidnappers/persons?page={page}&per-page=12",
         "max_pages": 26,
+        "type": "person",
+    },
+    {  # child kidnappers
+        "url": "https://war-sanctions.gur.gov.ua/en/kidnappers/companies?page={page}&per-page=12",
+        "max_pages": 14,
+        "type": "company",
     },
     {  # russian athletes
         "url": "https://war-sanctions.gur.gov.ua/en/sport/persons?page={page}&per-page=12",
         "max_pages": 9,
+        "type": "person",
     },
 ]
 
 
-def crawl_index_page(context: Context, index_page):
+def crawl_index_page(context: Context, index_page, data_type):
     index_page = context.fetch_html(index_page, cache_days=3)
     main_grid = index_page.find('.//div[@id="main-grid"]')
     if main_grid is not None:
@@ -27,44 +29,78 @@ def crawl_index_page(context: Context, index_page):
             for link in href:
                 if link.startswith("https:"):
                     detail_page = context.fetch_html(link, cache_days=3)
-
-                    details_container = detail_page.find(
-                        ".//div[@id='js_visibility'][@class='col-12 col-lg-9']"
-                    )
-                    if details_container is None:
-                        context.log.warning(
-                            f"Could not find details container on {link}"
+                    if data_type == "person":
+                        details_container = detail_page.find(
+                            ".//div[@id='js_visibility'][@class='col-12 col-lg-9']"
                         )
-                        continue
-                    data = {}
-                    for row in details_container.findall(".//div[@class='row']"):
-                        label_elem = row.find(
-                            ".//div[@class='col-12 col-md-4 col-lg-2 yellow']"
+                        crawl_person(context, details_container, link)
+                    if data_type == "company":
+                        details_container = detail_page.find(
+                            ".//div[@class='col-12 col-lg-9']"
                         )
-                        value_elem = row.find(
-                            ".//div[@class='col-12 col-md-8 col-lg-10']"
-                        )
-                        if value_elem is None:
-                            value_elem = row.find(
-                                ".//div[@class='js_visibility_target col-12 col-md-8 col-lg-10']"
-                            )
-                        if label_elem is not None and value_elem is not None:
-                            label = label_elem.text_content().strip().replace("\n", " ")
-                            value = value_elem.text_content().strip().replace("\n", " ")
-                            value = " ".join(value.split())
-                            value = " | ".join(
-                                [
-                                    text.strip()
-                                    for text in value_elem.itertext()
-                                    if text.strip()
-                                ]
-                            ).strip()
-
-                            data[label] = value
-                    crawl_item(context, data, link)
+                        crawl_company(context, details_container, link)
 
 
-def crawl_item(context: Context, data, link):
+def crawl_company(context: Context, details_container, link):
+    data = {}
+    for row in details_container.findall(".//div[@class='row']"):
+        label_elem = row.find(".//div[@class='col-12 col-sm-4 yellow']")
+        value_elem = row.find(".//div[@class='col-12 col-sm-8']")
+
+        if label_elem is not None and value_elem is not None:
+            label = label_elem.text_content().strip().replace("\n", " ")
+            value = value_elem.text_content().strip().replace("\n", " ")
+            value = " ".join(value.split())
+            value = " | ".join(
+                [text.strip() for text in value_elem.itertext() if text.strip()]
+            ).strip()
+            data[label] = value
+
+    name = data.pop("Name")
+    name_abbr = data.pop("Abbreviated name of the legal entity", None)
+    reg_num = data.pop("Registration number")
+
+    company = context.make("Company")
+    company.id = context.make_id(name, name_abbr, reg_num)
+    company.add("name", name)
+    company.add("name", name_abbr)
+    company.add("registrationNumber", reg_num)
+    company.add("address", data.pop("Address"))
+    company.add("country", data.pop("Country"))
+    company.add("taxNumber", data.pop("Tax Number"))
+    company.add("sourceUrl", data.pop("Links").split(" | "))
+    archive_links = data.pop("Archive links", None)
+    if archive_links is not None:
+        for archive_link in archive_links.split(" | "):
+            company.add("sourceUrl", archive_link)
+
+    company.add("topics", "sanction")
+    sanction = h.make_sanction(context, company)
+    sanction.add("reason", data.pop("Reasons"))
+    sanction.add("sourceUrl", link)
+
+    context.emit(company, target=True)
+    context.emit(sanction)
+    context.audit_data(data)
+
+
+def crawl_person(context: Context, details_container, link):
+    data = {}
+    for row in details_container.findall(".//div[@class='row']"):
+        label_elem = row.find(".//div[@class='col-12 col-md-4 col-lg-2 yellow']")
+        value_elem = row.find(".//div[@class='col-12 col-md-8 col-lg-10']")
+        if value_elem is None:
+            value_elem = row.find(
+                ".//div[@class='js_visibility_target col-12 col-md-8 col-lg-10']"
+            )
+        if label_elem is not None and value_elem is not None:
+            label = label_elem.text_content().strip().replace("\n", " ")
+            value = value_elem.text_content().strip().replace("\n", " ")
+            value = " ".join(value.split())
+            value = " | ".join(
+                [text.strip() for text in value_elem.itertext() if text.strip()]
+            ).strip()
+            data[label] = value
     names = data.pop("Name")
     positions = data.pop("Position", None)
 
@@ -99,7 +135,7 @@ def crawl_item(context: Context, data, link):
     person.add("topics", "crime.war")
 
     sanction = h.make_sanction(context, person)
-    sanction.add("reason", data.pop("Reasons", None))
+    sanction.add("reason", data.pop("Reasons"))
     sanction.add("sourceUrl", link)
 
     context.emit(person, target=True)
@@ -108,11 +144,11 @@ def crawl_item(context: Context, data, link):
 
 
 def crawl(context):
-    for link_info in LINKS_PERSONS:
+    for link_info in LINKS:
         base_url = link_info["url"]
         max_pages = link_info["max_pages"]
+        data_type = link_info["type"]
 
         for page in range(1, max_pages + 1):
             index_page = base_url.format(page=page)
-            print(f"Fetching {index_page}...")
-            crawl_index_page(context, index_page)
+            crawl_index_page(context, index_page, data_type)
