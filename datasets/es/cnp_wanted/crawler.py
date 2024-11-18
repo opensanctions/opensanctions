d07d7d7633e781306939138f37c3beef863606c7
JD Bothma <jbothma@gmail.com>
2024-11-18 15:26:07 +0000

    Update datasets/es/cnp_wanted/crawler.py

--- datasets/es/cnp_wanted/crawler.py
+++ datasets/es/cnp_wanted/crawler.py
@@ -52,8 +52,8 @@ def parse_link_element(
 
     # Extract names from the card element inside the container
     card = link_element.xpath(".//div[contains(@class, 'card-body')]")[0]
-    first_name_element = card.xpath(".//h5[@class='card-title text-center']/text()")
-    cleaned_names = [name.strip() for name in first_name_element]
+    name_elements = card.xpath(".//h5[@class='card-title text-center']/text()")
+    cleaned_names = [name.strip() for name in name_elements]
     first_name, last_name = cleaned_names
 
     # Adjusted XPath to ignore multiple spaces in the class attribute
