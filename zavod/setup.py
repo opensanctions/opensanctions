3168680e108bf4b34f82429ef7310163ecdd239e
Friedrich Lindenberg <friedrich@opensanctions.org>
2024-11-21 17:38:26 +0100

    use address formatter from rigour

--- zavod/setup.py
+++ zavod/setup.py
@@ -24,7 +24,7 @@ setup(
         "followthemoney == 3.7.*",
         "nomenklatura[leveldb,tantivy] == 3.13.2",
         "plyvel == 1.5.1",
-        "rigour == 0.6.2",
+        "rigour == 0.7.0",
         "datapatch >= 1.1,< 1.3",
         "fingerprints == 1.2.*",
         "addressformatting == 1.3.*",
