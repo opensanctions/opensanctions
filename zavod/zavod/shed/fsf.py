0407ca26fefc515923354876f3173293e90f2e0c
JD Bothma <jbothma@gmail.com>
2024-11-21 08:28:55 +0000

    Add function to Organizations as well

--- zavod/zavod/shed/fsf.py
+++ zavod/zavod/shed/fsf.py
@@ -133,7 +133,10 @@ def parse_entry(context: Context, entry: Element) -> None:
             lang=lang,
         )
         entity.add("title", name.get("title"), quiet=True, lang=lang)
-        entity.add("position", name.get("function"), quiet=True, lang=lang)
+        if entity.schema.is_a("Person"):
+            entity.add("position", name.get("function"), lang=lang)
+        else:
+            entity.add("notes", name.get("function"), lang=lang)
         entity.add("gender", name.get("gender"), quiet=True, lang=lang)
 
     for node in entry.findall("./identification"):
