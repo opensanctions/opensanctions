e3b670b35a93fe4c4277962ffd29c10b42e6386f
nvmbrasserie <ivan@opensanctions.org>
2024-11-28 12:42:36 +0100

    us_ice_wanted switched to zyte

--- datasets/us/ice_wanted/crawler.py
+++ datasets/us/ice_wanted/crawler.py
@@ -1,24 +1,12 @@
-from zavod import Context
 from xml.etree import ElementTree
 from normality import collapse_spaces
 
+from zavod import Context
+from zavod.shed.zyte_api import fetch_html
 
-HEADERS = {
-    # "Accept": "*/*",
-    # "Accept-Encoding": "gzip, deflate, br",
-    # "Accept-Language": "en-US,en;q=0.9",
-    # "Cache-Control": "no-cache",
-    # "Connection": "keep-alive",
-    # "Content-Length": "0",
-    # "Host": "region1.google-analytics.com",
-    # "Origin": "https://www.ice.gov",
-    # "Pragma": "no-cache",
-    # "Referer": "https://www.ice.gov/",
-    # "Sec-Fetch-Dest": "empty",
-    # "Sec-Fetch-Mode": "no-cors",
-    # "Sec-Fetch-Site": "cross-site",
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15",
-}
+
+def unblock_validator(doc) -> bool:
+    return doc.find(".//div[@class='grid-container']") is not None
 
 
 def get_element_text(doc: ElementTree, xpath_value: str, to_remove=[]) -> str:
@@ -44,7 +32,7 @@ def get_element_text(doc: ElementTree, xpath_value: str, to_remove=[]) -> str:
 
 
 def crawl_person(context: Context, url: str, wanted_for: str):
-    doc = context.fetch_html(url, cache_days=1, headers=HEADERS)
+    doc = context.fetch_html(url, cache_days=1)
 
     name = get_element_text(
         doc,
@@ -159,7 +147,14 @@ def crawl_person(context: Context, url: str, wanted_for: str):
 
 
 def crawl(context: Context):
-    doc = context.fetch_html(context.data_url, cache_days=1, headers=HEADERS)
+    doc = fetch_html(
+        context,
+        context.data_url,
+        unblock_validator=unblock_validator,
+        html_source="httpResponseBody",
+        geolocaion="us",
+        cache_days=1,
+    )
     doc.make_links_absolute(context.data_url)
 
     for person_node in doc.xpath('.//li[@class="grid"]//a'):
