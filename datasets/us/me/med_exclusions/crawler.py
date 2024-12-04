b432746dfb7cdb422a51a83a631a3707143aa116
Bruno Görresen Mello <bruno@aetos.tech>
2024-12-04 10:42:54 -0300

    applying changes from code review

--- datasets/us/me/med_exclusions/crawler.py
+++ datasets/us/me/med_exclusions/crawler.py
@@ -9,37 +9,52 @@ from zavod import Context, helpers as h
 def crawl_item(row: Dict[str, str], context: Context):
 
     if first_name := row.pop("provider_first_name"):
+        last_name = row.pop("provider_last_name")
+        middle_initial = row.pop("provider_mi")
+
         entity = context.make("Person")
-        entity.id = context.make_id(
-            first_name, row.get("provider_last_name"), row.get("provider_mi")
-        )
+        entity.id = context.make_id(first_name, last_name, middle_initial)
         h.apply_name(
             entity,
             first_name=first_name,
-            last_name=row.pop("provider_last_name"),
-            middle_name=row.pop("provider_mi"),
+            last_name=last_name,
+            middle_name=middle_initial,
         )
     else:
+        last_name = row.pop("provider_last_name")
         entity = context.make("Company")
-        entity.id = context.make_id(row.get("provider_last_name"))
-        entity.add("name", row.pop("provider_last_name"))
+        entity.id = context.make_id(last_name)
+        entity.add("name", last_name)
 
     # Number of alias
     for i in [1, 2, 3, 4]:
-        first_name = row.pop(f"alias_first_name_{i}")
-        last_name = row.pop(f"alias_last_name_{i}")
 
-        if first_name == "N/A":
-            first_name = None
-        if last_name == "N/A":
-            last_name = None
+        alias_first_name = row.pop(f"alias_first_name_{i}")
+        alias_last_name = row.pop(f"alias_last_name_{i}")
 
-        if first_name and last_name:
-            entity.add("alias", first_name + last_name)
-        elif first_name:
-            entity.add("alias", first_name)
-        elif last_name:
-            entity.add("alias", last_name)
+        if not alias_first_name and not alias_last_name:
+            continue
+
+        # If the entity is a company and there is an alias, we consider it as an unknown link
+        if entity.schema.name == "Company":
+            person = context.make("Person")
+            person.id = context.make_id(alias_first_name, alias_last_name)
+            h.apply_name(person, first_name=alias_first_name, last_name=alias_last_name)
+            link = context.make("UnknownLink")
+            link.id = context.make_id(person.id, entity.id)
+            link.add("object", entity)
+            link.add("subject", person)
+
+            context.emit(link)
+            context.emit(person)
+
+        else:
+            h.apply_name(
+                entity,
+                first_name=alias_first_name,
+                last_name=alias_last_name,
+                alias=True,
+            )
 
     entity.add("country", "us")
     entity.add("topics", "debarment")
@@ -72,5 +87,14 @@ def crawl(context: Context) -> None:
 
     wb = load_workbook(path, read_only=True)
 
+    sheet_names = wb.sheetnames
+
     for item in h.parse_xlsx_sheet(context, wb.active, skiprows=26):
         crawl_item(item, context)
+
+    sheet_names.remove(wb.active.title)
+
+    for sheet_name in sheet_names:
+        sheet = wb[sheet_name]
+        if not (sheet.max_row == 1 and sheet.max_column == 1 and not sheet["A1"].value):
+            context.log.warning(f"Sheet {sheet_name} is not empty")
