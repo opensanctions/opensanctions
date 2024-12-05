644fa5794c4fc54e5bc9e4bd3284f9172af9495b
nvmbrasserie <ivan@opensanctions.org>
2024-12-05 12:30:14 +0100

    program_id looked up for h.make_sanction

--- zavod/zavod/helpers/sanctions.py
+++ zavod/zavod/helpers/sanctions.py
@@ -4,7 +4,10 @@ from zavod.entity import Entity
 
 
 def make_sanction(
-    context: Context, entity: Entity, key: Optional[str] = None
+    context: Context,
+    entity: Entity,
+    key: Optional[str] = None,
+    program: Optional[str] = None,
 ) -> Entity:
     """Create and return a sanctions object derived from the dataset metadata.
 
@@ -15,6 +18,7 @@ def make_sanction(
         context: The runner context with dataset metadata.
         entity: The entity to which the sanctions object will be linked.
         key: An optional key to be included in the ID of the sanction.
+        program: An optional key for looking up the program ID in the YAML configuration.
 
     Returns:
         A new entity of type Sanction.
@@ -30,4 +34,8 @@ def make_sanction(
         sanction.add("country", dataset.publisher.country)
     sanction.add("authority", dataset.publisher.name)
     sanction.add("sourceUrl", dataset.url)
+
+    program_id = context.lookup_value("sanction", program)
+    if program_id is None:
+        context.log.warn(f"Program key '{program}' not found.")
     return sanction
