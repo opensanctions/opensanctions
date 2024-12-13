293d7b6e76b986dce5b1aa4e0804c309bb9ac63f
nvmbrasserie <ivan@opensanctions.org>
2024-12-13 16:13:48 +0100

    fsf h make sanction expanded

--- zavod/zavod/shed/fsf.py
+++ zavod/zavod/shed/fsf.py
@@ -58,9 +58,15 @@ def parse_sanctions(context: Context, entity: Entity, entry: Element) -> None:
     for regulation in regulations:
         url = regulation.findtext("./publicationUrl")
         assert url is not None, etree.tostring(regulation)
-        sanction = h.make_sanction(context, entity, key=url)
+        program = regulation.get("programme")
+        sanction = h.make_sanction(
+            context,
+            entity,
+            program=program,
+            program_key=program,
+            key=url,
+        )
         sanction.set("sourceUrl", url)
-        sanction.add("program", regulation.get("programme"))
         sanction.add("reason", regulation.get("numberTitle"))
         sanction.add("startDate", regulation.get("entryIntoForceDate"))
         sanction.add("listingDate", regulation.get("publicationDate"))
