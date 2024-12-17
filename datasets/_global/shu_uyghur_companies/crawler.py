1a01defb28b34a01d93fefb89f44fd7b2f6474a6
nvmbrasserie <ivan@opensanctions.org>
2024-12-17 18:26:14 +0100

    crawler post-review

--- datasets/_global/shu_uyghur_companies/crawler.py
+++ datasets/_global/shu_uyghur_companies/crawler.py
@@ -10,32 +10,40 @@ def crawl(context: Context):
     )
     path = context.fetch_resource("source.xlsx", url[0])
     workbook: openpyxl.Workbook = openpyxl.load_workbook(path, read_only=True)
+    assert set(workbook.sheetnames) == {
+        "Info ",
+        "1. Companies Operating in XUAR",
+        "2. Export Relevant Categories",
+    }
     for row in h.parse_xlsx_sheet(
         context, sheet=workbook["1. Companies Operating in XUAR"], skiprows=1
     ):
         name = row.pop("company")
-        name_en = row.pop("company_machine_translated_english")
-        city = row.pop("city")
-        city_en = row.pop("city_english")
         addr = row.pop("address")
-        addr_en = row.pop("address_machine_translated_english")
         sector = row.pop("sector")
-        sector_en = row.pop("sector_english")
 
         entity = context.make("Company")
         entity.id = context.make_id(name, addr, sector)
         entity.add("name", name, lang="zhu")
-        entity.add("name", name_en, lang="eng")
-        entity.add("address", addr, lang="zhu")
-        entity.add("address", addr_en, lang="eng")
+        entity.add("name", row.pop("company_machine_translated_english"), lang="eng")
         entity.add("sector", sector, lang="zhu")
-        entity.add("sector", sector_en, lang="eng")
+        entity.add("sector", row.pop("sector_english"), lang="eng")
         entity.add("topics", "export.control")
 
-        address_ent = h.make_address(context, full=addr, city=city, lang="zhu")
-        address_en_ent = h.make_address(context, full=addr_en, city=city_en, lang="eng")
-        h.copy_address(entity, address_ent)
-        h.copy_address(entity, address_en_ent)
+        address_ent = h.make_address(
+            context,
+            full=addr,
+            city=row.pop("city"),
+            lang="zhu",
+        )
+        address_en_ent = h.make_address(
+            context,
+            full=row.pop("address_machine_translated_english"),
+            city=row.pop("city_english"),
+            lang="eng",
+        )
+        h.apply_address(context, entity, address_ent)
+        h.apply_address(context, entity, address_en_ent)
 
         context.emit(entity)
 
