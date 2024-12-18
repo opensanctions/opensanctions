8dd2fc251c286cb20fc57d74c88fa108445cad53
nvmbrasserie <ivan@opensanctions.org>
2024-12-18 19:28:41 +0100

    cleaned up and updated splits

--- datasets/us/ar/med_exclusions/crawler.py
+++ datasets/us/ar/med_exclusions/crawler.py
@@ -7,12 +7,12 @@ from zavod import Context, helpers as h
 from zavod.shed.zyte_api import fetch_resource
 
 REGEX_AKA = re.compile(r"\baka\b", re.IGNORECASE)
-# Handling  ' d/b/aMontessori Day'
-REGEX_DBA = re.compile(r"\bd\s*/\s*b\s*/\s*a(?=\w)", re.IGNORECASE)
 
 
 def crawl_item(row: Dict[str, str], context: Context):
     zip_code = row.pop("Zip")
+    division = row.pop("Division")
+
     address = h.format_address(
         city=row.pop("City"),
         state=row.pop("State"),
@@ -20,15 +20,12 @@ def crawl_item(row: Dict[str, str], context: Context):
         country_code="us",
     )
 
-    division = row.pop("Division")
-
     if provider_name := row.pop("Provider Name"):
+        last_name, first_name = provider_name.split(",", 1)
+        names = REGEX_AKA.split(last_name)
+
         person = context.make("Person")
         person.id = context.make_id(provider_name, zip_code)
-
-        last_name, first_name = provider_name.split(",", 1)
-
-        names = REGEX_AKA.split(last_name)
         h.apply_name(person, last_name=names[0], alias=names[1:], first_name=first_name)
         person.add("country", "us")
         person.add("topics", "debarment")
@@ -39,37 +36,32 @@ def crawl_item(row: Dict[str, str], context: Context):
         context.emit(person, target=True)
         context.emit(sanction)
 
-    if raw_facility_name := row.pop("Facility Name"):
+    if facility_name := row.pop("Facility Name"):
+        entity = context.make("LegalEntity")  # Sometimes the person's name.
+        entity.id = context.make_id(facility_name, zip_code)
+        entity.add("name", facility_name)
+        entity.add("country", "us")
+        entity.add("topics", "debarment")
+        entity.add("address", address)
         # The d/b/a is a person's name and then the company name
-        names = REGEX_DBA.split(raw_facility_name)
-        if len(names) == 2:
-            dba_person_name, facility_name = names[0], names[1]
-        else:
-            facility_name = raw_facility_name
-            dba_person_name = None
-            if len(names) != 1:
-                context.log.warning("More names than expected", raw_facility_name)
+        if "d/b/a" in facility_name:
+            dba = context.lookup_value("names", facility_name)
+            dba_person_name, facility_name = dba[0], dba[1]
+            entity.add("alias", dba_person_name)
+            if not dba:
+                context.log.warning("No names found for", facility_name)
 
-        company = context.make("LegalEntity")  # Sometimes the person's name.
-        company.id = context.make_id(facility_name, zip_code)
-        company.add("name", facility_name)
-        if dba_person_name is not None:
-            company.add("alias", dba_person_name)
-        company.add("country", "us")
-        company.add("topics", "debarment")
-        company.add("address", address)
-
-        sanction = h.make_sanction(context, company)
+        sanction = h.make_sanction(context, entity)
         sanction.add("authority", division)
 
-        context.emit(company, target=True)
+        context.emit(entity, target=True)
         context.emit(sanction)
 
-    if provider_name and raw_facility_name:
+    if provider_name and facility_name:
         link = context.make("UnknownLink")
-        link.id = context.make_id(person.id, company.id)
-        link.add("object", company)
-        link.add("subject", person)
+        link.id = context.make_id(person.id, entity.id)
+        link.add("object", entity.id)
+        link.add("subject", person.id)
         context.emit(link)
 
     context.audit_data(row, ignore=[None])
