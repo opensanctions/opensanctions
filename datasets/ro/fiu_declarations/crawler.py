e5e5f77d16a06d8b55a762c00604babc0c6321a2
nvmbrasserie <ivan@opensanctions.org>
2024-12-20 18:26:36 +0100

    initial commit

---
+++ datasets/ro/fiu_declarations/crawler.py
@@ -0,0 +1,18 @@
+from zavod import Context
+
+
+def crawl(context: Context):
+    doc = context.fetch_html(context.data_url)
+    table = doc.find(".//table[@class='table table-bordered declarations']")
+    for row in table.findall(".//tr"):
+        tds = row.findall("./td")
+        if not tds:
+            continue
+        name = tds[0].text_content().strip()
+        # Filter out non-names (e.g., "Declaratie de...")
+        if name and not name.startswith("Declaratie de"):
+            pep = context.make("Person")
+            pep.id = context.make_id(name)
+            pep.add("name", name)
+            pep.add("country", "ro")
+            context.emit(pep)
