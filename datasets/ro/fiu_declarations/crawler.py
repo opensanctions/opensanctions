d1e0fcdb9ce4dafeaf69af6b16222d00bda5cace
nvmbrasserie <ivan@opensanctions.org>
2024-12-23 13:17:54 +0100

    cleaner way of extraction

--- datasets/ro/fiu_declarations/crawler.py
+++ datasets/ro/fiu_declarations/crawler.py
@@ -1,16 +1,19 @@
 from zavod import Context
 
+# from zavod.logic.pep import categorise
+
 
 def crawl(context: Context):
     doc = context.fetch_html(context.data_url)
     table = doc.find(".//table[@class='table table-bordered declarations']")
-    for row in table.findall(".//tr"):
-        tds = row.findall("./td")
-        if not tds:
-            continue
-        name = tds[0].text_content().strip()
-        # Filter out non-names (e.g., "Declaratie de...")
-        if name and not name.startswith("Declaratie de"):
+    rows = table.xpath("./tr")
+    for row in rows:
+        first_td = row.xpath("./td[1]")  # Get the first <td> under the current <tr>
+        if first_td:
+            # Extract and clean the text content
+            name = first_td[0].text_content().strip()
+            # categorisation = categorise(context, position, True)
+
             pep = context.make("Person")
             pep.id = context.make_id(name)
             pep.add("name", name)
