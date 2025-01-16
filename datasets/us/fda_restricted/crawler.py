ff150a6c64865bab4dc479f569f012f52637511d
JD Bothma <jbothma@gmail.com>
2025-01-16 17:12:53 +0000

    Fixes

--- datasets/us/fda_restricted/crawler.py
+++ datasets/us/fda_restricted/crawler.py
@@ -3,79 +3,88 @@ from typing import Dict
 from zavod import Context, helpers as h
 import re
 
-NAME_PATTERN = re.compile(
-    r"""
-    ^\s*([A-Z][a-zA-Z' -]+)          # Last name(s)
-    (?:,\s*(Jr\.?|Sr\.?|II|III|IV))? # Optional suffix
-    (?:,\s*(MD|PhD|DO|DVM))?         # Optional pre-first-name credential
-    ,\s*([A-Z][a-zA-Z]+)             # First given name
-    (?:\s+([A-Z](?:\.)?|[A-Z][a-zA-Z]+(?:\s+[A-Z](?:\.)?|[A-Z][a-zA-Z]+)*))? # Additional given names/initials
-    (?:,\s*(.*))?                    # Remaining credentials (best effort)
-    \s*$""",
-    re.VERBOSE,
-)
+REGEX_TITLE = re.compile("(?:, (MD|PhD|DO|DVM))")
+REGEX_SUFFIX = re.compile(r",? (Jr|Sr|II|III|IV).?,")
 
 
-def crawl_item(row: Dict[str, str], context: Context):
+def crawl_item(context: Context, row: Dict[str, str], row_elements):
 
     raw_name = row.pop("name")
-    address = h.make_address(
-        context, city=row.pop("city"), state=row.pop("state"), country_code="us"
-    )
 
     entity = context.make("Person")
     entity.id = context.make_id(raw_name)
 
-    match = NAME_PATTERN.match(raw_name)
-    if not match:
-        context.log.warning(f"Unable to parse {raw_name}")
-        entity.add("name", raw_name)
-    else:
-        last, suffix, precred, first, middle, postcreds = match.groups()
-        h.apply_name(
-            entity,
-            last_name=last,
-            suffix=suffix,
-            first_name=first,
-            middle_name=middle,
-            prefix=postcreds,
-        )
+    namestr = raw_name
+    title = None
+    suffix = None
+    # Extract title(s) from end or inbetween last and forenames
+    if title := REGEX_TITLE.findall(namestr):
+        namestr = REGEX_TITLE.sub("", namestr)
+    # Extract suffix from in between last and forenames
+    if match := REGEX_SUFFIX.search(namestr):
+        suffix = match.group(1)
+        namestr = REGEX_SUFFIX.sub(",", namestr, 1)
+    parts = namestr.split(", ", 1)
+    forenames = parts[1].split(" ")
+    lastname = parts[0]
+
+    h.apply_name(
+        entity,
+        first_name=forenames[0],
+        middle_name=forenames[1] if len(forenames) > 1 else None,
+        name3=forenames[2] if len(forenames) > 2 else None,
+        last_name=lastname,
+        suffix=suffix,
+    )
+    if len(forenames) > 3:
+        context.log.warning(f"Name has more than 3 forenames: {raw_name}")
+    entity.add("title", title)
 
     entity.add("country", "us")
-    entity.add("address", address)
-    entity.add("sector", row.pop("center"))
+    address = h.make_address(
+        context, city=row.pop("city"), state=row.pop("state"), country_code="us"
+    )
+    h.copy_address(entity, address)
 
     sanction = h.make_sanction(context, entity)
     h.apply_date(sanction, "startDate", row.pop("date_of_status"))
-    sanction.add("status", row.pop("status"))
-
-    # In those cases the sanction is not active anymore
-    target = sanction.get("status") not in [
-        "Restrictions Removed",
-        "Not Disqualified",
-        "Reinstated pursuant to Agreement",
-        "Not Disqualified – Adequate Assurances 1",
-        "Restrictions Removed (reinstated by correspondence)",
-    ]
-
-    if target:
-        entity.add("topics", "debarment")
-
-    context.emit(entity, target=target)
-    context.emit(sanction)
-
     # NIDPOE = Notice of Initiation of Disqualification Proceedings and Opportunity to Explain
     # NOOH = Notice of Opportunity for Hearing
+    h.apply_date(sanction, "date", row.pop("date_nidpoe_issued"))
+    h.apply_date(sanction, "date", row.pop("date_nooh_issued"))
+    h.apply_date(sanction, "date", row.pop("date_of_presiding_officer_report"))
+    h.apply_date(sanction, "date", row.pop("date_of_commissioner_s_decision"))
+    sanction.add("authority", row.pop("center"))
+    for link_key in [
+        "link_to_nidpoe_letter",
+        "link_to_nooh_letter",
+        "link_to_presiding_officer_report",
+        "link_to_commissioner_s_decision",
+    ]:
+        urls = row_elements.pop(link_key).xpath(".//a[text()='Text']/@href")
+        sanction.add("sourceUrl", urls)
+
+    status = row.pop("status")
+    res = context.lookup("is_debarred_status", status)
+    if res:
+        is_debarred = res.is_debarred
+    else:
+        is_debarred = True
+        context.log.warning(f"Unknown status: {status}")
+    sanction.add("status", status)
+
+    if is_debarred:
+        entity.add("topics", "debarment")
+
+    context.emit(entity)
+    context.emit(sanction)
+
     context.audit_data(
         row,
         ignore=[
-            "date_nidpoe_issued",
-            "date_nooh_issued",
             "link_to_nidpoe_letter",
             "link_to_nooh_letter",
-            "date_of_presiding_officer_report",
             "link_to_presiding_officer_report",
-            "date_of_commissioner_s_decision",
             "link_to_commissioner_s_decision",
         ],
     )
@@ -85,6 +94,6 @@ def crawl(context: Context) -> None:
 
     response = context.fetch_html(context.data_url)
 
-    for item in h.parse_html_table(response):
-        item = h.cells_to_str(item)
-        crawl_item(item, context)
+    for row_elements in h.parse_html_table(response):
+        row = h.cells_to_str(row_elements)
+        crawl_item(context, row, row_elements)
