b78202cfeab06bf426cea04fe49718c060627f9e
nvmbrasserie <ivan@opensanctions.org>
2025-01-21 17:15:55 +0100

    resturctured

--- datasets/ge/company_regisrty/crawler.py
+++ datasets/ge/company_regisrty/crawler.py
@@ -6,7 +6,6 @@ from zavod.shed.internal_data import fetch_internal_data, list_internal_data
 
 
 def crawl_row(context: Context, row: list) -> None:
-
     id = row.pop("id")
     name = row.pop("name")
     legal_form = row.pop("legal_form")
@@ -15,57 +14,98 @@ def crawl_row(context: Context, row: list) -> None:
     reg_date = row.pop("registration_date")
     email = row.pop("email")
     director = row.pop("director")
-    director_id = row.pop("director_id")
-    director_citizenship = row.pop("director_citizenship")
-    director_start_date = row.pop("director_start_date")
     partner = row.pop("partner")
-    partner_id = row.pop("partner_id")
-    partner_citizenship = row.pop("partner_citizenship")
-    partner_start_date = row.pop("partner_start_date")
 
     entity = context.make("Company")
     entity.id = context.make_id(id, name, reg_date)
     entity.add("name", name)
-    # entity.add("legalForm", legal_form)
+    entity.add("classification", legal_form)
     entity.add("incorporationDate", reg_date)
     entity.add("address", row.pop("address"))
+    entity.add("status", row.pop("status"))
     if email != "NULL":
         entity.add("email", email)
-    entity.add("status", row.pop("status"))
     context.emit(entity)
 
     if director != "NULL":
-        dir = context.make("Person")
-        dir.id = context.make_id(director, director_id)
-        dir.add("name", director)
-        if director_citizenship != "NULL":
-            dir.add("citizenship", director_citizenship)
-        context.emit(dir)
-
-        directorship = context.make("Directorship")
-        directorship.id = context.make_id(dir.id, "director", entity.id)
-        if director_start_date != "NULL":
-            h.apply_date(directorship, "startDate", director_start_date)
-        directorship.add("director", dir)
-        directorship.add("organization", entity)
-        context.emit(directorship)
-
+        emit_rel(
+            context,
+            "Directorship",
+            row,
+            director,
+            entity,
+            "director_id",
+            "director_citizenship",
+            "director_start_date",
+            "director",
+        )
+        context.audit_data(
+            row,
+            ignore=[
+                "partner_id",
+                "partner_citizenship",
+                "partner_start_date",
+                "partner_share",
+            ],
+        )
     if partner != "NULL":
-        partner = context.make("Person")
-        partner.id = context.make_id(partner, partner_id)
-        partner.add("name", partner)
-        if partner_citizenship != "NULL":
-            partner.add("citizenship", partner_citizenship)
-        context.emit(partner)
+        emit_rel(
+            context,
+            "Ownership",
+            row,
+            partner,
+            entity,
+            "partner_id",
+            "partner_citizenship",
+            "partner_start_date",
+            "owner",
+        )
+        context.audit_data(
+            row,
+            ignore=[
+                "director_id",
+                "director_citizenship",
+                "director_start_date",
+            ],
+        )
 
-        own = context.make("Ownership")
-        own.id = context.make_id(partner.id, "owns", entity.id)
-        if partner_start_date != "NULL":
-            h.apply_date(own, "startDate", partner_start_date)
-        own.add("asset", entity)
-        own.add("owner", partner)
-        own.add("percentage", row.pop("partner_share"))
-        context.emit(own)
+
+def emit_rel(
+    context: Context,
+    type_label: str,
+    row: list,
+    name: str,
+    entity,
+    id_key: str,
+    citizenship_key: str,
+    start_date_key: str,
+    relationship_type: str,
+):
+    """Generalized function to process a director or partner."""
+    person_id = row.pop(id_key)
+    person_citizenship = row.pop(citizenship_key)
+    person_start_date = row.pop(start_date_key)
+
+    person = context.make("Person")
+    person.id = context.make_id(name, person_id)
+    person.add("name", name)
+    if person_citizenship != "NULL":
+        person.add("citizenship", person_citizenship)
+    context.emit(person)
+
+    relationship = context.make(type_label)
+    relationship.id = context.make_id(person.id, relationship_type, entity.id)
+    if person_start_date != "NULL":
+        h.apply_date(relationship, "startDate", person_start_date)
+    relationship.add(relationship_type, person)
+    relationship.add(
+        "organization" if relationship_type == "director" else "asset", entity
+    )
+
+    if relationship_type == "owner":
+        relationship.add("percentage", row.pop("partner_share"))
+
+    context.emit(relationship)
 
 
 def crawl(context: Context) -> None:
@@ -97,5 +137,5 @@ def crawl(context: Context) -> None:
             dict_reader = csv.DictReader(fh, fieldnames=header_mapping, delimiter=";")
             for index, row in enumerate(dict_reader):
                 crawl_row(context, row)
-                if index >= 50:
+                if index >= 1000:
                     break
