7644f036de6f03ca1544adfbe3f9358ae7e703b4
nvmbrasserie <ivan@opensanctions.org>
2025-01-21 14:08:55 +0100

    cleaned up

--- datasets/ge/company_regisrty/crawler.py
+++ datasets/ge/company_regisrty/crawler.py
@@ -13,15 +13,12 @@ def crawl_row(context: Context, row: list) -> None:
     if legal_form != "შეზღუდული პასუხისმგებლობის საზოგადოება":
         context.log.warning("Unknown company type", legal_form=legal_form)
     reg_date = row.pop("registration_date")
-    address = row.pop("address")
     email = row.pop("email")
-    status = row.pop("status")
     director = row.pop("director")
     director_id = row.pop("director_id")
     director_citizenship = row.pop("director_citizenship")
     director_start_date = row.pop("director_start_date")
     partner = row.pop("partner")
-    partner_share = row.pop("partner_share")
     partner_id = row.pop("partner_id")
     partner_citizenship = row.pop("partner_citizenship")
     partner_start_date = row.pop("partner_start_date")
@@ -30,16 +27,16 @@ def crawl_row(context: Context, row: list) -> None:
     entity.id = context.make_id(id, name, reg_date)
     entity.add("name", name)
     # entity.add("legalForm", legal_form)
-    entity.add("registrationDate", reg_date)
-    entity.add("address", address)
+    entity.add("incorporationDate", reg_date)
+    entity.add("address", row.pop("address"))
     if email != "NULL":
         entity.add("email", email)
-    entity.add("status", status)
+    entity.add("status", row.pop("status"))
     context.emit(entity)
 
-    if director is not None:
+    if director != "NULL":
         dir = context.make("Person")
-        dir.id = context.make_id(director_id)
+        dir.id = context.make_id(director, director_id)
         dir.add("name", director)
         if director_citizenship != "NULL":
             dir.add("citizenship", director_citizenship)
@@ -53,9 +50,9 @@ def crawl_row(context: Context, row: list) -> None:
         directorship.add("organization", entity)
         context.emit(directorship)
 
-    if partner is not None:
+    if partner != "NULL":
         partner = context.make("Person")
-        partner.id = context.make_id(partner_id)
+        partner.id = context.make_id(partner, partner_id)
         partner.add("name", partner)
         if partner_citizenship != "NULL":
             partner.add("citizenship", partner_citizenship)
@@ -67,7 +64,7 @@ def crawl_row(context: Context, row: list) -> None:
             h.apply_date(own, "startDate", partner_start_date)
         own.add("asset", entity)
         own.add("owner", partner)
-        own.add("percentage", partner_share)
+        own.add("percentage", row.pop("partner_share"))
         context.emit(own)
 
 
@@ -100,5 +97,5 @@ def crawl(context: Context) -> None:
             dict_reader = csv.DictReader(fh, fieldnames=header_mapping, delimiter=";")
             for index, row in enumerate(dict_reader):
                 crawl_row(context, row)
-                if index >= 10000:
+                if index >= 50:
                     break
