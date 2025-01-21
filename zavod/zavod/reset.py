e80062f776135792fcb9c7ba0ab0184093341e6a
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-01-21 18:58:23 +0100

    Always forget to type -> None :)

--- zavod/zavod/reset.py
+++ zavod/zavod/reset.py
@@ -3,6 +3,6 @@ from rigour.reset import reset_caches as reset_rigour_caches
 from zavod.helpers.addresses import format_address
 
 
-def reset_caches():
+def reset_caches() -> None:
     reset_rigour_caches()
     format_address.cache_clear()
