871924b714e8ce85965d24832551994bd9e26de0
JD Bothma <jbothma@gmail.com>
2025-02-05 15:05:25 +0000

    They fixed the link. And Orbit is only effective March

--- datasets/us/cuba_sanctions/crawler.py
+++ datasets/us/cuba_sanctions/crawler.py
@@ -1,4 +1,5 @@
 import csv
+
 from rigour.mime.types import CSV
 
 from zavod import Context
@@ -11,7 +12,7 @@ ORIGINAL_ACCOMMODATIONS_URL = (
 ACCOMMODATIONS_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMquWjNWZ09dm9_mu9NKrxR33c6pe4hpiGFeheFT4tDZXwpelLudcYdCdME820aKJJo8TfMKbtoXTh/pub?gid=1890354374&single=true&output=csv"
 # The Restricted Entities list is currently nowhere to be found, the last known public snapshot is at
 # https://web.archive.org/web/20241209000143/https://www.state.gov/cuba-restricted-list/list-of-restricted-entities-and-subentities-associated-with-cuba-effective-january-8-2021/
-# The re-instation of the list and addition of one entity were announced on 2025-01-31, see
+# The resurrection of the list and addition of one entity were announced on 2025-01-31, see
 # https://www.state.gov/restoring-a-tough-u-s-cuba-policy/. Our copy of the data, including the added entity, are
 # available at our copy at ENTITIES_URL
 ENTITIES_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQMquWjNWZ09dm9_mu9NKrxR33c6pe4hpiGFeheFT4tDZXwpelLudcYdCdME820aKJJo8TfMKbtoXTh/pub?gid=0&single=true&output=csv"
@@ -33,7 +34,7 @@ def crawl_accommodations(context: Context):
         context, ORIGINAL_ACCOMMODATIONS_URL, CONTENT_XPATH, actions=ACTIONS
     )
     node = doc.find(CONTENT_XPATH)
-    if not h.assert_dom_hash(node, "52894f39893ab2432b411441f3bc21c27c3bea7d"):
+    if not h.assert_dom_hash(node, "7a2be818841181e19a1988c4d86789087d245c31"):
         context.log.warning("Accommodations page changed. Check for data updates.")
 
     path = context.fetch_resource("accommodations.csv", ACCOMMODATIONS_URL)
@@ -54,7 +55,7 @@ def crawl_accommodations(context: Context):
 
 def crawl_restricted_entities(context: Context):
     landing_doc = fetch_html(
-        context, context.dataset.url, CONTENT_XPATH, actions=ACTIONS, cache_days=1
+        context, context.dataset.url, CONTENT_XPATH, actions=ACTIONS
     )
     # The restricted list URL page currently points to an interim page that refers to ORIGINAL_ACCOMMODATIONS_URL.
     # So the page doesn't really contain that list. We just want to alert when it changes
@@ -63,7 +64,7 @@ def crawl_restricted_entities(context: Context):
     content_xpath = ".//div[@id='content']"
     doc = fetch_html(context, restricted_list_url[0], content_xpath)
     node = doc.find(content_xpath)
-    if not h.assert_dom_hash(node, "7719c273f6857afdd20207cd5672d735b96f657a"):
+    if not h.assert_dom_hash(node, "16aa93879cb1e04548ab319c749f9da1fa528a49"):
         context.log.warning(
             "Restricted List url destination content changed. Check for data updates"
         )
@@ -81,10 +82,11 @@ def crawl_restricted_entities(context: Context):
             proxy.add("sector", row.pop("Sector"))
             proxy.add("classification", row.pop("Category"))
             proxy.add("sourceUrl", row.pop("SourceURL").split(";"))
-            proxy.add("topics", "sanction")
 
             sanction = h.make_sanction(context, proxy)
             sanction.add("startDate", row.pop("EffectiveDate"))
+            if h.is_active(sanction):
+                proxy.add("topics", "sanction")
 
             parent = row.pop("Parent").strip()
             if len(parent):
@@ -100,11 +102,10 @@ def crawl_restricted_entities(context: Context):
 
 
 def crawl(context: Context):
-    doc = fetch_html(
-        context, context.dataset.url, CONTENT_XPATH, actions=ACTIONS, cache_days=1
-    )
+    doc = fetch_html(context, context.dataset.url, CONTENT_XPATH, actions=ACTIONS)
     node = doc.find(CONTENT_XPATH)
-    if not h.assert_dom_hash(node, "908452570b9fe11c54242c7b815d92ee862b6a58"):
+    if not h.assert_dom_hash(node, "68b8e034bd290f5d384ab74bebf24f60b43db282"):
         context.log.warning("Landing page changed. Check for added/removed lists.")
+
     crawl_accommodations(context)
     crawl_restricted_entities(context)
