92f1a2f26fa446ae76deb3bc5b8a3daf5f0f5b24
Leon Handreke <leon@opensanctions.org>
2025-02-26 08:38:38 +0100

    Retry on HTTP 500 & 502 and make backoff env-configurable (#1944)
    See https://github.com/opensanctions/opensanctions/issues/1943

--- zavod/zavod/meta/http.py
+++ zavod/zavod/meta/http.py
@@ -1,16 +1,21 @@
 from typing import Any, Dict, List
+
 from urllib3.util import Retry
 from banal import ensure_list
 from zavod import settings
 
+DEFAULT_RETRY_STATUS_CODES = [500, 502] + list(Retry.RETRY_AFTER_STATUS_CODES)
+
 
 class HTTP(object):
     def __init__(self, data: Dict[str, Any]) -> None:
-        self.total_retries: int = data.get("total_retries", 3)
-        self.backoff_factor: float = data.get("backoff_factor", 1)
-        statuses = ensure_list(
-            data.get("retry_statuses", list(Retry.RETRY_AFTER_STATUS_CODES))
+        self.total_retries: int = data.get("total_retries", settings.HTTP_RETRY_TOTAL)
+        self.backoff_factor: float = data.get(
+            "backoff_factor", settings.HTTP_RETRY_BACKOFF_FACTOR
         )
+        statuses = ensure_list(data.get("retry_statuses", DEFAULT_RETRY_STATUS_CODES))
+        self.backoff_max: int = settings.HTTP_RETRY_BACKOFF_MAX
+
         self.retry_statuses: List[int] = statuses
         retry_methods = ensure_list(
             data.get("retry_methods", list(Retry.DEFAULT_ALLOWED_METHODS))
