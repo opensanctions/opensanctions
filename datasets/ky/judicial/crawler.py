23b896f487ddc38afa9c970f02ef93eaca942459
nvmbrasserie <ivan@opensanctions.org>
2025-02-27 00:49:32 +0100

    initial rewrite

--- datasets/ky/judicial/crawler.py
+++ datasets/ky/judicial/crawler.py
@@ -1,5 +1,5 @@
-from urllib.parse import urljoin
-from lxml.etree import tostring
+# from urllib.parse import urljoin
+# from lxml.etree import tostring
 import re
 
 from normality import collapse_spaces
@@ -22,61 +22,120 @@ def get_judges(context: Context, doc):
     return current_pane.xpath(".//div[contains(@class, 'col-md-6 col-lg-8')]")
 
 
-def get_name_pos(context: Context, container, default_position: str):
-    name_els = container.xpath(".//h3")
-    assert len(name_els) == 1, (name_els, tostring(container))
-    name = name_els[0].text_content()
+# def get_name_pos(context: Context, container, default_position: str):
+#     name_els = container.xpath(".//h3")
+#     assert len(name_els) == 1, (name_els, tostring(container))
+#     name = name_els[0].text_content()
 
-    if "chief justice" in name.lower():
-        position = "Chief Justice"
-    elif "president of the court of appeal" in name.lower():
-        position = "President of the Court of Appeal"
-    else:
-        position = default_position
+#     if "chief justice" in name.lower():
+#         position = "Chief Justice"
+#     elif "president of the court of appeal" in name.lower():
+#         position = "President of the Court of Appeal"
+#     else:
+#         position = default_position
 
-    name = REGEX_CLEAN_NAME.sub("", name)
-    name = collapse_spaces(name)
-    return name, position
+#     name = REGEX_CLEAN_NAME.sub("", name)
+#     name = collapse_spaces(name)
+#     return name, position
 
 
-def crawl_judges(context: Context, url, default_position, min, max):
-    doc = context.fetch_html(url, cache_days=30)
-    doc.make_links_absolute(url)
+def get_name_pos(container):
+    name_el = container.xpath(".//h2[@class='tlp-member-title']")
+    position_el = container.xpath(".//div[@class='tlp-position']")
+    details_el = container.xpath(".//div[@class='tlp-member-detail']")
 
-    containers = get_judges(context, doc)
-    assert min <= len(containers) <= max, (min, len(containers), max)
+    name = name_el[0].text_content().strip()
+    name = collapse_spaces(REGEX_CLEAN_NAME.sub("", name))
+    # TODO: some positions should be extracted from the name element
+    position = position_el[0].text_content().strip() if position_el else ""
+    details = details_el[0].text_content().strip()
 
-    for judge_container in containers:
-        name, position = get_name_pos(context, judge_container, default_position)
-        person_proxy = context.make("Person")
-        person_proxy.id = context.make_id(name)
-        person_proxy.add("sourceUrl", url)
-        h.apply_name(person_proxy, full=name)
-        person_proxy.add("topics", "role.judge")
-
-        position = h.make_position(
-            context,
-            name=position,
-            country="Cayman Islands",
-        )
-        categorisation = categorise(context, position, is_pep=True)
-        if not categorisation.is_pep:
-            continue
-        occupancy = h.make_occupancy(
-            context, person_proxy, position, True, categorisation=categorisation
-        )
-        if not occupancy:
-            continue
-        context.emit(person_proxy)
-        context.emit(position)
-        context.emit(occupancy)
+    return name, position, details
 
 
 def crawl(context: Context):
-    crawl_judges(context, context.data_url, "Chief Justice", 1, 1)
+    doc = context.fetch_html(context.data_url, cache_days=1)
+    profile_links = [
+        link
+        for link in doc.xpath('//ul[@id="menu-judicial-officers"]//a/@href')
+        if link != "#"
+    ]
+    assert len(profile_links) >= 6, profile_links
+    for url in profile_links:
+        doc = context.fetch_html(url, cache_days=1)
+        profile_links = doc.xpath(
+            '//div[@class="single-team-area"]//a[@class="rt-ream-me-btn"]/@href'
+        )
+        for person_url in profile_links:
+            doc = context.fetch_html(person_url, cache_days=1)
+            containers = doc.xpath(
+                '//div[contains(@class, "tlp-member-description-container")]'
+            )
+            for judge_container in containers:
+                name, position, details = get_name_pos(judge_container)
+                person_proxy = context.make("Person")
+                person_proxy.id = context.make_id(name)
+                person_proxy.add("sourceUrl", person_url)
+                person_proxy.add("notes", details)
+                h.apply_name(person_proxy, full=name)
+                person_proxy.add("topics", "role.judge")
 
-    appeal_court_url = urljoin(context.data_url, "court-of-appeal")
-    crawl_judges(context, appeal_court_url, "Justice of the Court of Appeal", 3, 10)
+                position = h.make_position(
+                    context,
+                    name=position,
+                    country="Cayman Islands",
+                )
+                categorisation = categorise(context, position, is_pep=True)
+                if not categorisation.is_pep:
+                    continue
+                occupancy = h.make_occupancy(
+                    context, person_proxy, position, True, categorisation=categorisation
+                )
+                if not occupancy:
+                    continue
+                context.emit(person_proxy)
+                context.emit(position)
+                context.emit(occupancy)
 
-    grand_court_url = urljoin(context.data_url, "grand-court")
-    crawl_judges(context, grand_court_url, "Judge of the Grand Court", 3, 10)
+
+# def crawl_judges(context: Context, url, default_position, min, max):
+#     doc = context.fetch_html(url, cache_days=30)
+#     doc.make_links_absolute(url)
+
+#     containers = get_judges(context, doc)
+#     assert min <= len(containers) <= max, (min, len(containers), max)
+
+#     for judge_container in containers:
+#         name, position = get_name_pos(context, judge_container, default_position)
+#         person_proxy = context.make("Person")
+#         person_proxy.id = context.make_id(name)
+#         person_proxy.add("sourceUrl", url)
+#         h.apply_name(person_proxy, full=name)
+#         person_proxy.add("topics", "role.judge")
+
+#         position = h.make_position(
+#             context,
+#             name=position,
+#             country="Cayman Islands",
+#         )
+#         categorisation = categorise(context, position, is_pep=True)
+#         if not categorisation.is_pep:
+#             continue
+#         occupancy = h.make_occupancy(
+#             context, person_proxy, position, True, categorisation=categorisation
+#         )
+#         if not occupancy:
+#             continue
+#         context.emit(person_proxy)
+#         context.emit(position)
+#         context.emit(occupancy)
+
+
+# def crawl(context: Context):
+#     crawl_judges(context, context.data_url, "Chief Justice", 1, 1)
+
+#     appeal_court_url = urljoin(context.data_url, "court-of-appeal")
+#     crawl_judges(context, appeal_court_url, "Justice of the Court of Appeal", 3, 10)
+
+#     grand_court_url = urljoin(context.data_url, "grand-court")
+#     crawl_judges(context, grand_court_url, "Judge of the Grand Court", 3, 10)
