a625c2111e7f9817496a1ce10ea7da1a13f76862
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-03-06 13:30:42 +0100

    Suggested changes to senzing exporter from Ant C., Senzing

--- zavod/zavod/exporters/senzing.py
+++ zavod/zavod/exporters/senzing.py
@@ -4,16 +4,29 @@
 # This format can then be used to perform record linkage against other datasets.
 # As a next step, the matching results could be converted back into a
 # nomenklatura resolver file and then used to generate integrated FtM entities.
+
+import hashlib
 from itertools import product
-from typing import Dict, Any
+from pprint import pprint  # noqa
+from typing import Any, Dict
+
 from followthemoney.types import registry
 from rigour.ids.wikidata import is_qid
-from pprint import pprint  # noqa
 
 from zavod.entity import Entity
-from zavod.util import write_json
-from zavod.runtime.urls import make_entity_url
 from zavod.exporters.common import Exporter
+from zavod.logs import get_logger
+from zavod.runtime.urls import make_entity_url
+from zavod.util import write_json
+
+ADDR_ATTRS = ["ADDR_FULL", "PLACE_OF_BIRTH"]
+STMT_PROPS_TO_MAP = {
+    "IMONUMBER": "IMO_NUMBER",
+    "MMSI": "MMSI_NUMBER",
+    "CALLSIGN": "CALL_SIGN",
+    "ISIN": "ISIN_NUMBER",
+    "NPICODE": "NPI_NUMBER",
+}
 
 
 def push(obj: Dict[str, Any], section: str, value: Dict[str, Any]) -> None:
@@ -40,6 +53,14 @@ def clean(obj: Dict[str, Any]) -> Dict[str, Any]:
     return out
 
 
+def normalize_attr_value(value: str) -> str:
+    return value.strip(" ").lower()
+
+
+def hash_value(value: str) -> str:
+    return hashlib.md5(normalize_attr_value(value).encode("utf-8")).hexdigest()
+
+
 class SenzingExporter(Exporter):
     TITLE = "Senzing entity format"
     FILE_NAME = "senzing.json"
@@ -48,18 +69,21 @@ class SenzingExporter(Exporter):
     def setup(self) -> None:
         super().setup()
         self.fh = open(self.path, "wb")
-        self.domain_name = "OPEN_SANCTIONS"
-        self.source_name = f"OS_{self.dataset.name.upper()}"
+        self.domain_name = "OPEN-SANCTIONS"
+        self.source_name = f"OS-{self.dataset.name.upper()}"
         if self.dataset.is_collection and self.dataset.name != "openownership":
             self.source_name = self.domain_name
 
     def feed(self, entity: Entity) -> None:
         if not entity.schema.matchable:
             return None
+
         if entity.id is None:
             return None
+
         record_type = None
         is_org = False
+
         if entity.schema.is_a("Person"):
             record_type = "PERSON"
         elif entity.schema.is_a("Organization"):
@@ -67,10 +91,16 @@ class SenzingExporter(Exporter):
             is_org = True
         elif entity.schema.is_a("Airplane"):
             record_type = "AIRCRAFT"
+            is_org = True
         elif entity.schema.is_a("Vessel"):
             record_type = "VESSEL"
+            is_org = True
         elif entity.schema.is_a("Vehicle"):
             record_type = "VEHICLE"
+            is_org = True
+        # Skip address only records from FtM
+        elif entity.schema.is_a("Address"):
+            return None
 
         record: Dict[str, Any] = {
             "DATA_SOURCE": self.source_name,
@@ -79,11 +109,17 @@ class SenzingExporter(Exporter):
             "LAST_CHANGE": entity.last_change,
         }
 
-        name_field = "NAME_ORG" if is_org else "NAME_FULL"
-        push(record, "NAMES", {"NAME_TYPE": "PRIMARY", name_field: entity.caption})
+        # Collect name hashes to deduplicate names that have different case but are otherwise the same
+        # The hash_value() function calls a basic normalise function
+        name_hashes = set()
+        name_attr = "NAME_ORG" if is_org else "NAME_FULL"
+        name_hashes.add(hash_value(f"{name_attr}{entity.caption}"))
+        push(record, "NAMES", {"NAME_TYPE": "PRIMARY", name_attr: entity.caption})
+
         for name in entity.get_type_values(registry.name):
-            if name != entity.caption:
-                push(record, "NAMES", {"NAME_TYPE": "ALIAS", name_field: name})
+            if (name_hash := hash_value(f"{name_attr}{name}")) not in name_hashes:
+                name_hashes.add(name_hash)
+                push(record, "NAMES", {"NAME_TYPE": "ALIAS", name_attr: name})
 
         genders = entity.get("gender", quiet=True)
         if len(genders) == 1:
@@ -98,7 +134,13 @@ class SenzingExporter(Exporter):
         map(entity, "deathDate", record, "DATES", "DATE_OF_DEATH")
         map(entity, "incorporationDate", record, "DATES", "REGISTRATION_DATE")
         map(entity, "birthPlace", record, "ADDRESSES", "PLACE_OF_BIRTH")
-        map(entity, "country", record, "COUNTRIES", "NATIONALITY")
+        map(
+            entity,
+            "country",
+            record,
+            "COUNTRIES",
+            "COUNTRY_OF_ASSOCIATION" if is_org else "NATIONALITY",
+        )
         map(entity, "nationality", record, "COUNTRIES", "NATIONALITY")
         map(entity, "citizenship", record, "COUNTRIES", "CITIZENSHIP")
         map(entity, "jurisdiction", record, "COUNTRIES", "REGISTRATION_COUNTRY")
@@ -112,28 +154,23 @@ class SenzingExporter(Exporter):
         map(entity, "taxNumber", record, "IDENTIFIERS", "TAX_ID_NUMBER")
         map(entity, "innCode", record, "IDENTIFIERS", "TAX_ID_NUMBER")
         map(entity, "vatCode", record, "IDENTIFIERS", "TAX_ID_NUMBER")
+        map(entity, "socialSecurityNumber", record, "IDENTIFIERS", "SSN_NUMBER")
         map(entity, "leiCode", record, "IDENTIFIERS", "LEI_NUMBER")
         map(entity, "dunsCode", record, "IDENTIFIERS", "DUNS_NUMBER")
         map(entity, "sourceUrl", record, "SOURCE_LINKS", "SOURCE_URL")
 
         for _, adj in self.view.get_adjacent(entity):
             if adj.schema.name == "Address":
-                adj_data = {
-                    "ADDR_FULL": adj.first("full"),
-                    "ADDR_LINE1": adj.first("street"),
-                    "ADDR_LINE2": adj.first("street2"),
-                    "ADDR_CITY": adj.first("city"),
-                    "ADDR_STATE": adj.first("state"),
-                    "ADDR_COUNTRY": adj.first("country"),
-                    "ADDR_POSTAL_CODE": adj.first("postalCode"),
-                }
+                adj_data = {"ADDR_FULL": adj.first("full")}
                 push(record, "ADDRESSES", clean(adj_data))
+
             elif adj.schema.name == "Identification":
                 adj_data = {
                     "NATIONAL_ID_NUMBER": adj.first("number"),
                     "NATIONAL_ID_COUNTRY": adj.first("country"),
                 }
                 push(record, "IDENTIFIERS", clean(adj_data))
+
             elif adj.schema.name == "Passport":
                 adj_data = {
                     "PASSPORT_NUMBER": adj.first("number"),
@@ -146,8 +183,6 @@ class SenzingExporter(Exporter):
                 targets = adj.get(adj.schema.target_prop)
                 caption = adj.first("role", quiet=True) or adj.caption
                 for s, t in product(sources, targets):
-                    # if s != entity.id and t != entity.id:
-                    #     continue
                     if s == entity.id:
                         edge = {
                             "REL_POINTER_ROLE": caption,
@@ -169,8 +204,27 @@ class SenzingExporter(Exporter):
         for stmt in entity.get_type_statements(registry.identifier):
             if stmt.value in seen_identifiers:
                 continue
-            ident = {"OTHER_ID_TYPE": stmt.prop, "OTHER_ID_NUMBER": stmt.value}
-            push(record, "IDENTIFIERS", ident)
+
+            # Map to a specific attribute if the statement prop is in STMT_PROPS_TO_MAP, otherwise use OTHER_ID_NUMBER
+            if (stmt_prop_upper := stmt.prop.upper()) in STMT_PROPS_TO_MAP:
+                push(
+                    record,
+                    "IDENTIFIERS",
+                    {STMT_PROPS_TO_MAP[stmt_prop_upper]: stmt.value},
+                )
+            else:
+                push(
+                    record,
+                    "IDENTIFIERS",
+                    {"OTHER_ID_TYPE": stmt.prop, "OTHER_ID_NUMBER": stmt.value},
+                )
+
+        # Retrieve the OFAC ID from the OFAC URL and add to IDENTIFIERS
+        for stmt in entity.get_type_statements(registry.url):
+            if ".ofac.treas.gov/Details.aspx?id=" in stmt.value:
+                _, ofac_id = stmt.value.split("?id=")
+                if ofac_id:
+                    push(record, "IDENTIFIERS", {"OFAC_ID": ofac_id})
 
         for wd_id in (entity.id, entity.first("wikidataId")):
             if wd_id is not None and is_qid(wd_id):
@@ -187,9 +241,46 @@ class SenzingExporter(Exporter):
         entity_url = make_entity_url(entity)
         if entity_url is not None:
             record["URL"] = entity_url
+
         if entity.schema.is_a("Organization"):
             for addr in record.get("ADDRESSES", []):
                 addr["ADDR_TYPE"] = "BUSINESS"
+
+        # Collect address hashes to deduplicate addresses that have different case but are otherwise the same
+        # The hash_value() function calls a basic normalise function
+        if (addrs_list := record.get("ADDRESSES", [])) and len(addrs_list) > 1:
+            addr_hashes = set()
+            unique_addrs = []
+            for addr_dict in addrs_list:
+                # Try and find a value for each key seen in addresses, if can find a value hash it and keep distinct versions
+                for addr_attr in ADDR_ATTRS:
+                    if addr_value := addr_dict.get(addr_attr, ""):
+                        # Check if it is in the set instead of just doing an add to get PRIMARY type on uniques and not have an alias replace it
+                        if (
+                            addr_hash := hash_value(f"{addr_attr}{addr_value}")
+                        ) not in addr_hashes:
+                            addr_hashes.add(addr_hash)
+                            if addr_type := addr_dict.get("ADDR_TYPE", ""):
+                                unique_addrs.append(
+                                    {"ADDR_TYPE": addr_type, addr_attr: addr_value}
+                                )
+                            else:
+                                unique_addrs.append({addr_attr: addr_value})
+
+            if len(addrs_list) != len(addr_hashes):
+                record["ADDRESSES"] = unique_addrs
+
+        # Fix US- prefixed to the state, seen in lei-
+        # Change address from 'CORPORATION TRUST CENTER, WILMINGTON, US-DE 19801' -> 'CORPORATION TRUST CENTER, WILMINGTON, DE, 19801'
+        if entity.id.startswith("lei-"):
+            for addr_index, addr_dict in enumerate(record.get("ADDRESSES", [])):
+                if (addr_str := addr_dict.get("ADDR_FULL", "")) and ", US-" in addr_str:
+                    us_index = addr_str.rindex(" US-")
+                    us_list = addr_str[us_index:].strip().split(" ")
+                    us_list[0] = us_list[0].replace("US-", "")
+                    new_addr = f"{addr_str[:us_index + 1]}{', '.join(us_list)}"
+                    record["ADDRESSES"][addr_index]["ADDR_FULL"] = new_addr
+
         # pprint(record)
         write_json(record, self.fh)
 
