ce3f00b2b6dc098a8e627f1f7ed6e93b2cba1869
nvmbrasserie <ivan@opensanctions.org>
2025-03-11 01:38:08 +0100

    gpt extraction

--- datasets/bh/mfa_terrorists/crawler.py
+++ datasets/bh/mfa_terrorists/crawler.py
@@ -1,36 +1,42 @@
-# import pdfplumber
-import pytesseract
-from pdf2image import convert_from_path
+from rigour.mime.types import PDF
 
-# from rigour.mime.types import PDF
-from zavod import Context
+from zavod import Context, helpers as h
+from zavod.shed.gpt import run_image_prompt
+
+
+# prompt = """
+# Extract structured data from the following page of a PDF document. Return
+# a JSON list (`entities`) in which each object represents a sanctioned entity
+# and its alias (alias is always in the brackets). Each object should have the
+# following fields: `entity`, `alias`. It can be either a person or an entity.
+# If you cannot recognize the entity, return an empty string for both fields.
+# """
+
+  prompt = """
+    Extract structured data from the following page of a PDF document. Return
+    a JSON list (`entities`) in which each object represents an entry. 
+    Each object should have the fields: `name`, `alias`, `nationality`, and `type`.
+    If the entry is a person, set `type` to "person", and if it is an organization, 
+    set to "organization". If you can't recognize, set all fields to an empty string.
+
+    Example JSON output:
+    {
+        "entities": [
+            {"name": "حاكم عبيسان الحميدي المطيري", "alias": "", "nationality": "سعودي - كويتي", "type": "person"},
+            {"name": "Hezbollah", "alias": "", "type": "organization"},
+            {"name": "", "alias": "", "type": ""}
+        ]
+    }
+    If you cannot accurately determine or reconstruct a name or alias due to missing characters, 
+    set the value as an empty string.
+    """
 
 
 def crawl(context: Context):
-    images = convert_from_path(
-        "data/datasets/bh_mfa_terrorists/source.pdf", first_page=5
-    )  # Convert PDF page to image
-    text = pytesseract.image_to_string(images[0], lang="ara")  # Arabic OCR
-    print(text)
-
-
-# Previous attempt with pdfplumber, not being able to decode some chars
-
-# pdf_path = context.fetch_resource("source.pdf", context.data_url)
-# context.export_resource(pdf_path, PDF)
-# full_text = ""
-# try:
-#     with pdfplumber.open(pdf_path) as pdf:
-#         # Start extraction from page 4 onwards
-#         for page_num in range(3, len(pdf.pages)):
-#             page = pdf.pages[page_num]
-#             full_text += page.extract_text() or ""
-
-#     # context.log.info(f"Extracted Text: {full_text}")
-#     # text = full_text.encode("utf-8", "ignore").decode("utf-8")
-
-#     for char in full_text[:300]:
-#         print(f"{char}: {ord(char)}")
-
-# except Exception as e:
-#     context.log.warning(f"Error extracting text from {pdf_path}: {e}")
+    path = context.fetch_resource("source.pdf", context.data_url)
+    context.export_resource(path, PDF, title=context.SOURCE_TITLE)
+    for page_path in h.make_pdf_page_images(path):
+        data = run_image_prompt(context, prompt, page_path)
+        assert "entities" in data, data
+        for entity in data.get("entities", []):
+            print(entity)
