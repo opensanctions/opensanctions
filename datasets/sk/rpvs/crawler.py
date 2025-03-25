e3faa0c93b11695852c0ae3c098b053255ded902
nvmbrasserie <ivan@opensanctions.org>
2025-03-25 20:30:16 +0100

    links factored out and first ownerships mapped

--- datasets/sk/rpvs/crawler.py
+++ datasets/sk/rpvs/crawler.py
@@ -4,6 +4,11 @@ from zavod import Context, helpers as h
 
 # import zipfile
 # from zavod.shed.bods import parse_bods_fh
+BASE_URL = "https://rpvs.gov.sk/opendatav2"
+ENTITY_DETAILS_ENDPOINT = (
+    f"{BASE_URL}/PartneriVerejnehoSektora/{{id}}?$expand=Partner,PravnaForma,Adresa"
+)
+PARTNER_DETAILS_ENDPOINT = f"{BASE_URL}/Partneri/{{id}}?$expand=Vymaz,Pokuta,OverenieIdentifikacieKUV,konecniUzivateliaVyhod,verejniFunkcionari,kvalifikovanePodnety"
 
 
 TOTAL_COUNT = "https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora/$count"
@@ -22,27 +27,30 @@ LAST_PAGE = (
 #                 parse_bods_fh(context, fh)
 
 
+def check_failed_response(context, response, url):
+    if response.status_code != 200:
+        context.log.warn("Failed to fetch data", url=url)
+        return True
+    return False
+
+
 def crawl(context: Context):
     entity_ids = []
     headers = {"Accept": "application/json"}
     # url = context.data_url
     response = requests.get(context.data_url, headers=headers)
-    if response.status_code != 200:
-        context.log.warn("Failed to fetch data", url=context.data_url)
+    if check_failed_response(context, response, context.data_url):
         return
 
     # while url:
     data = response.json()
     entity_ids = [entry["Id"] for entry in data.get("value")]
     for id in entity_ids:
-        details_url = f"https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora/{id}?$expand=Partner,PravnaForma,Adresa"
+        details_url = ENTITY_DETAILS_ENDPOINT.format(id=id)
+        # details_url = f"https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora/{id}?$expand=Partner,PravnaForma,Adresa"
         details_response = requests.get(details_url, headers=headers)
-        if details_response.status_code != 200:
-            context.log.warn("Failed to fetch data", url=details_url)
-            return
-        # context.log.info(
-        #     "Fetched data", url=details_url, status=details_response.status_code
-        # )
+        if check_failed_response(context, details_response, details_url):
+            continue
         entity_data = details_response.json()
         entity_id = entity_data.get("Id")
         entry_number = entity_data.get("CisloVlozky")
@@ -88,8 +96,7 @@ def crawl(context: Context):
             postal_code = address.get("Psc")
             address = h.make_address(
                 context,
-                street=street_name,
-                street2=street_number,
+                street=street_name + " " + street_number,
                 city=city,
                 postal_code=postal_code,
             )
@@ -98,40 +105,49 @@ def crawl(context: Context):
         partner = entity_data.get("Partner", {})
         if partner:
             partner_id = partner.get("Id")
-            print(partner_id)
             partner_entry_number = partner.get("CisloVlozky")
+            partner_url = PARTNER_DETAILS_ENDPOINT.format(id=partner_id)
+            # partner_url = f"https://rpvs.gov.sk/opendatav2/Partneri/{id}?$expand=Vymaz,Pokuta,OverenieIdentifikacieKUV,konecniUzivateliaVyhod,verejniFunkcionari,kvalifikovanePodnety"
+            partner_response = requests.get(partner_url, headers=headers)
+            if check_failed_response(context, partner_response, partner_url):
+                continue
+            partner_data = partner_response.json()
+            entry_number = partner_data.get("CisloVlozky")
+            # fines = partner_data.get("Pokuta", "None")
+            # deletion_status = "Deleted" if partner_data.get("Vymaz") else "Active"
 
-        # if partner_id:
-        #     partner_url = f"https://rpvs.gov.sk/opendatav2/Partneri/{id}?$expand=Vymaz,Pokuta,OverenieIdentifikacieKUV,konecniUzivateliaVyhod,verejniFunkcionari,kvalifikovanePodnety"
-        #     partner_response = requests.get(partner_url, headers=headers)
-        #     if partner_response.status_code != 200:
-        #         context.log.warn("Failed to fetch data", url=partner_url)
-        #         return
-        #     partner_data = partner_response.json()
-        #     entry_number = partner_data.get("CisloVlozky")
-        #     fines = partner_data.get("Pokuta", "None")
-        #     deletion_status = "Deleted" if partner_data.get("Vymaz") else "Active"
+            # Extract beneficial owners
+            beneficial_owners = []
+            for owner in partner_data.get("KonecniUzivateliaVyhod", []):
+                owner_name = f"{owner.get('Meno')} {owner.get('Priezvisko')}"
+                owner_dob = owner.get("DatumNarodenia")
+                owner_id = owner.get("Id")
+                beneficial_owners.append((owner_name, owner_dob))
+                owner = context.make("Person")
+                owner.id = context.make_id(owner_name, owner_dob, owner_id)
+                h.apply_name(owner, owner_name)
+                h.apply_date(owner, "birthDate", owner_dob)
+                context.emit(owner)
 
-        #     # Extract beneficial owners
-        #     beneficial_owners = []
-        #     for owner in partner_data.get("KonecniUzivateliaVyhod", []):
-        #         owner_name = f"{owner.get('Meno')} {owner.get('Priezvisko')}"
-        #         owner_dob = owner.get("DatumNarodenia")
-        #         beneficial_owners.append((owner_name, owner_dob))
+                own = context.make("Ownership")
+                own.id = context.make_id(entity.id, "owned by", owner.id)
+                own.add("owner", owner.id)
+                own.add("asset", entity.id)
+                context.emit(own)
 
-        #     # Extract verification details
-        #     verification_status = "Not Verified"
-        #     verification_data = partner_data.get("OverenieIdentifikacieKUV")
-        #     if verification_data:
-        #         verification_status = (
-        #             f"Verified on {verification_data.get('DatumOverenia')}"
-        #         )
+            # # Extract verification details
+            # verification_status = "Not Verified"
+            # verification_data = partner_data.get("OverenieIdentifikacieKUV")
+            # if verification_data:
+            #     verification_status = (
+            #         f"Verified on {verification_data.get('DatumOverenia')}"
+            #     )
 
-        #     # Extract public officials
-        #     public_officials = [
-        #         f"{f.get('Meno')} {f.get('Priezvisko')}"
-        #         for f in partner_data.get("VerejniFunkcionari", [])
-        #     ]
-        #     print(public_officials)
+            # # Extract public officials
+            # public_officials = [
+            #     f"{f.get('Meno')} {f.get('Priezvisko')}"
+            #     for f in partner_data.get("VerejniFunkcionari", [])
+            # ]
+            # print(public_officials)
 
         # url = data.get("@odata.nextLink", None)
