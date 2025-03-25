9999681a098d1194e6efd6ac5020f0a6807a8f1d
nvmbrasserie <ivan@opensanctions.org>
2025-03-25 16:52:49 +0100

    fully unpacked

--- datasets/sk/rpvs/crawler.py
+++ datasets/sk/rpvs/crawler.py
@@ -6,6 +6,12 @@ from zavod import Context
 # from zavod.shed.bods import parse_bods_fh
 
 
+TOTAL_COUNT = "https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora/$count"
+FIRST_PAGE = "https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora?$skip=0"
+LAST_PAGE = (
+    "https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora?$skiptoken=Id-261529"
+)
+
 # def crawl(context: Context) -> None:
 #     fn = context.fetch_resource("source.zip", context.data_url)
 #     with zipfile.ZipFile(fn, "r") as zf:
@@ -17,36 +23,34 @@ from zavod import Context
 
 
 def crawl(context: Context):
-    endpoint = "KonecniUzivateliaVyhod"
-    base_url = "https://rpvs.gov.sk/opendatav2/"
-    full_url = f"{base_url}{endpoint}"
+    entity_ids = []
+    headers = {"Accept": "application/json"}
+    url = context.data_url
+    response = requests.get(context.data_url, headers=headers)
+    if response.status_code != 200:
+        context.log.warn("Failed to fetch data", url=url)
+        return
 
-    # Filter query
-    params = {
-        "$skip": 0,
-        "$filter": "partner/cisloVlozky eq 10",
-    }
+    # while url:
+    data = response.json()
+    entity_ids = [entry["Id"] for entry in data.get("value")]
+    entity_ids.extend(entity_ids)
+    for id in entity_ids:
+        details_url = f"https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora/{id}?$expand=Partner,PravnaForma,Adresa"
+        details_response = requests.get(url, headers=headers)
+        if details_response.status_code != 200:
+            context.log.warn("Failed to fetch data", url=details_url)
+            return
+        entity_details = details_response.json()
+        # print(entity_details)
+        partner_id = entity_details.get("Partner", {}).get("Id")
+        if partner_id:
+            partner_url = f"https://rpvs.gov.sk/opendatav2/Partneri/{id}$expand=%20Vymaz,Pokuta,OverenieIdentifikacieKUV"
+            partner_response = requests.get(partner_url, headers=headers)
+            if partner_response.status_code != 200:
+                context.log.warn("Failed to fetch data", url=partner_url)
+                return
+            partner_data = partner_response.json()
+            # print(partner_data)
 
-    headers = {"Accept": "application/json;odata.metadata=minimal"}
-
-    try:
-        response = requests.get(full_url, headers=headers, params=params)
-        if response.status_code == 200:
-            try:
-                results = response.json()
-                if results:
-                    for company in results.get("value", []):
-                        print(company)
-                else:
-                    context.log.info("No results found.")
-            except requests.exceptions.JSONDecodeError:
-                context.log.error("Failed to decode JSON. Response: %s", response.text)
-        else:
-            context.log.error(
-                "Failed to retrieve data. Status code: %s. Response: %s",
-                response.status_code,
-                response.text,
-            )
-
-    except requests.exceptions.RequestException as e:
-        context.log.error("Request failed: %s", str(e))
+        # url = data.get("@odata.nextLink", None)
