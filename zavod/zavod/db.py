edc0c7b4c4a6416e0febc98cc19561a7d666ba18
Friedrich Lindenberg <friedrich@opensanctions.org>
2025-03-29 10:11:55 +0100

    womp

--- zavod/zavod/db.py
+++ zavod/zavod/db.py
@@ -1,6 +1,5 @@
 from functools import cache
 from sqlalchemy.engine import Engine
-from nomenklatura import settings as nk_settings
 from nomenklatura.db import get_engine as get_nk_engine
 from nomenklatura.db import get_metadata as get_nk_metadata
 
@@ -15,5 +14,4 @@ meta = get_nk_metadata()
 def get_engine() -> Engine:
     """Get a SQLAlchemy engine for the given database URI."""
     get_nk_engine.cache_clear()
-    nk_settings.DB_STMT_TIMEOUT = settings.DB_STMT_TIMEOUT
     return get_nk_engine(settings.DATABASE_URI)
