8373a2c521bcb5ab633b30ce0b9eb4399df083fb
nvmbrasserie <ivan@opensanctions.org>
2025-04-07 16:45:55 +0200

    fully remapped keys

--- datasets/sk/rpvs/crawler.py
+++ datasets/sk/rpvs/crawler.py
@@ -16,7 +16,7 @@ FIRST_PAGE = "https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora?$skip=0"
 
 
 # TODO do we want to add contry "SK" everywhere?
-
+# TODO add some context
 # def crawl(context: Context) -> None:
 #     fn = context.fetch_resource("source.zip", context.data_url)
 #     with zipfile.ZipFile(fn, "r") as zf:
@@ -32,14 +32,25 @@ FIRST_PAGE = "https://rpvs.gov.sk/opendatav2/PartneriVerejnehoSektora?$skip=0"
 # https://rpvs.gov.sk/opendatav2/KonecniUzivateliaVyhod/45?$expand=Partner,PravnaForma,Adresa
 
 
+def rename_headers(context, entry):
+    result = {}
+    for old_key, value in entry.items():
+        new_key = context.lookup_value("columns", old_key)
+        if new_key is None:
+            context.log.warning("Unknown column title", column=old_key)
+            new_key = old_key
+        result[new_key] = value
+    return result
+
+
 def apply_address(context, entity, address):
     if not address:
         return
-    street_name = address.pop("MenoUlice")
-    street_number = address.pop("OrientacneCislo", "")
-    city = address.pop("Mesto")
-    city_code = address.pop("MestoKod", "")
-    postal_code = address.pop("Psc")
+    street_name = address.pop("street_name")
+    street_number = address.pop("house_number", "")
+    city = address.pop("city")
+    city_code = address.pop("city_code", "")
+    postal_code = address.pop("postal_code")
 
     address = h.make_address(
         context,
@@ -61,12 +72,12 @@ def fetch_related_data(context, related_id, endpoint):
 
 
 def emit_related_entity(context, entity_data, is_pep):
-    first_name = entity_data.pop("Meno")
-    last_name = entity_data.pop("Priezvisko")
-    dob = entity_data.pop("DatumNarodenia")
+    first_name = entity_data.pop("name")
+    last_name = entity_data.pop("surname")
+    dob = entity_data.pop("dob")
 
     # One more flag for public officials (used in ownership relationship)
-    public_official = entity_data.pop("JeVerejnyCinitel")
+    public_official = entity_data.pop("is_public_official")
 
     if not first_name and not last_name:
         context.log.warn("No first or last name", entity_data=entity_data)
@@ -76,9 +87,10 @@ def emit_related_entity(context, entity_data, is_pep):
     related.id = context.make_id(first_name, last_name, dob)
     h.apply_name(related, first_name=first_name, last_name=last_name)
     h.apply_date(related, "birthDate", dob)
-    related.add("title", entity_data.pop("TitulPred"))
-    related.add("title", entity_data.pop("TitulZa"))
-    if address := entity_data.pop("Adresa"):
+    related.add("title", entity_data.pop("title_prefix"))
+    related.add("title", entity_data.pop("title_suffix"))
+    if address := entity_data.pop("address"):
+        address = rename_headers(context, address)
         apply_address(context, related, address)
     if public_official or is_pep:
         # Based on the internal categorization provided by the source
@@ -87,7 +99,7 @@ def emit_related_entity(context, entity_data, is_pep):
 
     context.emit(related)
     context.audit_data(
-        entity_data, ["@odata.context", "Id", "PlatnostOd", "PlatnostDo", "Partner"]
+        entity_data, ["@odata.context", "valid_from", "valid_to", "partner", "id"]
     )
     return related
 
@@ -114,48 +126,54 @@ def process_entry(context, entry):
     details_url = ENTITY_DETAILS.format(id=entity_id)
 
     entity_data = context.fetch_json(details_url, cache_days=3)
-    legal_entity_name = entity_data.get("ObchodneMeno", "")
+    entity_data = rename_headers(context, entity_data)
+    legal_entity_name = entity_data.get("trading_name", "")
 
     entity = context.make("LegalEntity" if legal_entity_name else "Person")
     entity.id = context.make_id(
-        entity_data.pop("Id"), entity_data.pop("CisloVlozky", "")
+        entity_data.pop("id"), entity_data.pop("CisloVlozky", "")
     )
     if entity.schema.name == "Person":
         h.apply_name(
             entity,
-            first_name=entity_data.pop("Meno"),
-            last_name=entity_data.pop("Priezvisko"),
+            first_name=entity_data.pop("name"),
+            last_name=entity_data.pop("surname"),
         )
-        h.apply_date(entity, "birthDate", entity_data.pop("DatumNarodenia"))
+        h.apply_date(entity, "birthDate", entity_data.pop("dob"))
     else:
-        entity.add("name", entity_data.pop("ObchodneMeno"))
-        entity.add("registrationNumber", entity_data.pop("Ico"))
-        entity.add("legalForm", entity_data.pop("FormaOsoby"))
+        entity.add("name", entity_data.pop("trading_name"))
+        entity.add("registrationNumber", entity_data.pop("registration_number"))
+        entity.add("legalForm", entity_data.pop("entity_type"))
 
-    if legal_form := entity_data.pop("PravnaForma"):
-        entity.add("legalForm", legal_form.pop("Meno"))
-        entity.add("classification", legal_form.pop("StatistickyKod"))
+    if legal_form := entity_data.pop("legal_form"):
+        legal_form = rename_headers(context, legal_form)
+        entity.add("legalForm", legal_form.pop("name"))
+        entity.add("classification", legal_form.pop("economic_classification"))
 
-    if address := entity_data.pop("Adresa"):
+    if address := entity_data.pop("address"):
+        address = rename_headers(context, address)
         apply_address(context, entity, address)
     context.emit(entity)
     context.audit_data(
-        entity_data, ["@odata.context", "PlatnostOd", "PlatnostDo", "Partner"]
+        entity_data, ["@odata.context", "valid_from", "valid_to", "partner"]
     )
 
-    if partner := entity_data.pop("Partner"):
+    if partner := entity_data.pop("partner"):
         partner_id = partner.pop("Id")
         partner_url = PARTNER_DETAILS.format(id=partner_id)
         partner_data = context.fetch_json(partner_url)
+        partner_data = rename_headers(context, partner_data)
 
         # entry_number = partner_data.pop("CisloVlozky")
-        for owner in partner_data.pop("KonecniUzivateliaVyhod"):
+        for owner in partner_data.pop("beneficial_owners"):
             owner_data = fetch_related_data(context, owner.pop("Id"), BENEFICIAL_OWNERS)
+            owner_data = rename_headers(context, owner_data)
             rel_entity = emit_related_entity(context, owner_data, is_pep=False)
             emit_ownership(context, rel_entity, entity.id)
 
-        for pep in partner_data.pop("VerejniFunkcionari"):
-            pep_data = fetch_related_data(context, pep.pop("Id"), PUBLIC_OFFICIALS)
+        for pep in partner_data.pop("public_officials"):
+            pep_data = fetch_related_data(context, pep.pop("id"), PUBLIC_OFFICIALS)
+            pep_data = rename_headers(context, pep_data)
             rel_entity = emit_related_entity(context, pep_data, is_pep=True)
             emit_link(context, rel_entity, entity.id)
 
