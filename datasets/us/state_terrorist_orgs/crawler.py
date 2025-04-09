131f1ac2056fbfa26cc17be816702234e2d4cc1d
nvmbrasserie <ivan@opensanctions.org>
2025-04-09 21:39:44 +0200

    split_clean_name factored out

--- datasets/us/state_terrorist_orgs/crawler.py
+++ datasets/us/state_terrorist_orgs/crawler.py
@@ -3,6 +3,30 @@ from zavod import Context, helpers as h
 PROGRAM = "Foreign Terrorist Organizations designated under section 219 of the INA"
 
 
+def split_clean_name(context, name):
+    if len(name.split(" (")) == 2:
+        parts = name.split(" (")
+        name_clean = parts[0]
+        alias = parts[1].rstrip(")")
+        name_former = ""
+    elif len(name.split("— ")) > 1:
+        result = context.lookup_value("names", name)
+        if result is None:
+            context.log.warning("Name override is not found", name=name)
+            return name, "", ""
+        if result.names:
+            for name in result.names:
+                name_clean = name.get("name")
+                name_former = name.get("name_former")
+                alias = name.get("alias")
+    else:
+        name_clean = name
+        alias = ""
+        name_former = ""
+
+    return name_clean, name_former, alias
+
+
 def crawl(context: Context):
     doc = context.fetch_html(context.data_url)
 
@@ -16,17 +40,11 @@ def crawl(context: Context):
         name = str_row.pop("name")
         start_date = str_row.pop("date_designated")
 
-        if len(name.split(" (")) == 2:
-            parts = name.split(" (")
-            name_clean = parts[0]
-            alias = parts[1].rstrip(")")
-        else:
-            name_clean = name
-            print(name)
-            alias = ""
+        name_clean, name_former, alias = split_clean_name(context, name)
         entity = context.make("LegalEntity")
         entity.id = context.make_id(name, start_date)
         entity.add("name", name_clean)
+        entity.add("previousName", name_former)
         entity.add("alias", alias)
 
         sanction = h.make_sanction(context, entity)
