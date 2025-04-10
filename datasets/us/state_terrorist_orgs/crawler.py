9bf0bf5f0e4400d4ad9d4e56c3497326b6101696
nvmbrasserie <ivan@opensanctions.org>
2025-04-10 18:52:13 +0200

    both tables covered and restructured

--- datasets/us/state_terrorist_orgs/crawler.py
+++ datasets/us/state_terrorist_orgs/crawler.py
@@ -8,8 +8,8 @@ def split_clean_name(context, name):
     name_clean = name.strip()
     name_former = ""
     alias = ""
-    # Lookup override if name contains an em dash or en dash
-    if "—" in name or "–" in name:
+    # Lookup override if name contains a dash
+    if any(dash in name for dash in ["—", "–"]):
         result = context.lookup("names", name)
         if not result or not result.names:
             context.log.warning("Name override is not found", name=name)
@@ -28,32 +28,40 @@ def split_clean_name(context, name):
     return name_clean, name_former, alias
 
 
+def crawl_row(context, row):
+    name = row.pop("name")
+    start_date = row.pop("date_designated", None) or row.pop(
+        "date_originally_designated", None
+    )
+    end_date = row.pop("date_removed", None)
+
+    name_clean, name_former, alias = split_clean_name(context, name)
+    entity = context.make("LegalEntity")
+    entity.id = context.make_id(name, start_date)
+    entity.add("name", name_clean)
+    entity.add("alias", alias)
+    entity.add("previousName", name_former)
+    entity.add("topics", ["sanction", "crime.terror"])
+
+    sanction = h.make_sanction(context, entity)
+    sanction.add("program", PROGRAM)
+    h.apply_date(sanction, "startDate", start_date)
+    if end_date:
+        h.apply_date(sanction, "endDate", end_date)
+
+    context.emit(entity)
+    context.emit(sanction)
+
+    context.audit_data(row)
+
+
 def crawl(context: Context):
     doc = context.fetch_html(context.data_url)
 
     tables = doc.xpath(".//table")
     # We expect designated and delisted entities
     assert len(tables) == 2
-
-    designated = tables[0]
-    for row in h.parse_html_table(designated):
-        str_row = h.cells_to_str(row)
-        name = str_row.pop("name")
-        start_date = str_row.pop("date_designated")
-
-        name_clean, name_former, alias = split_clean_name(context, name)
-        entity = context.make("LegalEntity")
-        entity.id = context.make_id(name, start_date)
-        entity.add("name", name_clean)
-        entity.add("alias", alias)
-        entity.add("previousName", name_former)
-        entity.add("topics", ["sanction", "crime.terror"])
-
-        sanction = h.make_sanction(context, entity)
-        h.apply_date(sanction, "startDate", start_date)
-        sanction.add("program", PROGRAM)
-
-        context.emit(entity)
-        context.emit(sanction)
-
-        context.audit_data(str_row)
+    for table in tables:
+        for row in h.parse_html_table(table):
+            str_row = h.cells_to_str(row)
+            crawl_row(context, str_row)
