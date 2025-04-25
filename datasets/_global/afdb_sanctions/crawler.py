c1c2c87259403bc93afdb780d7f1dbf3a81c802e
JD Bothma <jbothma@gmail.com>
2025-04-25 10:28:12 +0100

    [afdb_sanctions] Aaaand we're back to datatable

--- datasets/_global/afdb_sanctions/crawler.py
+++ datasets/_global/afdb_sanctions/crawler.py
@@ -4,39 +4,38 @@ from zavod.shed.zyte_api import fetch_html
 
 
 def crawl(context: Context):
-    next_url = context.data_url
-    table_xpath = ".//table[contains(@class, 'views-table')]"
-    while next_url:
-        doc = fetch_html(context, next_url, table_xpath, cache_days=1)
-        doc.make_links_absolute(next_url)
-        next_link = doc.find(".//li[@class='next']/a")
-        next_url = next_link.get("href") if next_link is not None else None
-        tables = doc.xpath(table_xpath)
-        assert len(tables) == 1, tables
-        table = tables[0]
-        for row in h.parse_html_table(table):
-            cells = h.cells_to_str(row)
-            type_ = cells.pop("type")
-            schema = context.lookup_value("types", type_)
-            if schema is None:
-                context.log.error("Unknown entity type", type=type_)
-                schema = "LegalEntity"
+    tables_xpath = ".//table[contains(@id, 'datatable')]"
+    doc = fetch_html(
+        context, context.data_url, tables_xpath, html_source="httpResponseBody"
+    )
+    tables = doc.xpath(tables_xpath)
+    assert len(tables) == 1
+    table = tables[0]
+    for row in h.parse_html_table(table):
+        cells = h.cells_to_str(row)
 
-            name = cells.pop("name").strip()
-            country = cells.pop("nationality")
-            entity = context.make(schema)
-            entity.id = context.make_id(name, country)
-            entity.add("name", name)
-            entity.add("country", country)
+        type_ = cells.pop("type")
+        schema = context.lookup_value("types", type_)
+        if schema is None:
+            context.log.error("Unknown entity type", type=type_)
+            continue
 
-            sanction = h.make_sanction(context, entity)
-            sanction.add("reason", cells.pop("basis"))
-            h.apply_date(sanction, "startDate", cells.pop("from"))
-            h.apply_date(sanction, "endDate", cells.pop("to"))
-            if h.is_active(sanction):
-                entity.add("topics", "debarment")
+        name = cells.pop("name").strip()
+        if all(v == "" for v in cells.values()):
+            continue
+        country = cells.pop("nationality")
+        entity = context.make(schema)
+        entity.id = context.make_id(name, country)
+        entity.add("name", name)
+        entity.add("topics", "debarment")
+        entity.add("country", country)
 
-            context.emit(entity)
-            context.emit(sanction)
+        sanction = h.make_sanction(context, entity)
+        sanction.add("reason", cells.pop("basis"))
+        h.apply_date(sanction, "startDate", cells.pop("from"))
+        h.apply_date(sanction, "endDate", cells.pop("to"))
 
-            context.audit_data(cells, ["notes"])
+        context.emit(entity)
+        context.emit(sanction)
+
+        context.audit_data(cells, ["notes", "debarment_from_date"])
