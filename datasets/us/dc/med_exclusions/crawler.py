b71a08f11ed40c3ba7fed12f6f8f33e2798048d8
nvmbrasserie <ivan@opensanctions.org>
2025-04-28 18:42:20 +0200

    initial commit

---
+++ datasets/us/dc/med_exclusions/crawler.py
@@ -0,0 +1,63 @@
+from zavod import Context, helpers as h
+
+
+def emit_directorship(context, entity_id, principal):
+    director = context.make("Person")
+    director.id = context.make_id(principal)
+    director.add("name", principal)
+    director.add("country", "us")
+    context.emit(director)
+
+    dir = context.make("Directorship")
+    dir.id = context.make_id(director.id, "of", entity_id)
+    dir.add("organization", entity_id)
+    dir.add("director", director.id)
+    context.emit(dir)
+
+
+def crawl_row(context, row):
+    name = row.pop("name_of_individual", None)
+    company_name = row.pop("name_of_company", None)
+    schema = "Person" if name else "Company"
+
+    entity = context.make(schema)
+    entity.id = context.make_id(name, company_name)
+    if entity.schema.is_a("Person"):
+        entity.add("name", name)
+    else:
+        entity.add("name", company_name)
+        principal = row.pop("principals", None)
+        if principal:
+            emit_directorship(context, entity.id, principal)
+
+    entity.add("country", "us")
+    entity.add("address", row.pop("principal_address"))
+
+    sanction = h.make_sanction(context, entity)
+    sanction.set("authority", row.pop("agency_instituting_the_action", None))
+    sanction.add("reason", row.pop("reason_for_the_action", None))
+    h.apply_date(sanction, "startDate", row.pop("action_date"))
+    h.apply_date(
+        sanction,
+        "endDate",
+        row.pop("expiration_date", row.pop("termination_date", None)),
+    )
+    if h.is_active(sanction):
+        entity.add("topics", "debarment")
+
+    context.emit(entity)
+    context.emit(sanction)
+
+    context.audit_data(row)
+
+
+def crawl(context: Context):
+    doc = context.fetch_html(context.data_url, cache_days=1)
+    tables = doc.xpath(".//table")
+    # We expect 2 current and 2 historical tables
+    assert len(tables) == 4, "Expected 4 tables, found %d" % len(tables)
+
+    for table in tables:
+        for row in h.parse_html_table(table, header_tag="td"):
+            str_row = h.cells_to_str(row)
+            crawl_row(context, str_row)
