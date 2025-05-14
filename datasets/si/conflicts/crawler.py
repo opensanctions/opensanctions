47b0618acbd4e51724e2f982cd1fff79e0ee072b
nvmbrasserie <ivan@opensanctions.org>
2025-05-14 22:48:44 +0200

    [si_conflict] assertions updated and keys renamed

--- datasets/si/conflicts/crawler.py
+++ datasets/si/conflicts/crawler.py
@@ -11,52 +11,59 @@ RESTRICTION_NOTE = (
 )
 
 
-def crawl_entity(context: Context, record: Dict[str, Any]):
-    subject_name = record.pop("ps_naziv")
-    registration_number = record.pop("ps_maticna")
-    country = record.pop("drzava")
-    start_date = record.pop("datum_od")
-    end_date = record.pop("datum_do")
-    organization_name = record.pop("organ")
-    organization_number = record.pop("sifra_pu")
+def rename_record(context, entry):
+    result = {}
+    for old_key, value in entry.items():
+        new_key = context.lookup_value("columns", old_key)
+        if new_key is None:
+            context.log.warning("Unknown column title", column=old_key)
+            new_key = old_key
+        result[new_key] = value
+    return result
 
+
+def crawl_entity(context: Context, record: Dict[str, Any]):
+    subject_name = record.pop("entity_name")
+    registration_number = record.pop("entity_reg_number")
+    country = record.pop("country")
+    start_date = record.pop("start_date")
+    end_date = record.pop("end_date")
+    org_name = record.pop("org_name")
+    org_internal_id = record.pop("org_internal_id")
+
+    # There are some records with no subject name or registration number
     if not subject_name and not registration_number:
-        context.log.info(
-            "Subject name and registration number not found", record=record
-        )
         return
 
     legal_entity = context.make("LegalEntity")
     legal_entity.id = context.make_id(registration_number or subject_name)
     legal_entity.add("name", subject_name)
     legal_entity.add("registrationNumber", registration_number)
-    legal_entity.add("taxNumber", record.pop("ps_davcna"))
+    legal_entity.add("taxNumber", record.pop("entity_tax_number"))
     legal_entity.add("topics", "debarment")
     legal_entity.add("country", country)
-
-    address = h.make_address(
-        context,
-        street=record.pop("ps_naslov"),
-        place=record.pop("ps_posta"),
-        country=country,
-    )
-    h.copy_address(legal_entity, address)
-
-    if registration_number and organization_number:
-        legal_entity.add(
-            "sourceUrl",
-            f"https://registri.kpk-rs.si/registri/omejitve_poslovanja/seznam/#3={organization_number}&8={registration_number}",
-        )
-
     legal_entity.add(
         "program",
         RESTRICTION_NOTE.format(
-            organization=organization_name,
+            organization=org_name,
             subject=subject_name,
             start_date=start_date,
             until_clause=f"until {end_date}" if end_date else "until further notice",
         ),
     )
+    if registration_number and org_internal_id:
+        legal_entity.add(
+            "sourceUrl",
+            f"https://registri.kpk-rs.si/registri/omejitve_poslovanja/seznam/#3={org_internal_id}&8={registration_number}",
+        )
+
+    address = h.make_address(
+        context,
+        street=record.pop("entity_address"),
+        place=record.pop("entity_place"),
+        country=country,
+    )
+    h.copy_address(legal_entity, address)
 
     sanction = h.make_sanction(context, legal_entity)
     h.apply_date(sanction, "startDate", start_date)
@@ -66,11 +73,19 @@ def crawl_entity(context: Context, record: Dict[str, Any]):
     context.emit(sanction)
 
     context.audit_data(
-        record, ignore=["tip_omejitve", "posta", "naslov", "maticna", "davcna"]
+        record,
+        ignore=[
+            "restriction_type",
+            "org_place",
+            "org_address",
+            "org_reg_number",
+            "org_tax_number",
+        ],
     )
 
 
 def crawl(context: Context):
     response = context.fetch_json(context.data_url, cache_days=1)
     for record in response:
-        crawl_entity(context, record)
+        renamed_record = rename_record(context, record)
+        crawl_entity(context, renamed_record)
