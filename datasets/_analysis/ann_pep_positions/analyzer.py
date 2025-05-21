2255ffd7fb64a1abb3ca9e699bd4097c6a00a430
JD Bothma <jbothma@gmail.com>
2025-05-21 17:28:48 +0100

    Set keywords indicating status for all position holders

--- datasets/_analysis/ann_pep_positions/analyzer.py
+++ datasets/_analysis/ann_pep_positions/analyzer.py
@@ -1,24 +1,80 @@
+from typing import Dict, List, Set
+
 from zavod import Context, Entity
-from zavod.stateful.positions import categorise_many
-from zavod.meta import get_multi_dataset
-from zavod.store import get_store
 from zavod.integration import get_dataset_linker
+from zavod.meta import get_multi_dataset
+from zavod.stateful.positions import OccupancyStatus, categorise_many
+from zavod.store import get_store
 
 
-def analyze_position(context: Context, entity: Entity) -> None:
+INFLUENCE_TOPICS = {
+    "gov.national": "National government",
+    "gov.state": "State government",
+    "gov.igo": "International organization",
+    "gov.muni": "Local government",
+}
+STATUSES = {
+    OccupancyStatus.CURRENT.value: "current",
+    OccupancyStatus.ENDED.value: "past",
+    OccupancyStatus.UNKNOWN.value: "unknown status",
+}
+
+
+class Influence:
+    def __init__(self) -> None:
+        # influence topic -> best status
+        # e.g. {"gov.national": "current"}
+        self.topics: Dict[str, str] = {}
+
+    def add(self, topics: Set[str], statuses: List[str]) -> None:
+        for topic in topics:
+            if topic not in INFLUENCE_TOPICS:
+                continue
+            seen_status = self.topics.get(topic, None)
+            for status in statuses:
+                match (seen_status, status):
+                    case (OccupancyStatus.CURRENT.value, _):
+                        return  # Current trumps all
+                    case (OccupancyStatus.UNKNOWN.value, OccupancyStatus.ENDED.value):
+                        return  # Unknown trumps ended
+                    case _:
+                        self.topics[topic] = status
+
+    def make_keywords(self) -> List[str]:
+        keywords = []
+        for topic, status in self.topics.items():
+            level = INFLUENCE_TOPICS.get(topic, None)
+            if level is None:
+                continue
+            status_label = STATUSES.get(status, None)
+            if status_label is None:
+                continue
+            keywords.append(f"{level} ({status_label})")
+        return keywords
+
+
+def analyze_position(context: Context, entity: Entity) -> Set[str]:
     """Analyze a position entity and emit the categorisation."""
-    if entity.id is None:
-        return
+    topics = set()
+
+    assert entity.id is not None
     entity_ids = set(entity.referents)
     entity_ids.add(entity.id)
-    if not entity_ids:
-        return
-    for categorisation in categorise_many(context, entity_ids):
+
+    for categorisation in categorise_many(context, list(entity_ids)):
+        if not categorisation.topics:
+            continue
+
         proxy = context.make("Position")
         proxy.id = entity.id
+        # emit the topics for each referent under that ID
         proxy.add("topics", categorisation.topics)
         if proxy.get("topics"):
             context.emit(proxy)
+
+        # collect all the topics for the position
+        topics.update(categorisation.topics)
+    return topics
 
 
 def crawl(context: Context) -> None:
@@ -27,15 +83,39 @@ def crawl(context: Context) -> None:
     store = get_store(scope, linker)
     store.sync()
     view = store.view(scope)
-    position_count = 0
+    pep_count = 0
 
     for entity_idx, entity in enumerate(view.entities()):
-        if entity_idx > 0 and entity_idx % 1000 == 0:
+        if entity_idx > 0 and entity_idx % 10000 == 0:
             context.log.info("Processed %s entities" % entity_idx)
 
-        if entity.schema.is_a("Position"):
-            analyze_position(context, entity)
-            position_count += 1
-            if position_count % 1000 == 0:
-                context.log.info("Analyzed %s positions" % position_count)
-                context.flush()
+        if entity.schema.is_a("Person") and "role.pep" in entity.get("topics"):
+            pep_count += 1
+            if pep_count > 0 and pep_count % 10000 == 0:
+                context.log.info("Processed %s PEPs" % pep_count)
+            influence = Influence()
+
+            for prop, adjacent in view.get_adjacent(entity):
+                if prop.name != "positionOccupancies":
+                    continue
+
+                occupancy = adjacent
+
+                # Only one position expected per occupancy but handle surprises
+                for position_id in occupancy.get("post"):
+                    position = view.get_entity(position_id)
+                    if position is None:
+                        continue
+                    topics = analyze_position(context, position)
+                    if not topics:
+                        continue
+
+                    influence.add(topics, occupancy.get("status"))
+
+            keywords = influence.make_keywords()
+            if not keywords:
+                continue
+            person_proxy = context.make("Person")
+            person_proxy.id = entity.id
+            person_proxy.add("keywords", keywords)
+            context.emit(person_proxy)
