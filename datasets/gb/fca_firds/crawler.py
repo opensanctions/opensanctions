7b21b8a5e20ad71aca43c90e9ad19ef96671e018
nvmbrasserie <ivan@opensanctions.org>
2025-06-03 16:57:34 +0200

    post review

--- datasets/gb/fca_firds/crawler.py
+++ datasets/gb/fca_firds/crawler.py
@@ -1,16 +1,27 @@
+from urllib.parse import urlencode
+
 from zavod import Context
 from zavod.shed.firds import parse_xml_file
 
+# Total FIRDS files to fetch: 6246
+MAX_RECORDS = 10_000
+
 
 def get_full_dumps_index(context: Context):
-    page = 0
-    size = 100
+    params = {
+        "q": "file_type:FULINS",
+        "from": int(0),
+        "size": int(100),
+        "pretty": "true",
+    }
     total = None
     while True:
-        url = f"{context.data_url}?q=file_type:FULINS&from={page * size}&size={size}&pretty=true"
+        url = f"{context.data_url}?{urlencode(params)}"
         data = context.fetch_json(url)
         if total is None:
             total = data["hits"]["total"]
+            if total > MAX_RECORDS:
+                context.log.warning("Total FIRDS files to fetch exceeds 10,000")
             context.log.info(f"Total FIRDS files to fetch: {total}")
         hits = data["hits"]["hits"]
         if not hits:
@@ -18,8 +29,13 @@ def get_full_dumps_index(context: Context):
         for hit in hits:
             src = hit["_source"]
             yield src["file_name"], src["download_link"]
-        page += 1
-        if page * size >= total:
+
+        if len(hits) < params["size"]:
+            break  # Last page reached
+        params["from"] += params["size"]
+
+        if params["from"] >= MAX_RECORDS:
+            context.log.warning("Aborting after reaching 10,000 record cap.")
             break
 
 
