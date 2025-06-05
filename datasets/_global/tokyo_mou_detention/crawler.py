17ef4e5f0202f6825c7fb3550476d2ddc90e6e34
nvmbrasserie <ivan@opensanctions.org>
2025-06-05 22:09:01 +0200

    emit linked orgs

--- datasets/_global/tokyo_mou_detention/crawler.py
+++ datasets/_global/tokyo_mou_detention/crawler.py
@@ -12,9 +12,27 @@ data = {
 }
 
 
+def emit_linked_org(context, vessel_id, names, role):
+    for name in h.multi_split(names, ";"):
+        org = context.make("Organization")
+        org.id = context.make_id("org", name)
+        org.add("name", name)
+        context.emit(org)
+
+        link = context.make("UnknownLink")
+        link.id = context.make_id(vessel_id, org.id, role)
+        link.add("role", role)
+        link.add("subject", org.id)
+        link.add("object", vessel_id)
+        context.emit(link)
+
+
 def crawl_row(context: Context, clean_row: dict):
     ship_name = clean_row.pop("ship_name")
     imo = clean_row.pop("imo_no")
+    company_name = clean_row.pop("company")
+    ros = clean_row.pop("related_ros")
+    cs = clean_row.pop("classification_society")
 
     vessel = context.make("Vessel")
     vessel.id = context.make_id(ship_name, imo)
@@ -25,6 +43,16 @@ def crawl_row(context: Context, clean_row: dict):
     vessel.add("grossRegisteredTonnage", clean_row.pop("gross_tonnage"))
     vessel.add("type", clean_row.pop("ship_type"))
     vessel.add("topics", "reg.warn")
+    if company_name:
+        company = context.make("Company")
+        company.id = context.make_id("org", company_name)
+        company.add("name", company_name)
+        context.emit(company)
+        vessel.add("owner", company.id)
+    if ros:
+        emit_linked_org(context, vessel.id, ros, "Related Recognised Organization")
+    if cs:
+        emit_linked_org(context, vessel.id, cs, "Classification society")
 
     sanction = h.make_sanction(
         context,
@@ -32,23 +60,12 @@ def crawl_row(context: Context, clean_row: dict):
         start_date=clean_row.pop("date_of_detention"),
         end_date=clean_row.pop("date_of_release", None),
     )
-
-    sanction.add("summary", clean_row.pop("nature_of_deficiencies"))
+    sanction.add("reason", clean_row.pop("nature_of_deficiencies"))
 
     context.emit(vessel)
     context.emit(sanction)
 
-    context.audit_data(
-        clean_row,
-        ignore=[
-            "classification_society",
-            "related_ros",
-            "company",
-            "place_of_detention",
-            "nature_of_deficiencies",
-            "",
-        ],
-    )
+    context.audit_data(clean_row, ["place_of_detention"])
 
 
 def crawl(context: Context):
