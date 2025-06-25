52b85a45fa02354f1b1dddde7e9bf2763cbc7746
JD Bothma <jbothma@gmail.com>
2025-06-25 09:17:04 +0100

    Only use the set of files making up the latest release of the full dataset"

--- datasets/gb/fca_firds/crawler.py
+++ datasets/gb/fca_firds/crawler.py
@@ -1,46 +1,53 @@
+from collections import defaultdict
 from urllib.parse import urlencode
+import re
+from datetime import datetime, timedelta
 
 from zavod import Context
 from zavod.shed.firds import parse_xml_file
 
 # Total FIRDS files to fetch: 6246
 MAX_RECORDS = 10_000
+REGEX_DATE = re.compile(r"_(20\d{6})_")
 
 
-def get_full_dumps_index(context: Context):
+# https://api.data.fca.org.uk/fca_data_firds_files?q=((file_type:FULINS)%20AND%20(publication_date:[2017-10-15%20TO%202017-12-31]))&from=0&size=100&pretty=true
+def get_recent_full_data_files(context: Context):
+    from_date = (datetime.now() - timedelta(days=30)).isoformat()[:10]
+    to_date = datetime.now().isoformat()[:10]
     params = {
-        "q": "file_type:FULINS",
-        "from": int(0),
-        "size": int(100),
+        "q": f"file_type:FULINS AND publication_date:[{from_date} TO {to_date}]",
+        "from": 0,
+        "size": 100,
         "pretty": "true",
+        "sort": "file_name:asc",
     }
     total = None
-    while True:
+    while total is None or params["from"] <= total:
         url = f"{context.data_url}?{urlencode(params)}"
         data = context.fetch_json(url)
-        if total is None:
-            total = data["hits"]["total"]
-            if total > MAX_RECORDS:
-                context.log.warning("Total FIRDS files to fetch exceeds 10,000")
-            context.log.info(f"Total FIRDS files to fetch: {total}")
-        hits = data["hits"]["hits"]
-        if not hits:
-            break
-        for hit in hits:
+        total = data["hits"]["total"]
+        for hit in data["hits"]["hits"]:
             src = hit["_source"]
             yield src["file_name"], src["download_link"]
 
-        if len(hits) < params["size"]:
-            break  # Last page reached
         params["from"] += params["size"]
 
-        if params["from"] >= MAX_RECORDS:
-            context.log.warning("Aborting after reaching 10,000 record cap.")
-            break
+
+def get_latest_full_set(context: Context):
+    date_sets = defaultdict(list)
+    for file_name, url in get_recent_full_data_files(context):
+        match = REGEX_DATE.search(url)
+        if not match:
+            context.log.warning(f"URL {url} does not match expected date format.")
+            continue
+        date_str = match.group(1)
+        date_sets[date_str].append((file_name, url))
+    latest = max(date_sets.keys())
+    return date_sets[latest]
 
 
 def crawl(context: Context) -> None:
-    for file_name, url in get_full_dumps_index(context):
-        context.log.info("Fetching %s" % file_name, url=url)
+    for file_name, url in get_latest_full_set(context):
         path = context.fetch_resource(file_name, url)
         parse_xml_file(context, path)
