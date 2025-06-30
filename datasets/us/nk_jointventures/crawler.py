d98bd1ba0ef09f151cc23c1c00d410bda43e18b8
JD Bothma <jbothma@gmail.com>
2025-06-30 14:11:03 +0100

    Unblock with zyte, check PDF hash

--- datasets/us/nk_jointventures/crawler.py
+++ datasets/us/nk_jointventures/crawler.py
@@ -3,6 +3,7 @@ from rigour.mime.types import CSV
 import csv
 
 from zavod import Context, helpers as h
+from zavod.shed.zyte_api import fetch_html
 
 
 def crawl_item(context: Context, row: Dict[str, str]):
@@ -23,12 +24,18 @@ def crawl_item(context: Context, row: Dict[str, str]):
 
 
 def crawl(context: Context) -> None:
-    doc = context.fetch_html(context.dataset.url, cache_days=1)
-    table = doc.xpath(".//table[@aria-label='Table of Files associated with page']")
+    table_xpath = ".//table[@aria-label='Table of Files associated with page']"
+    doc = fetch_html(context, context.dataset.url, table_xpath, cache_days=1)
+    doc.make_links_absolute(context.dataset.url)
+    table = doc.xpath(table_xpath)
     assert len(table) == 1, "Expected exactly one table in the document"
-    h.assert_dom_hash(table[0], "86bc75bd5bd6ba998c7533323122f766e4007dfa")
-    # North Korea Sanctions & Enforcement Actions Advisory
-    # Last Modified: Aug 06, 2018
+    link = table[0].xpath(".//a/@href")
+    assert len(link) == 1, "Expected exactly one link in the table"
+
+    # Expect
+    # North Korea Sanctions & Enforcement Actions Advisory | 827.97 KB | 08/03/2018
+    # Assert hash of linked PDF (hopefully less fickle than HTML)
+    h.assert_url_hash(context, link[0], "cd9894479b1330bf0db3885ded3254e580af7acd")
 
     path = context.fetch_resource("source.csv", context.data_url)
     context.export_resource(path, CSV, title=context.SOURCE_TITLE)
