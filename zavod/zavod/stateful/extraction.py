effcaeb333b6bab2c3d95ec52a62a35c7645f915
JD Bothma <jbothma@gmail.com>
2025-07-08 13:20:38 +0100

    Naming and defaults

--- zavod/zavod/stateful/extraction.py
+++ zavod/zavod/stateful/extraction.py
@@ -1,6 +1,6 @@
 import json
-from typing import Type, TypeVar, List
-from pydantic import BaseModel, parse_obj_as
+from typing import Optional, TypeVar
+from pydantic import BaseModel
 from zavod.context import Context
 from zavod.db import get_engine
 from zavod.stateful.model import extraction_table
@@ -12,10 +12,12 @@ T = TypeVar("T", bound=BaseModel)
 
 
 def extract_items(
-    context: Context, key: str, raw_data: List[T], source_url: str, model_type: Type[T]
-) -> List[T]:
+    context: Context, key: str, raw_data: T, source_url: str
+) -> Optional[T]:
     """Add raw data to the extraction table and return accepted extracted data if hash matches, else empty list."""
-    raw_data_json = json.dumps([item.model_dump() for item in raw_data], sort_keys=True)
+    raw_data_dump = raw_data.model_dump()
+    # TODO: might need to sort arrays too
+    raw_data_json = json.dumps(raw_data_dump, sort_keys=True)
     data_hash = sha1(raw_data_json.encode("utf-8")).hexdigest()
     dataset = context.dataset.name
     engine = get_engine()
@@ -26,30 +28,32 @@ def extract_items(
         row = conn.execute(select_stmt).mappings().first()
         if row is None:
             # First insert
-            context.log.debug(f"Extraction key miss", key=key)
+            context.log.debug("Extraction key miss", key=key)
             ins = insert(extraction_table).values(
                 key=key,
                 dataset=dataset,
-                schema=model_type.model_json_schema(),
+                schema=type(raw_data).model_json_schema(),
                 source_url=source_url,
                 accepted=False,
-                raw_data=json.loads(raw_data_json),
-                extracted_data_hash=data_hash,
-                extracted_data=[],
+                raw_data=raw_data_dump,
+                raw_data_hash=data_hash,
+                extracted_data=raw_data_dump,
                 last_seen_version=context.version.id,
                 created_at=datetime.utcnow(),
             )
             conn.execute(ins)
-            return []
+            return None
         # Row exists
-        if row["extracted_data_hash"] == data_hash:
-            context.log.debug(f"Extraction key hit, hash matches", key=key, accepted=row["accepted"])
+        if row["raw_data_hash"] == data_hash:
+            context.log.debug(
+                "Extraction key hit, hash matches", key=key, accepted=row["accepted"]
+            )
             if row["accepted"]:
-                return [model_type.model_validate(x) for x in row["extracted_data"]]
+                return type(raw_data).model_validate(row["extracted_data"])
             else:
-                return []
+                return None
         # Hash differs, mark old row as deleted and insert new row
-        context.log.debug(f"Extraction key hit, hash differs", key=key)
+        context.log.debug("Extraction key hit, hash differs", key=key)
         now = datetime.utcnow()
         conn.execute(
             update(extraction_table)
@@ -59,14 +63,14 @@ def extract_items(
         ins = insert(extraction_table).values(
             key=key,
             dataset=dataset,
-            schema=model_type.model_json_schema(),
+            schema=type(raw_data).model_json_schema(),
             source_url=source_url,
             accepted=False,
-            raw_data=json.loads(raw_data_json),
-            extracted_data_hash=data_hash,
-            extracted_data=[],
+            raw_data=raw_data_dump,
+            raw_data_hash=data_hash,
+            extracted_data=raw_data_dump,
             last_seen_version=context.version.id,
             created_at=now,
         )
         conn.execute(ins)
-        return []
+        return None
