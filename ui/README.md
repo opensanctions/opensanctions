50171aa157cb47c26ccd5a72266f570780e5f254
JD Bothma <jbothma@gmail.com>
2025-07-15 18:12:10 +0100

    Add auth; fix save button valid/invalid states

--- ui/README.md
+++ ui/README.md
@@ -4,10 +4,17 @@ This project allows users to see automated data extraction results, fix any erro
 
 ## Getting Started
 
-First, run the development server:
+For local development, you'll want to connect to your local etl db using `ZAVOD_DATABASE_URI` and disable authentication using `ZAVOD_IAP_DISABLED`
 
 ```bash
 npm run dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+
+## Configuration
+
+- `ZAVOD_DATABASE_URI` - (e.g. `postgresql://user:pw@host.com/db`) - only postgres supported.
+- `ZAVOD_IAP_AUDIENCE` - https://cloud.google.com/iap/docs/signed-headers-howto#iap_validate_jwt-nodejs
+- `ZAVOD_IAP_AUTH_DISABLED` (default `false`; e.g. `true`) - skip IAP-based authentication **DON'T ENABLE WHEN PUBLICLY ACCESSIBLE**
--- ui/lib/auth.ts
+++ ui/lib/auth.ts
@@ -1,35 +1,37 @@
 import { OAuth2Client } from 'google-auth-library';
-import jwt from 'jsonwebtoken';
 
 const oAuth2Client = new OAuth2Client();
 // Expected Audience for App Engine.
 // expectedAudience = `/projects/${projectNumber}/apps/${projectId}`;
 // Expected Audience for Compute Engine
 //expectedAudience = `/projects/${projectNumber}/global/backendServices/${backendServiceId}`;
-const expectedAudience = process.env.IAP_AUDIENCE;
+const expectedAudience = process.env.ZAVOD_IAP_AUDIENCE;
 
-async function verify(request: Request) {
-    const iapAuthDisabled = process.env.IAP_AUTH_DISABLED === 'true';
-    if (iapAuthDisabled) {
-        return {
-            email: null,
-            name: 'Anonymous User',
-        };
+export async function verify(request: Request) {
+    if (process.env.ZAVOD_IAP_AUTH_DISABLED === 'true') {
+        return "anonymous"
     }
 
     // Verify the id_token, and access the claims.
     const iapJwt = request.headers.get('x-goog-iap-jwt-assertion');
     if (!iapJwt) {
+        console.log(
+            "No 'x-goog-iap-jwt-assertion' header in request",
+            { ip: request.headers.get('x-forwarded-for') }
+        );
         return null;
     }
     const response = await oAuth2Client.getIapPublicKeys();
     const ticket = await oAuth2Client.verifySignedJwtWithCertsAsync(
-      iapJwt,
-      response.pubkeys,
-      expectedAudience,
-      ['https://cloud.google.com/iap'],
+        iapJwt,
+        response.pubkeys,
+        expectedAudience,
+        ['https://cloud.google.com/iap'],
     );
+    // TODO: Remove after initial deployment when we've seen a token for a positive test.
     console.log(ticket);
-  }
+    console.log(ticket.getPayload())
+    return ticket.getPayload()?.email;
+}
 
-  verify().catch(console.error);
+
