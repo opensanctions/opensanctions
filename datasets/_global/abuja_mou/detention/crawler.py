b668841caf9f0d92aa13adcb7d7d9f9f925e11bd
nvmbrasserie <ivan@opensanctions.org>
2025-07-15 12:44:01 +0200

    data and headers for successful request

--- datasets/_global/abuja_mou/detention/crawler.py
+++ datasets/_global/abuja_mou/detention/crawler.py
@@ -1,20 +1,21 @@
 from datetime import datetime
-import re
 
 from zavod import Context, helpers as h
 
-START_YEAR = 2019
-START_MONTH = 1
-REGEX_AMPERSAND = re.compile(r"&? ?amp;", re.IGNORECASE)
 
-
-def clean_name(name: str) -> str:
-    name = REGEX_AMPERSAND.sub("&", name)
-    return name
-
-
-def is_future_month(year: int, month: int, now: datetime) -> bool:
-    return (year > now.year) or (year == now.year and month > now.month)
+DATA = {
+    "from": "01.01.2019",
+    "till": datetime.now().strftime("%d.%m.%Y"),
+    "auth": "0",
+    "held": "0",
+}
+HEADERS = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Referer": "https://abuja.marinet.ru/",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.2 Safari/605.1.15",
+    "X-Requested-With": "XMLHttpRequest",
+    "Origin": "https://abuja.marinet.ru",
+}
 
 
 def emit_linked_org(context, vessel_id, names, role, start_date, schema):
@@ -33,65 +34,62 @@ def emit_linked_org(context, vessel_id, names, role, start_date, schema):
         context.emit(link)
 
 
-def crawl_row(context: Context, clean_row: dict, row: dict):
-    ship_name = clean_row.pop("ship_name")
-    imo = clean_row.pop("imo_no")
-    company_name = clean_row.pop("company")
+def crawl_row(context: Context, row: dict):
+    ship_name = row.pop("name")
+    imo = row.pop("imo_number")
+    company_name = row.pop("company")
 
     vessel = context.make("Vessel")
     vessel.id = context.make_id(ship_name, imo)
     vessel.add("name", ship_name)
     vessel.add("imoNumber", imo)
-    vessel.add("flag", clean_row.pop("ship_flag"))
-    vessel.add("buildDate", clean_row.pop("year_of_build"))
-    vessel.add("grossRegisteredTonnage", clean_row.pop("gross_tonnage"))
-    vessel.add("type", clean_row.pop("ship_type"))
+    vessel.add("flag", row.pop("flag"))
+    vessel.add("buildDate", row.pop("year_of_build"))
+    vessel.add("grossRegisteredTonnage", row.pop("tonnage"))
+    vessel.add("type", row.pop("type"))
 
-    start_date = clean_row.pop("date_of_detention")
+    start_date = row.pop("date_of_detention")
     if company_name:
         emit_linked_org(
             context,
             vessel.id,
-            clean_name(company_name),
+            company_name,
             "Company",
             start_date,
             "Company",
         )
 
-    related_ros = clean_row.pop("related_ros")
+    related_ros = row.pop("related_ros")
     if related_ros:
         emit_linked_org(
             context,
             vessel.id,
-            clean_name(related_ros),
+            related_ros,
             "Related Recognised Organization",
             start_date,
             "Organization",
         )
-    class_soc = clean_row.pop("classification_society")
+    class_soc = row.pop("class")
     if class_soc:
         emit_linked_org(
             context,
             vessel.id,
-            clean_name(class_soc),
+            class_soc,
             "Classification society",
             start_date,
             "Organization",
         )
 
-    end_date = clean_row.pop("date_of_release", None)
-    reasons_cell = row.pop("nature_of_deficiencies")
-    for br in reasons_cell.xpath(".//br"):
-        br.tail = br.tail + "\n" if br.tail else "\n"
-    reason = reasons_cell.text_content().split("\n")
+    end_date = row.pop("date_of_release", None)
     sanction = h.make_sanction(
         context,
         vessel,
         start_date=start_date,
         end_date=end_date,
-        key=[start_date, end_date, sorted(reason)],
     )
-    sanction.add("reason", reason)
+    reasons = row.pop("nature_of_deficiencies")
+    for reason in reasons.split(";"):
+        sanction.add("reason", reason.strip())
 
     if h.is_active(sanction):
         vessel.add("topics", "reg.warn")
@@ -99,40 +97,20 @@ def crawl_row(context: Context, clean_row: dict, row: dict):
     context.emit(vessel)
     context.emit(sanction)
 
-    context.audit_data(clean_row, ["place_of_detention", "nature_of_deficiencies"])
+    context.audit_data(row, ["place", "nature_of_deficiencies", "None"])
 
 
 def crawl(context: Context):
-    now = datetime.utcnow()
-    year = START_YEAR
-    month = START_MONTH
-    while (year, month) <= (now.year, now.month):
-        # Break if the month is in the future
-        if is_future_month(year, month, now):
-            break
-        data = {
-            "Mode": "DetList",
-            "MOU": "TMOU",
-            "Src": "online",
-            "Type": "Auth",
-            "Month": f"{month:02}",  # pad month to two digits
-            "Year": str(year),
-            "SaveFile": "",
-        }
-        doc = context.fetch_html(
-            context.data_url, data=data, method="POST", cache_days=1
-        )
-        table = doc.xpath("//table[@cellspacing=1]")
-        assert len(table) == 1, "Expected one table in the document"
-        table = table[0]
-        for row in h.parse_html_table(table, header_tag="td", skiprows=1):
-            str_row = h.cells_to_str(row)
-            clean_row = {k: v for k, v in str_row.items() if k is not None}
-            crawl_row(context, clean_row, row)
-
-        # Increment month and roll over year
-        if month == 12:
-            month = 1
-            year += 1
-        else:
-            month += 1
+    doc = context.fetch_html(
+        "https://abuja.marinet.ru/public_det/?action=getinspections",
+        method="POST",
+        data=DATA,
+        headers=HEADERS,
+    )
+    table = doc.xpath("//table[@id='dvData']")
+    assert len(table) == 1, "Expected exactly one table"
+    table = table[0]
+    for row in h.parse_html_table(table):
+        str_row = h.cells_to_str(row)
+        cleaned_row = {k: v for k, v in str_row.items() if isinstance(k, str)}
+        crawl_row(context, cleaned_row)
