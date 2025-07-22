6fac0e622708bc6586f1142d58548cf9b6280e3f
nvmbrasserie <ivan@opensanctions.org>
2025-07-22 16:12:56 +0200

    [us_ms_med_exclusions] switched to the xlsx

--- datasets/us/ms/med_exclusions/crawler.py
+++ datasets/us/ms/med_exclusions/crawler.py
@@ -1,7 +1,6 @@
+import openpyxl
+from rigour.mime.types import XLSX
 from typing import Dict
-import csv
-
-from rigour.mime.types import CSV
 
 from zavod import Context, helpers as h
 
@@ -10,32 +9,26 @@ def crawl_item(row: Dict[str, str], context: Context):
     if not any(row.values()):  # Skip empty rows
         return
 
-    provider_name = row.pop("Provider Name")
-    dob = row.pop("Date of Birth")
-    npi = row.pop("NPI")
-
-    if dob:
-        schema = "Person"
-    else:
-        schema = "LegalEntity"
+    provider_name = row.pop("provider_name")
+    dob = row.pop("date_of_birth")
+    npi = row.pop("npi")
+    schema = "Person" if dob else "LegalEntity"
 
     entity = context.make(schema)
     entity.id = context.make_id(provider_name, row.get("npi"))
-
     entity.add("name", provider_name)
     entity.add("country", "us")
-    entity.add("sector", row.pop("Provider Type/Specialty"))
-    entity.add("address", row.pop("Provider Address"))
+    entity.add("sector", row.pop("provider_type_specialty"))
+    entity.add("address", row.pop("provider_address"))
     if entity.schema.name == "Person":
         h.apply_date(entity, "birthDate", dob)
     if npi:
-        entity.add("npiCode", npi.split("\n"))
+        entity.add("npiCode", npi.split(" "))
 
     sanction = h.make_sanction(context, entity)
-    h.apply_date(sanction, "startDate", row.pop("Termination Effective Date"))
-    sanction.add("reason", row.pop("Termination Reason"))
-
-    exclusion_period_str = row.pop("Exclusion Period")
+    h.apply_date(sanction, "startDate", row.pop("termination_effective_date"))
+    sanction.add("reason", row.pop("termination_reason"))
+    exclusion_period_str = row.pop("exclusion_period")
     exclusion_period_lookup_result = context.lookup(
         "exclusion_period", exclusion_period_str
     )
@@ -61,7 +54,7 @@ def crawl_item(row: Dict[str, str], context: Context):
     context.emit(entity)
     context.emit(sanction)
 
-    context.audit_data(row, ignore=["Sanction Type", ""])
+    context.audit_data(row, ignore=["column_0", "sanction_type"])
 
 
 def crawl_data_url(context: Context):
@@ -72,12 +65,14 @@ def crawl_data_url(context: Context):
 
 def crawl(context: Context) -> None:
     data_url = crawl_data_url(context)
-    path = context.fetch_resource("source.csv", data_url)
-    context.export_resource(path, CSV, title=context.SOURCE_TITLE)
+    path = context.fetch_resource("source.xlsx", data_url)
+    context.export_resource(path, XLSX, title=context.SOURCE_TITLE)
 
-    with open(path, "r", encoding="latin-1") as f:
-        lines = f.readlines()
-
-    # Skip the first 8 lines
-    for row in csv.DictReader(lines[9:]):
+    workbook: openpyxl.Workbook = openpyxl.load_workbook(path, read_only=True)
+    assert set(workbook.sheetnames) == set(["Sheet1", "Sheet2", "Sheet3"])
+    for row in h.parse_xlsx_sheet(
+        context,
+        sheet=workbook["Sheet1"],
+        skiprows=8,
+    ):
         crawl_item(row, context)
