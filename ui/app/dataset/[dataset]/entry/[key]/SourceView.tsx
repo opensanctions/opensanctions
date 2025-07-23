0766475c12a7187d6d237ce306e5e7d2f6147f3f
JD Bothma <jbothma@gmail.com>
2025-07-23 15:33:04 +0100

    Source link and better tab ordering

--- ui/app/dataset/[dataset]/entry/[key]/SourceView.tsx
+++ ui/app/dataset/[dataset]/entry/[key]/SourceView.tsx
@@ -9,44 +9,47 @@ import { markdown } from "@codemirror/lang-markdown";
 
 
 export default function SourceView({ sourceValue, sourceContentType, sourceLabel }: { sourceValue: string, sourceContentType: string, sourceLabel: string }) {
-  let tabTitle
+  const tabs: React.ReactNode[] = [];
+  const defaultViewer = <CodeMirror
+    value={sourceValue}
+    height="100%"
+    width="100%"
+    editable={false}
+    style={{ height: '100%', width: '100%' }}
+    extensions={[EditorView.lineWrapping]}
+    title={sourceLabel}
+  />;
+
   if (sourceContentType === 'text/html') {
-    tabTitle = 'Original HTML';
+    tabs.push(<Tab eventKey="rendered" title="As web page">
+      <div
+      style={{ height: '100%', width: '100%', overflow: 'auto', backgroundColor: '#fff', padding: '10px' }}
+      dangerouslySetInnerHTML={{ __html: sourceValue }} />
+    </Tab>)
+    var turndownService = new TurndownService();
+    tabs.push(<Tab eventKey="markdown" title="As Markdown">
+      <CodeMirror
+        value={turndownService.turndown(sourceValue)}
+        height="100%"
+        width="100%"
+        editable={false}
+        style={{ height: '100%', width: '100%' }}
+        extensions={[markdown(), EditorView.lineWrapping]}
+        title={sourceLabel}
+      />
+    </Tab>)
+    tabs.push(<Tab eventKey="source" title="Original HTML">{defaultViewer}</Tab>)
   } else if (sourceContentType === 'text/plain') {
-    tabTitle = 'Original Plain Text';
+    tabs.push(<Tab eventKey="source" title="Original Plain Text">{defaultViewer}</Tab>)
   } else if (sourceContentType === 'image/png') {
-    tabTitle = 'Original PNG';
+    tabs.push(<Tab eventKey="source" title="Original PNG"><img src={sourceValue} alt={sourceLabel} /></Tab>)
   } else {
-    tabTitle = sourceContentType;
+    tabs.push(<Tab eventKey="source" title={sourceContentType}>{defaultViewer}</Tab>)
   }
-  var turndownService = new TurndownService();
   return (
     <div className="flex-grow-1 d-flex flex-column source-view" style={{ height: '100%' }}>
-      <Tabs defaultActiveKey="source" className="flex-shrink-0">
-        <Tab eventKey="source" title={tabTitle}>
-          <CodeMirror
-            value={sourceValue}
-            height="100%"
-            width="100%"
-            editable={false}
-            style={{ height: '100%', width: '100%' }}
-            extensions={[EditorView.lineWrapping]}
-            title={sourceLabel}
-          />
-        </Tab>
-        {sourceContentType === 'text/html' && (
-          <Tab eventKey="markdown" title="Converted to Markdown">
-            <CodeMirror
-              value={turndownService.turndown(sourceValue)}
-              height="100%"
-              width="100%"
-              editable={false}
-              style={{ height: '100%', width: '100%' }}
-              extensions={[markdown(), EditorView.lineWrapping]}
-              title={sourceLabel}
-            />
-          </Tab>
-        )}
+      <Tabs className="flex-shrink-0">
+        {tabs}
       </Tabs>
     </div>
   )
