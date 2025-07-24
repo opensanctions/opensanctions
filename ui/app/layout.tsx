0c089d5d4d662e66dcd3cb07c4e72930acdc8a0f
JD Bothma <jbothma@gmail.com>
2025-07-24 11:41:02 +0100

    Up the IQ

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import version from '../version.json';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +30,7 @@ export default function RootLayout({
       >
         {children}
         <footer className="p-3" style={{ fontSize: '0.9em', height: '50px' }}>
-          Zavod UI ALPHA vsn: baa4dd4
+          Zavod UI vsn {version.git} ALPHA (built {version.buildTime})
         </footer>
       </body>
     </html>
