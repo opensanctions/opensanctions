9532b5073078a26ecbaccd38a6cf36b19539e394
nvmbrasserie <ivan@opensanctions.org>
2025-08-04 14:49:58 +0200

    [sec_harmed_investors] more headers to make the request successful

--- datasets/us/sec_harmed_investors/crawler.py
+++ datasets/us/sec_harmed_investors/crawler.py
@@ -3,7 +3,11 @@ from lxml.etree import _Element
 from zavod import Context, helpers as h
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.2 Safari/605.1.15",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
 }
 
 
