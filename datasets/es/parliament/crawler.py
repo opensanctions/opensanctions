2804d0755ab664e0f5d0f14c7f651cdfc9b205ea
JD Bothma <jbothma@gmail.com>
2025-08-12 16:29:12 +0100

    Add deputies birthdate

--- datasets/es/parliament/crawler.py
+++ datasets/es/parliament/crawler.py
@@ -1,20 +1,13 @@
 import os
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlencode, urlparse, parse_qs
 
 from zavod import Context, helpers as h
 from zavod.stateful.positions import categorise
 
 
 SENATORS_URL = "https://www.senado.es/web/composicionorganizacion/senadores/composicionsenado/index.html"
-DEPUTIES_URL = "https://www.congreso.es/en/busqueda-de-diputados?p_p_id=diputadomodule&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=searchDiputados&p_p_cacheability=cacheLevelPage"
-FORM_DATA = {
-    "_diputadomodule_idLegislatura": "15",
-    "_diputadomodule_genero": "0",
-    "_diputadomodule_grupo": "all",
-    "_diputadomodule_tipo": "0",
-    "_diputadomodule_formacion": "all",
-    "_diputadomodule_filtroProvincias": "[]",
-}
+DEPUTIES_URL = "https://www.congreso.es/en/busqueda-de-diputados"
+DEPUTIES_API_URL = "https://www.congreso.es/en/busqueda-de-diputados?p_p_id=diputadomodule&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=searchDiputados&p_p_cacheability=cacheLevelPage"
 IGNORE = ["constituency_id", "constituency_name", "legislative_term_id", "gender"]
 EXPECTED_EMPTY_XML = {"20019"}
 
@@ -64,10 +57,33 @@ def emit_pep_entities(
         context.emit(person)
 
 
-def crawl_deputy(context, item):
-    name = item.pop("full_name")
+def get_birth_date_and_place(context, profile_url):
+    doc = context.fetch_html(profile_url, cache_days=15)
+    born = doc.xpath("//h3[text() = 'Personal file']")[0].getnext().text_content()
+    parts = born.split(" in ")
+    birth_date = parts[0].replace("Born on", "").strip()
+    # very rough sanity check for date
+    assert "00:00:00 CE" in birth_date, birth_date
+    birth_place = parts[1].strip() if len(parts) > 1 else None
+    return birth_date, birth_place
+
+
+def crawl_deputy(context, item, current_leg_roman):
     id = item.pop("parliament_member_id")
+    query = {
+        "p_p_id": "diputadomodule",
+        "p_p_lifecycle": "0",
+        "p_p_state": "normal",
+        "p_p_mode": "view",
+        "_diputadomodule_mostrarFicha": "true",
+        "codParlamentario": id,
+        "idLegislatura": current_leg_roman,
+    }
+    profile_url = f"{DEPUTIES_URL}?{urlencode(query)}"
+
+    name = item.pop("full_name")
     party = item.pop("party")
+    birth_date, birth_place = get_birth_date_and_place(context, profile_url)
 
     person = context.make("Person")
     person.id = context.make_id(id, name, party)
@@ -79,8 +95,14 @@ def crawl_deputy(context, item):
         second_name=first_names[1] if len(first_names) > 1 else None,
         last_name=item.pop("last_name"),
     )
+    birth_date = birth_date.split(" ", 1)[1]
+    birth_date = birth_date.replace("CEST", "CET")
+    birth_date = birth_date.replace("00:00:00 CET", "")
+    h.apply_date(person, "birthDate", birth_date)
+    person.add("birthPlace", birth_place)
     person.add("political", party)
     person.add("political", item.pop("parliamentary_group"))
+    person.add("sourceUrl", profile_url)
     emit_pep_entities(
         context,
         person,
@@ -94,7 +116,17 @@ def crawl_deputy(context, item):
     context.audit_data(item, IGNORE)
 
 
-def crawl_senator(context, doc_xml, link):
+def crawl_senator(context, senator_url):
+    parsed_url = urlparse(senator_url)
+    query_params = parse_qs(parsed_url.query)
+    senator_id = query_params["id1"][0]
+    legis = query_params["legis"][0]
+    xml_url = f"https://www.senado.es/web/ficopendataservlet?tipoFich=1&cod={senator_id}&legis={legis}"
+    path = context.fetch_resource(f"source_{senator_id}.xml", xml_url)
+    if os.path.getsize(path) == 0 and senator_id in EXPECTED_EMPTY_XML:
+        context.log.info("Empty XML file", url=xml_url)
+        return
+    doc_xml = context.parse_resource_xml(path)
     datos = doc_xml.find("datosPersonales")
     web_id = datos.findtext("idweb")
     first_names = datos.findtext("nombre").split(" ", 1)
@@ -112,7 +144,7 @@ def crawl_senator(context, doc_xml, link):
     h.apply_date(person, "deathDate", datos.findtext("fechaFallecimiento"))
     person.add("birthPlace", datos.findtext("lugarNacimiento"))
     person.add("notes", datos.findtext("biografia"))
-    person.add("sourceUrl", link)
+    person.add("sourceUrl", senator_url)
 
     for legislatura in doc_xml.findall(".//legislatura"):
         # Additional parliamentary roles (cargos)
@@ -146,16 +178,29 @@ def crawl_senator(context, doc_xml, link):
 
 def crawl(context: Context):
     # Crawl Deputies
+    deputies_doc = context.fetch_html(DEPUTIES_URL, cache_days=1)
+    leg_select = deputies_doc.xpath("//select[@id='_diputadomodule_legislatura']")[0]
+    current_leg_option = leg_select.xpath("//option[@selected='selected']")[0]
+    current_leg_decimal = current_leg_option.xpath("@value")[0]
+    current_leg_roman = current_leg_option.text_content().split(" ")[0].strip()
+    form_data = {
+        "_diputadomodule_idLegislatura": current_leg_decimal,
+        "_diputadomodule_genero": "0",
+        "_diputadomodule_grupo": "all",
+        "_diputadomodule_tipo": "0",
+        "_diputadomodule_formacion": "all",
+        "_diputadomodule_filtroProvincias": "[]",
+    }
     data = context.fetch_json(
-        DEPUTIES_URL,
+        DEPUTIES_API_URL,
         method="POST",
         cache_days=1,
         headers={"Accept": "application/json"},
-        data=FORM_DATA,
+        data=form_data,
     )
     for item in data["data"]:
         item = rename_headers(context, item)
-        crawl_deputy(context, item)
+        crawl_deputy(context, item, current_leg_roman)
 
     # Crawl Senators
     doc = context.fetch_html(SENATORS_URL, cache_days=1)
@@ -164,14 +209,4 @@ def crawl(context: Context):
         letter_doc = context.fetch_html(letter_url, cache_days=1)
         letter_doc.make_links_absolute(letter_url)
         for senator_url in letter_doc.xpath(".//ul[@class='lista-alterna']//@href"):
-            parsed_url = urlparse(senator_url)
-            query_params = parse_qs(parsed_url.query)
-            senator_id = query_params["id1"][0]
-            legis = query_params["legis"][0]
-            xml_url = f"https://www.senado.es/web/ficopendataservlet?tipoFich=1&cod={senator_id}&legis={legis}"
-            path = context.fetch_resource(f"source_{senator_id}.xml", xml_url)
-            if os.path.getsize(path) == 0 and senator_id in EXPECTED_EMPTY_XML:
-                context.log.info("Empty XML file", url=xml_url)
-                continue
-            doc_xml = context.parse_resource_xml(path)
-            crawl_senator(context, doc_xml, senator_url)
+            crawl_senator(context, senator_url)
