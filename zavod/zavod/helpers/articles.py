4fc7daf994ed1d3387106c01979fc84c8910d862
JD Bothma <jbothma@gmail.com>
2025-08-18 13:14:50 +0100

    Keep date arg in case some date other than article's is preferred

--- zavod/zavod/helpers/articles.py
+++ zavod/zavod/helpers/articles.py
@@ -37,10 +37,12 @@ def make_documentation(
     entity: Entity,
     article: Entity,
     key_extra: Optional[str] = None,
+    date: Optional[str] = None,
 ) -> Entity:
     """
     Creates a documentation entity to link an article to a related entity.
-    The article's publishedAt date is added to the Documentation date property.
+    The article's publishedAt date is added to the Documentation date property
+    unless the date argument is provided.
 
     This is useful to link one or more entities to an article they were mentioned in.
 
@@ -51,6 +53,7 @@ def make_documentation(
         entity: The entity related to the article.
         article: The related article.
         key_extra: An optional value to be included in the generated Documentation ID hash.
+        date: The publication date of the article, added to the Documentation date property.
     """
 
     documentation = context.make("Documentation")
@@ -61,5 +64,9 @@ def make_documentation(
     )
     documentation.add("entity", entity)
     documentation.add("document", article)
-    documentation.set("date", article.get("publishedAt"))
+
+    if date:
+        h.apply_date(documentation, "date", date)
+    else:
+        documentation.set("date", article.get("publishedAt"))
     return documentation
