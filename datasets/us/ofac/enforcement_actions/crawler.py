d0d55b4118919101dd5cb21ea411c2c987982382
nvmbrasserie <ivan@opensanctions.org>
2025-08-20 17:38:21 +0200

    remove unused imports

--- datasets/us/ofac/enforcement_actions/crawler.py
+++ datasets/us/ofac/enforcement_actions/crawler.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime
+from datetime import date
 from lxml.html import HtmlElement, fromstring, tostring
 from pydantic import BaseModel, Field
 from rigour.mime.types import HTML
