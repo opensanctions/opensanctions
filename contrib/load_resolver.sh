0be2c9ee106ef51c7b4c8ef9f1db2644ee526866
Leon Handreke <leonh@ndreke.de>
2025-09-02 17:26:54 +0200

    Let contrib/load_resolver.sh use $ZAVOD_DATABASE_URI if set

--- contrib/load_resolver.sh
+++ contrib/load_resolver.sh
@@ -7,8 +7,11 @@ echo "Deleting old resolver data ($JSON_PATH, $SQLITE_PATH)"
 rm -f $JSON_PATH $SQLITE_PATH
 gcloud storage cp gs://resolver-backups.opensanctions.org/resolve.ijson $JSON_PATH
 
-echo "Loading resolver data from $JSON_PATH"
-export NOMENKLATURA_DB_URL=sqlite:///$SQLITE_PATH
-nomenklatura load-resolver $JSON_PATH
-
+if [ -n "$ZAVOD_DATABASE_URI" ]; then
+    export NOMENKLATURA_DB_URL=$ZAVOD_DATABASE_URI
+else
+    export NOMENKLATURA_DB_URL=sqlite:///$SQLITE_PATH
+fi
+echo "Loading resolver data from $JSON_PATH to $NOMENKLATURA_DB_URL"
+nomenklatura load-resolver "$JSON_PATH"
 echo "Resolver loaded to $NOMENKLATURA_DB_URL"
