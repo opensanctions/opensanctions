5ff220c3fc3f9c71eeb1a92fa539e1a8ad1e2e69
nvmbrasserie <ivan@opensanctions.org>
2025-09-05 14:24:38 +0200

    [nz_russia_sanctions] use just keys without hardcoding program names

--- datasets/nz/russia_sanctions/crawler.py
+++ datasets/nz/russia_sanctions/crawler.py
@@ -25,7 +25,7 @@ TYPES = {
 }
 ASSOCIATE = ("associate", "close associate")
 FAMILY = ("relative", "wife", "son", "daughter", "nephew")
-PROGRAM_NAME = "Russia Sanctions Act 2022"
+NZ_RSA2022 = "NZ-RSA2022"
 
 
 def clean_date(value: Any) -> Any:
@@ -117,8 +117,7 @@ def crawl_entity(context: Context, data: Dict[str, Any]) -> None:
     sanction = h.make_sanction(
         context,
         entity,
-        program_name=PROGRAM_NAME,
-        program_key=h.lookup_sanction_program_key(context, PROGRAM_NAME),
+        program_key=h.lookup_sanction_program_key(context, NZ_RSA2022),
     )
     sanction.add("startDate", data.pop("Date of Sanction"))
     modified_at = clean_date(data.pop("Date of Additional Sanction"))
