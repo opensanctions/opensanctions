dc57140f18ed81298b3fd73d388349e8964a79cc
Leon Handreke <leonh@ndreke.de>
2025-09-08 16:02:42 +0200

    [us_nh_med_exclusions] Switch to Zyte because of 403

--- datasets/us/nh/med_exclusions/crawler.py
+++ datasets/us/nh/med_exclusions/crawler.py
@@ -2,6 +2,7 @@ from typing import Dict
 from openpyxl import load_workbook
 
 from zavod import Context, helpers as h
+from zavod.shed import zyte_api
 
 
 def crawl_item(row: Dict[str, str], context: Context):
@@ -51,18 +52,21 @@ def crawl_item(row: Dict[str, str], context: Context):
 
 
 def crawl_excel_url(context: Context):
-    doc = context.fetch_html(context.data_url)
-    doc.make_links_absolute(context.data_url)
-    return doc.xpath(
+    sanction_list_xpath = (
         "//*[contains(text(), 'Medicaid Provider Exclusion and Sanction List')]"
-    )[0].get("href")
+    )
+    doc = zyte_api.fetch_html(
+        context, context.data_url, unblock_validator=sanction_list_xpath
+    )
+    doc.make_links_absolute(context.data_url)
+    return doc.xpath(sanction_list_xpath)[0].get("href")
 
 
 def crawl(context: Context) -> None:
     # First we find the link to the excel file
     excel_url = crawl_excel_url(context)
 
-    path = context.fetch_resource("source.xlsx", excel_url)
+    _, _, _, path = zyte_api.fetch_resource(context, "source.xlsx", excel_url)
     context.export_resource(path, title=context.SOURCE_TITLE)
 
     wb = load_workbook(path, read_only=True)
