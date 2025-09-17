f50995b189672a247895dff1ab223a64168dc9ab
Leon Handreke <leonh@ndreke.de>
2025-09-17 13:00:59 +0200

    [ui] Add eslint config

---
+++ ui/eslint.config.mjs
@@ -0,0 +1,48 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type'
+          ],
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              // Style imports last
+              pattern: '**/*.{css,scss,sass,less}',
+              group: 'type',
+              position: 'after'
+            }
+          ],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          },
+          warnOnUnassignedImports: true
+        }
+      ],
+    }
+  }
+]
+
+export default eslintConfig
