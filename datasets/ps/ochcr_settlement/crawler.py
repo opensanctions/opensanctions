1aff3d15e8aa60c27103fdf3942c65e0d6ea1166
nvmbrasserie <ivan@opensanctions.org>
2025-10-01 13:13:42 +0200

    minor touch

--- datasets/ps/ochcr_settlement/crawler.py
+++ datasets/ps/ochcr_settlement/crawler.py
@@ -4,13 +4,13 @@ from zavod.shed import zyte_api
 OHCHR_BHR = "OHCHR-BHR"
 
 
-def assert_dom_hash(doc):
+def assert_database_hash(doc):
     tables_div = doc.xpath(
         "//div[@data-block-plugin-id='entity_browser_block:oh_accordion_component']"
     )
     assert len(tables_div) == 1, len(tables_div)
-    # If the hash changes, update the xpath above and review the page structure
-    # for any new tables that may have been added.
+    # If the hash changes, update the 'table_xpath' below and review the page
+    # structure for any new tables that may have been added.
     h.assert_dom_hash(tables_div[0], "b3fda7012ede1df28021b765c68d71f6e46755ca")
 
 
@@ -39,17 +39,17 @@ def crawl_row(context: Context, row):
 
 
 def crawl(context: Context):
-    xpath = "//h5[contains(., 'Business enterprises involved in listed activities')]/following-sibling::div//table"
+    table_xpath = "//h5[contains(., 'Business enterprises involved in listed activities')]/following-sibling::div//table"
     doc = zyte_api.fetch_html(
         context,
         context.data_url,
-        xpath,
+        table_xpath,
         html_source="httpResponseBody",
         absolute_links=True,
         cache_days=1,
     )
-    assert_dom_hash(doc)
-    table = doc.xpath(xpath)
+    assert_database_hash(doc)
+    table = doc.xpath(table_xpath)
     assert len(table) == 1, len(table)
     for row in h.parse_html_table(table[0]):
         crawl_row(context, row)
