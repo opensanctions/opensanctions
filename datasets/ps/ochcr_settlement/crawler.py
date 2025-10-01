6770adb406ff2f5366adee9e6b85a835b8b9fe7d
nvmbrasserie <ivan@opensanctions.org>
2025-10-01 13:06:33 +0200

    [ps_ohchr_settlement] parse the webpage

--- datasets/ps/ochcr_settlement/crawler.py
+++ datasets/ps/ochcr_settlement/crawler.py
@@ -1,62 +1,55 @@
-"""
-Create list of OHCHR companies linked to illegal settlements from CSV.
-"""
+from zavod import Context, helpers as h
+from zavod.shed import zyte_api
 
-import csv
-from typing import Dict, Iterable
-
-import zavod.helpers as h
-from zavod import Context
+OHCHR_BHR = "OHCHR-BHR"
 
 
-def crawl_row(context: Context, row: Dict[str, str]):
-    """Process one row of the CSV data"""
-    row_id = row.pop("ID").strip(" \t.")
-    name = row.pop("Business enterprise").strip()
-    prev_name = row.pop("Previous name").strip()
-    notes = row.pop("Notes").strip()
-    activities = row.pop("Sub-paragraph of listed activity (2020 report)").strip()
-    country = row.pop("State concerned").strip()
-    section = row.pop("Section").strip()
-    date = row.pop("Date")
-    # FIXME: maybe that trailing space will go away?
-    if "Source URL" in row:
-        source_url = row.pop("Source URL").strip()
-    else:
-        source_url = row.pop("Source URL").strip()
-    context.log.info(f"Processing row ID {row_id}: {name}")
-    context.audit_data(row)
-    if section.startswith("A. ") or "no longer involved" in section:
-        context.log.info(
-            f"Skipping company {name} as marked as "
-            f"not involved (Section: {section})"
-        )
-        return
+def assert_dom_hash(doc):
+    tables_div = doc.xpath(
+        "//div[@data-block-plugin-id='entity_browser_block:oh_accordion_component']"
+    )
+    assert len(tables_div) == 1, len(tables_div)
+    # If the hash changes, update the xpath above and review the page structure
+    # for any new tables that may have been added.
+    h.assert_dom_hash(tables_div[0], "b3fda7012ede1df28021b765c68d71f6e46755ca")
+
+
+def crawl_row(context: Context, row):
+    response = h.links_to_dict(row.pop("response_from_business_enterprise"))
+    response_url = response.get("response")
+    str_row = h.cells_to_str(row)
+    name = str_row.pop("business_enterprise")
+    country = str_row.pop("home_state")
+    activities = str_row.pop("listed_activity_subparagraph_of_paragraph_96")
+
     entity = context.make("Company")
     entity.id = context.make_id(name, country)
-    context.log.debug(f"Unique ID {entity.id}")
-    h.apply_name(entity, name)
-    if prev_name:
-        entity.add("previousName", prev_name)
-    entity.add("program", section)
-    entity.add("notes", f"Listed activities: {activities}")
-    entity.add("notes", f"Date of last update: {date}")
+    entity.add("name", name)
+    entity.add("country", country)
+    entity.add("sourceUrl", context.data_url)
     entity.add("topics", "debarment")
-    if notes:
-        entity.add("notes", notes)
-    entity.add("sourceUrl", source_url)
+    entity.add("notes", f"Listed activities: {activities}")
+    if response:
+        entity.add("notes", f"Response from business enterprise: {response_url}")
     context.emit(entity)
+    context.audit_data(str_row, ["no"])
 
-
-def crawl_csv(context: Context, reader: Iterable[Dict[str, str]]):
-    """Process the CSV data"""
-    for row in reader:
-        crawl_row(context, row)
+    sanction = h.make_sanction(context, entity, program_key=OHCHR_BHR)
+    context.emit(sanction)
 
 
 def crawl(context: Context):
-    """Crawl the OHCHR database as converted to CSV"""
-    path = context.fetch_resource("ohchr_database.csv", context.data_url)
-    with open(path, "rt") as infh:
-        reader = csv.DictReader(infh)
-        crawl_csv(context, reader)
+    xpath = "//h5[contains(., 'Business enterprises involved in listed activities')]/following-sibling::div//table"
+    doc = zyte_api.fetch_html(
+        context,
+        context.data_url,
+        xpath,
+        html_source="httpResponseBody",
+        absolute_links=True,
+        cache_days=1,
+    )
+    assert_dom_hash(doc)
+    table = doc.xpath(xpath)
+    assert len(table) == 1, len(table)
+    for row in h.parse_html_table(table[0]):
+        crawl_row(context, row)
