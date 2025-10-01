e1010cbaabe56e62a05edf4dddc575843bd7e877
nvmbrasserie <ivan@opensanctions.org>
2025-10-01 15:41:40 +0200

    [il_mod_crypto] parse dobs

--- datasets/il/mod_crypto/crawler.py
+++ datasets/il/mod_crypto/crawler.py
@@ -2,7 +2,7 @@ import csv
 from typing import Dict
 from pathlib import Path
 
-from normality import collapse_spaces
+from normality import collapse_spaces, squash_spaces
 from rigour.text.scripts import is_latin
 
 from zavod import Context, helpers as h
@@ -100,7 +100,7 @@ def crawl_csv_row(context: Context, row: Dict[str, str]):
         person = context.make("Person")
         person.id = context.make_id(row.get("id_no") or row.get("passport_no") or name)
         h.apply_name(person, full=name, lang="eng")
-        h.apply_date(person, "birthDate", row.pop("dob"))
+        h.apply_date(person, "birthDate", squash_spaces(row.pop("dob")))
         person.add("email", row.pop("email").split(";"))
         person.add("phone", row.pop("phone"))
         for alias in row.pop("alias").split(";"):
