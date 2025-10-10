9754f3972908c246e728571229a9c70244780879
JD Bothma <jbothma@gmail.com>
2025-10-10 16:49:55 +0100

    [us_fed_enforcements] Update to new API

--- zavod/migrations/2025-09-19-reviews-columns.sql
+++ zavod/migrations/2025-09-19-reviews-columns.sql
@@ -3,3 +3,5 @@ ALTER TABLE review RENAME COLUMN orig_extraction_data TO original_extraction;
 ALTER TABLE review ADD COLUMN origin VARCHAR(65535);
 UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'us_ofac_enforcement_actions';
 UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'sg_mas_enforcement_actions';
+UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'us_fed_enforcements';
+UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'us_al_med_exclusions';
