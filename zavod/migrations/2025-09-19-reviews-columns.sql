d1fc6450e02e8bdc53d7757cd908a85a69faef17
JD Bothma <jbothma@gmail.com>
2025-10-11 11:35:04 +0100

    [us_ofac_press_releases] Update to new API

--- zavod/migrations/2025-09-19-reviews-columns.sql
+++ zavod/migrations/2025-09-19-reviews-columns.sql
@@ -5,3 +5,4 @@ UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'us_ofac_
 UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'sg_mas_enforcement_actions';
 UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'us_fed_enforcements';
 UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'us_al_med_exclusions';
+UPDATE review SET origin = 'gpt-4o' WHERE origin IS NULL AND dataset = 'us_ofac_press_releases';
