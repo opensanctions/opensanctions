95859bd63c1ac223d242f8854c9904cd286f7aab
Leon Handreke <leonh@ndreke.de>
2025-10-17 15:59:20 +0200

    [ui] Redirect / to /review for now

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/review',
+        permanent: true,
+      },
+    ];
+  },
   sassOptions: {
     quietDeps: true,
     silenceDeprecations: [
