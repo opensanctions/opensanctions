9021918ef488a383c4ce551c7c234dd4caf5d6a7
nvmbrasserie <ivan@opensanctions.org>
2025-10-27 17:50:24 +0100

    extract more details and apply dates

--- datasets/pl/sejm/crawler.py
+++ datasets/pl/sejm/crawler.py
@@ -2,7 +2,65 @@ from urllib.parse import urljoin
 
 from zavod import Context
 from zavod import helpers as h
-from zavod.stateful.positions import OccupancyStatus, categorise
+from zavod.stateful.positions import categorise
+
+
+START_DATE_XPATH = ".//ul[@class='data']/li[p[@class='left']='Wybrany dnia:' or p[@class='left']='Wybrana dnia:']/p[@class='right']/text()"
+
+
+def split_dob_pob(dob_pob: str) -> tuple[str, str]:
+    # Split on the first comma: ['12-09-1960, Bytom']
+    parts = dob_pob[0].split(",", 1)
+    dob = parts[0].strip()
+    pob = parts[1].strip() if len(parts) > 1 else ""
+    return dob, pob
+
+
+def crawl_person(context: Context, url: str) -> None:
+    pep_doc = context.fetch_html(url, cache_days=1)
+    name = pep_doc.findtext(".//div[@id='title_content']/h1")
+    if not name:
+        context.log.warning("Missing name for: {}".format(url))
+        return
+    # Extract the "Wybrany dnia:" value
+    start_date = pep_doc.xpath(START_DATE_XPATH)[0]
+    if not start_date:
+        context.log.warning("Missing start date for: {}".format(url))
+        return
+    dob_pob = pep_doc.xpath(".//p[@id='urodzony']/text()")
+    dob, pob = split_dob_pob(dob_pob)
+    entity = context.make("Person")
+    entity.id = context.make_id(name, dob_pob)
+    entity.add("name", name)
+    entity.add("citizenship", "pl")
+    entity.add("topics", "role.pep")
+    entity.add("sourceUrl", url)
+    entity.add("birthPlace", pob)
+    h.apply_date(entity, "birthDate", dob)
+
+    position = h.make_position(
+        context,
+        name="Member of the Sejm of Poland",
+        wikidata_id="Q19269361",
+        country="pl",
+        topics=["gov.legislative", "gov.national"],
+        lang="eng",
+    )
+    categorisation = categorise(context, position, is_pep=True)
+    if not categorisation.is_pep:
+        return
+
+    occupancy = h.make_occupancy(
+        context,
+        person=entity,
+        position=position,
+        start_date=start_date[0].strip(),
+        categorisation=categorisation,
+    )
+    if occupancy is not None:
+        context.emit(occupancy)
+        context.emit(position)
+        context.emit(entity)
 
 
 def crawl(context: Context) -> None:
@@ -15,39 +73,4 @@ def crawl(context: Context) -> None:
         href = link.get("href")
         assert href is not None, "Missing href"
         url = urljoin(context.data_url, href)
-        pep_doc = context.fetch_html(url, cache_days=1)
-        name = pep_doc.findtext(".//div[@id='title_content']/h1")
-        if not name:
-            context.log.warning("Missing name for: {}".format(url))
-            continue
-        entity = context.make("Person")
-        entity.id = context.make_id(name, href)
-        entity.add("citizenship", "pl")
-        entity.add("topics", "role.pep")
-        entity.add("sourceUrl", url)
-        entity.add("name", name)
-
-        position = h.make_position(
-            context,
-            name="Member of the Sejm of Poland",
-            wikidata_id="Q19269361",
-            country="pl",
-            topics=["gov.legislative", "gov.national"],
-            lang="eng",
-        )
-        categorisation = categorise(context, position, is_pep=True)
-        if not categorisation.is_pep:
-            continue
-
-        occupancy = h.make_occupancy(
-            context,
-            person=entity,
-            position=position,
-            no_end_implies_current=False,
-            categorisation=categorisation,
-            status=OccupancyStatus.UNKNOWN,
-        )
-        if occupancy is not None:
-            context.emit(occupancy)
-            context.emit(position)
-            context.emit(entity)
+        crawl_person(context, url)
