1b58d84880322dd6f4622646395b1f2e46609860
nvmbrasserie <ivan@opensanctions.org>
2025-10-29 13:05:19 +0100

    extract details and dob

--- datasets/pl/senate/crawler.py
+++ datasets/pl/senate/crawler.py
@@ -1,9 +1,28 @@
+import re
 from urllib.parse import urljoin
+from datapatch import Lookup
+from normality import squash_spaces
 
 from zavod import Context
 from zavod import helpers as h
 from zavod.shed import zyte_api
 from zavod.stateful.positions import OccupancyStatus, categorise
+
+# Born on 30 March 1973 in
+DOB_REGEX = re.compile(r"\b[Bb]orn\s+on\s+(\d{1,2}\s+[A-Z][a-z]+\s+\d{4})\s+in\b")
+
+
+def extract_dob(context, lookup: Lookup, text):
+    """Try to extract a date from text using regex, fallback to context lookup, log if missing."""
+    match = DOB_REGEX.search(text)
+    if match:
+        return match.group(1)
+    # Fallback to context lookup
+    res = lookup.match(text)
+    if res:
+        return res.value
+    context.log.warning(f"No {lookup} found for biography.", biography=text)
+    return None
 
 
 def crawl(context: Context) -> None:
@@ -18,17 +37,28 @@ def crawl(context: Context) -> None:
         link = row.find(".//a")
         if link is None:
             continue
+        assert link.text is not None, f"Missing name for: {link.get("href")}"
         name = link.text.strip()
         href = link.get("href")
         assert href is not None, "Missing href"
-        assert name, "Missing name for: {}".format(href)
+        assert name, f"Missing name for: {href}"
         url = urljoin(context.data_url, href)
+        pep_doc = zyte_api.fetch_html(
+            context,
+            url,
+            unblock_validator=".//div[@class='description']",
+            cache_days=1,
+        )
+        description_el = pep_doc.find(".//div[@class='description']")
+        description = squash_spaces(description_el.text_content().strip())
+        dob = extract_dob(context, context.get_lookup("birth_dates"), description)
+
         entity = context.make("Person")
-        entity.id = context.make_id(name, href)
-        entity.add("citizenship", "pl")
-        entity.add("topics", "role.pep")
-        entity.add("sourceUrl", url)
+        entity.id = context.make_id(name, dob)
         entity.add("name", name)
+        entity.add("sourceUrl", url)
+        entity.add("notes", description)
+        h.apply_date(entity, "birthDate", dob)
 
         position = h.make_position(
             context,
