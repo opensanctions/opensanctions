172376264e4e2637356a1bff7184b8b0c3eedd7c
Leon Handreke <leonh@ndreke.de>
2025-10-29 16:41:55 +0100

    format, some typechecker fixes

--- datasets/de/bundesrat/crawler.py
+++ datasets/de/bundesrat/crawler.py
@@ -1,20 +1,25 @@
 import re
+from typing import Optional
+
 from datapatch import Lookup
+from lxml.etree import _Element
 from lxml.html import HtmlElement
 from normality import squash_spaces
+from zavod.stateful.positions import OccupancyStatus, categorise
 
-from zavod import Context, helpers as h
-from zavod.stateful.positions import categorise, OccupancyStatus
+from zavod import Context
+from zavod import helpers as h
 
 # Geboren am DD.MM.YYYY
 # Geboren YYYY
 DOB_REGEX = re.compile(r"^Geboren(?: am\s+(\d{1,2}\.\d{2}\.\d{4})| (\d{4}))")
+# Some members have no positons/membership details listed below their name (not the same as the biography)
 NO_DETAILS_LIST = [
     "https://www.bundesrat.de/SharedDocs/personen/DE/laender/nw/feller-dorothee.html"
 ]
 
 
-def extract_dob(context, lookup: Lookup, text):
+def extract_dob(context: Context, lookup: Lookup, text: str) -> Optional[str]:
     """Try to extract a date from text using regex, fallback to context lookup, log if missing."""
     match = DOB_REGEX.search(text)
     if match:
@@ -27,16 +32,18 @@ def extract_dob(context, lookup: Lookup, text):
     return None
 
 
-def extract_position_and_memberships(context, details, url):
+def extract_position_and_memberships(
+    context: Context, details: _Element, url: str
+) -> tuple[Optional[str], list[str]]:
     position_el = details.find(".//h2")
-    memberships = details.findall(".//ul/li")
-    if position_el is None and not memberships:
+    memberships_els = details.findall(".//ul/li")
+    if position_el is None and not memberships_els:
         if url not in NO_DETAILS_LIST:
             context.log.warning(f"{url}: missing position name or memberships")
         return None, []
     else:
-        position_name = position_el.text.strip()
-        memberships = [el.text_content().strip() for el in memberships]
+        position_name = str(position_el.text).strip()
+        memberships = [str(el.text_content()).strip() for el in memberships_els]
         return position_name, memberships
 
 
@@ -45,11 +52,11 @@ def crawl_item(context: Context, item: HtmlElement) -> None:
     assert url, "No URL found for member"
     member_doc = context.fetch_html(url, cache_days=3)
     details = member_doc.find(".//div[@class='text-box']")
-    assert (
-        details is not None and details.text is not None
-    ), f"No details found for {url}"
-    if "N. N." in details.text_content().strip():
-        context.log.info("Skipping member with no name")
+    assert details is not None and details.text is not None, (
+        f"No details found for {url}"
+    )
+    if "N. N." in h.element_text(details):
+        context.log.info("Skipping member with no name", url=url)
         return
     # Three most important fields are name, party, and position.
     # If any of these are missing, we want to fail fast rather than silently skip.
@@ -101,7 +108,7 @@ def crawl_item(context: Context, item: HtmlElement) -> None:
             context.emit(person)
 
 
-def crawl(context: Context):
+def crawl(context: Context) -> None:
     doc = context.fetch_html(context.data_url, cache_days=3, absolute_links=True)
     container = doc.find(".//div[@class='row']/ul[@class='members-list']")
     for item in container.xpath(".//li[@class='even' or @class='odd']"):
