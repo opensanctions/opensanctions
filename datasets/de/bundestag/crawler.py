f59d1b3717c1773c8afa76f27a611dc211bf48a4
nvmbrasserie <ivan@opensanctions.org>
2025-10-31 15:42:22 +0100

    [de_bundestag] restructure to unpack xml

--- datasets/de/bundestag/crawler.py
+++ datasets/de/bundestag/crawler.py
@@ -1,15 +1,14 @@
+from lxml import etree
 from typing import Optional
 from urllib.parse import urljoin
+from zipfile import ZipFile
 
 from zavod import Context
 from zavod import helpers as h
+from zavod.entity import Entity
 
 
-def crawl_xml(context: Context, url: str) -> None:
-    path = context.fetch_resource("source.xml", url)
-    context.export_resource(path, "text/xml", title=context.SOURCE_TITLE)
-    doc = context.parse_resource_xml(path)
-
+def create_emit_position(context: Context) -> Entity:
     position = h.make_position(
         context,
         name="Member of the German Bundestag",
@@ -18,50 +17,52 @@ def crawl_xml(context: Context, url: str) -> None:
         wikidata_id="Q1939555",
     )
     context.emit(position)
+    return position
 
-    for mdb in doc.findall(".//MDB"):
-        person = context.make("Person")
-        person.id = context.make_slug("mdb", mdb.findtext("./ID"))
 
-        # STERBEDATUM: Date of death
-        if mdb.findtext(".//STERBEDATUM"):
-            # Skip deceased persons
-            continue
+def crawl_person(context: Context, mdb: etree._Element, position: Entity) -> None:
+    person = context.make("Person")
+    person.id = context.make_slug("mdb", mdb.findtext("./ID"))
 
-        person.add("citizenship", "de")
-        for nel in mdb.findall("./NAMEN/NAME"):
-            person.add("title", nel.findtext("./ANDREDE_TITLE"))
-            h.apply_name(
-                person,
-                first_name=nel.findtext("./VORNAME"),
-                last_name=nel.findtext("./NACHNAME"),
-            )
-        h.apply_date(person, "birthDate", mdb.findtext(".//GEBURTSDATUM"))
-        person.add("birthPlace", mdb.findtext(".//GEBURTSORT"))
-        person.add("birthCountry", mdb.findtext(".//GEBURTSLAND"))
-        person.add("gender", mdb.findtext(".//GESCHLECHT"))
-        person.add("religion", mdb.findtext(".//RELIGION"))
-        person.add("political", mdb.findtext(".//PARTEI_KURZ"))
-        person.add("education", mdb.findtext(".//BERUF"))
+    # STERBEDATUM: Date of death
+    if mdb.findtext(".//STERBEDATUM"):
+        # Skip deceased persons
+        return
 
-        has_occupancy = False
-        for period in mdb.findall(".//WAHLPERIODE"):
-            occupancy = h.make_occupancy(
-                context,
-                person,
-                position,
-                # MDBWP: Mitglied des Bundestags (MoP) Wahlperiode (term):
-                start_date=period.findtext("./MDBWP_VON"),
-                end_date=period.findtext("./MDBWP_BIS"),
-                birth_date=mdb.findtext(".//GEBURTSDATUM"),
-                propagate_country=True,
-            )
-            if occupancy is not None:
-                has_occupancy = True
-                context.emit(occupancy)
+    person.add("citizenship", "de")
+    for nel in mdb.findall("./NAMEN/NAME"):
+        person.add("title", nel.findtext("./ANDREDE_TITLE"))
+        h.apply_name(
+            person,
+            first_name=nel.findtext("./VORNAME"),
+            last_name=nel.findtext("./NACHNAME"),
+        )
+    h.apply_date(person, "birthDate", mdb.findtext(".//GEBURTSDATUM"))
+    person.add("birthPlace", mdb.findtext(".//GEBURTSORT"))
+    person.add("birthCountry", mdb.findtext(".//GEBURTSLAND"))
+    person.add("gender", mdb.findtext(".//GESCHLECHT"))
+    person.add("religion", mdb.findtext(".//RELIGION"))
+    person.add("political", mdb.findtext(".//PARTEI_KURZ"))
+    person.add("education", mdb.findtext(".//BERUF"))
 
-        if has_occupancy:
-            context.emit(person)
+    has_occupancy = False
+    for period in mdb.findall(".//WAHLPERIODE"):
+        occupancy = h.make_occupancy(
+            context,
+            person,
+            position,
+            # MDBWP: Mitglied des Bundestags (MoP) Wahlperiode (term):
+            start_date=period.findtext("./MDBWP_VON"),
+            end_date=period.findtext("./MDBWP_BIS"),
+            birth_date=mdb.findtext(".//GEBURTSDATUM"),
+            propagate_country=True,
+        )
+        if occupancy is not None:
+            has_occupancy = True
+            context.emit(occupancy)
+
+    if has_occupancy:
+        context.emit(person)
 
 
 def crawl(context: Context) -> None:
@@ -70,9 +71,19 @@ def crawl(context: Context) -> None:
     xml_url: Optional[str] = None
     for link in html.findall(".//ul[@class='bt-linkliste']//a"):
         url = urljoin(context.data_url, link.get("href"))
-        if not url.endswith("Stammdaten.xml"):
+        if not url.endswith("Stammdaten.zip"):
             continue
         xml_url = url
 
     assert xml_url, "Could not find XML data URL"
-    crawl_xml(context, xml_url)
+
+    path = context.fetch_resource("source.zip", url)
+    context.export_resource(path, "application/zip", title=context.SOURCE_TITLE)
+    with ZipFile(path, "r") as zip:
+        for name in zip.namelist():
+            if name.lower().endswith(".xml"):
+                with zip.open(name) as fh:
+                    doc = etree.parse(fh)
+                    position = create_emit_position(context)
+                    for mdb in doc.findall(".//MDB"):
+                        crawl_person(context, mdb, position)
