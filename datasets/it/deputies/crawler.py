e2123122ffa1dfce8f0839c2d2939ebcab6e308b
nvmbrasserie <ivan@opensanctions.org>
2025-11-05 11:50:32 +0100

    crawl 2 latest legislatures

--- datasets/it/deputies/crawler.py
+++ datasets/it/deputies/crawler.py
@@ -1,106 +1,118 @@
-from zavod import Context, helpers as h
-from zavod.stateful.positions import categorise, OccupancyStatus
+from zavod.stateful.positions import categorise
 
-# serving members of parliament: surname, first name, biographical information,
-# constituency, political group, committee membership
+from zavod import Context
+from zavod import helpers as h
+
+# The query retrieves a list of members of
+# the Italian Chamber of Deputies (Camera dei Deputati).
+#
+# Query builder: https://dati.camera.it/sparql
+
 QUERY = """
-#### deputati in carica: cognome, nome, info biografiche, collegio di elezione, gruppo di appartenenza, commissione di afferenza
+SELECT DISTINCT
+  ?persona ?cognome ?nome ?info
+  ?dataNascita ?nato ?luogoNascita ?genere
+  ?inizioMandato ?fineMandato
+  ?nomeGruppo ?sigla ?aggiornamento
+WHERE {{
+  # Person entity (a member of parliament)
+  ?persona ocd:rif_mandatoCamera ?mandato;
+           a foaf:Person.
 
-SELECT DISTINCT ?persona ?cognome ?nome 
-?dataNascita  ?nato ?luogoNascita ?genere 
-?collegio ?nomeGruppo ?sigla ?commissione ?aggiornamento  
-WHERE {
-?persona ocd:rif_mandatoCamera ?mandato; a foaf:Person.
+  # Deputy membership, group affiliation, and legislature
+  ?d a ocd:deputato;
+     ocd:aderisce ?aderisce;
+     ocd:rif_leg <http://dati.camera.it/ocd/legislatura.rdf/repubblica_{legislatura}>;
+     ocd:rif_mandatoCamera ?mandato.
+  OPTIONAL {{ ?d dc:description ?info }}
 
-## deputato
-?d a ocd:deputato; ocd:aderisce ?aderisce;
-ocd:rif_leg <http://dati.camera.it/ocd/legislatura.rdf/repubblica_19>;
-ocd:rif_mandatoCamera ?mandato.
+  # Personal information (name, gender, birth details)
+  ?d foaf:surname ?cognome;
+     foaf:gender ?genere;
+     foaf:firstName ?nome.
 
-##anagrafica
-?d foaf:surname ?cognome; foaf:gender ?genere;foaf:firstName ?nome.
-OPTIONAL{
-?persona <http://purl.org/vocab/bio/0.1/Birth> ?nascita.
-?nascita <http://purl.org/vocab/bio/0.1/date> ?dataNascita; 
-rdfs:label ?nato; ocd:rif_luogo ?luogoNascitaUri. 
-?luogoNascitaUri dc:title ?luogoNascita. 
-}
+  OPTIONAL {{
+    ?persona <http://purl.org/vocab/bio/0.1/Birth> ?nascita.
+    ?nascita <http://purl.org/vocab/bio/0.1/date> ?dataNascita;
+              rdfs:label ?nato;
+              ocd:rif_luogo ?luogoNascitaUri.
+    ?luogoNascitaUri dc:title ?luogoNascita.
+  }}
 
-##aggiornamento del sistema
-OPTIONAL{?d <http://lod.xdams.org/ontologies/ods/modified> ?aggiornamento.}
+  # System update timestamp (last modified)
+  OPTIONAL {{ ?d <http://lod.xdams.org/ontologies/ods/modified> ?aggiornamento. }}
 
-## mandato
-?mandato ocd:rif_elezione ?elezione.  
-MINUS{?mandato ocd:endDate ?fineMandato.}
+  # Mandate details (start and end dates)
+  ?mandato ocd:rif_elezione ?elezione.
+  OPTIONAL {{ ?mandato ocd:endDate ?fineMandato. }}
+  OPTIONAL {{ ?mandato ocd:startDate ?inizioMandato. }}
 
-## elezione
-?elezione dc:coverage ?collegio.
+  # Political group affiliation (name and abbreviation)
+  OPTIONAL {{
+    ?aderisce ocd:rif_gruppoParlamentare ?gruppo.
+    ?gruppo <http://purl.org/dc/terms/alternative> ?sigla.
+    ?gruppo dc:title ?nomeGruppo.
+  }}
 
-## adesione a gruppo
-OPTIONAL{
-  ?aderisce ocd:rif_gruppoParlamentare ?gruppo.
-  ?gruppo <http://purl.org/dc/terms/alternative> ?sigla.
-  ?gruppo dc:title ?nomeGruppo.
-}
-
-MINUS{?aderisce ocd:endDate ?fineAdesione}
-
-## organo
-OPTIONAL{
-?d ocd:membro ?membro.?membro ocd:rif_organo ?organo. 
-?organo dc:title ?commissione .
-}
-
-MINUS{?membro ocd:endDate ?fineMembership}
-}
+}}
 """
 
 
-def crawl(context: Context):
-    data = context.fetch_json(
-        context.data_url, params={"query": QUERY, "format": "json"}
+def crawl_item(context: Context, item):
+    first_name = item.pop("nome").get("value")
+    last_name = item.pop("cognome").get("value")
+    dob = item.pop("dataNascita").get("value")
+    start_date = item.pop("inizioMandato").get("value")
+    end_date = item.pop("fineMandato", {}).get("value")
+
+    entity = context.make("Person")
+    entity.id = context.make_id(first_name, last_name, dob)
+    h.apply_name(entity, first_name=first_name, last_name=last_name)
+    entity.add("sourceUrl", item.pop("persona").get("value"))
+    entity.add("birthPlace", item.pop("luogoNascita").get("value"))
+    entity.add("notes", item.pop("nato").get("value"))
+    entity.add("notes", item.pop("info", {}).get("value"))
+    entity.add("political", item.pop("nomeGruppo").get("value"))
+    entity.add("political", item.pop("sigla").get("value"))
+    entity.add("gender", item.pop("genere").get("value"))
+    entity.add("citizenship", "it")
+    h.apply_date(entity, "birthDate", dob)
+    h.apply_date(entity, "modifiedAt", item.pop("aggiornamento").get("value"))
+
+    position = h.make_position(
+        context,
+        name="Member of the Chamber of Deputies",
+        wikidata_id="Q18558478",
+        country="it",
+        topics=["gov.legislative", "gov.national"],
+        lang="eng",
     )
-    bindings = data["results"]["bindings"]
-    for item in bindings:
-        first_name = item.pop("nome").get("value")
-        last_name = item.pop("cognome").get("value")
-        dob = item.pop("dataNascita").get("value")
+    categorisation = categorise(context, position, is_pep=True)
+    if not categorisation.is_pep:
+        return
 
-        entity = context.make("Person")
-        entity.id = context.make_id(first_name, last_name, dob)
-        h.apply_name(entity, first_name=first_name, last_name=last_name)
-        entity.add("sourceUrl", item.pop("persona").get("value"))
-        entity.add("birthPlace", item.pop("luogoNascita").get("value"))
-        entity.add("notes", item.pop("nato").get("value"))
-        entity.add("political", item.pop("nomeGruppo").get("value"))
-        entity.add("political", item.pop("sigla").get("value"))
-        entity.add("gender", item.pop("genere").get("value"))
-        entity.add("citizenship", "it")
-        h.apply_date(entity, "birthDate", dob)
+    occupancy = h.make_occupancy(
+        context,
+        person=entity,
+        position=position,
+        categorisation=categorisation,
+        start_date=start_date,
+        end_date=end_date,
+    )
+    if occupancy is not None:
+        context.emit(occupancy)
+        context.emit(position)
+        context.emit(entity)
 
-        position = h.make_position(
-            context,
-            name="Member of the Chamber of Deputies",
-            wikidata_id="Q18558478",
-            country="it",
-            topics=["gov.legislative", "gov.national"],
-            lang="eng",
+    context.audit_data(item)
+
+
+def crawl(context: Context):
+    for leg in [18, 19]:
+        query = QUERY.format(legislatura=leg)
+        data = context.fetch_json(
+            context.data_url, params={"query": query, "format": "json"}
         )
-        categorisation = categorise(context, position, is_pep=True)
-        if not categorisation.is_pep:
-            return
-
-        occupancy = h.make_occupancy(
-            context,
-            person=entity,
-            position=position,
-            categorisation=categorisation,
-            # Data source indicates they hold the position now
-            status=OccupancyStatus.CURRENT,
-        )
-        if occupancy is not None:
-            context.emit(occupancy)
-            context.emit(position)
-            context.emit(entity)
-
-        context.audit_data(item, ["collegio", "commissione", "aggiornamento"])
+        bindings = data["results"]["bindings"]
+        for item in bindings:
+            crawl_item(context, item)
