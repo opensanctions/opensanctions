86cd1d14228a594828fdd1d1ef7a818bcdb7c3c5
JD Bothma <jd@opensanctions.org>
2026-01-08 17:30:04 +0000

    One last indonesia run

--- datasets/id/dttot/crawler.py
+++ datasets/id/dttot/crawler.py
@@ -1,54 +1,17 @@
-import re
 from typing import Any, Dict
 import xlrd  # type: ignore
 from lxml import etree
-from datetime import datetime
 
 from zavod import Context, Entity
 from zavod import helpers as h
+from zavod.shed.internal_data import fetch_internal_data
 
 
 DATETIME_FORMAT = "%Y%m%d%H%M%S"
-addr_delim = re.compile(r"[\W][a-zA-Z]\)|;")
-
-
-def is_sanctions_xlsx(a: etree._Element) -> bool:
-    if (
-        a.text is not None
-        and a.text.strip().lower().find("dttot") > -1
-        and (parse_link(a).endswith(".xls") or parse_link(a).endswith(".xlsx"))
-    ):
-        return True
-    return False
 
 
 def parse_link(a: etree._Element) -> str:
     return a.get("href", "").split("/")[-1]
-
-
-def find_last_link(context: Context, html: etree._Element) -> str:
-    last_time = datetime.strptime("19700101000000", DATETIME_FORMAT)
-    last_link = None
-    for link in html.findall(".//a"):
-        if not is_sanctions_xlsx(link):
-            continue
-        try:
-            ts_ = parse_link(link).split(".")[0]
-            ts = datetime.strptime(ts_, DATETIME_FORMAT)
-        except Exception:
-            context.log.error(f"Could not parse timestamp from {link.get('href')}")
-            continue
-        if ts > last_time:
-            last_time = ts
-            last_link = link
-    if last_link is None:
-        raise ValueError("No link found")
-    last_href = last_link.get("href")
-    if last_href is None:
-        raise ValueError("No href found")
-    # Fix backend URL pasted in instead of public URL or something
-    last_href = last_href.replace("https://be.ppatk.go.id/", "https://www.ppatk.go.id/")
-    return last_href
 
 
 def row_to_dict(row, headers: Dict[str, str]) -> dict:
@@ -82,10 +45,9 @@ def apply_date(entity: Entity, prop: str, value: str) -> None:
 
 
 def crawl(context: Context):
-    html = context.fetch_html(context.data_url)
-    last_link = find_last_link(context, html)
-    xls = context.fetch_resource("source.xls", last_link)
-    wb = xlrd.open_workbook(xls)  # This will break loudly once they switch to XLSX
+    path = context.get_resource_path("source.xls")
+    fetch_internal_data("id_dttot/20251208091237.xls", path)
+    wb = xlrd.open_workbook(path)  # This will break loudly once they switch to XLSX
     sh = wb.sheet_by_index(0)
     in_header = sh.row(0)
     headers = {}
@@ -106,8 +68,10 @@ def crawl(context: Context):
         entity.add("name", names[0])
         entity.add("alias", names[1:])
         if addr := drow.pop("address", None):
-            addr = str(addr).strip("-").strip()
-            for addr in addr_delim.split(addr):
+            for addr in str(addr).split("\n"):
+                addr = addr.strip().strip("-").strip()
+                if addr == "N/A":
+                    continue
                 entity.add("address", addr, lang="ind")
         entity.add("country", drow.pop("country", None))
         entity.add("notes", drow.pop("description", None), lang="ind")
