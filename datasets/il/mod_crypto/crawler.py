f3390c4921568a1f76e2d06788f4d3fdd215d973
nvmbrasserie <ivan@opensanctions.org>
2026-02-05 17:17:31 +0100

    [il_mod_crypto] split 'accountId' and 'publicKey' of the crypto wallet

--- datasets/il/mod_crypto/crawler.py
+++ datasets/il/mod_crypto/crawler.py
@@ -144,22 +144,22 @@ def crawl_csv_row(context: Context, row: Dict[str, str]):
         context.emit(entity)
 
     # --- Wallets --- are always created if wallet data is present
-    account_id = row.pop("account/wallet_id")
-    if account_id:
-        account_id = normalize_address(account_id)
-        if not is_latin(account_id):
-            context.log.warning(f"Non-latin account ID: {account_id}")
+    # account_id = row.pop("account/wallet_id")
+    wallet_address = row.pop("wallet_address")
+    account_id = row.pop("account_id")
+    # Use wallet_id if present, otherwise fall back to account_id
+    identifier = wallet_address or account_id
+    if identifier:
+        identifier = normalize_address(identifier)
+        if not is_latin(identifier):
+            context.log.warning(f"Non-latin identifier: {identifier}")
         wallet = context.make("CryptoWallet")
-        wallet.id = context.make_id(account_id)
-        wallet.set("publicKey", account_id)
-        platform = row.pop("platform")
-        if platform:
-            wallet.set("managingExchange", platform)
-        currency = row.pop("currency")
-        if currency:
-            wallet.set("currency", currency)
-        if person or entity:
-            wallet.set("holder", person or entity)
+        wallet.id = context.make_id(identifier)
+        wallet.set("publicKey", wallet_address)
+        wallet.set("accountId", account_id)
+        wallet.set("managingExchange", row.pop("platform"))
+        wallet.set("currency", row.pop("currency"))
+        wallet.set("holder", person or entity)
         wallets.append(wallet)
 
     # --- Sanction & Linking ---
