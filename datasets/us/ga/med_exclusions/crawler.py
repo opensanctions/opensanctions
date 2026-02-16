b0b1640858904b18967f8732aa903d429379d072
nvmbrasserie <ivan@opensanctions.org>
2026-02-16 21:57:31 +0100

    [us_ga_med_exclusions] apply latest helpers and update the logic for people with business names

--- datasets/us/ga/med_exclusions/crawler.py
+++ datasets/us/ga/med_exclusions/crawler.py
@@ -6,31 +6,34 @@ from rigour.mime.types import XLSX
 from zavod import Context, helpers as h
 
 
-def crawl_item(row: Dict[str, str], context: Context):
+def crawl_item(row: Dict[str, str | None], context: Context):
     first_name = row.pop("first_name")
     last_name = row.pop("last_name")
+    # TODO: clean up AKAs in middle_name
+    middle_name = row.pop("middle_name")
     business_name = row.pop("business_name")
+    npi = row.pop("npi")
 
     if not first_name and not last_name and not business_name:
         return
 
-    if business_name:
+    has_person_name = first_name or last_name
+    # Only business name -> Company
+    if business_name and not has_person_name:
         entity = context.make("Company")
-        entity.id = context.make_id(business_name, row.get("npi"))
+        entity.id = context.make_id(business_name, npi)
         entity.add("name", business_name)
+    # Person name (with optional business name as d.b.a. alias)
     else:
         entity = context.make("Person")
-        entity.id = context.make_id(first_name, last_name, row.get("npi"))
+        entity.id = context.make_id(first_name, last_name, npi)
         h.apply_name(
-            entity,
-            first_name=first_name,
-            middle_name=row.pop("middle_name", None),
-            last_name=last_name,
+            entity, first_name=first_name, middle_name=middle_name, last_name=last_name
         )
+        if business_name:
+            entity.add("alias", business_name)
 
-    if row.get("npi"):
-        entity.add("npiCode", row.pop("npi"))
-
+    entity.add("npiCode", npi)
     entity.add("topics", "debarment")
     entity.add("country", "us")
     entity.add("sector", row.pop("general"))
@@ -44,20 +47,14 @@ def crawl_item(row: Dict[str, str], context: Context):
     context.audit_data(row, ignore=["state"])
 
 
-def crawl_excel_url(context: Context):
-    doc = context.fetch_html(context.data_url, absolute_links=True)
-    return doc.xpath(".//a[contains(@data-text, 'List of Excluded Individuals')]")[
-        0
-    ].get("href")
-
-
 def crawl(context: Context) -> None:
-    # First we find the link to the excel file
-    excel_url = crawl_excel_url(context)
+    doc = context.fetch_html(context.data_url, absolute_links=True)
+    excel_url = h.xpath_string(
+        doc, ".//a[contains(@data-text, 'List of Excluded Individuals')]/@href"
+    )
     path = context.fetch_resource("list.xlsx", excel_url)
     context.export_resource(path, XLSX, title=context.SOURCE_TITLE)
 
     wb = load_workbook(path, read_only=True)
-
     for item in h.parse_xlsx_sheet(context, wb["Sheet1"], skiprows=2):
         crawl_item(item, context)
