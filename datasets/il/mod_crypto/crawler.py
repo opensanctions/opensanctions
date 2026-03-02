8e7826308b2eeb41a746e730896155982dedf024
Ivan Stavropoltsev <ivan@opensanctions.org>
2026-03-02 15:32:51 +0100

    Fixes-02-03 (#3683)
    * [cu_inventario_legislatura] add assertions
    
    * [fr_tresor_gels_avoir] datapatch
    
    * [il_mod_crypto] add new designations
    
    * [my_moha_sanctions] switch to the new file
    
    * [thesentry_atlas] add assertions
    
    * [us_ia_med_exclusions] fix xpath

--- datasets/il/mod_crypto/crawler.py
+++ datasets/il/mod_crypto/crawler.py
@@ -212,7 +212,7 @@ def crawl(context: Context):
 
     write_csv_for_manual_diff(tables[0], LOCAL_PATH / "releases.csv")
     write_csv_for_manual_diff(tables[1], LOCAL_PATH / "wallets.csv")
-    h.assert_dom_hash(container, "36f0215ed4ac0e1e8b76df5bf565f72aaac5eaba")
+    h.assert_dom_hash(container, "267b571bc14dbf4e2120121366b6239b03c0eae6")
 
     # At the time of writing, the table on the web page is missing some public keys,
     # so we maintain the data manually in a google sheet, but dump the table to csv
