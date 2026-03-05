c542316c03add5a3523b8a8ffc412dff5f483d3d
asya <asya@opensanctions.org>
2026-03-05 12:06:19 +0100

    skipping anonymous records & adding endDate based on past status & tidy

--- datasets/ae/dfsa_debarred/crawler.py
+++ datasets/ae/dfsa_debarred/crawler.py
@@ -13,30 +13,44 @@ def crawl_person(context: Context, row: _Element) -> None:
     url = row.get("href")  # link to case's PDF
     # not all rows have three values to unpack, this is a workaround to fetch text
     date, name, status = [el.tail for el in h.xpath_elements(row, ".//p/span")]
+    assert name is not None and status is not None
+
+    # skip anonymized entries
+    if name.lower() == "an individual":
+        return
 
     person = context.make("Person")
     person.id = context.make_id(name, url)
     person.add("name", name)
-    person.add("topics", "debarment")
 
     sanction = h.make_sanction(context, person)
     h.apply_date(sanction, "listingDate", date)
     sanction.add("sourceUrl", url)
-    sanction.add("status", status)
     sanction.add("country", "ae")
+    sanction.add("status", status)
+
+    # add endDate if the status is expired & don't add debarment topic
+    if "past" in status.lower() and "expired" in status.lower():
+        date_end = status.split("expired ")[1].strip(")")
+        h.apply_date(sanction, "endDate", date_end)
+    else:
+        person.add("topics", "debarment")
 
     context.emit(person)
     context.emit(sanction)
 
 
 def crawl(context: Context) -> None:
+    xpath_table = ".//div[@class='table-content prohibited--grid']"
+
+    # using Zyte API to bypass the 403 error
     doc = fetch_html(
         context,
         context.data_url,
-        ".//div[@class='table-content prohibited--grid']",
+        xpath_table,
         actions=ACTIONS,
     )
-    table = h.xpath_element(doc, ".//div[@class='table-content prohibited--grid']")
+    table = h.xpath_element(doc, xpath_table)
 
     for row in h.xpath_elements(table, ".//a"):
         crawl_person(context, row)
