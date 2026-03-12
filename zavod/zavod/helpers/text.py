aa3ebcff727d502513af1de300e8ed50da5c9f11
Friedrich Lindenberg <friedrich@opensanctions.org>
2026-03-12 12:54:48 +0100

    Warn about invalid multi_split sequences

--- zavod/zavod/helpers/text.py
+++ zavod/zavod/helpers/text.py
@@ -1,7 +1,12 @@
+from functools import cache
 import re
 from banal import is_listish, ensure_list
-from typing import Optional, List, Sequence, Union, Iterable
+from typing import Optional, List, Sequence, Tuple, Union, Iterable
 from normality import squash_spaces
+
+from zavod.logs import get_logger
+
+log = get_logger(__name__)
 
 PREFIX_ = r"INTERPOL-UN\s*Security\s*Council\s*Special\s*Notice\s*web\s*link:?"
 PREFIX = re.compile(PREFIX_, re.IGNORECASE)
@@ -39,6 +44,33 @@ def clean_note(text: Union[Optional[str], Sequence[Optional[str]]]) -> List[str]
     return out
 
 
+@cache
+def _validate_splitters(splitters: Tuple[str, ...]) -> None:
+    """Check that the splitters supplied to the multi_split function are sequenced such that
+    later splitters are not substrings of earlier splitters.
+    """
+    # The risk here is something like splitting on `i)` first, and then on `ii)` later, which
+    # would cause the `ii)` splitter to never be applied, and create a dangling `i` in the output.
+
+    previous: List[str] = []
+    for splitter in splitters:
+        if not isinstance(splitter, str):
+            log.warning("multi_split: not a string: %r", splitter)
+            continue
+        splitter = splitter.strip()
+        if len(splitter) == 0:
+            log.warning("multi_split: empty: %r", splitter)
+            continue
+        for prev in previous:
+            if splitter in prev:
+                log.warning(
+                    "multi_split: %r is a substring of preceding %r",
+                    splitter,
+                    prev,
+                )
+        previous.append(splitter)
+
+
 def multi_split(
     text: Optional[Union[str, Iterable[Optional[str]]]], splitters: Iterable[str]
 ) -> List[str]:
@@ -57,7 +89,11 @@ def multi_split(
     if text is None:
         return []
     fragments = ensure_list(text)
-    for splitter in splitters:
+    lsplitters = tuple(splitters)
+    # FIXME: this is meant to help us find things that are broken right now. Once we've
+    # remediated that, we should remove the check and sort splitters instead.
+    _validate_splitters(lsplitters)
+    for splitter in lsplitters:
         out: List[Optional[str]] = []
         for fragment in fragments:
             if fragment is None:
