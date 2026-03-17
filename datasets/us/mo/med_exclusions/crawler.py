e0164496f67e7d9ce4f5eee0d7bcffabe6d5a4c4
Leon Handreke <leon@opensanctions.org>
2026-03-17 15:26:21 +0100

    [us_mo_med_exclusions] beauty in types and variable names

--- datasets/us/mo/med_exclusions/crawler.py
+++ datasets/us/mo/med_exclusions/crawler.py
@@ -3,11 +3,11 @@ from rigour.mime.types import XLSX
 from openpyxl import load_workbook
 
 from zavod import Context
-from zavod.extract.zyte_api import fetch_html, fetch_resource
+from zavod.extract import zyte_api
 from zavod import helpers as h
 
 
-def crawl_item(row: Dict[str, str], context: Context):
+def crawl_item(row: Dict[str, str | None], context: Context) -> None:
     entity = context.make("LegalEntity")
     entity.id = context.make_id(row.get("provider_name"), row.get("npi"))
     entity.add("name", row.pop("provider_name"))
@@ -18,7 +18,7 @@ def crawl_item(row: Dict[str, str], context: Context):
     entity.add("topics", "debarment")
     entity.add("sector", row.pop("prov_type_specialty"))
     if row.get("license") and row.get("license") != "N/A":
-        entity.add("description", "License number: " + row.pop("license"))
+        entity.add("description", f"License number: {row.pop('license')}")
     else:
         row.pop("license")
     entity.add("country", "us")
@@ -34,14 +34,14 @@ def crawl_item(row: Dict[str, str], context: Context):
     context.audit_data(row)
 
 
-def crawl_excel_url(context: Context, sp_url: str):
+def crawl_excel_url(context: Context, sanctions_list_url: str) -> str:
     excel_xpath = (
         ".//a[contains(@href, 'Sanction-List') and contains(@href, '.xlsx')]/@href"
     )
-    exclusions_page = fetch_html(
+    exclusions_page = zyte_api.fetch_html(
         context,
-        sp_url,
-        excel_xpath,
+        sanctions_list_url,
+        unblock_validator=excel_xpath,
     )
     excel_url = h.xpath_string(exclusions_page, excel_xpath)
     return excel_url
@@ -49,17 +49,17 @@ def crawl_excel_url(context: Context, sp_url: str):
 
 def crawl(context: Context) -> None:
     # Locate the Sanctions page
-    sp_xpath = ".//a[contains(@href, 'sanction-list')]/@href"
-    landing_page = fetch_html(
+    sanctions_list_url_xpath = ".//a[contains(@href, 'sanction-list')]/@href"
+    landing_page = zyte_api.fetch_html(
         context,
         context.data_url,
-        sp_xpath,
+        unblock_validator=sanctions_list_url_xpath,
     )
-    sp_url = h.xpath_string(landing_page, sp_xpath)
+    sp_url = h.xpath_string(landing_page, sanctions_list_url_xpath)
 
     # Find the link to the excel file on the Sanctions page
     excel_url = crawl_excel_url(context, sp_url)
-    _, _, _, path = fetch_resource(
+    _, _, _, path = zyte_api.fetch_resource(
         context, "source.xlsx", excel_url, expected_media_type=XLSX, geolocation="US"
     )
     context.export_resource(path, XLSX, title=context.SOURCE_TITLE)
@@ -68,5 +68,6 @@ def crawl(context: Context) -> None:
     if len(wb.sheetnames) > 1:
         context.log.warning("Additional sheet found")
 
+    assert wb.active is not None, "No active sheet found in workbook"
     for item in h.parse_xlsx_sheet(context, wb.active, skiprows=1):
         crawl_item(item, context)
