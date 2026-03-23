957331f1cbc41d99d22b169b4e846077e3a94c64
asya <asya@opensanctions.org>
2026-03-23 18:13:15 +0100

    adding a draft name reviews

--- datasets/us/fincen_enforcement/crawler.py
+++ datasets/us/fincen_enforcement/crawler.py
@@ -1,13 +1,35 @@
 from lxml.etree import _Element
 from datetime import date
+from typing import List
+from pydantic import BaseModel
 
 from zavod import Context, helpers as h
 from rigour.names.split_phrases import contains_split_phrase
 from zavod.shed import enforcements
+from zavod.extract.llm import run_typed_text_prompt, DEFAULT_MODEL
+from zavod.stateful.review import review_extraction, HtmlSourceValue
 
 start = date.today()
 end = date(start.year + 10, start.month, start.day)
 MAX_AGE_DAYS = (end - start).days
+
+
+class Respondent(BaseModel):
+    name: str
+    aliases: List[str] = []
+
+
+class Respondents(BaseModel):
+    respondents: List[Respondent]
+
+
+PROMPT = """
+Extract the entity name(s) from the string and its aliases if any, following the schema provided.
+ONLY include names mentioned in the input string.
+
+- name: The name of the entity precisely as expressed in the text.
+- aliases: ONLY extract aliases that follow an explicit indication of an _alternative_ name, such as "also known as", "alias", "formerly", "aka", "fka". Otherwise the aliases field should just be an empty array.
+"""
 
 
 def crawl_row(context: Context, row: dict[str, _Element]) -> None:
@@ -34,8 +56,32 @@ def crawl_row(context: Context, row: dict[str, _Element]) -> None:
 
     # send to cleaning if a string contrains split entities and aliases
     if contains_split_phrase(case_name) or " and " in case_name:
-        original = h.Names(name=case_name)
-        h.review_names(context, entity, original=original, llm_cleaning=True)
+        source_value = HtmlSourceValue(
+            key_parts=url,
+            label="FinCEN Enforcement Action",
+            element=url_el,
+            url=url[0],
+        )
+        prompt_result = run_typed_text_prompt(
+            context,
+            prompt=PROMPT,
+            string=case_name,
+            response_type=Respondents,
+        )
+        review = review_extraction(
+            context,
+            source_value=source_value,
+            original_extraction=prompt_result,
+            origin=DEFAULT_MODEL,
+        )
+        if not review.accepted:
+            return
+
+        for item in review.extracted_data.respondents:
+            entity.add("name", item.name, origin=review.origin)
+            entity.add("alias", item.aliases, origin=review.origin)
+        # original = h.Names(name=case_name)
+        # h.review_names(context, entity, original=original, llm_cleaning=True)
     else:
         entity.add("name", case_name)
 
@@ -58,3 +104,5 @@ def crawl(context: Context) -> None:
 
     for row in h.parse_html_table(table):
         crawl_row(context, row)
+
+    # assert_all_accepted(context, raise_on_unaccepted=False)
