38c5aef593113b889a4866e326890aaa6af57f8e
JD Bothma <jd@opensanctions.org>
2026-03-25 13:26:25 +0000

    Small fixes
    - integer IDs appear to be internal to this system and not a standard reference
    - add unended political affiliation rather than the one starting most recently
    - add position wikidata id
    - context.lookup_value returns str|None
    - warn on unexpected period types/statuses
    - add categorisations to make_occupancy
    - don't assume all leadership positions are PEPs - new ones might slip in
    - fix warning in position helper

--- datasets/pt/crawler.py
+++ datasets/pt/crawler.py
@@ -19,20 +19,20 @@ def crawl_parliament(context: Context, url: str) -> None:
         person = context.make("Person")
         person.id = context.make_id(name, id)
         person.add("name", name)
-        # unique MP identifier
-        person.add("idNumber", id)
+
         # shorter parliamentary name
         person.add("alias", member.pop("DepNomeParlamentar"))
         person.add("citizenship", "pt")
 
         constituency_name = member.pop("DepCPDes")
-        constituency_id = member.pop("DepCPId")
 
         party_history = member.pop("DepGP") or []
         if party_history:
             # record only the most recent party affiliation
-            latest_party = max(party_history, key=lambda party: party["gpDtInicio"])
-            person.add("political", latest_party["gpSigla"])
+            party_affiliations = filter(
+                lambda party: party["gpDtFim"] is None, party_history
+            )
+            person.add("political", [aff["gpSigla"] for aff in party_affiliations])
         ### each member also has:
         # gpId -- party group ID
         # gpDtFim and gpDtInicio -- dates of party affiliation
@@ -42,6 +42,7 @@ def crawl_parliament(context: Context, url: str) -> None:
             name="Member of the Assembly of the Portuguese Republic",
             topics=["gov.national", "gov.legislative"],
             country=["pt"],
+            wikidata_id="Q19953703",
         )
         categorisation = categorise(context, position, is_pep=True)
 
@@ -50,10 +51,20 @@ def crawl_parliament(context: Context, url: str) -> None:
 
         # --- fetch non-leadership seat occupancy in the plenary ---
         for seat_occupancy in member.pop("DepSituacao") or []:
-            # sioDes describes the status change type; only proceed for active periods
-            # sioDtInicio/sioDtFim are dates of the status change, not the MP seat itself
-            seatstatus = seat_occupancy.pop("sioDes")
-            is_active = context.lookup_value("seat_status", seatstatus)
+            # sioDes describes the status change type; only proceed for active periods.
+            # sioDtInicio/sioDtFim are dates of the status change, not the MP seat itself.
+            # There are periods in the data for durations when the member has suspended/had
+            # resigned early. We don't want occupancies for those.
+            period_status = seat_occupancy.pop("sioDes")
+            is_active = (
+                context.lookup_value(
+                    "seat_status",
+                    period_status,
+                    warn_unmatched=True,
+                )
+                == "true"
+            )
+
             if not is_active:
                 continue
 
@@ -63,11 +74,12 @@ def crawl_parliament(context: Context, url: str) -> None:
                 position,
                 start_date=seat_occupancy.pop("sioDtInicio"),
                 end_date=seat_occupancy.pop("sioDtFim"),
+                categorisation=categorisation,
             )
 
             if occupancy is not None:
                 occupancy.add("constituency", constituency_name)
-                occupancy.add("constituency", constituency_id)
+                occupancy.add("summary", period_status)
                 context.emit(person)
                 context.emit(occupancy)
                 context.emit(position)
@@ -80,6 +92,11 @@ def crawl_parliament(context: Context, url: str) -> None:
                 topics=["gov.national", "gov.legislative"],
                 country=["pt"],
             )
+            categorisation_leadership = categorise(
+                context, position_leadership, is_pep=None
+            )
+            if not categorisation_leadership.is_pep:
+                continue
 
             occupancy_leadership = h.make_occupancy(
                 context,
@@ -99,23 +116,22 @@ def crawl_parliament(context: Context, url: str) -> None:
 def crawl(context: Context) -> None:
     # locate the link to the list of Portugese parliaments
     doc_landing = context.fetch_html(context.data_url, absolute_links=True)
-    url_parliament_list = h.xpath_string(
+    url_parliament = h.xpath_string(
         doc_landing,
         "//a[@title='Recursos' and contains(@href,'DAComposicaoOrgaos')]/@href",
     )
 
     ### --- iterate over parliaments ---
-    doc_parliament_list = context.fetch_html(url_parliament_list, absolute_links=True)
+    doc_parliament_list = context.fetch_html(url_parliament, absolute_links=True)
     parliament_urls = h.xpath_strings(
-        doc_parliament_list,
-        "//div[@class='archive-item']//a/@href",
+        doc_parliament_list, "//div[@class='archive-item']//a/@href"
     )
     parliament_names = h.xpath_strings(
         doc_parliament_list,
         "//div[@class='archive-item']//a/text()",
     )
 
-    for name, u in zip(parliament_names, parliament_urls):
+    for name, u in zip(parliament_names, parliament_urls, strict=True):
         # skip 'Constituinte' and parliaments older than the 10th (ran from 10 March 2005 to 14 October 2009)
         if name == "IX Legislatura":
             break
