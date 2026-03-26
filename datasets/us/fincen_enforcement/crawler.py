bdd53bf09eaa0561c3a58231c7f16e27b8902e89
JD Bothma <jd@opensanctions.org>
2026-03-26 13:03:40 +0000

    Small fixes
    - simplify 10 years of history - within_max does the work for you
    - case number in sanction id
    - source value's source url is the scraped url, not what it links to
    - add previous name support

--- datasets/us/fincen_enforcement/crawler.py
+++ datasets/us/fincen_enforcement/crawler.py
@@ -1,5 +1,4 @@
 from lxml.etree import _Element
-from datetime import date
 from typing import List
 from pydantic import BaseModel
 
@@ -9,14 +8,14 @@ from zavod.shed import enforcements
 from zavod.extract.llm import run_typed_text_prompt, DEFAULT_MODEL
 from zavod.stateful.review import review_extraction, HtmlSourceValue
 
-start = date.today()
-end = date(start.year + 10, start.month, start.day)
-MAX_AGE_DAYS = (end - start).days
+
+MAX_AGE_DAYS = 365 * 10  # keep 10 years of history
 
 
 class Respondent(BaseModel):
     name: str
     aliases: List[str] = []
+    previous_names: List[str] = []
 
 
 class Respondents(BaseModel):
@@ -29,6 +28,7 @@ ONLY include names mentioned in the input string.
 
 - name: The name of the entity precisely as expressed in the text.
 - aliases: ONLY extract aliases that follow an explicit indication of an _alternative_ name, such as "also known as", "alias", "formerly", "aka", "fka". Otherwise the aliases field should just be an empty array.
+- previous_names: ONLY extract previous names that follow an explicit indication of a _former_ name, such as "formerly", "fka". Otherwise the previous_names field should just be an empty array.
 """
 
 
@@ -36,31 +36,31 @@ def crawl_row(context: Context, row: dict[str, _Element]) -> None:
     # fetch case's url
     url_el = row.get("enforcement_action")
     assert url_el is not None
-    url = h.xpath_strings(url_el, ".//a/@href")
+    detail_url = h.xpath_strings(url_el, ".//a/@href")
 
     # process row
     str_row = h.cells_to_str(row)
     case_name = str_row.pop("enforcement_action")
     sanction_date = str_row.pop("date_sort_ascending")
-    if sanction_date and not enforcements.within_max_age(
-        context, sanction_date, MAX_AGE_DAYS
-    ):
+    assert sanction_date is not None
+    if not enforcements.within_max_age(context, sanction_date, MAX_AGE_DAYS):
         return
 
     assert case_name is not None
-    if "in the matter of" in case_name.lower():
-        case_name = case_name.split("In the Matter of")[1].strip()
+    case_name = case_name.replace("In the Matter of", "")
+    assert "in the matter of" not in case_name.lower()
 
     entity = context.make("LegalEntity")
-    entity.id = context.make_id(case_name, str_row.pop("matter_number"))
+    matter_number = str_row.pop("matter_number")
+    entity.id = context.make_id(case_name, matter_number)
 
-    # send to cleaning if a string contrains split entities and aliases
+    # send to cleaning if a string contains split entities and aliases
     if contains_split_phrase(case_name) or " and " in case_name:
         source_value = HtmlSourceValue(
-            key_parts=url,
-            label="FinCEN Enforcement Action",
+            key_parts=case_name,
+            label="Case Name",
             element=url_el,
-            url=url[0],
+            url=context.data_url,
         )
         prompt_result = run_typed_text_prompt(
             context,
@@ -80,17 +80,16 @@ def crawl_row(context: Context, row: dict[str, _Element]) -> None:
         for item in review.extracted_data.respondents:
             entity.add("name", item.name, origin=review.origin)
             entity.add("alias", item.aliases, origin=review.origin)
-        # original = h.Names(name=case_name)
-        # h.review_names(context, entity, original=original, llm_cleaning=True)
+            entity.add("previous_name", item.previous_names, origin=review.origin)
     else:
         entity.add("name", case_name)
 
-    entity.add("sourceUrl", url)
+    entity.add("sourceUrl", detail_url)
     entity.add("sector", str_row.pop("financial_institution"))
     entity.add("topics", "reg.action")
     entity.add("country", "us")
 
-    sanction = h.make_sanction(context, entity)
+    sanction = h.make_sanction(context, entity, key=matter_number)
     h.apply_date(sanction, "listingDate", sanction_date)
 
     context.emit(entity)
