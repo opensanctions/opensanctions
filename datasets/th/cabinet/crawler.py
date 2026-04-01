4f4750e397d854fc93f5689b057dd6d7f124736e
nvmbrasserie <ivan@opensanctions.org>
2026-04-01 17:11:13 +0200

    [th_cabinet] switch to the new data source

--- datasets/th/cabinet/crawler.py
+++ datasets/th/cabinet/crawler.py
@@ -1,110 +1,75 @@
-import re
+from lxml.html import HtmlElement
 
-from normality import collapse_spaces
-from zavod.shed.trans import (
-    apply_translit_full_name,
-    make_position_translation_prompt,
-    ENGLISH,
-)
-from zavod.extract.zyte_api import fetch_html
 from zavod.stateful.positions import categorise
+from zavod.extract.zyte_api import fetch_html
+from zavod import Context, helpers as h
 
-from zavod import Context
-from zavod import helpers as h
 
-ROLE_PATTERNS = re.compile(
-    r"(?P<name>.+?)\s*(?P<role>รองนายกรัฐมนตรี.*|รัฐมนตรีว่าการ.*|รัฐมนตรีประจำ.*|รัฐมนตรีช่วยว่าการ.*|นายกรัฐมนตรี)"
-)
-# Pattern specifically for "นายกรัฐมนตรี" (Prime Minister)
-PRIME_MINISTER_PATTERN = re.compile(r"(?P<role>นายกรัฐมนตรี)\s*(?P<name>.*)")
-REGEX_TITLES = re.compile(
-    r"^(นางสาว|นาง|นาย|พลตำรวจเอก|พันตำรวจเอก|พลเอก|พลตำรวจตรี|ร้อยเอก|พลโท|จ่าเอก|พลตำรวจโท|-)"
-)
-POSITION_PROMPT = prompt = make_position_translation_prompt("tha")
-TRANSLIT_OUTPUT = [ENGLISH]
-# For lack of anything more semantic, we select persons based on sizing of their containers
-# The prime minister section can be slightly bigger than the others
-PRIME_MINISTER_XPATH = ".//div[contains(@style, 'min-height: 480; max-height: 600')]"
-UNBLOCK_ACTIONS = [
-    {
-        "action": "waitForSelector",
-        "selector": {
-            "type": "xpath",
-            "value": PRIME_MINISTER_XPATH,
-            "state": "visible",
-        },
-        "timeout": 15,
-        "onError": "return",
-    },
-]
+NAME_XPATH = ".//div[contains(@class, '__title')]"
+ROLE_XPATH = ".//p[contains(@class, 'fw-normal')]"
+
+
+def crawl_prime_minister(
+    context: Context, main_container: HtmlElement
+) -> tuple[str, str] | None:
+    name = h.xpath_string(main_container, ".//div[contains(@class, 'fs-3')]/text()")
+    role = h.xpath_string(main_container, ".//p[contains(@class, 'fw-medium')]/text()")
+    if not name or not role:
+        context.log.warning("Could not extract PM name/role")
+        return None
+    print(name, role)
+    return name, role
+
+
+def crawl_person(context: Context, name: str, role: str) -> None:
+    person = context.make("Person")
+    person.id = context.make_id(name, role)
+    person.add("name", name, lang="eng")
+    person.add("topics", "role.pep")
+    person.add("citizenship", "th")
+
+    position = h.make_position(
+        context,
+        name=role,
+        country="th",
+        lang="eng",
+        topics=["gov.executive", "gov.national"],
+    )
+    categorisation = categorise(context, position, is_pep=True)
+    if not categorisation.is_pep:
+        return
+
+    occupancy = h.make_occupancy(context, person, position)
+    if occupancy:
+        context.emit(person)
+        context.emit(position)
+        context.emit(occupancy)
 
 
 def crawl(context: Context):
-    doc = fetch_html(
-        context, context.data_url, PRIME_MINISTER_XPATH, actions=UNBLOCK_ACTIONS
-    )
-    main_container = doc.find(".//div[@class='wonderplugintabs-panel-inner']")
+    page = 1
+    while True:
+        url = f"{context.data_url}?page={page}"
+        doc = fetch_html(context, url, NAME_XPATH, cache_days=1)
+        main_container = h.xpath_element(
+            doc, ".//div[@class='container']//div[@class='row']"
+        )
+        # crawl PM only once on the first page
+        if page == 1:
+            name, role = crawl_prime_minister(context, main_container)
+            if name and role:
+                crawl_person(context, name, role)
 
-    prime_minister_containers = main_container.xpath(PRIME_MINISTER_XPATH)
-    assert len(prime_minister_containers) == 1, prime_minister_containers
-    prime_minister = prime_minister_containers[0]
-    persons = main_container.findall(
-        ".//div[@style='min-height: 360; max-height: 500;']"
-    )
-    persons.append(prime_minister)
-    for person in persons:
-        # Ensure there's whitespace between headings
-        for heading in person.xpath(
-            ".//*[self::h3 or self::h4 or self::h5 or self::h6]"
-        ):
-            heading.tail = heading.tail + "\n" if heading.tail else "\n"
+        persons = h.xpath_elements(main_container, ".//div[@class='col']")
+        if page > 7 or not persons:
+            break
 
-        collected_text = collapse_spaces(person.text_content())
-        if not collected_text:
-            continue
-
-        # Match the text based on "Prime Minister", "Deputy Prime Minister", "Minister" in Thai
-        match = ROLE_PATTERNS.search(collected_text)
-        if match:
-            name = match.group("name").strip()
-            role = match.group("role").strip()
-        else:
-            match = PRIME_MINISTER_PATTERN.search(collected_text)
-            if match:
-                name = match.group("name").strip()
-                role = match.group("role").strip()
-            else:
-                context.log.warning("Could not match name and role", collected_text)
+        for person in persons:
+            name = h.xpath_string(person, ".//div[contains(@class, '__title')]/text()")
+            role = h.xpath_string(person, ".//p[contains(@class, 'fw-normal')]/text()")
+            if not name or not role:
+                context.log.warning("Name or role not found")
                 continue
+            crawl_person(context, name, role)
 
-        person = context.make("Person")
-        person.id = context.make_id(name, role)
-        title_match = REGEX_TITLES.match(name)
-        if not title_match:
-            context.log.warning("Could not match title in name.", name=name)
-            continue
-        name = REGEX_TITLES.sub("", name)
-        # Skip if name is empty after removing titles
-        if not name:
-            continue
-        person.add("name", name, lang="tha")
-        person.add("topics", "role.pep")
-
-        position = h.make_position(
-            context,
-            name=role,
-            country="th",
-            lang="tha",
-            topics=["gov.executive", "gov.national"],
-        )
-        apply_translit_full_name(
-            context, position, "tha", role, TRANSLIT_OUTPUT, POSITION_PROMPT
-        )
-
-        categorisation = categorise(context, position, is_pep=True)
-        if categorisation.is_pep:
-            occupancy = h.make_occupancy(context, person, position)
-            if occupancy:
-                context.emit(person)
-                context.emit(position)
-                context.emit(occupancy)
+        page += 1
