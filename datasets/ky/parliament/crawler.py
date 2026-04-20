a158a39321a72804e59f3c96c2713c1ff714eebc
nvmbrasserie <ivan@opensanctions.org>
2026-04-20 14:47:22 +0200

    clean up the types and add more details

--- datasets/ky/parliament/crawler.py
+++ datasets/ky/parliament/crawler.py
@@ -1,13 +1,13 @@
 import csv
-from normality import collapse_spaces
-from rigour.mime.types import CSV
-from typing import Dict
 import re
+from typing import Dict, Optional
+from rigour.mime.types import CSV
 
 from zavod import Context
 from zavod import helpers as h
+from zavod.util import Element
+from zavod.entity import Entity
 from zavod.stateful.positions import categorise
-from zavod.util import ElementOrTree
 
 
 HISTORICAL_DATA_CSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRM-hl1drkQMn4wHXxHkHXHZb2TkUyIFWxGXwJ_UqZNRpH00DGWrdHk_zDrHUsZ4YCeVYYojqSMmZuX/pub?output=csv"
@@ -39,55 +39,70 @@ HONORIFICS = ["Hon. ", "Hon ", "Ms. ", "Mr. ", "Mrs. ", "Sir ", "Dr. "]
 NAMES_ALLOW_LIST = ["A. Royston (Roy) Tatum", "Johany S. (Jay) Ebanks"]
 
 
-def crawl_card_2025(context: Context, position: str, el: ElementOrTree):
-    name_el = el.find("./h1")
-    name = name_el.text
+def clean_name(context: Context, name: str) -> str:
     name = re.sub(r",.+", "", name)
     for honorific in HONORIFICS:
         name = name.replace(honorific, "")
     if not REGEX_NAME.match(name) and name not in NAMES_ALLOW_LIST:
         context.log.warning("Name doesn't look like a name", name=name)
+    return name.strip()
 
+
+def crawl_card_2025(
+    context: Context, position_str: str, el: Element
+) -> Optional[Entity]:
+    content_el = h.xpath_element(el, ".//div[@class='member-select-content']")
+    links_el = h.xpath_element(el, ".//div[@class='member-select-links']")
+    name = clean_name(context, h.xpath_string(content_el, "./h1/text()"))
     entity = context.make("Person")
     entity.id = context.make_id(name)
     # BOTC and Caymanian: https://portal.elections.ky/index.php/candidates-agents/qualifications-for-candidates
     entity.add("citizenship", "KY")
     entity.add("name", name)
+    personal_page_url = h.xpath_string(
+        links_el, ".//p[@class='change-add-to-cart-text']/a/@href"
+    )
+    entity.add("sourceUrl", personal_page_url)
 
-    position_el = el.find(".//p")
-    if position_el is not None:
-        position_detail = collapse_spaces(position_el.text_content())
-        if REGEX_POSITIONISH.search(position_detail):
-            entity.add("position", position_detail)
-        else:
-            context.log.warning(
-                "Position detail value doesn't look like position detail",
-                value=position_detail,
-            )
+    position_detail = h.xpath_string(content_el, ".//p/text()")
+    if REGEX_POSITIONISH.search(position_detail):
+        entity.add("position", position_detail)
+    else:
+        context.log.warning(
+            "Position detail value doesn't look like position detail",
+            value=position_detail,
+        )
 
     position = h.make_position(
         context,
-        position,
+        position_str,
         topics=TOPICS,
         country="ky",
     )
     categorisation = categorise(context, position, True)
-    if categorisation.is_pep:
-        occupancy = h.make_occupancy(
-            context,
-            entity,
-            position,
-            start_date="2025",
-            end_date="2029",
-            categorisation=categorisation,
-        )
+    if not categorisation.is_pep:
+        return None
+    occupancy = h.make_occupancy(
+        context,
+        entity,
+        position,
+        period_start="2025",
+        period_end="2029",
+        categorisation=categorisation,
+    )
+    constituency = h.xpath_strings(
+        links_el, ".//p[@class='change-add-to-cart-text']/text()"
+    )
+    if occupancy is not None:
+        occupancy.add("constituency", constituency)
         context.emit(entity)
         context.emit(position)
         context.emit(occupancy)
         return entity
+    return None
 
 
-def crawl_row(context: Context, row: Dict[str, str]):
+def crawl_row(context: Context, row: Dict[str, str]) -> None:
     start_date = row.pop("Start date", None)
     if start_date and start_date < h.earliest_term_start(TOPICS):
         context.log.info(
@@ -109,32 +124,37 @@ def crawl_row(context: Context, row: Dict[str, str]):
         country="ky",
     )
     categorisation = categorise(context, position, True)
-    if categorisation.is_pep:
-        occupancy = h.make_occupancy(
-            context,
-            entity,
-            position,
-            no_end_implies_current=False,
-            start_date=start_date,
-            end_date=row.pop("End date", None),
-            categorisation=categorisation,
-        )
+    if not categorisation.is_pep:
+        return
+
+    occupancy = h.make_occupancy(
+        context,
+        entity,
+        position,
+        no_end_implies_current=False,
+        period_start=start_date,
+        period_end=row.pop("End date", None),
+        categorisation=categorisation,
+    )
+    if occupancy is not None:
         context.emit(entity)
         context.emit(position)
         context.emit(occupancy)
 
 
-def crawl(context: Context):
+def crawl(context: Context) -> None:
     doc = context.fetch_html(context.data_url, cache_days=1)
     # crawl_card assumes 2021
-    assert "2025-2029 Members" in doc.text_content()
+    assert "2025-2029 Members" in h.xpath_strings(doc, ".//h3/text()")
     expected_current_member_count = 22
     current_member_count = 0
     heading = None
-    for section in doc.findall(".//section"):
-        heading_el = section.xpath(".//h2[contains(@class, 'elementor-heading-title')]")
-        if heading_el:
-            heading = collapse_spaces(heading_el[0].text_content()).strip()
+    for section in h.xpath_elements(doc, ".//section"):
+        heading_str = h.xpath_strings(
+            section, ".//h2[contains(@class, 'elementor-heading-title')]/text()"
+        )
+        if heading_str:
+            heading = heading_str[0]
             if heading in IGNORE_HEADINGS:
                 heading = None
                 continue
@@ -146,8 +166,8 @@ def crawl(context: Context):
         else:
             if heading is None:
                 continue
-            for el in section.xpath(
-                ".//div[contains(@class, 'member-select-content')]"
+            for el in h.xpath_elements(
+                section, ".//div[contains(@class, 'member-select-main')]"
             ):
                 if crawl_card_2025(context, heading, el):
                     current_member_count += 1
