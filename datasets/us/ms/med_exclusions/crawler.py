a1150375043671031493c50503673da7b2ccbb37
JD Bothma <jd@opensanctions.org>
2026-06-17 15:59:00 +0100

    [us_ms_med_exclusions] Add both org and individual when both are available.

--- datasets/us/ms/med_exclusions/crawler.py
+++ datasets/us/ms/med_exclusions/crawler.py
@@ -1,59 +1,19 @@
 import openpyxl
 from rigour.mime.types import XLSX
 
-from zavod import Context, helpers as h
-
+from zavod import Context, Entity, helpers as h
 
 INDEFINITE_END_DATE = "2999-12-31"
 
 
-def crawl_item(row: dict[str, str | None], context: Context) -> None:
-    org_name = row.pop("organization_name")
-    first_name = row.pop("first_name")
-    middle_name = row.pop("middle_name")
-    last_name = row.pop("last_name")
-    suffix = row.pop("suffix")
-
-    if not any(v and v.strip() for v in (org_name, first_name, last_name)):
-        return
-
-    entity_type = row.pop("entity_type")
-    npi = row.pop("npi")
-    medicaid_id = row.pop("medicaid_id")
-    dob = row.pop("date_of_birth")
-
-    if entity_type == "Organization":
-        schema = "LegalEntity"
-    elif entity_type == "Individual":
-        schema = "Person"
-    elif first_name or last_name:
-        schema = "Person"
-    elif org_name:
-        schema = "LegalEntity"
-    else:
-        context.log.warning("Could not determine entity schema", row=row)
-        return
-
-    entity = context.make(schema)
-    if schema == "Person":
-        entity.id = context.make_id(first_name, last_name, npi, dob)
-        h.apply_name(
-            entity,
-            first_name=first_name,
-            middle_name=middle_name,
-            last_name=last_name,
-            suffix=suffix,
-        )
-        h.apply_date(entity, "birthDate", dob)
-    else:
-        entity.id = context.make_id(org_name, npi)
-        entity.add("name", org_name)
-
+def crawl_detail(
+    context: Context, entity: Entity, row: dict[str, str], npi: str
+) -> None:
     entity.add("country", "us")
-    entity.add("sector", row.pop("provider_type"))
-    entity.add("sector", row.pop("speciality"))
-    if npi:
-        entity.add("npiCode", npi.split(" "))
+    entity.add("npiCode", npi.split(" "))
+    entity.add("idNumber", row.pop("medicaid_id"))
+    entity.add("sector", row.pop("provider_type", None))
+    entity.add("sector", row.pop("speciality", None))
 
     address = h.make_address(
         context,
@@ -64,7 +24,7 @@ def crawl_item(row: dict[str, str | None], context: Context) -> None:
         postal_code=row.pop("zipcode"),
         country_code="us",
     )
-    h.apply_address(context, entity, address)
+    h.copy_address(entity, address)
 
     sanction = h.make_sanction(context, entity)
     h.apply_date(sanction, "startDate", row.pop("termination_effective_date"))
@@ -74,8 +34,6 @@ def crawl_item(row: dict[str, str | None], context: Context) -> None:
     sanction.add("reason", row.pop("termination_reason"))
     sanction.add("provisions", row.pop("sanction_type"))
     sanction.add("description", row.pop("additional_notes"))
-    if medicaid_id:
-        sanction.add("recordId", medicaid_id)
 
     if h.is_active(sanction):
         entity.add("topics", "debarment")
@@ -83,7 +41,54 @@ def crawl_item(row: dict[str, str | None], context: Context) -> None:
     context.emit(entity)
     context.emit(sanction)
 
-    context.audit_data(row, ignore=["column_0", "role", "exclusion_period"])
+    context.audit_data(row, ignore=["column_0", "exclusion_period", "role"])
+
+
+def crawl_item(row: dict[str, str], context: Context) -> None:
+    org_name = row.pop("organization_name")
+    first_name = row.pop("first_name")
+    middle_name = row.pop("middle_name")
+    last_name = row.pop("last_name")
+    suffix = row.pop("suffix")
+    dob = row.pop("date_of_birth")
+    npi = row.pop("npi")
+    entity_type = row.pop("entity_type")
+
+    org_entity = None
+    if org_name:
+        org_entity = context.make("Organization")
+        org_entity.id = context.make_id(org_name, npi)
+        org_entity.add("name", org_name)
+        if entity_type == "Organization":
+            org_entity.add("sector", row.pop("provider_type"))
+            org_entity.add("sector", row.pop("speciality"))
+        crawl_detail(context, org_entity, row.copy(), npi)
+
+    person_entity = None
+    if first_name or last_name:
+        person_entity = context.make("Person")
+        person_entity.id = context.make_id(first_name, last_name, npi, dob)
+        h.apply_name(
+            person_entity,
+            first_name=first_name,
+            middle_name=middle_name,
+            last_name=last_name,
+            suffix=suffix,
+        )
+        person_entity.add("npiCode", npi.split(" ") if npi else [])
+        h.apply_date(person_entity, "birthDate", dob)
+        if entity_type == "Individual":
+            person_entity.add("sector", row.pop("provider_type"))
+            person_entity.add("sector", row.pop("speciality"))
+
+        crawl_detail(context, person_entity, row.copy(), npi)
+
+    if org_entity is not None and person_entity is not None:
+        link = context.make("UnknownLink")
+        link.id = context.make_id(org_entity.id, person_entity.id)
+        link.add("subject", person_entity)
+        link.add("object", org_entity)
+        context.emit(link)
 
 
 def crawl_data_url(context: Context) -> str:
@@ -107,4 +112,7 @@ def crawl(context: Context) -> None:
         skiprows=8,
         header_lookup=header_lookup,
     ):
-        crawl_item(row, context)
+        strings_row = {
+            k: str(v).strip() if v is not None else "" for k, v in row.items()
+        }
+        crawl_item(strings_row, context)
