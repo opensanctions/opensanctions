936c713ad6447f951666cb62073b7cd7168c91f1
dennismdejong <dennis@caberdo.nl>
2026-08-03 22:40:06 +0200

    esma_saris: skip rows with invalid ISIN values

--- datasets/eu/esma_saris/crawler.py
+++ datasets/eu/esma_saris/crawler.py
@@ -66,6 +66,9 @@ def crawl(context: Context) -> None:
                 context.log.warn("No ISIN", row=row)
                 return
             entity = h.make_security(context, isin)
+            if entity is None:
+                context.log.warn("Invalid ISIN, skipping row", isin=isin)
+                continue
             entity.add("name", row.pop("instrumentFullName", isin))
 
             sanction = h.make_sanction(
