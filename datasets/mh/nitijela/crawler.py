8c88ea9eaa3aa407b8bf87d12c71fcce961cbe9a
Leon Handreke <leon@opensanctions.org>
2026-08-05 13:34:23 +0200

    [mh_nitijela] Sort imports (ruff check --fix)

--- datasets/mh/nitijela/crawler.py
+++ datasets/mh/nitijela/crawler.py
@@ -1,9 +1,10 @@
 import re
 
-from zavod import Context
-from zavod import helpers as h
 from zavod.entity import Entity
 from zavod.stateful.positions import PositionCategorisation, categorise
+
+from zavod import Context
+from zavod import helpers as h
 
 # Each member is a heading "<n>. <role> - <honorific> <name>", e.g.
 # "17. Senator - Honorable Daisy Alik Momotaro".
