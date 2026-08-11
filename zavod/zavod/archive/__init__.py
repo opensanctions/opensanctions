f322144cc8ce23444a23e13e76cdc1d1b41b9a06
JD Bothma <jd@opensanctions.org>
2026-08-11 16:43:40 +0100

    Build a catalog of the latest _successful_ runs

--- zavod/zavod/archive/__init__.py
+++ zavod/zavod/archive/__init__.py
@@ -210,23 +210,38 @@ def get_artifact_object(
     dataset_name: str, resource: str, version: str | None = None
 ) -> ArchiveObject | None:
     backend = get_archive_backend()
+    if version is None:
+        versions_data = get_versions_data(dataset_name)
+        if versions_data is not None:
+            history = VersionHistory.from_json(versions_data)
+            version = history.last_successful.id if history.last_successful else None
+
     if version is not None:
         name = f"{ARTIFACTS}/{dataset_name}/{version}/{resource}"
         object = backend.get_object(name)
         if object.exists():
             return object
-    else:
-        for v in iter_dataset_versions(dataset_name):
-            name = f"{ARTIFACTS}/{dataset_name}/{v.id}/{resource}"
-            object = backend.get_object(name)
-            if object.exists():
-                return object
+
+    # Temporary fallback for datasets that haven't succeeded since last_successful
+    # TODO: Remove after the stragglers have succeeded or been removed.
+    # stragglers: lt_pep_declarations, hu_national_assembly, ps_local_freezing
+    for v in iter_dataset_versions(dataset_name):
+        # We could check if the index.json has a resources array here, but only
+        # lt_pep_declarations needs it, and it's being removed 2026-08-17
+        # so let's nto bother
+        name = f"{ARTIFACTS}/{dataset_name}/{v.id}/{resource}"
+        object = backend.get_object(name)
+        if object.exists():
+            return object
 
     # FIXME: legacy fallback option of using the latest release
     # REMOVE THIS AFTER MIGRATION
     name = f"{DATASETS}/{LATEST}/{dataset_name}/{resource}"
     object = backend.get_object(name)
     if object.exists():
+        log.warning(
+            f"Couldn't find artifact object the modern way {dataset_name} {resource}"
+        )
         return object
     return None
 
